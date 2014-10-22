bfaa46a24a72f94cd969c58d744b17d9760d00b0
David Rajchenbach-Teller <dteller@mozilla.com>
2014-10-22 12:37:41 +0200

    Bug 1087255 - Convert Places JS clients of RemovePage(s) to History.remove; Patch by Yoric, updated by Standard8. r=mak
    MozReview-Commit-ID: CA0g8ccQNB4

--- toolkit/components/places/UnifiedComplete.js
+++ toolkit/components/places/UnifiedComplete.js
@@ -2094,7 +2094,7 @@ UnifiedComplete.prototype = {
 
   onValueRemoved(result, spec, removeFromDB) {
     if (removeFromDB) {
-      PlacesUtils.history.removePage(NetUtil.newURI(spec));
+      PlacesUtils.history.remove(spec).catch(Cu.reportError);
     }
   },
 
