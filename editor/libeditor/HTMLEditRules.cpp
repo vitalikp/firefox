2fe13f19de4fd5809dbead41a8e101f6d91d7223
Aryeh Gregor <ayg@aryeh.name>
2016-08-24 16:29:20 +0300

    Bug 1297414 - Support <div> separator in ReturnIn(Header|ListItem); r=masayuki
    MozReview-Commit-ID: EkvFpyTmXDc

--- editor/libeditor/HTMLEditRules.cpp
+++ editor/libeditor/HTMLEditRules.cpp
@@ -6225,6 +6225,16 @@ HTMLEditRules::IsInListItem(nsINode* aNode)
   return nullptr;
 }
 
+nsIAtom&
+HTMLEditRules::DefaultParagraphSeparator()
+{
+  MOZ_ASSERT(mHTMLEditor);
+  if (!mHTMLEditor) {
+    return *nsGkAtoms::div;
+  }
+  return ParagraphSeparatorElement(mHTMLEditor->GetDefaultParagraphSeparator());
+}
+
 /**
  * ReturnInHeader: do the right thing for returns pressed in headers
  */
@@ -6280,8 +6290,12 @@ HTMLEditRules::ReturnInHeader(Selection& aSelection,
       htmlEditor->mTypeInState->ClearAllProps();
 
       // Create a paragraph
+      nsIAtom& paraAtom = DefaultParagraphSeparator();
+      // We want a wrapper element even if we separate with <br>
       nsCOMPtr<Element> pNode =
-        htmlEditor->CreateNode(nsGkAtoms::p, headerParent, offset + 1);
+        htmlEditor->CreateNode(&paraAtom == nsGkAtoms::br ? nsGkAtoms::p
+                                                           : &paraAtom,
+                                headerParent, offset + 1);
       NS_ENSURE_STATE(pNode);
 
       // Append a <br> to it
@@ -6552,8 +6566,12 @@ HTMLEditRules::ReturnInListItem(Selection& aSelection,
       NS_ENSURE_SUCCESS(rv, rv);
 
       // Time to insert a paragraph
+      nsIAtom& paraAtom = DefaultParagraphSeparator();
+      // We want a wrapper even if we separate with <br>
       nsCOMPtr<Element> pNode =
-        htmlEditor->CreateNode(nsGkAtoms::p, listParent, offset + 1);
+        htmlEditor->CreateNode(&paraAtom == nsGkAtoms::br ? nsGkAtoms::p
+                                                           : &paraAtom,
+                                listParent, offset + 1);
       NS_ENSURE_STATE(pNode);
 
       // Append a <br> to it
