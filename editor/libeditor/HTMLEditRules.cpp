d26637cd2c1fa253757ac5e8dd6cf0e948a2087c
Aryeh Gregor <ayg@aryeh.name>
2016-08-24 16:03:01 +0300

    Bug 1297414 - Treat <div> like <p> for break insertion; r=masayuki
    I don't personally agree with this behavior (although I did spec it some
    years ago), but it's the behavior of all other UAs, so we should do it
    anyway.
    
    MozReview-Commit-ID: IiIg41kMJIU

--- editor/libeditor/HTMLEditRules.cpp
+++ editor/libeditor/HTMLEditRules.cpp
@@ -1549,7 +1549,7 @@ HTMLEditRules::WillInsertBreak(Selection& aSelection,
     ReturnInHeader(aSelection, *blockParent, node, offset);
     *aHandled = true;
     return NS_OK;
-  } else if (blockParent->IsHTMLElement(nsGkAtoms::p)) {
+  } else if (blockParent->IsAnyOfHTMLElements(nsGkAtoms::p, nsGkAtoms::div)) {
     // Paragraphs: special rules to look for <br>s
     nsresult rv =
       ReturnInParagraph(&aSelection, GetAsDOMNode(blockParent),
@@ -6255,6 +6255,9 @@ HTMLEditRules::ReturnInHeader(Selection& aSelection,
 
 /**
  * ReturnInParagraph() does the right thing for returns pressed in paragraphs.
+ * For our purposes, this means either <p> or <div>, which is not in keeping
+ * with the semantics of <div>, but is necessary for compatibility with other
+ * browsers.
  */
 nsresult
 HTMLEditRules::ReturnInParagraph(Selection* aSelection,
