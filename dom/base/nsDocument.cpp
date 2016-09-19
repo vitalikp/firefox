20cfea3520d3c6cb664b6a031247480b710295ce
Bill McCloskey <billm@mozilla.com>
2016-09-19 11:59:54 -0700

    Bug 1318506 - Label some common runnables with a DocGroup (r=ehsan)
    MozReview-Commit-ID: 19gyZRfbzXP

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -11919,7 +11919,7 @@ nsDocument::PostVisibilityUpdateEvent()
 {
   nsCOMPtr<nsIRunnable> event =
     NewRunnableMethod(this, &nsDocument::UpdateVisibilityState);
-  NS_DispatchToMainThread(event);
+  Dispatch("UpdateVisibility", TaskCategory::Other, event.forget());
 }
 
 void
