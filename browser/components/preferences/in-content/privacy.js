bf7357b29a2c6dac08650f10d34f5246549592ed
David Malaschonok <davidmalaschonok@gmail.com>
2016-10-30 11:59:29 +0100

    Bug 552434 - Allow the history mode to stay in Custom even if the settings match the preference values for keeping history. r=jaws
    MozReview-Commit-ID: BCX4h3CdBhc

--- browser/components/preferences/in-content/privacy.js
+++ browser/components/preferences/in-content/privacy.js
@@ -283,7 +283,9 @@ var gPrivacyPane = {
     let mode;
     let getVal = aPref => document.getElementById(aPref).value;
 
-    if (this._checkHistoryValues(this.prefsForKeepingHistory)) {
+    if (getVal("privacy.history.custom"))
+      mode = "custom";
+    else if (this._checkHistoryValues(this.prefsForKeepingHistory)) {
       if (getVal("browser.privatebrowsing.autostart"))
         mode = "dontremember";
       else
@@ -313,6 +315,7 @@ var gPrivacyPane = {
       break;
     }
     document.getElementById("historyPane").selectedIndex = selectedIndex;
+    document.getElementById("privacy.history.custom").value = selectedIndex == 2;
   },
 
   /**
