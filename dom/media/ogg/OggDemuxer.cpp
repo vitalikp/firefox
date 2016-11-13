16968a8c07238434eb50e9d70fe550a104b80e2e
Gerald Squelart <gsquelart@mozilla.com>
2016-11-13 15:13:51 +1100

    Bug 1313497 - Pass TimeUnit by const& in MediaDataDemuxer - r=jya
    Thanks to the previous patch, MediaDataDemuxer::Seek and
    SkipToNextRandomAccessPoint (and all overrides in derived demuxers) can now
    take their TimeUnit parameter by const&.
    
    MozReview-Commit-ID: 6CqfjAXZ7Yk

--- dom/media/ogg/OggDemuxer.cpp
+++ dom/media/ogg/OggDemuxer.cpp
@@ -1471,7 +1471,7 @@ OggTrackDemuxer::GetInfo() const
 }
 
 RefPtr<OggTrackDemuxer::SeekPromise>
-OggTrackDemuxer::Seek(TimeUnit aTime)
+OggTrackDemuxer::Seek(const TimeUnit& aTime)
 {
   // Seeks to aTime. Upon success, SeekPromise will be resolved with the
   // actual time seeked to. Typically the random access point time
@@ -1559,7 +1559,7 @@ OggTrackDemuxer::Reset()
 }
 
 RefPtr<OggTrackDemuxer::SkipAccessPointPromise>
-OggTrackDemuxer::SkipToNextRandomAccessPoint(TimeUnit aTimeThreshold)
+OggTrackDemuxer::SkipToNextRandomAccessPoint(const TimeUnit& aTimeThreshold)
 {
   uint32_t parsed = 0;
   bool found = false;
