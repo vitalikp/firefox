05db9303c7848fc766ebfe52ecd3bbe235c6b253
Chris Pearce <cpearce@mozilla.com>
2016-11-16 10:59:08 +1300

    Bug 1316215 - Block GMPContentParent close while a GMPService::GetContentParent is being processed. r=gerald
    When GMPService::GetContentParent returns a MozPromise, we end up failing in
    test_peerConnection_scaleResolution.html with e10s enabled because we Close()
    the GMPContentParent twice. The test causes two GMPVideoEncoderParents to
    be created. When the number of IPDL actors on the GMPContentParent reach 0,
    we close the IPC connection. With GetContentParent() returning a MozPromise,
    it's more async, and so we can end up requesting the content parent in order
    to create the second GMPVideoEncoderParent, but while we're waiting for
    the promise to resolve the previous GMPVideoEncoderParent is destroyed and
    the GMPContentParent closes its IPC connection. Then the GetContentParent
    promise resolves, and that fails to operate correctly since it's closed its
    IPC connection.
    
    My solution here is to add a "blocker" that prevents the GMPContentParent from
    being shutdown while we're waiting for the GetContentParent promise to resolve.
    
    MozReview-Commit-ID: HxBkFkmv0tV

--- dom/media/gmp/GMPService.h
+++ dom/media/gmp/GMPService.h
@@ -23,6 +23,7 @@
 #include "nsClassHashtable.h"
 #include "nsISupportsImpl.h"
 #include "mozilla/MozPromise.h"
+#include "GMPContentParent.h"
 
 template <class> struct already_AddRefed;
 
@@ -56,8 +57,7 @@ extern LogModule* GetGMPLog();
 
 namespace gmp {
 
-class GMPContentParent;
-typedef MozPromise<RefPtr<GMPContentParent>, nsresult, /* IsExclusive = */ true> GetGMPContentParentPromise;
+typedef MozPromise<RefPtr<GMPContentParent::CloseBlocker>, nsresult, /* IsExclusive = */ true> GetGMPContentParentPromise;
 
 class GeckoMediaPluginService : public mozIGeckoMediaPluginService
                               , public nsIObserver
