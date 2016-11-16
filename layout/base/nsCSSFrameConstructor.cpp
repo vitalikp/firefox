76caf92e7382da69e9eeb64038279cc1164caa96
Ting-Yu Lin <tlin@mozilla.com>
2016-11-16 14:02:58 +0800

    Bug 1271549 - Remove details and summary preference. r=bz
    Bug 1259889 Part 2 [1] cannot be reverted cleanly, so I manually undo those
    changes in this patch. That is, remove the ability for html.css to
    invalidate dynamically since it was added specifically for details element.
    
    Although reftest-stylo.list explicit mentions "DO NOT EDIT!", but I still
    remove details pref from the file, since it doesn't harm to edit it anyway.
    
    [1] https://hg.mozilla.org/mozilla-central/rev/30aaf3805b56
    
    MozReview-Commit-ID: FsyTGQTxujh

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -3577,10 +3577,6 @@ nsCSSFrameConstructor::FindHTMLData(Element* aElement,
     return nullptr;
   }
 
-  if (aTag == nsGkAtoms::details && !HTMLDetailsElement::IsDetailsEnabled()) {
-    return nullptr;
-  }
-
   static const FrameConstructionDataByTag sHTMLData[] = {
     SIMPLE_TAG_CHAIN(img, nsCSSFrameConstructor::FindImgData),
     SIMPLE_TAG_CHAIN(mozgeneratedcontentimage,
@@ -5811,7 +5807,7 @@ nsCSSFrameConstructor::AddFrameConstructionItemsInternal(nsFrameConstructorState
   // ::before and ::after); we always want to create "internal" anonymous
   // content.
   auto* details = HTMLDetailsElement::FromContentOrNull(parent);
-  if (details && details->IsDetailsEnabled() && !details->Open() &&
+  if (details && !details->Open() &&
       (!aContent->IsRootOfNativeAnonymousSubtree() ||
        aContent->IsGeneratedContentContainerForBefore() ||
        aContent->IsGeneratedContentContainerForAfter())) {
@@ -5979,7 +5975,7 @@ nsCSSFrameConstructor::AddFrameConstructionItemsInternal(nsFrameConstructorState
   }
 
   FrameConstructionItem* item = nullptr;
-  if (details && details->IsDetailsEnabled() && details->Open()) {
+  if (details && details->Open()) {
     auto* summary = HTMLSummaryElement::FromContentOrNull(aContent);
     if (summary && summary->IsMainSummary()) {
       // If details is open, the main summary needs to be rendered as if it is
