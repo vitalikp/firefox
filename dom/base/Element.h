669d48d0b2094b4522014de854ba360905dd41e9
Neil Deakin <neil@mozilla.com>
2016-11-17 09:56:43 -0500

    Bug 1311279, add a chrome-only setCapture method that can ignore the allowed state, r=smaug

--- dom/base/Element.h
+++ dom/base/Element.h
@@ -831,6 +831,14 @@ public:
         (aRetargetToElement ? CAPTURE_RETARGETTOELEMENT : 0));
     }
   }
+
+  void SetCaptureAlways(bool aRetargetToElement)
+  {
+    nsIPresShell::SetCapturingContent(this,
+        CAPTURE_PREVENTDRAG | CAPTURE_IGNOREALLOWED |
+        (aRetargetToElement ? CAPTURE_RETARGETTOELEMENT : 0));
+  }
+
   void ReleaseCapture()
   {
     if (nsIPresShell::GetCapturingContent() == this) {
