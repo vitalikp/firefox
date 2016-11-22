15067fa042c47f8552626b98de58be10d51a4368
Jon Coppeard <jcoppeard@mozilla.com>
2016-11-22 10:32:11 +0000

    Bug 1318384 - Always allocate into fresh arenas while marking r=billm

--- js/src/jsgc.h
+++ js/src/jsgc.h
@@ -454,6 +454,11 @@ class ArenaList {
         return !*cursorp_;
     }
 
+    void moveCursorToEnd() {
+        while (!isCursorAtEnd())
+            cursorp_ = &(*cursorp_)->next;
+    }
+
     // This can return nullptr.
     Arena* arenaAfterCursor() const {
         check();
@@ -739,7 +744,7 @@ class ArenaLists
             freeLists[i] = &placeholder;
     }
 
-    inline void prepareForIncrementalGC(JSRuntime* rt);
+    inline void prepareForIncrementalGC();
 
     /* Check if this arena is in use. */
     bool arenaIsInUse(Arena* arena, AllocKind kind) const {
