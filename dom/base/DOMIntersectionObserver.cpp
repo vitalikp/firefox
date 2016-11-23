f90f6efc7fb6fa2222996287f0ba62e26bedc564
Tobias Schneider <tschneider@mozilla.com>
2016-11-23 13:14:00 -0500

    Bug 1315837 - Crash in mozilla::dom::Element::UpdateIntersectionObservation. r=mrbkap

--- dom/base/DOMIntersectionObserver.cpp
+++ dom/base/DOMIntersectionObserver.cpp
@@ -169,11 +169,12 @@ DOMIntersectionObserver::UnlinkTarget(Element& aTarget)
     if (!mObservationTargets.Contains(&aTarget)) {
         return false;
     }
-    if (mObservationTargets.Count() == 1) {
+
+    mObservationTargets.RemoveEntry(&aTarget);
+    if (mObservationTargets.Count() == 0) {
         Disconnect();
         return false;
     }
-    mObservationTargets.RemoveEntry(&aTarget);
     return true;
 }
 
@@ -194,6 +195,8 @@ DOMIntersectionObserver::Disconnect()
   if (!mConnected) {
     return;
   }
+
+  mConnected = false;
   for (auto iter = mObservationTargets.Iter(); !iter.Done(); iter.Next()) {
     Element* target = iter.Get()->GetKey();
     target->UnregisterIntersectionObserver(this);
@@ -203,7 +206,6 @@ DOMIntersectionObserver::Disconnect()
     nsIDocument* document = mOwner->GetExtantDoc();
     document->RemoveIntersectionObserver(this);
   }
-  mConnected = false;
 }
 
 void
@@ -277,7 +279,10 @@ DOMIntersectionObserver::Update(nsIDocument* aDocument, DOMHighResTimeStamp time
       if (rootFrame) {
         nsPresContext* presContext = rootFrame->PresContext();
         while (!presContext->IsRootContentDocument()) {
-          presContext = rootFrame->PresContext()->GetParentPresContext();
+          presContext = presContext->GetParentPresContext();
+          if (!presContext) {
+            break;
+          }
           rootFrame = presContext->PresShell()->GetRootScrollFrame();
         }
         root = rootFrame->GetContent()->AsElement();
