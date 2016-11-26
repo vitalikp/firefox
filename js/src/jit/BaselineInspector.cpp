43a4bc009b646c6303a059131b2b600e8255637e
Jan de Mooij <jdemooij@mozilla.com>
2016-11-26 21:19:23 +0100

    Bug 1319437 part 4 - Port code for unshadowed GETPROP on DOM proxies to CacheIR. r=nbp

--- js/src/jit/BaselineInspector.cpp
+++ js/src/jit/BaselineInspector.cpp
@@ -1021,8 +1021,6 @@ BaselineInspector::expectedPropertyAccessInputType(jsbytecode* pc)
             // Either an object or magic arguments.
             return MIRType::Value;
 
-          case ICStub::GetProp_CallDOMProxyNative:
-          case ICStub::GetProp_CallDOMProxyWithGenerationNative:
           case ICStub::GetElem_NativeSlotName:
           case ICStub::GetElem_NativeSlotSymbol:
           case ICStub::GetElem_NativePrototypeSlotName:
