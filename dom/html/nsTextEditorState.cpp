82479dc555305711a107d9f2151bbf46d8b35cc9
Botond Ballo <botond@mozilla.com>
2016-11-25 19:39:17 -0500

    Bug 1320200 - Propagate scroll-behavior on a text input to the anonymous scrollable <div> inside it. r=tnikkel
    MozReview-Commit-ID: Cur3x41wAfU

--- dom/html/nsTextEditorState.cpp
+++ dom/html/nsTextEditorState.cpp
@@ -2252,6 +2252,7 @@ nsTextEditorState::InitializeRootNode()
         disp->mOverflowX != NS_STYLE_OVERFLOW_CLIP) {
       classValue.AppendLiteral(" inherit-overflow");
     }
+    classValue.AppendLiteral(" inherit-scroll-behavior");
   }
   nsresult rv = mRootNode->SetAttr(kNameSpaceID_None, nsGkAtoms::_class,
                                    classValue, false);
