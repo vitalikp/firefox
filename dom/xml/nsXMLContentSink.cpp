58117090995db1d3315e45eae2540661564ec75a
Jonathan Kingston <jkt@mozilla.com>
2016-12-01 07:41:22 +0000

    Bug 1216893 - Add in disabled namespace for SVG r=hsivonen,smaug
    MozReview-Commit-ID: 7Gum6wazraS

--- dom/xml/nsXMLContentSink.cpp
+++ dom/xml/nsXMLContentSink.cpp
@@ -1069,6 +1069,9 @@ nsXMLContentSink::HandleEndElement(const char16_t *aName,
                (debugNameSpaceID == kNameSpaceID_MathML &&
                 content->NodeInfo()->NamespaceID() == kNameSpaceID_disabled_MathML &&
                 content->NodeInfo()->Equals(debugTagAtom)) ||
+               (debugNameSpaceID == kNameSpaceID_SVG &&
+                content->NodeInfo()->NamespaceID() == kNameSpaceID_disabled_SVG &&
+                content->NodeInfo()->Equals(debugTagAtom)) ||
                isTemplateElement, "Wrong element being closed");
 #endif
 
