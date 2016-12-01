b4c620419e5b4f53790626ba1038a5726825003e
Andrea Marchesini <amarchesini@mozilla.com>
2016-12-01 08:49:46 +0100

    Bug 1321270 - Get rid of nsIWindowCreator2::PARENT_IS_LOADING_OR_RUNNING_TIMEOUT, r=smaug

--- dom/base/nsPIDOMWindow.h
+++ dom/base/nsPIDOMWindow.h
@@ -196,8 +196,6 @@ protected:
   void MaybeCreateDoc();
 
 public:
-  inline bool IsLoadingOrRunningTimeout() const;
-
   // Check whether a document is currently loading
   inline bool IsLoading() const;
   inline bool IsHandlingResizeEvent() const;
