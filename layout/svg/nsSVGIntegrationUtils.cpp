2dd79080e8c15a2fc6397e8ca179a94b923e1645
cku <cku@mozilla.com>
2016-12-01 17:13:18 +0800

    Bug 1319667 - (follow-up) Remove incorrect assertion. r=me
    If the size of mask is empty, we wil hit this assertion, which is wrong.
    
    MozReview-Commit-ID: LgulkGPsjyH

--- layout/svg/nsSVGIntegrationUtils.cpp
+++ layout/svg/nsSVGIntegrationUtils.cpp
@@ -895,7 +895,6 @@ nsSVGIntegrationUtils::PaintMaskAndClipPath(const PaintFramesParams& aParams)
                                   maskFrames, offsetToUserSpace);
 
       if (paintResult.transparentBlackMask) {
-        MOZ_ASSERT(paintResult.result != DrawResult::SUCCESS);
         return paintResult.result;
       }
 
