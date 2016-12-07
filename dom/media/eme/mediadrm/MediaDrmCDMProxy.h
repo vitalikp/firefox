fb0d17bbd2da698f9f3f77fb14bbc9a7eb9cc0e2
Andrea Marchesini <amarchesini@mozilla.com>
2016-12-07 07:07:09 -1000

    Bug 1317927 - Media caching needs to use origin attributes, r=cpearce, r=jesup

--- dom/media/eme/mediadrm/MediaDrmCDMProxy.h
+++ dom/media/eme/mediadrm/MediaDrmCDMProxy.h
@@ -38,8 +38,7 @@ public:
   void Init(PromiseId aPromiseId,
             const nsAString& aOrigin,
             const nsAString& aTopLevelOrigin,
-            const nsAString& aGMPName,
-            bool aInPrivateBrowsing) override;
+            const nsAString& aGMPName) override;
 
   void CreateSession(uint32_t aCreateSessionToken,
                      MediaKeySessionType aSessionType,
