891280f75961e80a2d2e7e5e179d565482cbdbdc
Boris Zbarsky <bzbarsky@mit.edu>
2016-12-06 23:41:52 -1000

    Bug 1321879 part 1. Add an API on GlobalObject to get the caller type, for use in things like WebIDL constructors. r=bholley

--- dom/bindings/BindingUtils.cpp
+++ dom/bindings/BindingUtils.cpp
@@ -2321,6 +2321,13 @@ GlobalObject::GetSubjectPrincipal() const
   return nsJSPrincipals::get(principals);
 }
 
+CallerType
+GlobalObject::CallerType() const
+{
+  return nsContentUtils::ThreadsafeIsSystemCaller(mCx) ?
+    dom::CallerType::System : dom::CallerType::NonSystem;
+}
+
 static bool
 CallOrdinaryHasInstance(JSContext* cx, JS::CallArgs& args)
 {
