bee25df8ee32b45513bb3beb58b319b3e1b63387
Benjamin Smedberg <benjamin@smedbergs.us>
2016-12-09 08:52:28 -1000

    Bug 1304790 - Add a telemetry probe that measures how long it takes for a content process to start up and communicate back to the parent, r=mconley data-r=francois
    MozReview-Commit-ID: 5OaeqZXL6zS

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -17,6 +17,7 @@
 #include "mozilla/HalTypes.h"
 #include "mozilla/LinkedList.h"
 #include "mozilla/StaticPtr.h"
+#include "mozilla/TimeStamp.h"
 #include "mozilla/UniquePtr.h"
 
 #include "nsDataHashtable.h"
@@ -1055,6 +1056,7 @@ private:
   // details.
 
   GeckoChildProcessHost* mSubprocess;
+  const TimeStamp mLaunchTS; // used to calculate time to start content process
   ContentParent* mOpener;
 
   nsString mRemoteType;
