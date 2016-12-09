c71bd0a63ad2b39e9c0b06a146b433dae8cb03e0
Boris Zbarsky <bzbarsky@mit.edu>
2016-12-08 16:41:45 -1000

    Bug 1310796 part 3. Remove the aTargetable argument of contentShellAdded. r=mystor

--- xpfe/appshell/nsXULWindow.cpp
+++ xpfe/appshell/nsXULWindow.cpp
@@ -1784,7 +1784,7 @@ nsXULWindow::GetWindowDOMElement() const
 }
 
 nsresult nsXULWindow::ContentShellAdded(nsIDocShellTreeItem* aContentShell,
-   bool aPrimary, bool aTargetable, const nsAString& aID)
+   bool aPrimary, const nsAString& aID)
 {
   nsContentShellInfo* shellInfo = nullptr;
 
