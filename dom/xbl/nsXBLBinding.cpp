4d44cfe0566f95458fa6522e29f80ccc2d1b40c6
Bobby Holley <bobbyholley@gmail.com>
2016-12-10 20:11:36 -1000

    Bug 1322945 - Change skip_root to unstyled_children_only and use StyleNewChildren in more places. r=heycam
    I noticed that our current behavior in ContentRangeInserted is incorrect. Unlike
    ContentInserted (where this code lived originally), ContentRangeInserted takes a
    start and end element. I'm not sure if we ever take that path for new content that
    needs style, but it seemed sketchy. And generally, it seems nice to just always
    style new content the same way (though we still need to style NAC by the subtree
    root, since it hasn't been attached to the parent yet).
    
    For situations where there is indeed only one unstyled child, the traversal
    overhead should be neglible, since we special-case the single-element in
    parallel.rs to avoid calling into rayon.
    
    Being more explicit about what we want here also makes us more robust against
    the other handful of callpaths that can take us into
    nsCSSFrameConstructor::{ContentRangeInserted,ContentAppended}. Currently we
    can call StyleNewSubtree on an already-styled element via RecreateFramesForContent,
    which triggers an assertion in the servo traversal.
    
    MozReview-Commit-ID: DqCGh90deHH

--- dom/xbl/nsXBLBinding.cpp
+++ dom/xbl/nsXBLBinding.cpp
@@ -425,7 +425,7 @@ nsXBLBinding::GenerateAnonymousContent()
   nsIPresShell* presShell = mBoundElement->OwnerDoc()->GetShell();
   ServoStyleSet* servoSet = presShell->StyleSet()->GetAsServo();
   if (servoSet) {
-    servoSet->StyleNewChildren(mBoundElement);
+    servoSet->StyleNewChildren(mBoundElement->AsElement());
   }
 }
 
