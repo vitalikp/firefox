b93597c16cbef4521ae381d46f459b34a1eb0e74
Jan de Mooij <jdemooij@mozilla.com>
2016-12-11 21:17:47 -1000

    Bug 1322091 part 4 - Port Baseline dense element GETELEM stub to CacheIR. r=evilpie

--- js/src/jit/BaselineInspector.cpp
+++ js/src/jit/BaselineInspector.cpp
@@ -1018,7 +1018,6 @@ BaselineInspector::expectedPropertyAccessInputType(jsbytecode* pc)
           case ICStub::GetProp_Generic:
             return MIRType::Value;
 
-          case ICStub::GetElem_Dense:
           case ICStub::GetElem_TypedArray:
           case ICStub::GetElem_UnboxedArray:
             stubType = MIRType::Object;
