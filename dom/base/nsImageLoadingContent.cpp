fa9aef65d9d83507f4a0cf2d40a3224e498d9e5e
Andrew McCreight <continuation@gmail.com>
2016-12-14 16:28:57 -0800

    Bug 1323207, part 2 - Assert early if we're painting at various points we enter JS. r=billm
    nsContentUtils::IsPatternMatching is the most common by far, but the
    other is a generic location that may cover a number of issues.
    
    MozReview-Commit-ID: Kli39btsqdd

--- dom/base/nsImageLoadingContent.cpp
+++ dom/base/nsImageLoadingContent.cpp
@@ -44,6 +44,7 @@
 
 #include "mozAutoDocUpdate.h"
 #include "mozilla/AsyncEventDispatcher.h"
+#include "mozilla/CycleCollectedJSContext.h"
 #include "mozilla/EventStates.h"
 #include "mozilla/dom/Element.h"
 #include "mozilla/dom/ImageTracker.h"
@@ -148,6 +149,9 @@ nsImageLoadingContent::Notify(imgIRequest* aRequest,
   }
 
   {
+    MOZ_RELEASE_ASSERT(js::AllowGCBarriers(CycleCollectedJSContext::Get()->Context()),
+                       "ImageObservers can be implement in JS, so they should not be called during painting. See bug 1311841");
+
     // Calling Notify on observers can modify the list of observers so make
     // a local copy.
     AutoTArray<nsCOMPtr<imgINotificationObserver>, 2> observers;
