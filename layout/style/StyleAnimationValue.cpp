c1e620512d6af8cc725981736664dcfd9c68cd81
cku <cku@mozilla.com>
2016-12-16 15:46:26 +0800

    NO BUG - Remove empty lines.
    MozReview-Commit-ID: K7GMiRAJYPJ

--- layout/style/StyleAnimationValue.cpp
+++ layout/style/StyleAnimationValue.cpp
@@ -4415,10 +4415,6 @@ StyleAnimationValue::ExtractComputedValue(nsCSSPropertyID aProperty,
             static_cast<const nsStyleBackground*>(styleStruct)->mImage;
           ExtractImageLayerPositionYList(layers, aComputedValue);
           break;
-
-
-
-
         }
 #ifdef MOZ_ENABLE_MASK_AS_SHORTHAND
         case eCSSProperty_mask_position_x: {
