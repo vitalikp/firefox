bf6270547416a94767058ff77be7de83726d6976
Ting-Yu Chou <janus926@gmail.com>
2016-12-16 15:56:40 +0800

    Bug 1322465 part 7 - Use explicit/MOZ_IMPLICIT for the unary constructors in media/. r=cpearce,Ehsan
    MozReview-Commit-ID: Ln63tzmkynd

--- media/gmp-clearkey/0.1/WMFUtils.h
+++ media/gmp-clearkey/0.1/WMFUtils.h
@@ -57,8 +57,8 @@ public:
 
   CComPtr(const CComPtr& aOther) : mPtr(nullptr) { Set(aOther.Get()); }
   CComPtr() : mPtr(nullptr) { }
-  CComPtr(T* const & aPtr) : mPtr(nullptr) { Set(aPtr); }
-  CComPtr(const std::nullptr_t& aNullPtr) : mPtr(aNullPtr) { }
+  MOZ_IMPLICIT CComPtr(T* const & aPtr) : mPtr(nullptr) { Set(aPtr); }
+  MOZ_IMPLICIT CComPtr(const std::nullptr_t& aNullPtr) : mPtr(aNullPtr) { }
   T** operator&() { return &mPtr; }
   T* operator->(){ return mPtr; }
   operator T*() { return mPtr; }
@@ -179,7 +179,7 @@ public:
   {
   }
 
-  AutoPtr(Type* aPtr)
+  explicit AutoPtr(Type* aPtr)
     : mPtr(aPtr)
   {
   }
