74e21b79f0dd258ad5401aec3475774ec9297219
Ehsan Akhgari <ehsan@mozilla.com>
2016-12-16 17:15:16 -0500

    Bug 1170190 - Part 3: Add tests for the e10s case too; r=mayhemer

--- netwerk/protocol/http/HttpChannelParent.cpp
+++ netwerk/protocol/http/HttpChannelParent.cpp
@@ -1823,5 +1823,13 @@ HttpChannelParent::IssueWarning(uint32_t aWarning, bool aAsError)
   return NS_OK;
 }
 
+void
+HttpChannelParent::DoSendSetPriority(int16_t aValue)
+{
+  if (!mIPCClosed) {
+    Unused << SendSetPriority(aValue);
+  }
+}
+
 } // namespace net
 } // namespace mozilla
