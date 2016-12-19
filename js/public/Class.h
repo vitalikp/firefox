c74cff356835d0b286bcf880a78eaf0f7ebc57dc
Nicholas Nethercote <nnethercote@mozilla.com>
2016-12-19 14:18:41 +1100

    Bug 1324330 (part 1) - Mark js::Class and related types as NONHEAP. r=mccr8.

--- js/public/Class.h
+++ js/public/Class.h
@@ -627,7 +627,12 @@ typedef void
         cOps->trace(trc, obj); \
     }
 
-struct ClassOps
+// XXX: MOZ_NONHEAP_CLASS allows objects to be created statically or on the
+// stack. We actually want to ban stack objects too, but that's currently not
+// possible. So we define JS_STATIC_CLASS to make the intention clearer.
+#define JS_STATIC_CLASS MOZ_NONHEAP_CLASS
+
+struct JS_STATIC_CLASS ClassOps
 {
     /* Function pointer members (may be null). */
     JSAddPropertyOp     addProperty;
@@ -653,7 +658,7 @@ typedef bool (*FinishClassInitOp)(JSContext* cx, JS::HandleObject ctor,
 
 const size_t JSCLASS_CACHED_PROTO_WIDTH = 6;
 
-struct ClassSpec
+struct JS_STATIC_CLASS ClassSpec
 {
     ClassObjectCreationOp createConstructor;
     ClassObjectCreationOp createPrototype;
@@ -689,7 +694,7 @@ struct ClassSpec
     }
 };
 
-struct ClassExtension
+struct JS_STATIC_CLASS ClassExtension
 {
     /**
      * If an object is used as a key in a weakmap, it may be desirable for the
@@ -721,7 +726,7 @@ struct ClassExtension
 #define JS_NULL_CLASS_SPEC  nullptr
 #define JS_NULL_CLASS_EXT   nullptr
 
-struct ObjectOps
+struct JS_STATIC_CLASS ObjectOps
 {
     LookupPropertyOp lookupProperty;
     DefinePropertyOp defineProperty;
@@ -745,7 +750,7 @@ struct ObjectOps
 
 typedef void (*JSClassInternal)();
 
-struct JSClassOps
+struct JS_STATIC_CLASS JSClassOps
 {
     /* Function pointer members (may be null). */
     JSAddPropertyOp     addProperty;
@@ -871,7 +876,7 @@ struct JSClass {
 
 namespace js {
 
-struct Class
+struct JS_STATIC_CLASS Class
 {
     JS_CLASS_MEMBERS(js::ClassOps, FreeOp);
     const ClassSpec* spec;
