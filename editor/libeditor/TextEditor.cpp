37315b37439ba71d0017b179401ac6317851ed46
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2016-12-20 19:24:08 +0900

    Bug 1324996 - Part 1. Implement nsIAtom version of SetAttribute/RemoveAttribute/CloneAttirubte. r=masayuki
    Add nsIAtom version of the following.
     - CloneAttribute
     - RemoveAttribute
     - RemoveAttributeOrEquivalent
     - SetAttribute
     - SetAttributeOrEquivalent
    
    MozReview-Commit-ID: 8CutpdyVuew
    
    P1
    
    MozReview-Commit-ID: 9MdmGcTqaxT

--- editor/libeditor/TextEditor.cpp
+++ editor/libeditor/TextEditor.cpp
@@ -1609,8 +1609,8 @@ TextEditor::GetDOMEventTarget()
 
 
 nsresult
-TextEditor::SetAttributeOrEquivalent(nsIDOMElement* aElement,
-                                     const nsAString& aAttribute,
+TextEditor::SetAttributeOrEquivalent(Element* aElement,
+                                     nsIAtom* aAttribute,
                                      const nsAString& aValue,
                                      bool aSuppressTransaction)
 {
@@ -1618,8 +1618,8 @@ TextEditor::SetAttributeOrEquivalent(nsIDOMElement* aElement,
 }
 
 nsresult
-TextEditor::RemoveAttributeOrEquivalent(nsIDOMElement* aElement,
-                                        const nsAString& aAttribute,
+TextEditor::RemoveAttributeOrEquivalent(Element* aElement,
+                                        nsIAtom* aAttribute,
                                         bool aSuppressTransaction)
 {
   return EditorBase::RemoveAttribute(aElement, aAttribute);
