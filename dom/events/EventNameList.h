d265a06707eac62b95dc0992c239d0babf31967f
Mantaroh Yoshinaga <mantaroh@gmail.com>
2016-12-20 16:03:28 +0900

    Bug 1264125 part 1 - Add transitioncancel event handler. r=masayuki
    We moved 'transitioncancel' event to the CSS-Transition Level 1.
    The definition of 'transitioncancel' is as follow:
    https://drafts.csswg.org/css-transitions-1/#transitioncancle
    
    MozReview-Commit-ID: 1rEH0KjBrlL

--- dom/events/EventNameList.h
+++ dom/events/EventNameList.h
@@ -934,6 +934,10 @@ EVENT(transitionend,
       eTransitionEnd,
       EventNameType_All,
       eTransitionEventClass)
+EVENT(transitioncancel,
+      eTransitionCancel,
+      EventNameType_All,
+      eTransitionEventClass)
 EVENT(animationstart,
       eAnimationStart,
       EventNameType_All,
