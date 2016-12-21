9441ceea65b00aa998bb33b80a9a79ae803939c4
JW Wang <jwwang@mozilla.com>
2016-12-22 04:32:47 +0800

    Bug 1325905. Part 6 - let DecodingFirstFrameState::Enter() call Request{Audio,Video} instead. r=kaku
    MozReview-Commit-ID: AH1F9hMJorM

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -1909,7 +1909,12 @@ DecodingFirstFrameState::Enter()
   MOZ_ASSERT(!mMaster->mVideoDecodeSuspended);
 
   // Dispatch tasks to decode first frames.
-  mMaster->DispatchDecodeTasksIfNeeded();
+  if (mMaster->HasAudio()) {
+    mMaster->RequestAudioData();
+  }
+  if (mMaster->HasVideo()) {
+    mMaster->RequestVideoData(false, media::TimeUnit());
+  }
 }
 
 void
