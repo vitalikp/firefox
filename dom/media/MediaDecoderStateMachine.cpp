6ad374f7ec1e6606ef552ed9021893f74a79295d
JW Wang <jwwang@mozilla.com>
2016-12-21 16:03:16 +0800

    Bug 1325004. Part 5 - let BufferingState call Push{Audio,Video} instead. r=kikuo
    BufferingState will call DispatchDecodeTasksIfNeeded() if not done buffering.
    
    MozReview-Commit-ID: Kzqqn8lXPLm

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -1552,7 +1552,7 @@ public:
   {
     // This might be the sample we need to exit buffering.
     // Schedule Step() to check it.
-    mMaster->Push(aAudio);
+    mMaster->PushAudio(aAudio);
     mMaster->ScheduleStateMachine();
   }
 
@@ -1560,7 +1560,7 @@ public:
   {
     // This might be the sample we need to exit buffering.
     // Schedule Step() to check it.
-    mMaster->Push(aVideo);
+    mMaster->PushVideo(aVideo);
     mMaster->ScheduleStateMachine();
   }
 
@@ -2165,6 +2165,7 @@ BufferingState::Step()
       SLOG("Buffering: wait %ds, timeout in %.3lfs",
            mBufferingWait, mBufferingWait - elapsed.ToSeconds());
       mMaster->ScheduleStateMachineIn(USECS_PER_S);
+      mMaster->DispatchDecodeTasksIfNeeded();
       return;
     }
   } else if (mMaster->OutOfDecodedAudio() || mMaster->OutOfDecodedVideo()) {
