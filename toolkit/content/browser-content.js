8e6fa5092da77a36505c1d7105ddb9068f1ce6a9
Michael <wrigh517@msu.edu>
2016-12-21 11:09:39 -0500

    Bug 1300784 - Combine non-e10s and e10s select dropdown implementations, preffed off. r=mconley
    Patch by Michael Wright and Freddy (Junshan) Luo.
    
    MozReview-Commit-ID: G0CaZplABpC

--- toolkit/content/browser-content.js
+++ toolkit/content/browser-content.js
@@ -15,6 +15,8 @@ XPCOMUtils.defineLazyModuleGetter(this, "ReaderMode",
   "resource://gre/modules/ReaderMode.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "BrowserUtils",
   "resource://gre/modules/BrowserUtils.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "SelectContentHelper",
+  "resource://gre/modules/SelectContentHelper.jsm");
 
 var global = this;
 
@@ -1756,3 +1758,22 @@ let DateTimePickerListener = {
 }
 
 DateTimePickerListener.init();
+
+addEventListener("mozshowdropdown", event => {
+  if (!event.isTrusted)
+    return;
+
+  if (!SelectContentHelper.open) {
+    new SelectContentHelper(event.target, {isOpenedViaTouch: false}, this);
+  }
+});
+
+addEventListener("mozshowdropdown-sourcetouch", event => {
+  if (!event.isTrusted)
+    return;
+
+  if (!SelectContentHelper.open) {
+    new SelectContentHelper(event.target, {isOpenedViaTouch: true}, this);
+  }
+});
+
