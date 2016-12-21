01e1bbbdc768351743d883fd572d3d15b2b3c9d1
JW Wang <jwwang@mozilla.com>
2016-12-21 17:21:21 +0800

    Bug 1325905. Part 1 - add assertions to MDSM::Request{Audio,Video}Data. r=kaku
    MozReview-Commit-ID: JjRsv2wdLUq

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -1099,16 +1099,12 @@ private:
   void RequestAudioData()
   {
     MOZ_ASSERT(!mDoneAudioSeeking);
-    MOZ_ASSERT(!mMaster->IsRequestingAudioData());
-    MOZ_ASSERT(!mMaster->IsWaitingAudioData());
     mMaster->RequestAudioData();
   }
 
   void RequestVideoData()
   {
     MOZ_ASSERT(!mDoneVideoSeeking);
-    MOZ_ASSERT(!mMaster->IsRequestingVideoData());
-    MOZ_ASSERT(!mMaster->IsWaitingVideoData());
     mMaster->RequestVideoData(false, media::TimeUnit());
   }
 
@@ -2999,6 +2995,9 @@ void
 MediaDecoderStateMachine::RequestAudioData()
 {
   MOZ_ASSERT(OnTaskQueue());
+  MOZ_ASSERT(IsAudioDecoding());
+  MOZ_ASSERT(!IsRequestingAudioData());
+  MOZ_ASSERT(!IsWaitingAudioData());
   SAMPLE_LOG("Queueing audio task - queued=%i, decoder-queued=%o",
              AudioQueue().GetSize(), mReader->SizeOfAudioQueueInFrames());
 
@@ -3060,6 +3059,9 @@ MediaDecoderStateMachine::RequestVideoData(bool aSkipToNextKeyframe,
                                            const media::TimeUnit& aCurrentTime)
 {
   MOZ_ASSERT(OnTaskQueue());
+  MOZ_ASSERT(IsVideoDecoding());
+  MOZ_ASSERT(!IsRequestingVideoData());
+  MOZ_ASSERT(!IsWaitingVideoData());
   SAMPLE_LOG("Queueing video task - queued=%i, decoder-queued=%o, skip=%i, time=%lld",
              VideoQueue().GetSize(), mReader->SizeOfVideoQueueInFrames(), aSkipToNextKeyframe,
              aCurrentTime.ToMicroseconds());
