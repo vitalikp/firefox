9497ca222f580e5490dfe08e6ae3bc2baf40e80d
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2016-12-23 15:54:10 +0000

    Bug 1286505 part 1 - Convert AbortReason enum to an enum class. r=h4writer

--- js/src/jit/IonAnalysis.cpp
+++ js/src/jit/IonAnalysis.cpp
@@ -4199,7 +4199,7 @@ jit::AnalyzeNewScriptDefiniteProperties(JSContext* cx, HandleFunction fun,
     if (!builder.build()) {
         if (cx->isThrowingOverRecursed() || cx->isThrowingOutOfMemory())
             return false;
-        if (builder.abortReason() == AbortReason_Alloc) {
+        if (builder.abortReason() == AbortReason::Alloc) {
             ReportOutOfMemory(cx);
             return false;
         }
@@ -4437,7 +4437,7 @@ jit::AnalyzeArgumentsUsage(JSContext* cx, JSScript* scriptArg)
     if (!builder.build()) {
         if (cx->isThrowingOverRecursed() || cx->isThrowingOutOfMemory())
             return false;
-        if (builder.abortReason() == AbortReason_Alloc) {
+        if (builder.abortReason() == AbortReason::Alloc) {
             ReportOutOfMemory(cx);
             return false;
         }
