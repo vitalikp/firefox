aab68023142fc36ce86f41f6e1151f8fbb6b72be
Jan de Mooij <jdemooij@mozilla.com>
2016-12-23 08:15:01 +0100

    Bug 1322093 part 18 - Add IonCacheIRCompiler to compile Ion IC stubs from CacheIR. r=h4writer

--- js/src/jit/MacroAssembler.cpp
+++ js/src/jit/MacroAssembler.cpp
@@ -2390,6 +2390,12 @@ MacroAssembler::icBuildOOLFakeExitFrame(void* fakeReturnAddr, AfterICSaveLive& a
     return buildOOLFakeExitFrame(fakeReturnAddr);
 }
 
+bool
+MacroAssembler::icBuildOOLFakeExitFrame(void* fakeReturnAddr, AutoSaveLiveRegisters& save)
+{
+    return buildOOLFakeExitFrame(fakeReturnAddr);
+}
+
 void
 MacroAssembler::icRestoreLive(LiveRegisterSet& liveRegs, AfterICSaveLive& aic)
 {
