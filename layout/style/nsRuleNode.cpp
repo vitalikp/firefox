fd9271029ef9a311c6aa2445df5ad813f31299f9
Mats Palmgren <mats@mozilla.com>
2016-12-23 19:11:03 +0100

    Bug 1322191 part 1 - [css-display] Add style system support for display:flow-root. Enable it by default. r=dholbert

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -242,6 +242,7 @@ nsRuleNode::EnsureBlockDisplay(StyleDisplay& display,
   case StyleDisplay::Flex:
   case StyleDisplay::WebkitBox:
   case StyleDisplay::Grid:
+  case StyleDisplay::FlowRoot:
     // do not muck with these at all - already blocks
     // This is equivalent to nsStyleDisplay::IsBlockOutside.  (XXX Maybe we
     // should just call that?)
@@ -285,6 +286,7 @@ nsRuleNode::EnsureInlineDisplay(StyleDisplay& display)
   // see if the display value is already inline
   switch (display) {
     case StyleDisplay::Block:
+    case StyleDisplay::FlowRoot:
       display = StyleDisplay::InlineBlock;
       break;
     case StyleDisplay::Table:
