fe505ce315b990edd51a01efb74074a08af975ab
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2016-12-26 14:32:57 +0900

    Bug 1325843 - Use explicitly static method call for EditorBase::IsTextNode. r=masayuki
    IsTextNode is static method, so we should use explicitly static method call into EditRule.
    
    MozReview-Commit-ID: EX2BbbN2vNJ

--- editor/libeditor/TextEditRules.cpp
+++ editor/libeditor/TextEditRules.cpp
@@ -713,7 +713,7 @@ TextEditRules::WillInsertText(EditAction aAction,
 
   // don't put text in places that can't have it
   NS_ENSURE_STATE(mTextEditor);
-  if (!mTextEditor->IsTextNode(selNode) &&
+  if (!EditorBase::IsTextNode(selNode) &&
       !mTextEditor->CanContainTag(*selNode, *nsGkAtoms::textTagName)) {
     return NS_ERROR_FAILURE;
   }
@@ -939,8 +939,7 @@ TextEditRules::DidDeleteSelection(Selection* aSelection,
   NS_ENSURE_TRUE(startNode, NS_ERROR_FAILURE);
 
   // delete empty text nodes at selection
-  NS_ENSURE_STATE(mTextEditor);
-  if (mTextEditor->IsTextNode(startNode)) {
+  if (EditorBase::IsTextNode(startNode)) {
     nsCOMPtr<nsIDOMText> textNode = do_QueryInterface(startNode);
     uint32_t strLength;
     rv = textNode->GetLength(&strLength);
@@ -948,6 +947,7 @@ TextEditRules::DidDeleteSelection(Selection* aSelection,
 
     // are we in an empty text node?
     if (!strLength) {
+      NS_ENSURE_STATE(mTextEditor);
       rv = mTextEditor->DeleteNode(startNode);
       NS_ENSURE_SUCCESS(rv, rv);
     }
