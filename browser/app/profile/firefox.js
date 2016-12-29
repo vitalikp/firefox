315cb711ef597f1743d7033c1c24513db051147a
Scott Wu <scottcwwu@gmail.com>
2016-12-29 16:09:33 +0800

    Bug 1326153 - Change Form Autofill feature flag to browser.formautofill.experimental r=MattN
    MozReview-Commit-ID: 6QF0RVLRS61

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1510,4 +1510,4 @@ pref("services.sync.validation.enabled", true);
 #endif
 
 // Preferences for the form autofill system extension
-pref("browser.formautofill.enabled", false);
+pref("browser.formautofill.experimental", false);
