888cf8f3550c6c56798d2000bd0e2e1d1910284a
Emanuel Hoogeveen <emanuel.hoogeveen@gmail.com>
2017-01-02 13:11:00 +0100

    Bug 1326302 - Part 1: Redesign PageProtectingVector for minimum overhead. r=jandem

--- js/src/jit/x86-shared/AssemblerBuffer-x86-shared.h
+++ js/src/jit/x86-shared/AssemblerBuffer-x86-shared.h
@@ -188,7 +188,7 @@ namespace jit {
 
 #ifndef RELEASE_OR_BETA
         PageProtectingVector<unsigned char, 256, AssemblerBufferAllocPolicy,
-                             /* ProtectUsed = */ false, /* ProtectUnused = */ true,
+                             /* ProtectUsed = */ false, /* ProtectUnused = */ false,
                              /* InitialLowerBound = */ 32 * 1024> m_buffer;
 #else
         mozilla::Vector<unsigned char, 256, AssemblerBufferAllocPolicy> m_buffer;
