e530ab07303e3b30a1c27c2b76b7357333f1f0d3
George Wright <george@mozilla.com>
2017-01-03 15:13:36 -0500

    Bug 1324095 - Add GPU_PROCESS_INITIALIZATION_TIME_MS to track how long the GPU process takes to initialise excluding fork/xpcom initialisation times r=dvander,data-review=bsmedberg

--- gfx/ipc/GPUParent.cpp
+++ gfx/ipc/GPUParent.cpp
@@ -12,6 +12,8 @@
 #include "gfxPrefs.h"
 #include "GPUProcessHost.h"
 #include "mozilla/Assertions.h"
+#include "mozilla/Telemetry.h"
+#include "mozilla/TimeStamp.h"
 #include "mozilla/gfx/2D.h"
 #include "mozilla/gfx/gfxVars.h"
 #include "mozilla/ipc/ProcessChild.h"
@@ -45,6 +47,7 @@ using namespace layers;
 static GPUParent* sGPUParent;
 
 GPUParent::GPUParent()
+  : mLaunchTime(TimeStamp::Now())
 {
   sGPUParent = this;
 }
@@ -181,6 +184,7 @@ GPUParent::RecvInit(nsTArray<GfxPrefSetting>&& prefs,
   RecvGetDeviceStatus(&data);
   Unused << SendInitComplete(data);
 
+  Telemetry::AccumulateTimeDelta(Telemetry::GPU_PROCESS_INITIALIZATION_TIME_MS, mLaunchTime);
   return IPC_OK();
 }
 
