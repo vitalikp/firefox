1b0eef93aaf01ad422865ad9e99c306bd492b963
Olli Pettay <Olli.Pettay@helsinki.fi>
2017-01-03 21:50:10 +0200

    Bug 1326507, rename Traverse to TraverseNative, r=mccr8

--- xpcom/base/CycleCollectedJSContext.cpp
+++ xpcom/base/CycleCollectedJSContext.cpp
@@ -352,8 +352,8 @@ CheckParticipatesInCycleCollection(JS::GCCellPtr aThing, const char* aName,
 }
 
 NS_IMETHODIMP
-JSGCThingParticipant::Traverse(void* aPtr,
-                               nsCycleCollectionTraversalCallback& aCb)
+JSGCThingParticipant::TraverseNative(void* aPtr,
+                                     nsCycleCollectionTraversalCallback& aCb)
 {
   auto runtime = reinterpret_cast<CycleCollectedJSContext*>(
     reinterpret_cast<char*>(this) - offsetof(CycleCollectedJSContext,
@@ -369,7 +369,8 @@ JSGCThingParticipant::Traverse(void* aPtr,
 static JSGCThingParticipant sGCThingCycleCollectorGlobal;
 
 NS_IMETHODIMP
-JSZoneParticipant::Traverse(void* aPtr, nsCycleCollectionTraversalCallback& aCb)
+JSZoneParticipant::TraverseNative(void* aPtr,
+                                  nsCycleCollectionTraversalCallback& aCb)
 {
   auto runtime = reinterpret_cast<CycleCollectedJSContext*>(
     reinterpret_cast<char*>(this) - offsetof(CycleCollectedJSContext,
