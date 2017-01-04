2e3c8dc97e55f7a4e7ef154587d64589d87c3787
Shu-yu Guo <shu@rfrn.org>
2017-01-05 02:15:00 +0300

    Bug 1319443 - Fix GlobalScopes XDR to encode global function bindings. (r=Waldo)

--- js/src/vm/Scope.cpp
+++ js/src/vm/Scope.cpp
@@ -1009,6 +1009,8 @@ GlobalScope::XDR(XDRState<mode>* xdr, ScopeKind kind, MutableHandleScope scope)
         if (mode == XDR_DECODE)
             uniqueData.emplace(cx, data);
 
+        if (!xdr->codeUint32(&data->varStart))
+            return false;
         if (!xdr->codeUint32(&data->letStart))
             return false;
         if (!xdr->codeUint32(&data->constStart))
@@ -1016,6 +1018,7 @@ GlobalScope::XDR(XDRState<mode>* xdr, ScopeKind kind, MutableHandleScope scope)
 
         if (mode == XDR_DECODE) {
             if (!data->length) {
+                MOZ_ASSERT(!data->varStart);
                 MOZ_ASSERT(!data->letStart);
                 MOZ_ASSERT(!data->constStart);
             }
@@ -1600,7 +1603,10 @@ js::DumpBindings(JSContext* cx, Scope* scopeArg)
         fprintf(stderr, "%s %s ", BindingKindString(bi.kind()), bytes.ptr());
         switch (bi.location().kind()) {
           case BindingLocation::Kind::Global:
-            fprintf(stderr, "global\n");
+            if (bi.isTopLevelFunction())
+                fprintf(stderr, "global function\n");
+            else
+                fprintf(stderr, "global\n");
             break;
           case BindingLocation::Kind::Argument:
             fprintf(stderr, "arg slot %u\n", bi.location().argumentSlot());
