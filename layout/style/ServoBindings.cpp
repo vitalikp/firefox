20d7556fcc56cb6e99f555a6c0dc9a5df5221f3d
Boris Zbarsky <bzbarsky@mit.edu>
2017-01-04 14:52:27 -0500

    Bug 1298588 part 15, gecko piece. Rip out the initial() methods on style structs in stylo. r=bholley

--- layout/style/ServoBindings.cpp
+++ layout/style/ServoBindings.cpp
@@ -1059,12 +1059,6 @@ NS_IMPL_THREADSAFE_FFI_REFCOUNTING(nsCSSValueSharedList, CSSValueSharedList);
 #define STYLE_STRUCT(name, checkdata_cb)                                      \
                                                                               \
 void                                                                          \
-Gecko_Construct_nsStyle##name(nsStyle##name* ptr)                             \
-{                                                                             \
-  new (ptr) nsStyle##name(StyleStructContext::ServoContext());                \
-}                                                                             \
-                                                                              \
-void                                                                          \
 Gecko_Construct_Default_nsStyle##name(nsStyle##name* ptr,                     \
                                       const nsPresContext* pres_context)      \
 {                                                                             \
@@ -1082,6 +1076,12 @@ void                                                                          \
 Gecko_Destroy_nsStyle##name(nsStyle##name* ptr)                               \
 {                                                                             \
   ptr->~nsStyle##name();                                                      \
+}
+
+void
+Gecko_Construct_nsStyleVariables(nsStyleVariables* ptr)
+{
+  new (ptr) nsStyleVariables();
 }
 
 #include "nsStyleStructList.h"
--- layout/style/nsStyleStruct.cpp
+++ layout/style/nsStyleStruct.cpp
@@ -4126,6 +4126,11 @@ nsStyleUIReset::CalcDifference(const nsStyleUIReset& aNewData) const
 // nsStyleVariables
 //
 
+nsStyleVariables::nsStyleVariables()
+{
+  MOZ_COUNT_CTOR(nsStyleVariables);
+}
+
 nsStyleVariables::nsStyleVariables(StyleStructContext aContext)
 {
   MOZ_COUNT_CTOR(nsStyleVariables);
