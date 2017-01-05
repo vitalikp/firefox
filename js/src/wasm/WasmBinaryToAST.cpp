fceb2862043fae4f54cac07208fed936444399f0
Benjamin Bouvier <benj@benj.me>
2017-01-05 17:14:46 +0100

    Bug 1328911: Introduce resilient mode for wasm::BinaryToAst; r=luke
    MozReview-Commit-ID: 3kPyoVPDBYk

--- js/src/wasm/WasmBinaryToAST.cpp
+++ js/src/wasm/WasmBinaryToAST.cpp
@@ -1833,7 +1833,6 @@ AstDecodeCodeSection(AstDecodeContext& c)
     if (sectionStart == Decoder::NotStarted) {
         if (c.env().numFuncDefs() != 0)
             return c.d.fail("expected function bodies");
-
         return true;
     }
 
@@ -1852,10 +1851,7 @@ AstDecodeCodeSection(AstDecodeContext& c)
             return false;
     }
 
-    if (!c.d.finishSection(sectionStart, sectionSize, "code"))
-        return false;
-
-    return true;
+    return c.d.finishSection(sectionStart, sectionSize, "code");
 }
 
 // Number of bytes to display in a single fragment of a data section (per line).
@@ -1895,15 +1891,15 @@ AstDecodeModuleTail(AstDecodeContext& c)
 }
 
 bool
-wasm::BinaryToAst(JSContext* cx, const uint8_t* bytes, uint32_t length,
-                  LifoAlloc& lifo, AstModule** module)
+wasm::BinaryToAst(JSContext* cx, const uint8_t* bytes, uint32_t length, LifoAlloc& lifo,
+                  AstModule** module)
 {
     AstModule* result = new(lifo) AstModule(lifo);
     if (!result || !result->init())
         return false;
 
     UniqueChars error;
-    Decoder d(bytes, bytes + length, &error);
+    Decoder d(bytes, bytes + length, &error, /* resilient */ true);
     AstDecodeContext c(cx, lifo, d, *result, true);
 
     if (!AstDecodeEnvironment(c) ||
