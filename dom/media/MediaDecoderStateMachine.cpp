6cc0880631be04bbc89a69a84e4df9241cff13a5
JW Wang <jwwang@mozilla.com>
2017-01-05 11:50:20 +0800

    Bug 1328781. part 2 - don't convert a double to an int64_t to avoid rounding errors. r=kikuo
    MozReview-Commit-ID: 83Zxqr4aiMa

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -2505,10 +2505,11 @@ MediaDecoderStateMachine::GetDecodedAudioDuration()
   return AudioQueue().Duration();
 }
 
-bool MediaDecoderStateMachine::HaveEnoughDecodedAudio()
+bool
+MediaDecoderStateMachine::HaveEnoughDecodedAudio()
 {
   MOZ_ASSERT(OnTaskQueue());
-  int64_t ampleAudioUSecs = mAmpleAudioThresholdUsecs * mPlaybackRate;
+  auto ampleAudioUSecs = mAmpleAudioThresholdUsecs * mPlaybackRate;
   return AudioQueue().GetSize() > 0 &&
          GetDecodedAudioDuration() >= ampleAudioUSecs;
 }
