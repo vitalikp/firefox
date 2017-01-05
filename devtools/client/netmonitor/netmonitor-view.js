ef3450ac20960e323594b3cfea074dded666d0da
Jarda Snajdr <jsnajdr@gmail.com>
2017-01-05 21:42:04 +0100

    Bug 1329165 - Use Immutable.Map for request list, optimize sorting r=Honza
    MozReview-Commit-ID: L31hiD5CETE

--- devtools/client/netmonitor/netmonitor-view.js
+++ devtools/client/netmonitor/netmonitor-view.js
@@ -190,7 +190,7 @@ var NetMonitorView = {
         console.error(ex);
       }
 
-      const requests = requestsView.store.getState().requests.requests;
+      const requests = requestsView.store.getState().requests.requests.valueSeq();
       statisticsView.createPrimedCacheChart(requests);
       statisticsView.createEmptyCacheChart(requests);
     });
