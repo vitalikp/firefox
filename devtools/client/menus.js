845c020419a27dd2b3a1416a1d7ed024827b179b
Alexandre Poirot <poirot.alex@gmail.com>
2017-01-05 10:27:31 -0800

    Bug 1320149 - Prevent loading gcli when opening a toolbox. r=jwalker,miker
    MozReview-Commit-ID: KE209SRA15u

--- devtools/client/menus.js
+++ devtools/client/menus.js
@@ -137,11 +137,7 @@ exports.menuitems = [
       let window = event.target.ownerDocument.defaultView;
       let target = TargetFactory.forTab(window.gBrowser.selectedTab);
 
-      CommandUtils.createRequisition(target, {
-        environment: CommandUtils.createEnvironment({target})
-      }).then(requisition => {
-        requisition.updateExec("eyedropper --frommenu");
-      }, e => console.error(e));
+      CommandUtils.executeOnTarget(target, "eyedropper --frommenu");
     },
     checkbox: true
   },
