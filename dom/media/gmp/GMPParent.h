c15e20be2b52f87336f8851dfbf687469e65e394
Andrew McCreight <continuation@gmail.com>
2017-01-05 12:55:27 -0800

    Bug 1321871, part 1 - Replace use of opens and bridges in GMP protocols with endpoints. r=peterv
    This removes the open of PGMPContent from PGMP, the bridge of
    PGMPService and PGMP from PGMPContent, and the spawn of PGMP from
    PGMPService. I did these changes all at once because the way the
    bridges works it was hard to split it up.

--- dom/media/gmp/GMPParent.h
+++ dom/media/gmp/GMPParent.h
@@ -134,12 +134,14 @@ public:
   // Called when the child process has died.
   void ChildTerminated();
 
+  bool OpenPGMPContent();
+
   void GetGMPContentParent(UniquePtr<MozPromiseHolder<GetGMPContentParentPromise>>&& aPromiseHolder);
   already_AddRefed<GMPContentParent> ForgetGMPContentParent();
 
   bool EnsureProcessLoaded(base::ProcessId* aID);
 
-  bool Bridge(GMPServiceParent* aGMPServiceParent);
+  void IncrementGMPContentChildCount();
 
   const nsTArray<GMPCapability>& GetCapabilities() const { return mCapabilities; }
 
@@ -155,9 +157,6 @@ private:
   mozilla::ipc::IPCResult RecvPGMPStorageConstructor(PGMPStorageParent* actor) override;
   PGMPStorageParent* AllocPGMPStorageParent() override;
   bool DeallocPGMPStorageParent(PGMPStorageParent* aActor) override;
-
-  PGMPContentParent* AllocPGMPContentParent(Transport* aTransport,
-                                            ProcessId aOtherPid) override;
 
   mozilla::ipc::IPCResult RecvPGMPTimerConstructor(PGMPTimerParent* actor) override;
   PGMPTimerParent* AllocPGMPTimerParent() override;
