75626293eb8561d8166dc5f1fa7274a768d525c6
Nathan Froyd <froydnj@mozilla.com>
2017-01-06 09:21:01 -0500

    Bug 1142197 - manage ProfilerBacktrace with UniquePtr; r=mstange
    Smart pointers are better than raw pointers.

--- layout/base/nsRefreshDriver.cpp
+++ layout/base/nsRefreshDriver.cpp
@@ -1085,8 +1085,6 @@ nsRefreshDriver::ChooseTimer() const
 
 nsRefreshDriver::nsRefreshDriver(nsPresContext* aPresContext)
   : mActiveTimer(nullptr),
-    mReflowCause(nullptr),
-    mStyleCause(nullptr),
     mPresContext(aPresContext),
     mRootRefresh(nullptr),
     mPendingTransaction(0),
@@ -1140,9 +1138,6 @@ nsRefreshDriver::~nsRefreshDriver()
     shell->InvalidatePresShellIfHidden();
   }
   mPresShellsToInvalidateIfHidden.Clear();
-
-  profiler_free_backtrace(mStyleCause);
-  profiler_free_backtrace(mReflowCause);
 }
 
 // Method for testing.  See nsIDOMWindowUtils.advanceTimeAndRefresh
@@ -1823,7 +1818,7 @@ nsRefreshDriver::Tick(int64_t aNowEpoch, TimeStamp aNowTime)
 
           if (!tracingStyleFlush) {
             tracingStyleFlush = true;
-            profiler_tracing("Paint", "Styles", mStyleCause, TRACING_INTERVAL_START);
+            profiler_tracing("Paint", "Styles", Move(mStyleCause), TRACING_INTERVAL_START);
             mStyleCause = nullptr;
           }
 
@@ -1863,7 +1858,7 @@ nsRefreshDriver::Tick(int64_t aNowEpoch, TimeStamp aNowTime)
 
         if (!tracingLayoutFlush) {
           tracingLayoutFlush = true;
-          profiler_tracing("Paint", "Reflow", mReflowCause, TRACING_INTERVAL_START);
+          profiler_tracing("Paint", "Reflow", Move(mReflowCause), TRACING_INTERVAL_START);
           mReflowCause = nullptr;
         }
 
