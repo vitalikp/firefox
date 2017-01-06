906048825636310321e1d2b3ab28da71e493ba19
Cameron McCormack <cam@mcc.id.au>
2017-01-06 15:05:24 +0800

    Bug 1290209 - Part 1: Move CSSStyleSheet::SetTitle up to StyleSheet. r=xidorn
    MozReview-Commit-ID: 2rzn5gba7gw

--- layout/style/Loader.cpp
+++ layout/style/Loader.cpp
@@ -1300,7 +1300,7 @@ Loader::PrepareSheet(StyleSheet* aSheet,
 
   sheet->SetMedia(mediaList);
 
-  sheet->SetTitle(aTitle);
+  aSheet->SetTitle(aTitle);
   sheet->SetEnabled(!isAlternate);
   sheet->SetScopeElement(aScopeElement);
 }
