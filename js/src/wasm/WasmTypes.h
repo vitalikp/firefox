51f3b54b2a9a18eed47572b848c41f84f4546bea
Yury Delendik <ydelendik@mozilla.com>
2017-01-12 13:55:18 -0600

    Bug 1330489 - Ignore imports for wasm debug frames and discard frames in activation on throw. r=luke
    MozReview-Commit-ID: HmZkqnUvZXm

--- js/src/wasm/WasmTypes.h
+++ js/src/wasm/WasmTypes.h
@@ -1054,7 +1054,7 @@ enum class SymbolicAddress
     ReportOverRecursed,
     HandleExecutionInterrupt,
     HandleDebugTrap,
-    HandleDebugThrow,
+    HandleThrow,
     ReportTrap,
     ReportOutOfBounds,
     ReportUnalignedAccess,
