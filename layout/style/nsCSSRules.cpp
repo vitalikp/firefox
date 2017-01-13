b0aa279216b23d7c1a9796065cf5929daa23b44e
Boris Zbarsky <bzbarsky@mit.edu>
2017-01-13 10:41:04 -0500

    Bug 851892 part 15. Convert CSSFontFaceRule to WebIDL. r=peterv,heycam

--- layout/style/nsCSSRules.cpp
+++ layout/style/nsCSSRules.cpp
@@ -40,6 +40,7 @@
 #include "mozilla/dom/CSSSupportsRuleBinding.h"
 #include "mozilla/dom/CSSMozDocumentRuleBinding.h"
 #include "mozilla/dom/CSSPageRuleBinding.h"
+#include "mozilla/dom/CSSFontFaceRuleBinding.h"
 #include "StyleRule.h"
 #include "nsFont.h"
 #include "nsIURI.h"
@@ -1676,7 +1677,6 @@ nsCSSFontFaceRule::IsCCLeaf() const
 // QueryInterface implementation for nsCSSFontFaceRule
 NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION_INHERITED(nsCSSFontFaceRule)
   NS_INTERFACE_MAP_ENTRY(nsIDOMCSSFontFaceRule)
-  NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(CSSFontFaceRule)
 NS_INTERFACE_MAP_END_INHERITING(Rule)
 
 #ifdef DEBUG
@@ -1732,6 +1732,12 @@ nsCSSFontFaceRule::GetCssTextImpl(nsAString& aCssText) const
   aCssText.Append('}');
 }
 
+nsICSSDeclaration*
+nsCSSFontFaceRule::Style()
+{
+  return &mDecl;
+}
+
 NS_IMETHODIMP
 nsCSSFontFaceRule::GetStyle(nsIDOMCSSStyleDeclaration** aStyle)
 {
@@ -1779,8 +1785,7 @@ nsCSSFontFaceRule::SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) const
 nsCSSFontFaceRule::WrapObject(JSContext* aCx,
                               JS::Handle<JSObject*> aGivenProto)
 {
-  NS_NOTREACHED("We called SetIsNotDOMBinding() in our constructor");
-  return nullptr;
+  return CSSFontFaceRuleBinding::Wrap(aCx, this, aGivenProto);
 }
 
 // -----------------------------------
