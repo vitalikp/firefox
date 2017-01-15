a628484070d91ee826e7903977beff9ef162c725
Emilio Cobos Álvarez <emilio@crisal.io>
2017-01-15 17:07:38 +0100

    Bug 1331213: Add an API to drop a nsCSSValue in the stack. r=heycam
    The getter function may create an nsCSSValue with allocated stuff, and we don't
    want to leak it.
    
    MozReview-Commit-ID: DYkUD8CW88E
    Signed-off-by: Emilio Cobos Álvarez <emilio@crisal.io>

--- layout/style/ServoBindings.cpp
+++ layout/style/ServoBindings.cpp
@@ -1038,6 +1038,12 @@ Gecko_PropertyId_IsPrefEnabled(nsCSSPropertyID id)
 }
 
 void
+Gecko_CSSValue_Drop(nsCSSValueBorrowedMut aCSSValue)
+{
+  aCSSValue->~nsCSSValue();
+}
+
+void
 Gecko_LoadStyleSheet(css::Loader* aLoader,
                      ServoStyleSheet* aParent,
                      RawServoImportRuleBorrowed aImportRule,
