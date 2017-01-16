c3d18209ae4bcba4c42b09c0f4021c79e6071b61
JW Wang <jwwang@mozilla.com>
2017-01-16 15:21:45 +0800

    Bug 1331554. Part 2 - remove the call to ScheduleUpdate(). r=jya
    ScheduleUpdate() will be called when we have new decoding requests.
    
    MozReview-Commit-ID: FkOc4zQmfSo

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -2749,9 +2749,6 @@ MediaFormatReader::SetBlankDecode(TrackType aTrack, bool aIsBlankDecode)
   decoder.mIsBlankDecode = aIsBlankDecode;
   decoder.Flush();
   decoder.ShutdownDecoder();
-  ScheduleUpdate(TrackInfo::kVideoTrack);
-
-  return;
 }
 
 void
