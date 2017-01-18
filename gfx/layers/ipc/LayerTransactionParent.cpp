7771fade065c1866f4adb089a10d943e7e91170b
David Anderson <dvander@alliedmods.net>
2017-01-17 18:47:06 -0800

    Link Compositables via IDs instead of actors. (bug 1323957 part 4, r=mattwoodrow)

--- gfx/layers/ipc/LayerTransactionParent.cpp
+++ gfx/layers/ipc/LayerTransactionParent.cpp
@@ -571,7 +571,7 @@ LayerTransactionParent::RecvUpdate(const TransactionInfo& aInfo,
     }
     case Edit::TOpAttachCompositable: {
       const OpAttachCompositable& op = edit.get_OpAttachCompositable();
-      CompositableHost* host = CompositableHost::FromIPDLActor(op.compositableParent());
+      RefPtr<CompositableHost> host = FindCompositable(op.compositable());
       if (mPendingCompositorUpdates) {
         // Do not attach compositables from old layer trees. Return true since
         // content cannot handle errors.
@@ -596,7 +596,7 @@ LayerTransactionParent::RecvUpdate(const TransactionInfo& aInfo,
       if (!imageBridge) {
         return IPC_FAIL_NO_REASON(this);
       }
-      CompositableHost* host = imageBridge->FindCompositable(op.compositable());
+      RefPtr<CompositableHost> host = imageBridge->FindCompositable(op.compositable());
       if (!host) {
         NS_ERROR("CompositableHost not found in the map");
         return IPC_FAIL_NO_REASON(this);
@@ -939,18 +939,6 @@ LayerTransactionParent::RecvForceComposite()
   return IPC_OK();
 }
 
-PCompositableParent*
-LayerTransactionParent::AllocPCompositableParent(const TextureInfo& aInfo)
-{
-  return CompositableHost::CreateIPDLActor(this, aInfo);
-}
-
-bool
-LayerTransactionParent::DeallocPCompositableParent(PCompositableParent* aActor)
-{
-  return CompositableHost::DestroyIPDLActor(aActor);
-}
-
 void
 LayerTransactionParent::ActorDestroy(ActorDestroyReason why)
 {
@@ -1038,6 +1026,15 @@ LayerTransactionParent::AsLayer(const LayerHandle& aHandle)
 }
 
 mozilla::ipc::IPCResult
+LayerTransactionParent::RecvNewCompositable(const CompositableHandle& aHandle, const TextureInfo& aInfo)
+{
+  if (!AddCompositable(aHandle, aInfo)) {
+    return IPC_FAIL_NO_REASON(this);
+  }
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult
 LayerTransactionParent::RecvReleaseLayer(const LayerHandle& aHandle)
 {
   if (!aHandle || !mLayerMap.Contains(aHandle.Value())) {
@@ -1049,6 +1046,13 @@ LayerTransactionParent::RecvReleaseLayer(const LayerHandle& aHandle)
     (*maybeLayer)->Disconnect();
   }
 
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult
+LayerTransactionParent::RecvReleaseCompositable(const CompositableHandle& aHandle)
+{
+  ReleaseCompositable(aHandle);
   return IPC_OK();
 }
 
