f43a0cbc049330bcd2530b645ac051b98a2c258f
JW Wang <jwwang@mozilla.com>
2017-01-19 10:42:29 +0800

    Bug 1332146 - use stronger assertions in MediaDecoder.h r=kaku
    MozReview-Commit-ID: 60umMQWhead

--- dom/media/MediaDecoder.h
+++ dom/media/MediaDecoder.h
@@ -414,7 +414,7 @@ private:
   void UpdateLogicalPosition()
   {
     MOZ_ASSERT(NS_IsMainThread());
-    MOZ_ASSERT(!IsShutdown());
+    MOZ_DIAGNOSTIC_ASSERT(!IsShutdown());
     // Per spec, offical position remains stable during pause and seek.
     if (mPlayState == PLAY_STATE_PAUSED || IsSeeking()) {
       return;
@@ -481,7 +481,7 @@ private:
   void UpdateReadyState()
   {
     MOZ_ASSERT(NS_IsMainThread());
-    MOZ_ASSERT(!IsShutdown());
+    MOZ_DIAGNOSTIC_ASSERT(!IsShutdown());
     mOwner->UpdateReadyState();
   }
 
@@ -523,7 +523,7 @@ protected:
 
   void SetExplicitDuration(double aValue)
   {
-    MOZ_ASSERT(!IsShutdown());
+    MOZ_DIAGNOSTIC_ASSERT(!IsShutdown());
     mExplicitDuration.Set(Some(aValue));
 
     // We Invoke DurationChanged explicitly, rather than using a watcher, so
