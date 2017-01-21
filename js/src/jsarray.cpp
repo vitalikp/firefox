2538b011c6fb0de23de747d765029587937437ec
Tooru Fujisawa <arai_a@mac.com>
2017-01-21 17:25:44 +0900

    Bug 1320408 - Part 9: Change JSObject::setFlags and depending methods to static method. r=jandem

--- js/src/jsarray.cpp
+++ js/src/jsarray.cpp
@@ -3304,7 +3304,7 @@ CreateArrayPrototype(JSContext* cx, JSProtoKey key)
                                                               metadata));
     if (!arrayProto ||
         !JSObject::setSingleton(cx, arrayProto) ||
-        !arrayProto->setDelegate(cx) ||
+        !JSObject::setDelegate(cx, arrayProto) ||
         !AddLengthProperty(cx, arrayProto))
     {
         return nullptr;
