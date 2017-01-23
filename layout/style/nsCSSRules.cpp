aba43de17282aee5e85fab73bf06815305e7c7df
Boris Zbarsky <bzbarsky@mit.edu>
2017-01-23 11:15:58 -0500

    Bug 1332704. Have ~nsDocument actually clear the self-pointer from _all_ its sheets, and reenable Rule::IsKnownLive bits that depends on that pointer being managed correctly. r=heycam

--- layout/style/nsCSSRules.cpp
+++ layout/style/nsCSSRules.cpp
@@ -84,10 +84,6 @@ Rule::IsKnownLive() const
     return true;
   }
 
-#if 1
-  return false;
-#else
-  // Disabled pending bug 1332704 getting resolved for now.
   StyleSheet* sheet = GetStyleSheet();
   if (!sheet) {
     return false;
@@ -99,7 +95,6 @@ Rule::IsKnownLive() const
 
   return nsCCUncollectableMarker::InGeneration(
     sheet->GetAssociatedDocument()->GetMarkedCCGeneration());
-#endif
 }
 
 NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_BEGIN(Rule)
