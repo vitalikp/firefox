a7c0bdfdd97f39c55737e898105c3748df2f7a2d
Boris Chiou <boris.chiou@gmail.com>
2017-01-24 15:33:10 +0800

    Bug 1317209 - Part 6: Support transition cascade level. r=heycam
    Implement the mapping between EffectCompositor::CascadeLevel in Gecko and
    EffectCompositor_CascadeLevel in Servo, so we can pass it as a parameter into
    Gecko_GetAnimationRule.
    
    MozReview-Commit-ID: GRedooyGE8c

--- layout/style/ServoBindings.cpp
+++ layout/style/ServoBindings.cpp
@@ -314,7 +314,8 @@ Gecko_GetServoDeclarationBlock(RawGeckoElementBorrowed aElement)
 
 RawServoDeclarationBlockStrong
 Gecko_GetAnimationRule(RawGeckoElementBorrowed aElement,
-                       nsIAtom* aPseudoTag)
+                       nsIAtom* aPseudoTag,
+                       EffectCompositor::CascadeLevel aCascadeLevel)
 {
   MOZ_ASSERT(aElement, "Invalid GeckoElement");
 
@@ -328,7 +329,6 @@ Gecko_GetAnimationRule(RawGeckoElementBorrowed aElement,
     return emptyDeclarationBlock;
   }
 
-  // FIXME: support different cascading levels in the later patch
   CSSPseudoElementType pseudoType =
     aPseudoTag
     ? nsCSSPseudoElements::GetPseudoType(
@@ -342,10 +342,8 @@ Gecko_GetAnimationRule(RawGeckoElementBorrowed aElement,
   }
 
   ServoAnimationRule* rule =
-    presContext->EffectCompositor()->GetServoAnimationRule(
-      aElement,
-      pseudoType,
-      EffectCompositor::CascadeLevel::Animations);
+    presContext->EffectCompositor()
+               ->GetServoAnimationRule(aElement, pseudoType, aCascadeLevel);
   if (!rule) {
     return emptyDeclarationBlock;
   }
