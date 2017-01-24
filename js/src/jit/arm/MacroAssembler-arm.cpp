fb2e5a8e7224b8fb437407651af25108840d15bf
Nicholas Nethercote <nnethercote@mozilla.com>
2017-01-25 09:08:15 +1100

    Bug 1333296 (part 6) - Remove SPS references in js/. r=shu.
    The most significant thing about this is that it changes some built-in function
    names:
    - enableSPSProfiling() -> enableGeckoProfiling()
    - enableSPSProfilingWithSlowAssertions() -> enableGeckoProfiling(WithSlowAssertions)
    - disableSPSProfiling() -> disableGeckoProfiling()
    - {en,dis}ableSPSProfiling*() -> {en,dis}ableGeckoProfiling*()
    - readSPSProfilingStack() -> readGeckoProfilingStack()
    
    It also requires two filename changes:
    - SPSProfiler.{h,cpp} -> GeckoProfiler.{h,cpp}
    
    And some type name changes, e.g.:
    - AutoSPSEntry -> AutoGeckoProfilerEntry
    - SPSProfiler -> GeckoProfiler
    - SPSInstrumentation -> GeckoProfilerInstrumentation
    - SPSEntryMarker -> GeckoProfilerEntryMarker
    - SPSBaselineOSRMarker -> GeckoProfilerBaselineOSRMarker
    
    And various method/function/variable name changes, and some comment changes.

--- js/src/jit/arm/MacroAssembler-arm.cpp
+++ js/src/jit/arm/MacroAssembler-arm.cpp
@@ -3710,7 +3710,7 @@ MacroAssemblerARMCompat::handleFailureWithHandlerTail(void* handler)
     {
         Label skipProfilingInstrumentation;
         // Test if profiler enabled.
-        AbsoluteAddress addressOfEnabled(GetJitContext()->runtime->spsProfiler().addressOfEnabled());
+        AbsoluteAddress addressOfEnabled(GetJitContext()->runtime->geckoProfiler().addressOfEnabled());
         asMasm().branch32(Assembler::Equal, addressOfEnabled, Imm32(0),
                           &skipProfilingInstrumentation);
         profilerExitFrame();
--- js/src/jit/x86/MacroAssembler-x86.cpp
+++ js/src/jit/x86/MacroAssembler-x86.cpp
@@ -280,7 +280,7 @@ MacroAssemblerX86::handleFailureWithHandlerTail(void* handler)
     {
         Label skipProfilingInstrumentation;
         // Test if profiler enabled.
-        AbsoluteAddress addressOfEnabled(GetJitContext()->runtime->spsProfiler().addressOfEnabled());
+        AbsoluteAddress addressOfEnabled(GetJitContext()->runtime->geckoProfiler().addressOfEnabled());
         asMasm().branch32(Assembler::Equal, addressOfEnabled, Imm32(0),
                           &skipProfilingInstrumentation);
         profilerExitFrame();
