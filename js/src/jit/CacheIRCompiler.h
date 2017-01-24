44ae8b7d605aa9b33397b7d3e61519a1f33d1235
Tom Schuster <evilpies@gmail.com>
2017-01-24 19:47:48 +0100

    Bug 1332946 - CacheIR: IC for function.length. r=jandem

--- js/src/jit/CacheIRCompiler.h
+++ js/src/jit/CacheIRCompiler.h
@@ -37,6 +37,7 @@ namespace jit {
     _(LoadInt32ArrayLengthResult)         \
     _(LoadUnboxedArrayLengthResult)       \
     _(LoadArgumentsObjectLengthResult)    \
+    _(LoadFunctionLengthResult)           \
     _(LoadStringLengthResult)             \
     _(LoadStringCharResult)               \
     _(LoadArgumentsObjectArgResult)       \
