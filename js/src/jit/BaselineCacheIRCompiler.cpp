3011345802e78e73c42606e84babdce402226b57
Ted Campbell <tcampbell@mozilla.com>
2017-01-25 12:08:25 -0500

    Bug 1334187 - Add CacheIR scaffolding for JSOP_IN. r=jandem
    MozReview-Commit-ID: C7WHxYqJXmZ

--- js/src/jit/BaselineCacheIRCompiler.cpp
+++ js/src/jit/BaselineCacheIRCompiler.cpp
@@ -1233,6 +1233,7 @@ BaselineCacheIRCompiler::init(CacheKind kind)
         break;
       case CacheKind::GetElem:
       case CacheKind::SetProp:
+      case CacheKind::In:
         MOZ_ASSERT(numInputs == 2);
         allocator.initInputLocation(0, R0);
         allocator.initInputLocation(1, R1);
@@ -1282,6 +1283,10 @@ jit::AttachBaselineCacheIRStub(JSContext* cx, const CacheIRWriter& writer,
     uint32_t stubDataOffset;
     CacheIRStubKind stubKind;
     switch (kind) {
+      case CacheKind::In:
+        stubDataOffset = sizeof(ICCacheIR_Regular);
+        stubKind = CacheIRStubKind::Regular;
+        break;
       case CacheKind::GetProp:
       case CacheKind::GetElem:
       case CacheKind::GetName:
