d14cd13a1ed530a0faedb187f760866f3d000857
Andrew McCreight <continuation@gmail.com>
2017-01-25 16:29:46 -0800

    Bug 1334221 - Remove the unused PluginModuleChild::mTransport field. r=jimm
    MozReview-Commit-ID: 77zCom8y6Ty

--- dom/plugins/ipc/PluginModuleChild.cpp
+++ dom/plugins/ipc/PluginModuleChild.cpp
@@ -114,7 +114,6 @@ PluginModuleChild::PluginModuleChild(bool aIsChrome)
   , mQuirks(QUIRKS_NOT_INITIALIZED)
   , mIsChrome(aIsChrome)
   , mHasShutdown(false)
-  , mTransport(nullptr)
   , mShutdownFunc(0)
   , mInitializeFunc(0)
 #if defined(OS_WIN) || defined(OS_MACOSX)
@@ -199,8 +198,6 @@ PluginModuleChild::InitForContent(base::ProcessId aParentPid,
     if (!CommonInit(aParentPid, aIOLoop, aChannel)) {
         return false;
     }
-
-    mTransport = aChannel;
 
     mLibrary = GetChrome()->mLibrary;
     mFunctions = GetChrome()->mFunctions;
