ced993e6ccb0944f5db72f532846ba58c1c9db16
Olli Pettay <Olli.Pettay@helsinki.fi>
2017-01-26 12:00:16 +0200

    Bug 1333901 - support document.createEvent(ErrorEvent), r=masayuki

--- dom/events/EventDispatcher.cpp
+++ dom/events/EventDispatcher.cpp
@@ -26,6 +26,7 @@
 #include "mozilla/dom/CloseEvent.h"
 #include "mozilla/dom/CustomEvent.h"
 #include "mozilla/dom/DeviceOrientationEvent.h"
+#include "mozilla/dom/ErrorEvent.h"
 #include "mozilla/dom/EventTarget.h"
 #include "mozilla/dom/FocusEvent.h"
 #include "mozilla/dom/HashChangeEvent.h"
@@ -1153,6 +1154,13 @@ EventDispatcher::CreateEvent(EventTarget* aOwner,
     LOG_EVENT_CREATION(STORAGEEVENT);
     RefPtr<Event> event =
       StorageEvent::Constructor(aOwner, EmptyString(), StorageEventInit());
+    event->MarkUninitialized();
+    return event.forget();
+  }
+  if (aEventType.LowerCaseEqualsLiteral("errorevent")) {
+    LOG_EVENT_CREATION(ERROREVENT);
+    RefPtr<Event> event =
+      ErrorEvent::Constructor(aOwner, EmptyString(), ErrorEventInit());
     event->MarkUninitialized();
     return event.forget();
   }
