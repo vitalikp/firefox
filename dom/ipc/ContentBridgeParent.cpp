088895011d95ea91fdfab51dcd5f4f9a92079db1
Andrew McCreight <continuation@gmail.com>
2017-01-26 15:05:36 -0800

    Bug 1334328 - Remove unused fields ContentBridge{Parent,Child}::mTransport. r=kanru
    MozReview-Commit-ID: 39cytTfmGFk

--- dom/ipc/ContentBridgeParent.cpp
+++ dom/ipc/ContentBridgeParent.cpp
@@ -22,8 +22,7 @@ NS_IMPL_ISUPPORTS(ContentBridgeParent,
                   nsIContentParent,
                   nsIObserver)
 
-ContentBridgeParent::ContentBridgeParent(Transport* aTransport)
-  : mTransport(aTransport)
+ContentBridgeParent::ContentBridgeParent()
 {}
 
 ContentBridgeParent::~ContentBridgeParent()
@@ -43,8 +42,7 @@ ContentBridgeParent::ActorDestroy(ActorDestroyReason aWhy)
 /*static*/ ContentBridgeParent*
 ContentBridgeParent::Create(Transport* aTransport, ProcessId aOtherPid)
 {
-  RefPtr<ContentBridgeParent> bridge =
-    new ContentBridgeParent(aTransport);
+  RefPtr<ContentBridgeParent> bridge = new ContentBridgeParent();
   bridge->mSelfRef = bridge;
 
   DebugOnly<bool> ok = bridge->Open(aTransport, aOtherPid,
