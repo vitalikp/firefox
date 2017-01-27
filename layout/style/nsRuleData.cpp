6cdb260822a674ac29d2d03f0f699c96862916bb
Manish Goregaokar <manishearth@gmail.com>
2017-01-26 16:51:01 -0800

    Bug 1334330 - Part 11: stylo: Use ServoUtils abstraction for GenericSpecifiedValues to remove virtual dispatch overhead in nostylo mode; r=bz,emilio
    MozReview-Commit-ID: 8yGWs2uOjES

--- layout/style/nsRuleData.cpp
+++ layout/style/nsRuleData.cpp
@@ -30,8 +30,7 @@ nsRuleData::GetPoisonOffset()
 
 nsRuleData::nsRuleData(uint32_t aSIDs, nsCSSValue* aValueStorage,
                        nsPresContext* aContext, nsStyleContext* aStyleContext)
-  : mSIDs(aSIDs),
-    mPresContext(aContext),
+  : GenericSpecifiedValues(StyleBackendType::Gecko, aContext, aSIDs),
     mStyleContext(aStyleContext),
     mValueStorage(aValueStorage)
 {
