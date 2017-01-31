a91095af9eb06252f154215118f7325d49306068
Alexandre Poirot <poirot.alex@gmail.com>
2017-01-31 15:31:37 +0100

    Bug 1320939 - Lazy load all tooltip widgets until each is really used. r=jdescottes
    MozReview-Commit-ID: 9P93GocdLm8

--- devtools/client/inspector/rules/rules.js
+++ devtools/client/inspector/rules/rules.js
@@ -177,7 +177,6 @@ function CssRuleView(inspector, document, store, pageStyle) {
 
   // Add the tooltips and highlighters to the view
   this.tooltips = new TooltipsOverlay(this);
-  this.tooltips.addToView();
 
   this.highlighters.addToView(this);
 
