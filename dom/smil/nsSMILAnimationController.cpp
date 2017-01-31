dfef3e48bbbe148acd3f650f23f658d63933e1f4
Cameron McCormack <cam@mcc.id.au>
2017-01-31 17:37:08 +0100

    Bug 1324669 - stylo: Downgrade SMIL assertion even further to a non-test-failing warning. r=me
    MozReview-Commit-ID: 5fk6C9qaOgW

--- dom/smil/nsSMILAnimationController.cpp
+++ dom/smil/nsSMILAnimationController.cpp
@@ -327,7 +327,7 @@ nsSMILAnimationController::DoSample(bool aSkipUnchangedContainers)
   mResampleNeeded = false;
 
   if (mDocument->IsStyledByServo()) {
-    NS_ERROR("stylo: SMIL animations not supported yet");
+    NS_WARNING("stylo: SMIL animations not supported yet");
     return;
   }
 
