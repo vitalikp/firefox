78d69f7faaf82ae0bd9634df1a730a3548668db0
Bobby Holley <bobbyholley@gmail.com>
2017-02-02 15:38:52 -0800

    Bug 1335308 - Assert against destroying images and counters in generated content off-main-thread. r=emilio
    We'll need to deal with this at some point, but let's just forbid it for now
    to appease the static analysis.

--- layout/style/nsStyleStruct.cpp
+++ layout/style/nsStyleStruct.cpp
@@ -3582,9 +3582,11 @@ nsStyleContentData::~nsStyleContentData()
   MOZ_COUNT_DTOR(nsStyleContentData);
 
   if (mType == eStyleContentType_Image) {
+    MOZ_ASSERT(NS_IsMainThread());
     mContent.mImage->Release();
   } else if (mType == eStyleContentType_Counter ||
              mType == eStyleContentType_Counters) {
+    MOZ_ASSERT(NS_IsMainThread());
     mContent.mCounters->Release();
   } else if (mContent.mString) {
     free(mContent.mString);
