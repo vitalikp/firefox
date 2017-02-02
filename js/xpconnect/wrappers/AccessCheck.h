f49e1e6bbf0e39fd3ea6faf6935feac0f6a82904
Boris Zbarsky <bzbarsky@mit.edu>
2017-02-02 12:48:49 -0500

    Bug 1335654 part 2. Change the deny() methods of our wrapper security policies to take a JSContext and a mayThrow boolean. r=bholley

--- js/xpconnect/wrappers/AccessCheck.h
+++ js/xpconnect/wrappers/AccessCheck.h
@@ -51,7 +51,8 @@ struct Opaque : public Policy {
     static bool check(JSContext* cx, JSObject* wrapper, jsid id, js::Wrapper::Action act) {
         return false;
     }
-    static bool deny(js::Wrapper::Action act, JS::HandleId id) {
+    static bool deny(JSContext* cx, js::Wrapper::Action act, JS::HandleId id,
+                     bool mayThrow) {
         return false;
     }
     static bool allowNativeCall(JSContext* cx, JS::IsAcceptableThis test, JS::NativeImpl impl) {
@@ -64,7 +65,8 @@ struct OpaqueWithCall : public Policy {
     static bool check(JSContext* cx, JSObject* wrapper, jsid id, js::Wrapper::Action act) {
         return act == js::Wrapper::CALL;
     }
-    static bool deny(js::Wrapper::Action act, JS::HandleId id) {
+    static bool deny(JSContext* cx, js::Wrapper::Action act, JS::HandleId id,
+                     bool mayThrow) {
         return false;
     }
     static bool allowNativeCall(JSContext* cx, JS::IsAcceptableThis test, JS::NativeImpl impl) {
@@ -81,7 +83,8 @@ struct CrossOriginAccessiblePropertiesOnly : public Policy {
     static bool check(JSContext* cx, JS::HandleObject wrapper, JS::HandleId id, js::Wrapper::Action act) {
         return AccessCheck::isCrossOriginAccessPermitted(cx, wrapper, id, act);
     }
-    static bool deny(js::Wrapper::Action act, JS::HandleId id) {
+    static bool deny(JSContext* cx, js::Wrapper::Action act, JS::HandleId id,
+                     bool mayThrow) {
         // Silently fail for enumerate-like operations.
         if (act == js::Wrapper::ENUMERATE)
             return true;
@@ -97,7 +100,8 @@ struct CrossOriginAccessiblePropertiesOnly : public Policy {
 struct ExposedPropertiesOnly : public Policy {
     static bool check(JSContext* cx, JS::HandleObject wrapper, JS::HandleId id, js::Wrapper::Action act);
 
-    static bool deny(js::Wrapper::Action act, JS::HandleId id);
+    static bool deny(JSContext* cx, js::Wrapper::Action act, JS::HandleId id,
+                     bool mayThrow);
     static bool allowNativeCall(JSContext* cx, JS::IsAcceptableThis test, JS::NativeImpl impl) {
         return false;
     }
