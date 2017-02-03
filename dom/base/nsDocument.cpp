b2a33fe07f9b39b61289dcbbb90d72cf6f597c25
Alastor Wu <alwu@mozilla.com>
2017-02-03 10:48:38 +0800

    Bug 1319771 - part1 : only resume the window when there has active media components. r=baku
    For the first pinned tab, it would be set to visible first and then set to
    invisible if there exists other tabs after restarting the whole browser.
    
    If the tab is set to visible, we would activate the media component (set the
    |mMediaSuspended| in outer window to none-suspend). In this case, the first
    pinned tab would be set to visible briefly, but it doesn't mean the tab is in
    the foreground, it's just how DOM manage the tab's visibility.
    
    In that moment, none of the media component has been created yet. Therefore, we
    would only activate the media component after the audio channel service exists.
    
    MozReview-Commit-ID: 1FgdMq84yWX

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -8,6 +8,7 @@
  * Base class for all our document implementations.
  */
 
+#include "AudioChannelService.h"
 #include "nsDocument.h"
 #include "nsIDocumentInlines.h"
 #include "mozilla/AnimationComparator.h"
@@ -12070,7 +12071,8 @@ nsDocument::MaybeActiveMediaComponents()
   }
 
   mEverInForeground = true;
-  if (GetWindow()->GetMediaSuspend() == nsISuspendedTypes::SUSPENDED_BLOCK) {
+  if (GetWindow()->GetMediaSuspend() == nsISuspendedTypes::SUSPENDED_BLOCK &&
+      AudioChannelService::IsServiceStarted()) {
     GetWindow()->SetMediaSuspend(nsISuspendedTypes::NONE_SUSPENDED);
   }
 }
