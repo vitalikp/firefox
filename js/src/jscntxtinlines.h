1d49f6a12ed5c7ca57131a71ec2f6cec7ecbc599
Jan de Mooij <jdemooij@mozilla.com>
2017-02-03 16:12:38 +0100

    Bug 1333000 part 4 - Add more release asserts to TI code. r=jonco

--- js/src/jscntxtinlines.h
+++ js/src/jscntxtinlines.h
@@ -417,7 +417,7 @@ CheckForInterrupt(JSContext* cx)
 inline js::LifoAlloc&
 JSContext::typeLifoAlloc()
 {
-    return zone()->types.typeLifoAlloc.ref();
+    return zone()->types.typeLifoAlloc();
 }
 
 inline js::Nursery&
