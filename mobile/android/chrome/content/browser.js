bfc442ad7d328a08cc979a2d16a4fdd081887876
Tim Huang <tihuang@mozilla.com>
2017-02-02 22:18:00 -0500

    Bug 1312954 - Part 11: Update speculativeConnect to speculativeConnect2 for Fennec. r=sebastian

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -4487,7 +4487,9 @@ var BrowserEventHandler = {
     let uri = this._getLinkURI(target);
     if (uri) {
       try {
-        Services.io.QueryInterface(Ci.nsISpeculativeConnect).speculativeConnect(uri, null);
+        Services.io.QueryInterface(Ci.nsISpeculativeConnect).speculativeConnect2(uri,
+                                                                                 target.ownerDocument.nodePrincipal,
+                                                                                 null);
       } catch (e) {}
     }
     this._doTapHighlight(target);
@@ -6648,9 +6650,13 @@ var Tabs = {
       case "Session:Prefetch":
         if (aData) {
           try {
-            let uri = Services.io.newURI(aData, null, null);
+            let uri = Services.io.newURI(aData);
             if (uri && !this._domains.has(uri.host)) {
-              Services.io.QueryInterface(Ci.nsISpeculativeConnect).speculativeConnect(uri, null);
+              Services.io.QueryInterface(Ci.nsISpeculativeConnect).speculativeConnect2(uri,
+                                                                                       BrowserApp.selectedBrowser
+                                                                                                 .contentDocument
+                                                                                                 .nodePrincipal,
+                                                                                       null);
               this._domains.add(uri.host);
             }
           } catch (e) {}
