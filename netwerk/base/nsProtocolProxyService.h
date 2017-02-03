5e1a3faedce160a2c237e6a34c146b5e1bb23276
Jed Davis <jld@mozilla.com>
2017-02-03 16:32:04 -0700

    Bug 778201 - Remove nsPluginHost::FindProxyByURL and nsProtocolProxyService::DeprecatedBlockingResolve. r=bsmedberg,mcmanus
    MozReview-Commit-ID: 9xuddxdw5cd

--- netwerk/base/nsProtocolProxyService.h
+++ netwerk/base/nsProtocolProxyService.h
@@ -52,9 +52,6 @@ public:
     nsProtocolProxyService();
 
     nsresult Init();
-    nsresult DeprecatedBlockingResolve(nsIChannel *aChannel,
-                                       uint32_t aFlags,
-                                       nsIProxyInfo **retval);
 
 protected:
     friend class nsAsyncResolveRequest;
