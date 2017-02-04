87f66e2a8a1397eb05ad3627934535e3ac42998e
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-02-04 14:45:36 +0100

    Bug 1336715 - Use https://hg.mozilla.org instead of http in devtools comments. r=pbro
    MozReview-Commit-ID: iSeSOEv0B

--- devtools/client/debugger/new/debugger.js
+++ devtools/client/debugger/new/debugger.js
@@ -8893,7 +8893,7 @@ var Debugger =
 	     *      => state === "insecure"
 	     *
 	     * - request is HTTPS but it uses a weak cipher or old protocol, see
-	     *   http://hg.mozilla.org/mozilla-central/annotate/def6ed9d1c1a/
+	     *   https://hg.mozilla.org/mozilla-central/annotate/def6ed9d1c1a/
 	     *   security/manager/ssl/nsNSSCallbacks.cpp#l1233
 	     * - request is mixed content (which makes no sense whatsoever)
 	     *   => .securityState has STATE_IS_BROKEN flag
@@ -9066,7 +9066,7 @@ var Debugger =
 	    const wpl = Ci.nsIWebProgressListener;
 	
 	    // If there's non-fatal security issues the request has STATE_IS_BROKEN
-	    // flag set. See http://hg.mozilla.org/mozilla-central/file/44344099d119
+	    // flag set. See https://hg.mozilla.org/mozilla-central/file/44344099d119
 	    // /security/manager/ssl/nsNSSCallbacks.cpp#l1233
 	    let reasons = [];
 	
@@ -37580,7 +37580,7 @@ var Debugger =
 	     *      => state === "insecure"
 	     *
 	     * - request is HTTPS but it uses a weak cipher or old protocol, see
-	     *   http://hg.mozilla.org/mozilla-central/annotate/def6ed9d1c1a/
+	     *   https://hg.mozilla.org/mozilla-central/annotate/def6ed9d1c1a/
 	     *   security/manager/ssl/nsNSSCallbacks.cpp#l1233
 	     * - request is mixed content (which makes no sense whatsoever)
 	     *   => .securityState has STATE_IS_BROKEN flag
@@ -37753,7 +37753,7 @@ var Debugger =
 	    const wpl = Ci.nsIWebProgressListener;
 	
 	    // If there's non-fatal security issues the request has STATE_IS_BROKEN
-	    // flag set. See http://hg.mozilla.org/mozilla-central/file/44344099d119
+	    // flag set. See https://hg.mozilla.org/mozilla-central/file/44344099d119
 	    // /security/manager/ssl/nsNSSCallbacks.cpp#l1233
 	    let reasons = [];
 	
