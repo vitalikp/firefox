9aa0eb3bcd820c92778b2062d205752313ef8eeb
Masayuki Nakano <masayuki@d-toybox.com>
2017-02-07 22:52:23 +0900

    Bug 1336011 part.3 Create an alias of the type of mDocStateListeners r=smaug
    MozReview-Commit-ID: L0TczDcH4t8

--- editor/libeditor/EditorBase.cpp
+++ editor/libeditor/EditorBase.cpp
@@ -2628,8 +2628,7 @@ EditorBase::NotifyDocumentListeners(
     return NS_OK;
   }
 
-  nsTArray<OwningNonNull<nsIDocumentStateListener>>
-    listeners(mDocStateListeners);
+  AutoDocumentStateListenerArray listeners(mDocStateListeners);
   nsresult rv = NS_OK;
 
   switch (aNotificationType) {
