7d87d0d2f5f488be970816956310209d99a0d346
Jonathan Watt <jwatt@jwatt.org>
2017-02-07 21:11:53 +0000

    Bug 1345908 - Remove nsLayoutUtils::HasCSSBoxLayout. r=cjku

--- layout/base/nsLayoutUtils.h
+++ layout/base/nsLayoutUtils.h
@@ -2878,11 +2878,6 @@ public:
   static nsRect ComputeGeometryBox(nsIFrame* aFrame,
                                    StyleGeometryBox aGeometryBox);
 
-  /*
-   * Check whether aFrame is associated with CSS layout box.
-   */
-  static bool HasCSSBoxLayout(nsIFrame* aFrame);
-
 private:
   static uint32_t sFontSizeInflationEmPerLine;
   static uint32_t sFontSizeInflationMinTwips;
