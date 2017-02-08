e8bc20581ff6f5f0e9c60811038326a54c16bf19
Nicholas Nethercote <nnethercote@mozilla.com>
2017-02-09 09:02:41 +1100

    Bug 1337189 (part 20) - Move Start() and Stop() out of Sampler. r=mstange.
    The patch also renames them as PlatformStart() and PlatformStop() to make it
    clearer they are platform-specific.

--- tools/profiler/core/platform.h
+++ tools/profiler/core/platform.h
@@ -205,10 +205,6 @@ public:
   Sampler();
   ~Sampler();
 
-  // Start and stop sampler.
-  void Start();
-  void Stop();
-
   // We can't new/delete the type safely without defining it
   // (-Wdelete-incomplete).  Use these to hide the details from
   // clients.
