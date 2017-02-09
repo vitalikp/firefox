cb0678f36a4d9bf78933935f6502e11cc3c3ca90
Sylvestre Ledru <sledru@mozilla.com>
2017-02-09 10:52:55 +0100

    Bug 1338086 - Remove useless else blocks in order to reduce complexity in toolkit/components/telemetry/ r=Dexter
    MozReview-Commit-ID: EjMufQ1PReI

--- toolkit/components/telemetry/TelemetryScalar.cpp
+++ toolkit/components/telemetry/TelemetryScalar.cpp
@@ -1069,7 +1069,8 @@ internal_UpdateScalar(const nsACString& aName, ScalarActionType aType,
 
   if (aType == ScalarActionType::eAdd) {
     return scalar->AddValue(aValue);
-  } else if (aType == ScalarActionType::eSet) {
+  }
+  if (aType == ScalarActionType::eSet) {
     return scalar->SetValue(aValue);
   }
 
@@ -1239,7 +1240,8 @@ internal_UpdateKeyedScalar(const nsACString& aName, const nsAString& aKey,
 
   if (aType == ScalarActionType::eAdd) {
     return scalar->AddValue(aKey, aValue);
-  } else if (aType == ScalarActionType::eSet) {
+  }
+  if (aType == ScalarActionType::eSet) {
     return scalar->SetValue(aKey, aValue);
   }
 
