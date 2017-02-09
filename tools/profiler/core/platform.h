d304f843c79b61bac46f0456ecff95d81f8b7b58
Nicholas Nethercote <nnethercote@mozilla.com>
2017-02-09 17:09:39 +1100

    Bug 1337189 (part 26) - Remove Sampler.cpp. r=mstange.
    Sampler's constructor and destructor are inlined into profiler_{start,stop}(),
    respectively. A trivial Sampler class is left behind (now in platform.cpp)
    because there are still numerous gSampler null checks, which I will remove in a
    follow-up bug.
    
    PseudoStack::flushSamplerOnShutdown() moves into platform.cpp. MAXPATHLEN is
    unused and is deleted.

--- tools/profiler/core/platform.h
+++ tools/profiler/core/platform.h
@@ -177,27 +177,9 @@ bool set_profiler_scan(const char*);
 bool is_native_unwinding_avail();
 
 // ----------------------------------------------------------------------------
-// Sampler
-//
-// A sampler periodically samples the state of the VM and optionally
-// (if used for profiling) the program counter and stack pointer for
-// the thread that created it.
+// Miscellaneous
 
-class ThreadInfo;
-
-struct JSContext;
-class JSObject;
 class PlatformData;
-class ProfileBuffer;
-struct PseudoStack;
-class SpliceableJSONWriter;
-class SyncProfile;
-
-namespace mozilla {
-namespace dom {
-class Promise;
-}
-}
 
 // We can't new/delete the type safely without defining it
 // (-Wdelete-incomplete).  Use these to hide the details from clients.
@@ -210,12 +192,5 @@ typedef mozilla::UniquePtr<PlatformData, PlatformDataDestructor>
 UniquePlatformData AllocPlatformData(int aThreadId);
 
 mozilla::UniquePtr<char[]> ToJSON(double aSinceTime);
-
-class Sampler {
-public:
-  // Initialize sampler.
-  Sampler();
-  ~Sampler();
-};
 
 #endif /* ndef TOOLS_PLATFORM_H_ */
