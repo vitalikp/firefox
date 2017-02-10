678c30b5cd1feaa7736595fe67a3d24e279d1d8a
Benjamin Bouvier <benj@benj.me>
2017-02-10 11:44:23 +0100

    Bug 1338127: Introduce a special MIR node for wasm float literals; r=luke
    MozReview-Commit-ID: KGTHDDFK4eV

--- js/src/jit/MOpcodes.h
+++ js/src/jit/MOpcodes.h
@@ -316,6 +316,7 @@ namespace jit {
     _(WasmCall)                                                             \
     _(WasmSelect)                                                           \
     _(WasmReinterpret)                                                      \
+    _(WasmFloatConstant)                                                    \
     _(WasmTruncateToInt64)
 
 // Forward declarations of MIR types.
