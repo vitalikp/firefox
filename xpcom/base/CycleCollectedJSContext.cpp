fe6474c8a0406a0ede242980820df431a7e33d0f
Brian Hackett <bhackett1024@gmail.com>
2017-02-10 16:47:50 -0700

    Bug 1337112 - Remove links from JSRuntime to its single context and zone group, r=jandem,jonco.

--- xpcom/base/CycleCollectedJSContext.cpp
+++ xpcom/base/CycleCollectedJSContext.cpp
@@ -490,7 +490,7 @@ MozCrashWarningReporter(JSContext*, JSErrorReport*)
 }
 
 nsresult
-CycleCollectedJSContext::Initialize(JSContext* aParentContext,
+CycleCollectedJSContext::Initialize(JSRuntime* aParentRuntime,
                                     uint32_t aMaxBytes,
                                     uint32_t aMaxNurseryBytes)
 {
@@ -501,7 +501,7 @@ CycleCollectedJSContext::Initialize(JSContext* aParentContext,
   mBaseRecursionDepth = RecursionDepth();
 
   mozilla::dom::InitScriptSettings();
-  mJSContext = JS_NewContext(aMaxBytes, aMaxNurseryBytes, aParentContext);
+  mJSContext = JS_NewContext(aMaxBytes, aMaxNurseryBytes, aParentRuntime);
   if (!mJSContext) {
     return NS_ERROR_OUT_OF_MEMORY;
   }
