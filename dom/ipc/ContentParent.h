26ad0c932e2b22637da558a530dfa8270b23c2f5
Ehsan Akhgari <ehsan@mozilla.com>
2017-02-10 18:46:59 -0500

    Bug 1332036 - Avoid the sync IPC message added in bug 1331676; r=dvander

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -463,8 +463,6 @@ public:
                                                             const bool& aIsAudio,
                                                             const bool& aIsVideo) override;
 
-  virtual mozilla::ipc::IPCResult RecvGetGfxInfoFeatureStatus(nsTArray<mozilla::dom::GfxInfoFeatureStatus>* aFS) override;
-
   bool CycleCollectWithLogs(bool aDumpAllTraces,
                             nsICycleCollectorLogSink* aSink,
                             nsIDumpGCAndCCLogsCallback* aCallback);
