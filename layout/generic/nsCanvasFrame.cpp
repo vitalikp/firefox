5fa972acb64f0436d93d531a6f0c51dd00b75c0e
Ting-Yu Lin <tlin@mozilla.com>
2017-02-11 22:54:26 +0800

    Bug 775624 Part 6 - Remove NS_FRAME_OVERFLOW_IS_INCOMPLETE. r=dholbert
    This patch is written by the help of the following script.
    
    function rename() {
    find layout\
         -type f\
         \( -name "*.cpp" -or\
            -name "*.h" \)\
            -exec sed -i -r "s/$1/$2/g" "{}" \;
    }
    
    rename "NS_FRAME_OVERFLOW_IS_INCOMPLETE\(([a-zA-Z0-9.*]*)\)" "\1.IsOverflowIncomplete()"
    
    MozReview-Commit-ID: GOd4y2N6dcz

--- layout/generic/nsCanvasFrame.cpp
+++ layout/generic/nsCanvasFrame.cpp
@@ -733,7 +733,7 @@ nsCanvasFrame::Reflow(nsPresContext*           aPresContext,
         // aren't any other frames we need to isolate them from
         // during reflow.
       }
-      if (NS_FRAME_OVERFLOW_IS_INCOMPLETE(aStatus)) {
+      if (aStatus.IsOverflowIncomplete()) {
         nextFrame->AddStateBits(NS_FRAME_IS_OVERFLOW_CONTAINER);
       }
     }
