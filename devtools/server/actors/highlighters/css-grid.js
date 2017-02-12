ebeb58949654a6a0b0d79655f5fe660fff23e148
Mats Palmgren <mats@mozilla.com>
2017-02-12 01:44:06 +0100

    Bug 1337607 - Provide grid highlighter button also for display:inline-grid elements. r=gl

--- devtools/server/actors/highlighters/css-grid.js
+++ devtools/server/actors/highlighters/css-grid.js
@@ -56,7 +56,7 @@ const COLUMN_KEY = {};
 
 /**
  * The CssGridHighlighter is the class that overlays a visual grid on top of
- * display:grid elements.
+ * display:[inline-]grid elements.
  *
  * Usage example:
  * let h = new CssGridHighlighter(env);
