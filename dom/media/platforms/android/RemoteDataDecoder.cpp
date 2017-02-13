1f686db67acf4deedd10f157fcec34477d969c53
Jean-Yves Avenard <jyavenard@mozilla.com>
2017-02-13 15:32:29 +0100

    Bug 1336431: P6. Handle change of resolution in RemoteDataDecoder. r=JamesCheng
    MozReview-Commit-ID: 8XOHESQiJ3t

--- dom/media/platforms/android/RemoteDataDecoder.cpp
+++ dom/media/platforms/android/RemoteDataDecoder.cpp
@@ -13,6 +13,7 @@
 #include "VideoUtils.h"
 #include "VPXDecoder.h"
 
+#include "mozilla/Mutex.h"
 #include "nsThreadUtils.h"
 #include "nsPromiseFlatString.h"
 #include "nsIGfxInfo.h"
@@ -181,6 +182,8 @@ public:
       }
 
       if (size > 0) {
+        MutexAutoLock lock(mDecoder->mMutex);
+
         RefPtr<layers::Image> img = new SurfaceTextureImage(
           mDecoder->mSurfaceTexture.get(), mDecoder->mConfig.mDisplay,
           gl::OriginPos::BottomLeft);
@@ -220,6 +223,7 @@ public:
     : RemoteDataDecoder(MediaData::Type::VIDEO_DATA, aConfig.mMimeType,
                         aFormat, aDrmStubId, aTaskQueue)
     , mImageContainer(aImageContainer)
+    , mMutex("RemoteVideoDecoder Mutex")
     , mConfig(aConfig)
   {
   }
@@ -276,13 +280,20 @@ public:
   {
     return mIsCodecSupportAdaptivePlayback;
   }
+  void ConfigurationChanged(const TrackInfo& aConfig) override
+  {
+    MOZ_ASSERT(aConfig.GetAsVideoInfo());
+    MutexAutoLock lock(mMutex);
+    mConfig = *aConfig.GetAsVideoInfo();
+  }
 
 private:
   layers::ImageContainer* mImageContainer;
-  const VideoInfo mConfig;
   RefPtr<AndroidSurfaceTexture> mSurfaceTexture;
   DurationMap mInputDurations;
   bool mIsCodecSupportAdaptivePlayback = false;
+  Mutex mMutex; // Protects mConfig
+  VideoInfo mConfig;
 };
 
 class RemoteAudioDecoder : public RemoteDataDecoder
