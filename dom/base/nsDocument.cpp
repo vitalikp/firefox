9f635aa01cf9ccaf09a5b965c540fa722af39d9c
Xidorn Quan <me@upsuper.org>
2017-02-14 18:26:10 +1100

    Bug 1339341- Downgrade several stylo errors to warnings. r=heycam
    MozReview-Commit-ID: 2Sa8dS8uSo5

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -2294,7 +2294,7 @@ nsDocument::ResetStylesheetsToURI(nsIURI* aURI)
         RemoveStyleSheetsFromStyleSets(*sheetService->AuthorStyleSheets(), SheetType::Doc);
       }
     } else {
-      NS_ERROR("stylo: nsStyleSheetService doesn't handle ServoStyleSheets yet");
+      NS_WARNING("stylo: nsStyleSheetService doesn't handle ServoStyleSheets yet");
     }
 
     mStyleSetFilled = false;
--- layout/style/ServoStyleSet.cpp
+++ layout/style/ServoStyleSet.cpp
@@ -292,7 +292,7 @@ ServoStyleSet::ResolvePseudoElementStyle(Element* aOriginatingElement,
                                          Element* aPseudoElement)
 {
   if (aPseudoElement) {
-    NS_ERROR("stylo: We don't support CSS_PSEUDO_ELEMENT_SUPPORTS_USER_ACTION_STATE yet");
+    NS_WARNING("stylo: We don't support CSS_PSEUDO_ELEMENT_SUPPORTS_USER_ACTION_STATE yet");
   }
 
   // NB: We ignore aParentContext, on the assumption that pseudo element styles
