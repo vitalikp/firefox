b8b0b2158578e19c4fb09b0e36596fca66bbd8f1
Olli Pettay <Olli.Pettay@helsinki.fi>
2017-02-15 17:04:16 +0200

    Bug 1338886 - nsDocument::UpdateIntersectionObservations() should call perf->Now() only if needed, r=tschneider

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -12734,6 +12734,10 @@ nsDocument::RemoveIntersectionObserver(DOMIntersectionObserver* aObserver)
 void
 nsDocument::UpdateIntersectionObservations()
 {
+  if (mIntersectionObservers.IsEmpty()) {
+    return;
+  }
+
   DOMHighResTimeStamp time = 0;
   if (nsPIDOMWindowInner* window = GetInnerWindow()) {
     Performance* perf = window->GetPerformance();
