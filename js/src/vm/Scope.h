d2a6f54b17f24de8cd85a039a0c504250c0481a0
Shu-yu Guo <shu@rfrn.org>
2017-02-14 16:22:00 -0800

    Bug 1339137 - Don't do Annex B lexical function behavior when redeclaring a parameter name in a function with parameter expressions. (r=Waldo)

--- js/src/vm/Scope.h
+++ js/src/vm/Scope.h
@@ -425,10 +425,11 @@ Scope::is<LexicalScope>() const
 }
 
 //
-// Scope corresponding to a function. Holds formal parameter names and, if the
-// function parameters contain no expressions that might possibly be
-// evaluated, the function's var bindings.  For example, in these functions,
-// the FunctionScope will store a/b/c bindings but not d/e/f bindings:
+// Scope corresponding to a function. Holds formal parameter names, special
+// internal names (see FunctionScope::isSpecialName), and, if the function
+// parameters contain no expressions that might possibly be evaluated, the
+// function's var bindings. For example, in these functions, the FunctionScope
+// will store a/b/c bindings but not d/e/f bindings:
 //
 //   function f1(a, b) {
 //     var c;
@@ -543,6 +544,8 @@ class FunctionScope : public Scope
     uint32_t numPositionalFormalParameters() const {
         return data().nonPositionalFormalStart;
     }
+
+    static bool isSpecialName(JSContext* cx, JSAtom* name);
 
     static Shape* getEmptyEnvironmentShape(JSContext* cx, bool hasParameterExprs);
 };
