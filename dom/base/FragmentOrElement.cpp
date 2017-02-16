6271a705410466b007984f1a36759c248d9a050f
Xidorn Quan <me@upsuper.org>
2017-02-16 18:35:52 +1100

    Bug 1340061 part 2 - Remove explicit base URI facility. r=bz
    MozReview-Commit-ID: JaUzecbdT2F

--- dom/base/FragmentOrElement.cpp
+++ dom/base/FragmentOrElement.cpp
@@ -381,12 +381,6 @@ nsIContent::GetBaseURI(bool aTryUseXHRDocBaseURI) const
       }
     }
 
-    nsIURI* explicitBaseURI = elem->GetExplicitBaseURI();
-    if (explicitBaseURI) {
-      base = explicitBaseURI;
-      break;
-    }
-
     // Otherwise check for xml:base attribute
     elem->GetAttr(kNameSpaceID_XML, nsGkAtoms::base, attr);
     if (!attr.IsEmpty()) {
