d441cff51dbb1355a7cad91a6dafb78f0675896e
Bobby Holley <bobbyholley@gmail.com>
2017-02-15 19:36:57 -0800

    Bug 1340022 - Implement "handled for descendants" tracking for RestyleDamage. r=emilio
    MozReview-Commit-ID: Bbk99ogILXC

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -247,6 +247,7 @@ nsStyleContext* Gecko_GetStyleContext(RawGeckoNodeBorrowed node,
                                       nsIAtom* aPseudoTagOrNull);
 nsChangeHint Gecko_CalcStyleDifference(nsStyleContext* oldstyle,
                                        ServoComputedValuesBorrowed newstyle);
+nsChangeHint Gecko_HintsHandledForDescendants(nsChangeHint aHint);
 
 // Element snapshot.
 ServoElementSnapshotOwned Gecko_CreateElementSnapshot(RawGeckoElementBorrowed element);
