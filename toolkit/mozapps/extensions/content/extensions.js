b5ec76d930a2a297465c9951a7b0fde721a4dbd5
jakehm <jacob.harrowmortelliti@maine.edu>
2017-02-15 20:32:53 -0500

    Bug 1328624 - Added .zip to default extensions when installing an add-on from file. r=rhelmer
    MozReview-Commit-ID: 41JOiaFHxPU

--- toolkit/mozapps/extensions/content/extensions.js
+++ toolkit/mozapps/extensions/content/extensions.js
@@ -1300,7 +1300,7 @@ var gViewController = {
                 nsIFilePicker.modeOpenMultiple);
         try {
           fp.appendFilter(gStrings.ext.GetStringFromName("installFromFile.filterName"),
-                          "*.xpi;*.jar");
+                          "*.xpi;*.jar;*.zip");
           fp.appendFilters(nsIFilePicker.filterAll);
         } catch (e) { }
 
