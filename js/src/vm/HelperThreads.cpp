94f5db02a037cb6a255e9f9be93e7c1d1015cac5
Brian Hackett <bhackett1024@gmail.com>
2017-02-17 05:13:11 -0700

    Bug 1337968 - Add API and shell harness for cooperative multithreading, r=jandem.

--- js/src/vm/HelperThreads.cpp
+++ js/src/vm/HelperThreads.cpp
@@ -1892,6 +1892,11 @@ HelperThread::threadLoop()
     AutoLockHelperThreadState lock;
 
     JSContext cx(nullptr, JS::ContextOptions());
+    {
+        AutoEnterOOMUnsafeRegion oomUnsafe;
+        if (!cx.init(ContextKind::Background))
+            oomUnsafe.crash("HelperThread cx.init()");
+    }
     cx.setHelperThread(this);
 
     // Compute the thread's stack limit, for over-recursed checks.
