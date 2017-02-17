5b08ebd6d26910f82487dfa8d8dabd3795a5c5ff
Markus Stange <mstange@themasta.com>
2017-02-16 19:18:37 -0500

    Bug 1275347 - Move the display list profiler marker to the right place. r=mattwoodrow
    MozReview-Commit-ID: JcSmqlXlr4A

--- view/nsViewManager.cpp
+++ view/nsViewManager.cpp
@@ -403,7 +403,6 @@ nsViewManager::ProcessPendingUpdatesForView(nsView* aView,
     return; // presentation might have been torn down
   }
   if (aFlushDirtyRegion) {
-    GeckoProfilerTracingRAII tracer("Paint", "DisplayList");
     nsAutoScriptBlocker scriptBlocker;
     SetPainting(true);
     for (uint32_t i = 0; i < widgets.Length(); ++i) {
