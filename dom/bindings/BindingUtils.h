84c684d487a92a7a0d8e04eacf09ed6afda26c1b
Edgar Chen <echen@mozilla.com>
2017-02-17 18:33:44 +0800

    Bug 1299363 - Part 2: Allow prototype swizzling in html constructor. r=bz
    MozReview-Commit-ID: KGVfbAxpbfq

--- dom/bindings/BindingUtils.h
+++ dom/bindings/BindingUtils.h
@@ -3397,7 +3397,7 @@ GetCustomElementReactionsStack(JS::Handle<JSObject*> aObj);
 // that constructor function.
 already_AddRefed<nsGenericHTMLElement>
 CreateHTMLElement(const GlobalObject& aGlobal, const JS::CallArgs& aCallArgs,
-                  ErrorResult& aRv);
+                  JS::Handle<JSObject*> aGivenProto, ErrorResult& aRv);
 
 void
 SetDocumentAndPageUseCounter(JSContext* aCx, JSObject* aObject,
