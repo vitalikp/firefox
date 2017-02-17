831697837b0b67f4a16f757c935d47e7e06b117d
Markus Stange <mstange@themasta.com>
2017-02-17 13:03:37 -0500

    Bug 1340709 - Add profiler tracing markers for more phases of the refresh tick. r=mattwoodrow
    This adds markers for FireScrollEvent, DispatchSynthMouseMove, ForwardTransaction, and NotifyDidPaint.
    
    MozReview-Commit-ID: 9XahFGzIOls

--- layout/base/PresShell.cpp
+++ layout/base/PresShell.cpp
@@ -3834,6 +3834,7 @@ void
 PresShell::DispatchSynthMouseMove(WidgetGUIEvent* aEvent,
                                   bool aFlushOnHoverChange)
 {
+  GeckoProfilerTracingRAII tracer("Paint", "DispatchSynthMouseMove");
   RestyleManager* restyleManager = mPresContext->RestyleManager();
   uint32_t hoverGenerationBefore =
     restyleManager->GetHoverGeneration();
