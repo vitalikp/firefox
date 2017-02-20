bc06ef5590b6c4b2757b8a7c8e683f8f54c4816e
Ting-Yu Lin <tlin@mozilla.com>
2017-02-20 14:58:58 +0800

    Bug 775624 Part 10a - Move ReflowInput::SetTruncated() into nsReflowStatus. r=dholbert
    Per bug 775624 comment 95, the method modifies only nsReflowStatus, and is
    lived in ReflowInput only because nsReflowStatus was an uint32_t. So it's
    better to move it into nsReflowStatus.
    
    MozReview-Commit-ID: DSY2u9az67A

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -289,6 +289,9 @@ public:
   bool NextInFlowNeedsReflow() const { return mNextInFlowNeedsReflow; }
   void SetNextInFlowNeedsReflow() { mNextInFlowNeedsReflow = true; }
 
+  void UpdateTruncated(const mozilla::ReflowInput& aReflowInput,
+                       const mozilla::ReflowOutput& aMetrics);
+
 private:
   uint32_t mStatus;
 
@@ -360,8 +363,8 @@ private:
 #define NS_FRAME_TRUNCATED  0x0010
 #define NS_FRAME_IS_TRUNCATED(status) \
   (0 != ((status) & NS_FRAME_TRUNCATED))
-#define NS_FRAME_SET_TRUNCATION(status, aReflowInput, aMetrics) \
-  aReflowInput.SetTruncated(aMetrics, &status);
+#define NS_FRAME_SET_TRUNCATION(aStatus, aReflowInput, aMetrics) \
+  aStatus.UpdateTruncated(aReflowInput, aMetrics);
 
 // Merge the incompleteness, truncation and NS_FRAME_REFLOW_NEXTINFLOW
 // status from aSecondary into aPrimary.
