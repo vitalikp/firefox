ceffa5e70e2defd2eca1edab757372317a2e8196
Kevin Jones <kevinhowjones@gmail.com>
2017-02-23 17:19:28 +0100

    Bug 1287330 - Insert tabs' linkedBrowser lazily into the document. r=dao

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4861,6 +4861,7 @@ nsBrowserAccess.prototype = {
                                       fromExternal: aIsExternal,
                                       inBackground: loadInBackground,
                                       forceNotRemote: aForceNotRemote,
+                                      forceBrowserInsertion: true,
                                       opener: aOpener,
                                       });
     let browser = win.gBrowser.getBrowserForTab(tab);
