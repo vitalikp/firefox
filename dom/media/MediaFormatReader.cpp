0521e1e7226465485577328540e69369a0c32795
Jean-Yves Avenard <jyavenard@mozilla.com>
2017-02-23 14:10:12 +0100

    Bug 1313548: P1. Always resolve waiting promise, when clearing waitforkey. r=gerald
    MozReview-Commit-ID: 6xu8Rcn9WMO

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -2151,13 +2151,9 @@ MediaFormatReader::Update(TrackType aTrack)
     return;
   }
 
-  if (decoder.mWaitingForKey) {
-    decoder.mWaitingForKey = false;
-    if (decoder.HasWaitingPromise() && !decoder.IsWaiting()) {
-      LOGV("No longer waiting for key. Resolving waiting promise");
-      decoder.mWaitingPromise.Resolve(decoder.mType, __func__);
-      return;
-    }
+  if (decoder.CancelWaitingForKey()) {
+    LOGV("No longer waiting for key. Resolving waiting promise");
+    return;
   }
 
   if (!needInput) {
