971f97d1621d11a4552b11f4c3162cd8cc2ec440
Andrew McCreight <continuation@gmail.com>
2017-02-24 15:35:35 -0800

    Bug 1342580 - Don't generate actor "state" method. r=kanru
    MozReview-Commit-ID: 8Am8TIQfH0h

--- ipc/ipdl/ipdl/lower.py
+++ ipc/ipdl/ipdl/lower.py
@@ -1123,9 +1123,6 @@ class Protocol(ipdl.ast.Protocol):
         return Type(_actorName(self._ipdlmgrtype().name(), side),
                     ptr=ptr)
 
-    def stateMethod(self):
-        return ExprVar('state');
-
     def registerMethod(self):
         return ExprVar('Register')
 
@@ -2777,12 +2774,6 @@ class _GenerateProtocolActorCode(ipdl.ast.Visitor):
             refmeth.addstmt(StmtReturn(p.managedVar(managed, self.side)))
 
             self.cls.addstmts([ meth, refmeth, Whitespace.NL ])
-
-        statemethod = MethodDefn(MethodDecl(
-            p.stateMethod().name,
-            ret=p.fqStateType()))
-        statemethod.addstmt(StmtReturn(p.stateVar()))
-        self.cls.addstmts([ statemethod, Whitespace.NL ])
 
         ## OnMessageReceived()/OnCallReceived()
 
