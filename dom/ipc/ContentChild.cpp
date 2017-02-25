9844090f4bc51631cbbcc7f1168bccd991ebc7e2
Ehsan Akhgari <ehsan@mozilla.com>
2017-02-25 11:14:04 -0500

    Bug 1343731 - Remove the sync IPC during DataStorage initialization; r=keeler,billm
    Instead of initializing DataStorage objects on demand in the content
    process, we initialize them at content process startup by getting the
    parent to send down the information about the existing DataStorages at
    child process startup.  After that point, the dynamic change
    notifications added in bug 1215723 will take care of keeping the
    information in sync.

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -1009,6 +1009,8 @@ ContentChild::InitXPCOM(const XPCOMInitData& aXPCOMInit,
   mozilla::dom::time::InitializeDateCacheCleaner();
 
   GfxInfoBase::SetFeatureStatus(aXPCOMInit.gfxFeatureStatus());
+
+  DataStorage::SetCachedStorageEntries(aXPCOMInit.dataStorage());
 }
 
 mozilla::ipc::IPCResult
