00161cf65d63e47fb6646e8cae9dc24377364e99
Nicholas Nethercote <nnethercote@mozilla.com>
2017-02-27 12:31:44 +1100

    Bug 1328378 (part 2) - Remove BeginUnwind() and EndUnwind(). r=mstange.
    Both of these functions are now trivial and identical in both ThreadInfo and
    SyncProfile. This patch inlines and removes them.

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -2484,9 +2484,9 @@ profiler_get_backtrace()
   sample.isSamplingCurrentThread = true;
   sample.timestamp = mozilla::TimeStamp::Now();
 
-  profile->BeginUnwind();
+  profile->GetMutex().Lock();
   Tick(&sample);
-  profile->EndUnwind();
+  profile->GetMutex().Unlock();
 
   return UniqueProfilerBacktrace(new ProfilerBacktrace(profile));
 }
