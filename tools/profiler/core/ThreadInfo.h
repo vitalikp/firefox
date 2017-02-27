529a1ddeb3caec45e1e603e8f51bd87392ae2af3
Nicholas Nethercote <nnethercote@mozilla.com>
2017-02-27 13:52:58 +1100

    Bug 1342306 (part 2) - Avoid profiler_time() calls deep in profiler streaming code. r=mstange.
    Because profiler_time() is going to need the global lock when I add it, and the
    lock will already be held when streaming is happening, so it'll cause the
    thread to deadlock itself.
    
    Unfortunately this requires adding an |aStartTime| parameter to a lot of
    functions, but this is the least worst way I can think of handling it.
    
    This also removes the need for one of the profiler_time() functions, which the
    patch removes.

--- tools/profiler/core/ThreadInfo.h
+++ tools/profiler/core/ThreadInfo.h
@@ -68,11 +68,12 @@ public:
 
   mozilla::Mutex& GetMutex();
   void StreamJSON(ProfileBuffer* aBuffer, SpliceableJSONWriter& aWriter,
-                  double aSinceTime = 0);
+                  const mozilla::TimeStamp& aStartTime, double aSinceTime);
 
   // Call this method when the JS entries inside the buffer are about to
   // become invalid, i.e., just before JS shutdown.
-  void FlushSamplesAndMarkers(ProfileBuffer* aBuffer);
+  void FlushSamplesAndMarkers(ProfileBuffer* aBuffer,
+                              const mozilla::TimeStamp& aStartTime);
 
   ThreadResponsiveness* GetThreadResponsiveness() { return &mRespInfo; }
 
@@ -82,6 +83,7 @@ public:
 
   void StreamSamplesAndMarkers(ProfileBuffer* aBuffer,
                                SpliceableJSONWriter& aWriter,
+                               const mozilla::TimeStamp& aStartTime,
                                double aSinceTime,
                                UniqueStacks& aUniqueStacks);
 
