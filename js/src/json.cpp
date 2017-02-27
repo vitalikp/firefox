cc8a31bcd175753184ec7023b2edd2e54ca0e412
Tom Schuster <evilpies@gmail.com>
2017-02-27 16:43:24 +0100

    Bug 1342402 - Use optimized GetElement in JSON. r=jandem

--- js/src/json.cpp
+++ js/src/json.cpp
@@ -23,6 +23,7 @@
 #include "vm/JSONParser.h"
 #include "vm/StringBuffer.h"
 
+#include "jsarrayinlines.h"
 #include "jsatominlines.h"
 #include "jsboolinlines.h"
 
@@ -497,7 +498,7 @@ JA(JSContext* cx, HandleObject obj, StringifyContext* scx)
                 }
             }
 #endif
-            if (!GetElement(cx, obj, obj, i, &outputValue))
+            if (!GetElement(cx, obj, i, &outputValue))
                 return false;
             if (!PreprocessValue(cx, obj, i, &outputValue, scx))
                 return false;
@@ -645,7 +646,7 @@ js::Stringify(JSContext* cx, MutableHandleValue vp, JSObject* replacer_, const V
                     return false;
 
                 /* Step 4b(iii)(5)(a-b). */
-                if (!GetElement(cx, replacer, replacer, k, &item))
+                if (!GetElement(cx, replacer, k, &item))
                     return false;
 
                 RootedId id(cx);
