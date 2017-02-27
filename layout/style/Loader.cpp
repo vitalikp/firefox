359eb53a1b0d7611cf30d134e76b4785983c6d23
Brad Werth <bwerth@mozilla.com>
2017-02-27 11:20:48 -0800

    Bug 1339627 Part 2: Change StyleSheet Loader to attempt to cache ServoStyleSheets. r=heycam
    MozReview-Commit-ID: 55PJm0H9ltv

--- layout/style/Loader.cpp
+++ layout/style/Loader.cpp
@@ -1102,16 +1102,11 @@ Loader::CreateSheet(nsIURI* aURI,
 
     // First, the XUL cache
 #ifdef MOZ_XUL
-    // The XUL cache is a singleton that only holds Gecko-style sheets, so
-    // only use the cache if the loader is also Gecko.
-    if (IsChromeURI(aURI) &&
-        GetStyleBackendType() == StyleBackendType::Gecko) {
+    if (IsChromeURI(aURI)) {
       nsXULPrototypeCache* cache = nsXULPrototypeCache::GetInstance();
-      if (cache) {
-        if (cache->IsEnabled()) {
-          sheet = cache->GetStyleSheet(aURI, GetStyleBackendType());
-          LOG(("  From XUL cache: %p", sheet.get()));
-        }
+      if (cache && cache->IsEnabled()) {
+        sheet = cache->GetStyleSheet(aURI, GetStyleBackendType());
+        LOG(("  From XUL cache: %p", sheet.get()));
       }
     }
 #endif
@@ -1937,8 +1932,7 @@ Loader::DoSheetComplete(SheetLoadData* aLoadData, nsresult aStatus,
       data = data->mNext;
     }
 #ifdef MOZ_XUL
-    if (IsChromeURI(aLoadData->mURI) &&
-        GetStyleBackendType() == StyleBackendType::Gecko) {
+    if (IsChromeURI(aLoadData->mURI)) {
       nsXULPrototypeCache* cache = nsXULPrototypeCache::GetInstance();
       if (cache && cache->IsEnabled()) {
         if (!cache->GetStyleSheet(aLoadData->mURI, GetStyleBackendType())) {
