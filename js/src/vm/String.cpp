668d383428a05477377c67867bce78ec0db49a5c
Jan de Mooij <jdemooij@mozilla.com>
2017-02-28 11:38:42 +0100

    Bug 1337390 - Fix JSExternalString::dumpRepresentation. r=jwalden

--- js/src/vm/String.cpp
+++ js/src/vm/String.cpp
@@ -1125,8 +1125,7 @@ JSExternalString::dumpRepresentation(FILE* fp, int indent) const
     indent += 2;
 
     fprintf(fp, "%*sfinalizer: ((JSStringFinalizer*) %p)\n", indent, "", externalFinalizer());
-    fprintf(fp, "%*sbase: ", indent, "");
-    base()->dumpRepresentation(fp, indent);
+    dumpRepresentationChars(fp, indent);
 }
 #endif /* DEBUG */
 
