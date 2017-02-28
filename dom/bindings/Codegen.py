0f3a3171cb97996bca247bca2233e1d5183b30f7
Michael Layzell <michael@thelayzells.com>
2017-02-28 14:45:04 -0500

    Bug 1331434 - Part 5: Mark some Throw calls in /dom/bindings as having custom cleanup, r=baku
    MozReview-Commit-ID: CooKyfkMlWq

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -5458,7 +5458,7 @@ def getJSToNativeConversionInfo(type, descriptorProvider, failureCode=None,
               nsCOMPtr<nsIGlobalObject> global =
                 do_QueryInterface(promiseGlobal.GetAsSupports());
               if (!global) {
-                promiseRv.Throw(NS_ERROR_UNEXPECTED);
+                promiseRv.ThrowWithCustomCleanup(NS_ERROR_UNEXPECTED);
                 promiseRv.MaybeSetPendingException(cx);
                 $*{exceptionCode}
               }
