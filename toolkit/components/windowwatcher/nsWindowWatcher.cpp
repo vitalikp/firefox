965eeb55fc8d1afda1f687bdaa16abc410556c0d
Bob Owen <bobowencode@gmail.com>
2017-02-28 07:28:36 +0000

    Bug 1338375 Part 1: Fix window settings and navigation when creating a new window in a new process.

--- toolkit/components/windowwatcher/nsWindowWatcher.cpp
+++ toolkit/components/windowwatcher/nsWindowWatcher.cpp
@@ -475,12 +475,6 @@ CheckUserContextCompatibility(nsIDocShell* aDocShell)
   return subjectPrincipal->GetUserContextId() == userContextId;
 }
 
-NS_IMETHODIMP
-nsWindowWatcher::OpenWindowWithoutParent(nsITabParent** aResult)
-{
-  return OpenWindowWithTabParent(nullptr, EmptyCString(), true, 1.0f, aResult);
-}
-
 nsresult
 nsWindowWatcher::CreateChromeWindow(const nsACString& aFeatures,
                                     nsIWebBrowserChrome* aParentChrome,
