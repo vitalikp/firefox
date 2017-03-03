e25e5db28f6a8ff8f7e088eea4be1b2c2419079c
Boris Zbarsky <bzbarsky@mit.edu>
2017-03-03 16:16:11 -0500

    Bug 1340277. Fix stylo to properly update styles on the anonymous scrolled-content frame inside a scrollframe. r=emilio
    MozReview-Commit-ID: DE4C6pIp8SC

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -4609,6 +4609,7 @@ nsCSSFrameConstructor::BeginBuildingScrollFrame(nsFrameConstructorState& aState,
   }
 
   aNewFrame = gfxScrollFrame;
+  gfxScrollFrame->AddStateBits(NS_FRAME_OWNS_ANON_BOXES);
 
   // we used the style that was passed in. So resolve another one.
   StyleSetHandle styleSet = mPresShell->StyleSet();
