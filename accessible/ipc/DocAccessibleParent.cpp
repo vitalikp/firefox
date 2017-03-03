f2406ef1316cae1bcb351aad57dfc69dfcdf1b6e
Trevor Saunders <tbsaunde@tbsaunde.org>
2017-03-02 21:27:24 -0500

    bug 1332690 - don't try and bind child docs to a shutdown DocAccessibleParent r=yzen
    a BindChildDoc message can race with the parent process shutting down a tab.
    The result of that is that RecvBindChildDoc() can be called on a
    DocAccessibleParent that has already been shut down by the destruction of the
    owning TabParent.

--- accessible/ipc/DocAccessibleParent.cpp
+++ accessible/ipc/DocAccessibleParent.cpp
@@ -352,6 +352,10 @@ DocAccessibleParent::RecvBindChildDoc(PDocAccessibleParent* aChildDoc, const uin
   if (!aID)
     return IPC_FAIL(this, "ID is 0!");
 
+  if (mShutdown) {
+    return IPC_OK();
+  }
+
   MOZ_ASSERT(CheckDocTree());
 
   auto childDoc = static_cast<DocAccessibleParent*>(aChildDoc);
