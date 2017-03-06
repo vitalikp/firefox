9141b31eaefd81546d970097587d4a137b9bb713
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2017-03-06 13:19:09 +0900

    Bug 1344603 - Make Stop{Animations|Transitions}ForElement as a member function of CommonAnimationManager. r=boris
    MozReview-Commit-ID: LSSpWYjoPn9

--- layout/base/RestyleManager.cpp
+++ layout/base/RestyleManager.cpp
@@ -1817,7 +1817,7 @@ RestyleManager::AnimationsWithDestroyedFrame
     dom::Element* element = content->AsElement();
 
     animationManager->StopAnimationsForElement(element, aPseudoType);
-    transitionManager->StopTransitionsForElement(element, aPseudoType);
+    transitionManager->StopAnimationsForElement(element, aPseudoType);
 
     // All other animations should keep running but not running on the
     // *compositor* at this point.
