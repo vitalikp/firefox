ab4441789bae1a9bb26b07cdc118903424319d0f
Alastor Wu <alwu@mozilla.com>
2017-03-06 18:16:04 +0800

    Bug 1344661 - capture input sample for CheckForSPSChange(). r=jya
    When the SPS changed, we save the input sample in |mPendingSample|, and then call CreateDecoderAndInit(mPendingSample) after calling shutdown().
    However, the |mPendingSample| has been clear in shutdown(), so actually we send the nullptr to CreateDecoderAndInit().
    
    MozReview-Commit-ID: JTRVdKxyEy6

--- dom/media/platforms/wrappers/H264Converter.cpp
+++ dom/media/platforms/wrappers/H264Converter.cpp
@@ -325,7 +325,7 @@ H264Converter::CheckForSPSChange(MediaRawData* aSample)
         return NS_OK;
       }
 
-  mPendingSample = aSample;
+  RefPtr<MediaRawData> sample = aSample;
 
   if (CanRecycleDecoder()) {
     // Do not recreate the decoder, reuse it.
@@ -343,10 +343,9 @@ H264Converter::CheckForSPSChange(MediaRawData* aSample)
     mDecoder->Flush()
       ->Then(AbstractThread::GetCurrent()->AsTaskQueue(),
              __func__,
-             [self, this]() {
+             [self, sample, this]() {
                mFlushRequest.Complete();
-               DecodeFirstSample(mPendingSample);
-               mPendingSample = nullptr;
+               DecodeFirstSample(sample);
              },
              [self, this](const MediaResult& aError) {
                mFlushRequest.Complete();
@@ -365,17 +364,16 @@ H264Converter::CheckForSPSChange(MediaRawData* aSample)
   mDecoder->Flush()
     ->Then(AbstractThread::GetCurrent()->AsTaskQueue(),
            __func__,
-           [self, this]() {
+           [self, sample, this]() {
              mFlushRequest.Complete();
              mShutdownPromise = Shutdown();
              mShutdownPromise
                ->Then(AbstractThread::GetCurrent()->AsTaskQueue(),
                       __func__,
-                      [self, this]() {
+                      [self, sample, this]() {
                         mShutdownRequest.Complete();
                         mShutdownPromise = nullptr;
                         mNeedAVCC.reset();
-                        RefPtr<MediaRawData> sample = mPendingSample.forget();
                         nsresult rv = CreateDecoderAndInit(sample);
                         if (rv == NS_ERROR_DOM_MEDIA_INITIALIZING_DECODER) {
                           // All good so far, will continue later.
