0041d6a20cffc86442599e3b88c797fdbee627cb
Nicholas Nethercote <nnethercote@mozilla.com>
2017-03-07 16:54:56 +1100

    Bug 1347348 (part 3) - Rejig thread registration in the profiler. r=mstange.
    Specifically:
    
    - Renames kGeckoThreadName as kMainThreadName, and RegisterCurrentThread() as
      locked_register_thread().
    
    - Removes the aIsMainThread parameter from locked_register_thread(). It can be
      computed via NS_IsMainThread().
    
    - Moves the PseudoStack initialization within locked_register_thread(), so it's
      done in a single place. Also calls init() at that place; previously the
      non-main threads didn't have init() called on their tlsPseudoStack! According
      to ThreadLocal.h this shouldn't have worked...
    
    - Adds some !NS_IsMainThread() assertions to
      profiler_{register,unregister}_thread().

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -342,7 +342,7 @@ PS* gPS = nullptr;
 static PS::Mutex gPSMutex;
 
 // The name of the main thread.
-static const char* const kGeckoThreadName = "GeckoMain";
+static const char* const kMainThreadName = "GeckoMain";
 
 static bool
 CanNotifyObservers()
@@ -1565,12 +1565,6 @@ ReadProfilerEnvVars(PS::LockRef aLock)
        gPS->EnvVarInterval(aLock));
 }
 
-static bool
-is_main_thread_name(const char* aName)
-{
-  return aName && (strcmp(aName, kGeckoThreadName) == 0);
-}
-
 #ifdef HAVE_VA_COPY
 #define VARARGS_ASSIGN(foo, bar)     VA_COPY(foo,bar)
 #elif defined(HAVE_VA_LIST_AS_ARRAY)
@@ -1730,9 +1724,7 @@ MaybeSetProfile(PS::LockRef aLock, ThreadInfo* aInfo)
 }
 
 static void
-RegisterCurrentThread(PS::LockRef aLock, const char* aName,
-                      NotNull<PseudoStack*> aPseudoStack, bool aIsMainThread,
-                      void* stackTop)
+locked_register_thread(PS::LockRef aLock, const char* aName, void* stackTop)
 {
   // This function runs both on and off the main thread.
 
@@ -1750,8 +1742,14 @@ RegisterCurrentThread(PS::LockRef aLock, const char* aName,
     }
   }
 
+  if (!tlsPseudoStack.init()) {
+    return;
+  }
+  NotNull<PseudoStack*> stack = WrapNotNull(new PseudoStack());
+  tlsPseudoStack.set(stack);
+
   ThreadInfo* info =
-    new ThreadInfo(aName, id, aIsMainThread, aPseudoStack, stackTop);
+    new ThreadInfo(aName, id, NS_IsMainThread(), stack, stackTop);
 
   MaybeSetProfile(aLock, info);
 
@@ -1759,8 +1757,8 @@ RegisterCurrentThread(PS::LockRef aLock, const char* aName,
   if (gPS->IsActive(aLock) && info->HasProfile() && gPS->FeatureJS(aLock)) {
     // This startJSSampling() call is on-thread, so we can poll manually to
     // start JS sampling immediately.
-    aPseudoStack->startJSSampling();
-    aPseudoStack->pollJSSampling();
+    stack->startJSSampling();
+    stack->pollJSSampling();
   }
 
   threads.push_back(info);
@@ -1853,11 +1851,6 @@ profiler_init(void* aStackTop)
   {
     PS::AutoLock lock(gPSMutex);
 
-    if (!tlsPseudoStack.init()) {
-      LOG("END   profiler_init: TLS init failed");
-      return;
-    }
-
     // We've passed the possible failure point. Instantiate gPS, which
     // indicates that the profiler has initialized successfully.
     gPS = new PS();
@@ -1870,12 +1863,7 @@ profiler_init(void* aStackTop)
     // Read settings from environment variables.
     ReadProfilerEnvVars(lock);
 
-    NotNull<PseudoStack*> stack = WrapNotNull(new PseudoStack());
-    tlsPseudoStack.set(stack);
-
-    bool isMainThread = true;
-    RegisterCurrentThread(lock, kGeckoThreadName, stack, isMainThread,
-                          aStackTop);
+    locked_register_thread(lock, kMainThreadName, aStackTop);
 
     // Platform-specific initialization.
     PlatformInit(lock);
@@ -2684,25 +2672,19 @@ profiler_set_frame_number(int aFrameNumber)
 void
 profiler_register_thread(const char* aName, void* aGuessStackTop)
 {
-  // This function runs both on and off the main thread.
-
+  MOZ_RELEASE_ASSERT(!NS_IsMainThread());
   MOZ_RELEASE_ASSERT(gPS);
 
   PS::AutoLock lock(gPSMutex);
 
-  MOZ_ASSERT(tlsPseudoStack.get() == nullptr);
-  NotNull<PseudoStack*> stack = WrapNotNull(new PseudoStack());
-  tlsPseudoStack.set(stack);
-  bool isMainThread = is_main_thread_name(aName);
   void* stackTop = GetStackTop(aGuessStackTop);
-  RegisterCurrentThread(lock, aName, stack, isMainThread, stackTop);
+  locked_register_thread(lock, aName, stackTop);
 }
 
 void
 profiler_unregister_thread()
 {
-  // This function runs both on and off the main thread.
-
+  MOZ_RELEASE_ASSERT(!NS_IsMainThread());
   MOZ_RELEASE_ASSERT(gPS);
 
   PS::AutoLock lock(gPSMutex);
