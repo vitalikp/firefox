c7935dbcd91655c61a8540bd38d5c6cfd553a1de
Kaku Kuo <kaku@mozilla.com>
2017-03-08 19:41:08 +0800

    Bug 1345403 part 2 - Mark element tainted when DrawImage is used; r=jwwang,mattwoodrow
    Mark video element as tainted (stored on the decoder owned by video element) when the video is used as source to drawImage() on canvas.
    
    MozReview-Commit-ID: HdciVwhqPu3

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -7280,13 +7280,8 @@ nsLayoutUtils::SurfaceFromElement(HTMLVideoElement* aElement,
   if (!principal)
     return result;
 
-  ImageContainer* container = aElement->GetImageContainer();
-  if (!container)
-    return result;
+  result.mLayersImage = aElement->GetCurrentImage();
 
-  AutoLockImage lockImage(container);
-
-  result.mLayersImage = lockImage.GetImage();
   if (!result.mLayersImage)
     return result;
 
