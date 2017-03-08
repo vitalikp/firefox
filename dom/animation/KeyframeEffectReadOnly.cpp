e5214be6b6573763678eae4c69e4ef60d5aff5a7
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2017-03-09 05:20:17 +0900

    Bug 1344619 - Part 1: Introduce WillCompose() to update various state that we need to update as the result of ComposeStyle(). r=birtles
    MozReview-Commit-ID: JznBR3OghKG

--- dom/animation/KeyframeEffectReadOnly.cpp
+++ dom/animation/KeyframeEffectReadOnly.cpp
@@ -514,6 +514,14 @@ KeyframeEffectReadOnly::EnsureBaseStyles(
 }
 
 void
+KeyframeEffectReadOnly::WillComposeStyle()
+{
+  ComputedTiming computedTiming = GetComputedTiming();
+  mProgressOnLastCompose = computedTiming.mProgress;
+  mCurrentIterationOnLastCompose = computedTiming.mCurrentIteration;
+}
+
+void
 KeyframeEffectReadOnly::ComposeStyle(
   AnimationRule& aStyleRule,
   const nsCSSPropertyIDSet& aPropertiesToSkip)
@@ -528,8 +536,6 @@ KeyframeEffectReadOnly::ComposeStyle(
   mIsComposingStyle = true;
 
   ComputedTiming computedTiming = GetComputedTiming();
-  mProgressOnLastCompose = computedTiming.mProgress;
-  mCurrentIterationOnLastCompose = computedTiming.mCurrentIteration;
 
   // If the progress is null, we don't have fill data for the current
   // time so we shouldn't animate.
