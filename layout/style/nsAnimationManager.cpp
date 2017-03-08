133b4e73d2ca6a455a5b1f32d8aceeccebafd6cf
Daisuke Akatsuka <dakatsuka@mozilla.com>
2017-03-08 10:07:08 +0900

    Bug 1339332 - Part 1: Introduce neutral value concept for missing keyframe in CSS Animation. r=birtles,hiro
    MozReview-Commit-ID: F3qvRY3SRAp

--- layout/style/nsAnimationManager.cpp
+++ layout/style/nsAnimationManager.cpp
@@ -484,7 +484,6 @@ private:
     nsCSSKeyframeRule* aKeyframeRule,
     nsCSSPropertyIDSet& aAnimatedProperties);
   void FillInMissingKeyframeValues(
-    nsPresContext* aPresContext,
     nsCSSPropertyIDSet aAnimatedProperties,
     nsCSSPropertyIDSet aPropertiesSetAtStart,
     nsCSSPropertyIDSet aPropertiesSetAtEnd,
@@ -819,9 +818,9 @@ GeckoCSSAnimationBuilder::BuildAnimationFrames(nsPresContext* aPresContext,
   // Finally, we need to look for any animated properties that have an
   // implicit 'to' or 'from' value and fill in the appropriate keyframe
   // with the current computed style.
-  FillInMissingKeyframeValues(aPresContext, animatedProperties,
-                              propertiesSetAtStart, propertiesSetAtEnd,
-                              inheritedTimingFunction, keyframes);
+  FillInMissingKeyframeValues(animatedProperties, propertiesSetAtStart,
+                              propertiesSetAtEnd, inheritedTimingFunction,
+                              keyframes);
 
   return keyframes;
 }
@@ -941,7 +940,6 @@ FindMatchingKeyframe(
 
 void
 GeckoCSSAnimationBuilder::FillInMissingKeyframeValues(
-    nsPresContext* aPresContext,
     nsCSSPropertyIDSet aAnimatedProperties,
     nsCSSPropertyIDSet aPropertiesSetAtStart,
     nsCSSPropertyIDSet aPropertiesSetAtEnd,
@@ -998,10 +996,14 @@ GeckoCSSAnimationBuilder::FillInMissingKeyframeValues(
     }
 
     if (startKeyframe && !aPropertiesSetAtStart.HasProperty(prop)) {
-      AppendProperty(aPresContext, prop, startKeyframe->mPropertyValues);
+      PropertyValuePair propertyValue;
+      propertyValue.mProperty = prop;
+      startKeyframe->mPropertyValues.AppendElement(Move(propertyValue));
     }
     if (endKeyframe && !aPropertiesSetAtEnd.HasProperty(prop)) {
-      AppendProperty(aPresContext, prop, endKeyframe->mPropertyValues);
+      PropertyValuePair propertyValue;
+      propertyValue.mProperty = prop;
+      endKeyframe->mPropertyValues.AppendElement(Move(propertyValue));
     }
   }
 }
