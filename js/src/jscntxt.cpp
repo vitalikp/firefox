3b8b8808f07e2de04288b4ffdd06266c10c61dc0
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2017-03-08 13:33:07 +0000

    Bug 1325073 - Rename mozilla::MakeGenericErrorResult to mozilla::Err. r=Waldo

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -1109,7 +1109,7 @@ JSContext::alreadyReportedOOM()
         MOZ_ASSERT(isThrowingOutOfMemory());
     }
 #endif
-    return mozilla::MakeGenericErrorResult(reportedOOM);
+    return mozilla::Err(reportedOOM);
 }
 
 mozilla::GenericErrorResult<JS::Error&>
@@ -1119,7 +1119,7 @@ JSContext::alreadyReportedError()
     if (!helperThread())
         MOZ_ASSERT(isExceptionPending());
 #endif
-    return mozilla::MakeGenericErrorResult(reportedError);
+    return mozilla::Err(reportedError);
 }
 
 JSContext::JSContext(JSRuntime* runtime, const JS::ContextOptions& options)
