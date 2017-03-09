ba6602ebc599570b261827e8b82a5734db3233c3
Boris Zbarsky <bzbarsky@mit.edu>
2017-03-08 23:39:45 -0500

    Bug 1345362 part 1. Use mozilla::EnumeratedArray for mNonInheritingStyleContexts. r=heycam
    MozReview-Commit-ID: EddWYLSmfGL

--- layout/style/ServoStyleSet.cpp
+++ layout/style/ServoStyleSet.cpp
@@ -281,8 +281,7 @@ already_AddRefed<nsStyleContext>
 ServoStyleSet::ResolveStyleForPlaceholder()
 {
   RefPtr<nsStyleContext>& cache =
-    mNonInheritingStyleContexts[
-      static_cast<nsCSSAnonBoxes::NonInheritingBase>(nsCSSAnonBoxes::NonInheriting::oofPlaceholder)];
+    mNonInheritingStyleContexts[nsCSSAnonBoxes::NonInheriting::oofPlaceholder];
   if (cache) {
     RefPtr<nsStyleContext> retval = cache;
     return retval.forget();
