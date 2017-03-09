efd9370c1d589cc148db7c0c29acf22e1ca4eef7
Nicholas Nethercote <nnethercote@mozilla.com>
2017-03-10 09:04:23 +1100

    Bug 1345262 (part 5) - Fix how JS sampling is started/stopped by the profiler. r=mstange,djvj.
    Currently, JS sampling has major problems.
    
    - JS sampling is enabled for all JS threads from the thread that runs
      locked_profiler_start() -- currently only the main thread -- but the JS
      engine can't handle enabling from off-thread, and asserts. This makes
      profiling workers impossible in a debug build.
    
    - No JS thread will be JS sampled unless enableJSSampling() is called, but that
      only happens in locked_profiler_start(). That means any worker threads
      created while the profiler is active won't be JS sampled.
    
    - Only the thread that runs locked_profiler_stop() -- currently only the main
      thread -- ever calls disableJSSampling(). This means that worker threads that
      start being JS sampled never stop being JS sampled.
    
    This patch fixes these three problems in the following ways.
    
    - locked_profiler_start() now sets a flag in PseudoStack that indicates
      JS sampling is desired, but doesn't directly enable it. Instead, the JS
      thread polls that flag and enables JS sampling itself when it sees the flag
      is set. The polling is done by the interrupt callback. There was already a
      flag of this sort (mJSSampling) but the new one is better.
    
      This required adding a call to profiler_js_operation_callback() to the
      InterruptCallback() in XPCJSContext.cpp. (In comparison, the
      InterruptCallback() in dom/workers/RuntimeService.cpp already had such a
      call.)
    
    - RegisterCurrentThread() now requests JS sampling of a JS thread when the
      profiler is active, the thread is being profiled, and JS sampling is enabled.
    
    - locked_profiler_stop() now calls stopJSSampling() on all live threads.
    
    The patch makes the following smaller changes as well.
    
    - Renames profiler_js_operation_callback() as profiler_js_interrupt_callback(),
      because "interrupt callback" is the standard name (viz.
      JS_AddInterruptCallback()).
    
    - Calls js::RegisterContextProfilingEventMarker() with nullptr when stopping
      JS sampling, so that ProfilerJSEventMarker won't fire unnecessarily.
    
    - Some minor formatting changes.

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -1748,6 +1748,14 @@ RegisterCurrentThread(PS::LockRef aLock, const char* aName,
 
   MaybeSetProfile(aLock, info);
 
+  // This must come after the MaybeSetProfile() call.
+  if (gPS->IsActive(aLock) && info->HasProfile() && gPS->FeatureJS(aLock)) {
+    // This startJSSampling() call is on-thread, so we can poll manually to
+    // start JS sampling immediately.
+    aPseudoStack->startJSSampling();
+    aPseudoStack->pollJSSampling();
+  }
+
   threads.push_back(info);
 }
 
@@ -2279,12 +2287,20 @@ locked_profiler_start(PS::LockRef aLock, int aEntries, double aInterval,
 
     MaybeSetProfile(aLock, info);
 
-    if (info->IsPendingDelete() || !info->HasProfile()) {
-      continue;
+    if (info->HasProfile() && !info->IsPendingDelete()) {
+      info->Stack()->reinitializeOnResume();
+
+      if (featureJS) {
+        info->Stack()->startJSSampling();
+      }
     }
-    info->Stack()->reinitializeOnResume();
-    if (featureJS) {
-      info->Stack()->enableJSSampling();
+  }
+
+  if (featureJS) {
+    // We just called startJSSampling() on all relevant threads. We can also
+    // manually poll the current thread so it starts sampling immediately.
+    if (PseudoStack* stack = tlsPseudoStack.get()) {
+      stack->pollJSSampling();
     }
   }
 
@@ -2412,16 +2428,25 @@ locked_profiler_stop(PS::LockRef aLock)
   }
 #endif
 
-  // Destroy ThreadInfo for dead threads.
   PS::ThreadVector& threads = gPS->Threads(aLock);
   for (uint32_t i = 0; i < threads.size(); i++) {
     ThreadInfo* info = threads.at(i);
-    // We've stopped profiling. We no longer need to retain information for a
-    // dead thread.
     if (info->IsPendingDelete()) {
+      // We've stopped profiling. Destroy ThreadInfo for dead threads.
       delete info;
       threads.erase(threads.begin() + i);
       i--;
+    } else if (info->HasProfile() && gPS->FeatureJS(aLock)) {
+      // Stop JS sampling live threads.
+      info->Stack()->stopJSSampling();
+    }
+  }
+
+  if (gPS->FeatureJS(aLock)) {
+    // We just called stopJSSampling() on all relevant threads. We can also
+    // manually poll the current thread so it stops profiling immediately.
+    if (PseudoStack* stack = tlsPseudoStack.get()) {
+      stack->pollJSSampling();
     }
   }
 
@@ -2431,12 +2456,6 @@ locked_profiler_stop(PS::LockRef aLock)
 
   delete gPS->Buffer(aLock);
   gPS->SetBuffer(aLock, nullptr);
-
-  if (gPS->FeatureJS(aLock)) {
-    PseudoStack *stack = tlsPseudoStack.get();
-    MOZ_ASSERT(stack != nullptr);
-    stack->disableJSSampling();
-  }
 
   gPS->SetFeatureDisplayListDump(aLock, false);
   gPS->SetFeatureGPU(aLock, false);
@@ -2685,6 +2704,9 @@ profiler_unregister_thread()
     }
   }
 
+  // We don't call PseudoStack::stopJSSampling() here; there's no point doing
+  // that for a JS thread that is in the process of disappearing.
+
   if (!wasPseudoStackTransferred) {
     delete tlsPseudoStack.get();
   }
@@ -2699,7 +2721,7 @@ profiler_thread_sleep()
   MOZ_RELEASE_ASSERT(gPS);
 
   PseudoStack *stack = tlsPseudoStack.get();
-  if (stack == nullptr) {
+  if (!stack) {
     return;
   }
   stack->setSleeping();
@@ -2713,7 +2735,7 @@ profiler_thread_wake()
   MOZ_RELEASE_ASSERT(gPS);
 
   PseudoStack *stack = tlsPseudoStack.get();
-  if (stack == nullptr) {
+  if (!stack) {
     return;
   }
   stack->setAwake();
@@ -2726,16 +2748,17 @@ profiler_thread_is_sleeping()
   MOZ_RELEASE_ASSERT(gPS);
 
   PseudoStack *stack = tlsPseudoStack.get();
-  if (stack == nullptr) {
+  if (!stack) {
     return false;
   }
   return stack->isSleeping();
 }
 
 void
-profiler_js_operation_callback()
+profiler_js_interrupt_callback()
 {
-  // This function runs both on and off the main thread.
+  // This function runs both on and off the main thread, on JS threads being
+  // sampled.
 
   MOZ_RELEASE_ASSERT(gPS);
 
@@ -2744,7 +2767,7 @@ profiler_js_operation_callback()
     return;
   }
 
-  stack->jsOperationCallback();
+  stack->pollJSSampling();
 }
 
 double
@@ -2989,6 +3012,9 @@ profiler_clear_js_context()
 
     gPS->SetIsPaused(lock, false);
   }
+
+  // We don't call stack->stopJSSampling() here; there's no point doing
+  // that for a JS thread that is in the process of disappearing.
 
   stack->mContext = nullptr;
 }
