152e03deaab8c625d08213769ee34d06a0709f5d
Bill McCloskey <billm@mozilla.com>
2017-03-09 12:32:32 -0800

    Bug 1350436 - Rename ValidatingDispatcher to SchedulerGroup (r=froydnj)
    MozReview-Commit-ID: 1gWB9ZLPQfY

--- dom/base/TabGroup.cpp
+++ dom/base/TabGroup.cpp
@@ -108,10 +108,10 @@ TabGroup::GetFromActor(TabChild* aTabChild)
 
   // We have an event target. We assume the IPC code created it via
   // TabGroup::CreateEventTarget.
-  RefPtr<ValidatingDispatcher> dispatcher =
-    ValidatingDispatcher::FromEventTarget(target);
-  MOZ_RELEASE_ASSERT(dispatcher);
-  auto tabGroup = dispatcher->AsTabGroup();
+  RefPtr<SchedulerGroup> group =
+    SchedulerGroup::FromEventTarget(target);
+  MOZ_RELEASE_ASSERT(group);
+  auto tabGroup = group->AsTabGroup();
   MOZ_RELEASE_ASSERT(tabGroup);
 
   // We delay creating the event targets until now since the TabGroup
@@ -245,7 +245,7 @@ TabGroup::EventTargetFor(TaskCategory aCategory) const
   if (aCategory == TaskCategory::Worker || aCategory == TaskCategory::Timer) {
     MOZ_RELEASE_ASSERT(mThrottledQueuesInitialized || mIsChrome);
   }
-  return ValidatingDispatcher::EventTargetFor(aCategory);
+  return SchedulerGroup::EventTargetFor(aCategory);
 }
 
 AbstractThread*
@@ -259,7 +259,7 @@ TabGroup::AbstractMainThreadForImpl(TaskCategory aCategory)
     return AbstractThread::MainThread();
   }
 
-  return ValidatingDispatcher::AbstractMainThreadForImpl(aCategory);
+  return SchedulerGroup::AbstractMainThreadForImpl(aCategory);
 }
 
 } // namespace dom
