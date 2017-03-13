0ae46c81350e518a9e73edd4067a604cf6b53f2d
BharatR123 <bharatraghunthan9767@gmail.com>
2017-03-13 20:39:49 +0530

    Bug 1302888 - Replace GetPresContext() with nsContentUtils::GetContextForContent() to obtain nsPresContext* in dom/animation r=hiro
    Edited all function calls in KeyFrameEffectReadOnly.cpp and one function call in EffectCompositor.cpp
    
    MozReview-Commit-ID: BzSHLZna4xX

--- dom/animation/EffectCompositor.cpp
+++ dom/animation/EffectCompositor.cpp
@@ -20,6 +20,7 @@
 #include "mozilla/ServoStyleSet.h"
 #include "mozilla/StyleAnimationValue.h"
 #include "nsComputedDOMStyle.h" // nsComputedDOMStyle::GetPresShellForContent
+#include "nsContentUtils.h"
 #include "nsCSSPseudoElements.h"
 #include "nsCSSPropertyIDSet.h"
 #include "nsCSSProps.h"
@@ -917,7 +918,7 @@ EffectCompositor::UpdateCascadeResults(EffectSet& aEffectSet,
 
   aEffectSet.MarkCascadeUpdated();
 
-  nsPresContext* presContext = GetPresContext(aElement);
+  nsPresContext* presContext = nsContentUtils::GetContextForContent(aElement);
   if (!presContext) {
     return;
   }
@@ -944,17 +945,6 @@ EffectCompositor::UpdateCascadeResults(EffectSet& aEffectSet,
                      EffectCompositor::RestyleType::Layer,
                      EffectCompositor::CascadeLevel::Transitions);
   }
-}
-
-/* static */ nsPresContext*
-EffectCompositor::GetPresContext(Element* aElement)
-{
-  MOZ_ASSERT(aElement);
-  nsIPresShell* shell = nsComputedDOMStyle::GetPresShellForContent(aElement);
-  if (!shell) {
-    return nullptr;
-  }
-  return shell->GetPresContext();
 }
 
 /* static */ void
