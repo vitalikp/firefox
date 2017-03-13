8702b88fca1ddaea20b18f761425cae64a2b2d3f
Luke Wagner <luke@mozilla.com>
2017-03-13 12:25:03 -0500

    Bug 1342641 - Baldr: validate and compile in a single pass in Ion (r=bbouvier)
    MozReview-Commit-ID: HRW2BuA6GSs

--- js/src/wasm/WasmBinaryToAST.cpp
+++ js/src/wasm/WasmBinaryToAST.cpp
@@ -1456,7 +1456,7 @@ AstDecodeFunctionBody(AstDecodeContext &c, uint32_t funcIndex, AstFunc** func)
     }
     c.exprs().shrinkTo(c.depths().popCopy());
 
-    if (!c.iter().readFunctionEnd())
+    if (!c.iter().readFunctionEnd(bodyEnd))
         return false;
 
     c.endFunction();
