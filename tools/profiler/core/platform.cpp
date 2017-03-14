249929ca7b5be0928d340c2ee9e0a9ba145a2cd9
Markus Stange <mstange@themasta.com>
2017-03-14 18:59:20 -0400

    Bug 1329111 - Change the nsIProfiler shared library information API. r=njn
    API before this change:
     - nsIProfiler::getSharedLibraryInformation() returns a string containing a
       JSON array of libraries.
     - The profile format is at version 3.
     - Every profile has a "libs" field that contains the same JSON string as the
       return value of nsIProfiler::getSharedLibraryInformation.
     - The array of libraries is not sorted.
     - Each library has a "name" field that contains:
        - The module's debug name on Windows
        - The full path to the binary on Mac + Linux
    
    API after this change:
     - nsIProfiler::getSharedLibraryInformation() is removed.
     - nsIProfiler has a readonly property called sharedLibraries.
     - The profile format is at version 4.
     - Every profile has a "libs" field that contains the same array as
       nsIProfiler.sharedLibraries, no longer as a JSON string but as a regular
       array.
     - The array of libraries is sorted by start address.
     - Each library has a "name" field that contains the binary file's basename,
       on all platforms.
     - Each library has a "path" field that contains the full path to the binary,
       on all platforms.
     - Each library has a "debugName" field that contains the library's debug
       name, on all platforms. On Windows, the debug name is the filename
       (basename) of the pdb file for that binary. On other platforms, debugName
       is the same as |name|.
     - Each library has a "debugPath" field that contains the absolute path
       library's pdb file on Windows; on non-Windows, debugPath and path are the
       same.
     - Each library has an "arch" field that is either an empty string (Linux +
       Windows) or the library's architecture; it'll differentiate between the
       architectures "x86_64" and "x86_64h". (x86_64h is used for binaries that
       contain instructions that are specific to the Intel Haswell
       microarchitecture.)
    
    MozReview-Commit-ID: 8Nrs4dyHhDS

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -1048,22 +1048,22 @@ AddSharedLibraryInfoToStream(JSONWriter& aWriter, const SharedLibrary& aLib)
   aWriter.IntProperty("start", SafeJSInteger(aLib.GetStart()));
   aWriter.IntProperty("end", SafeJSInteger(aLib.GetEnd()));
   aWriter.IntProperty("offset", SafeJSInteger(aLib.GetOffset()));
-  aWriter.StringProperty("name", aLib.GetNativeDebugPath().c_str());
+  aWriter.StringProperty("name", NS_ConvertUTF16toUTF8(aLib.GetModuleName()).get());
+  aWriter.StringProperty("path", NS_ConvertUTF16toUTF8(aLib.GetModulePath()).get());
+  aWriter.StringProperty("debugName", NS_ConvertUTF16toUTF8(aLib.GetDebugName()).get());
+  aWriter.StringProperty("debugPath", NS_ConvertUTF16toUTF8(aLib.GetDebugPath()).get());
+  aWriter.StringProperty("arch", aLib.GetArch().c_str());
   aWriter.EndObject();
 }
 
-static std::string
-GetSharedLibraryInfoStringInternal()
+void
+AppendSharedLibraries(JSONWriter& aWriter)
 {
   SharedLibraryInfo info = SharedLibraryInfo::GetInfoForSelf();
-  std::ostringstream os;
-  JSONWriter w(MakeUnique<OStreamJSONWriteFunc>(os));
-  w.StartArrayElement();
+  info.SortByAddress();
   for (size_t i = 0; i < info.GetSize(); i++) {
-    AddSharedLibraryInfoToStream(w, info.GetEntry(i));
+    AddSharedLibraryInfoToStream(aWriter, info.GetEntry(i));
   }
-  w.EndArray();
-  return os.str();
 }
 
 static void
@@ -1111,7 +1111,7 @@ StreamMetaJSCustomObject(PS::LockRef aLock, SpliceableJSONWriter& aWriter)
 {
   MOZ_RELEASE_ASSERT(NS_IsMainThread());
 
-  aWriter.IntProperty("version", 3);
+  aWriter.IntProperty("version", 4);
   aWriter.DoubleProperty("interval", gPS->Interval(aLock));
   aWriter.IntProperty("stackwalk", gPS->FeatureStackWalk(aLock));
 
@@ -1266,8 +1266,9 @@ StreamJSON(PS::LockRef aLock, SpliceableJSONWriter& aWriter, double aSinceTime)
   aWriter.Start(SpliceableJSONWriter::SingleLineStyle);
   {
     // Put shared library info
-    aWriter.StringProperty("libs",
-                           GetSharedLibraryInfoStringInternal().c_str());
+    aWriter.StartArrayProperty("libs");
+    AppendSharedLibraries(aWriter);
+    aWriter.EndArray();
 
     // Put meta data
     aWriter.StartObjectProperty("meta");
