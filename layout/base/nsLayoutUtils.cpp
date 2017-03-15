d7096bf62e2b4adde8cc2e36b6d42f45765b1f27
Nicholas Nethercote <nnethercote@mozilla.com>
2017-03-16 08:17:56 +1100

    Bug 1346132 (part 2) - Remove set_stderr_callback(). r=mstange.
    It's a very general mechanism for replacing the implementation of
    printf_stderr().
    
    It's primarily used by the profiler, sparingly, and not in an important way.
    Worse, it prevents us from using MOZ_LOG in the profiler, which is something I
    want. Because if any code that locks gPSMutex also calls MOZ_LOG, that then
    calls printf_stderr(), which calls profiler_log(), which locks gPSMutex, which
    deadlocks.
    
    The only other use of set_stderr_callback() is for the ultra-hacky,
    for-local-use-only copy_stderr_to_file() function, which was added for B2G
    debugging and is no longer necessary.
    
    This patch removes set_stderr_callback() altogether.

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -3645,7 +3645,6 @@ nsLayoutUtils::PaintFrame(nsRenderingContext* aRenderingContext, nsIFrame* aFram
       if (profilerNeedsDisplayList && !consoleNeedsDisplayList) {
         profiler_log(ss->str().c_str());
       } else {
-        // Send to the console which will send to the profiler if required.
         fprint_stderr(gfxUtils::sDumpPaintFile, *ss);
       }
       ss = MakeUnique<std::stringstream>();
@@ -3719,7 +3718,6 @@ nsLayoutUtils::PaintFrame(nsRenderingContext* aRenderingContext, nsIFrame* aFram
     if (profilerNeedsDisplayList && !consoleNeedsDisplayList) {
       profiler_log(ss->str().c_str());
     } else {
-      // Send to the console which will send to the profiler if required.
       fprint_stderr(gfxUtils::sDumpPaintFile, *ss);
     }
 
