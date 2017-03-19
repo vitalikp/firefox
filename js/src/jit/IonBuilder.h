3821c6dcb8008db369ffa55d09bf56f6ee2cf068
Tooru Fujisawa <arai_a@mac.com>
2017-03-19 14:16:36 +0900

    Bug 1344477 - Part 2: Optimize Array.prototype.splice with JSOP_NORVCALL. r=jandem

--- js/src/jit/IonBuilder.h
+++ js/src/jit/IonBuilder.h
@@ -629,7 +629,6 @@ class IonBuilder
     InliningResult inlineArrayPush(CallInfo& callInfo);
     InliningResult inlineArraySlice(CallInfo& callInfo);
     InliningResult inlineArrayJoin(CallInfo& callInfo);
-    InliningResult inlineArraySplice(CallInfo& callInfo);
 
     // Math natives.
     InliningResult inlineMathAbs(CallInfo& callInfo);
