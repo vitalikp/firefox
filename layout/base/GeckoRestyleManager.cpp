5b0390f6ca8d6b1c65dab602aae7d5bf61938809
Emilio Cobos Álvarez <emilio@crisal.io>
2017-03-19 20:19:06 +0100

    Bug 1296516: Convert UndisplayedMap to a typed hashtable. r=heycam
    MozReview-Commit-ID: g54ekayP2y

--- layout/base/GeckoRestyleManager.cpp
+++ layout/base/GeckoRestyleManager.cpp
@@ -1549,7 +1549,7 @@ ElementRestyler::ConditionallyRestyleUndisplayedNodes(
   }
 
   for (UndisplayedNode* undisplayed = aUndisplayed; undisplayed;
-       undisplayed = undisplayed->mNext) {
+       undisplayed = undisplayed->getNext()) {
 
     if (!undisplayed->mContent->IsElement()) {
       continue;
@@ -3215,7 +3215,7 @@ ElementRestyler::RestyleUndisplayedNodes(nsRestyleHint      aChildRestyleHint,
   if (undisplayed) {
     pusher.PushAncestorAndStyleScope(undisplayedParent);
   }
-  for (; undisplayed; undisplayed = undisplayed->mNext) {
+  for (; undisplayed; undisplayed = undisplayed->getNext()) {
     NS_ASSERTION(undisplayedParent ||
                  undisplayed->mContent ==
                    mPresContext->Document()->GetRootElement(),
