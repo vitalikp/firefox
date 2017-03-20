ebd513a7954f8997084ebc1689ca79ee0cc22bf5
Lars T Hansen <lhansen@mozilla.com>
2017-03-20 12:47:38 +0100

    Bug 1343007 - Insert explicit casts to enforce signed wasm div/mod operation. r=bbouvier, r=nbp

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -6963,7 +6963,7 @@ class MDiv : public MBinaryArithInstruction
     bool canBeNegativeOverflow_;
     bool canBeDivideByZero_;
     bool canBeNegativeDividend_;
-    bool unsigned_;
+    bool unsigned_;             // If false, signedness will be derived from operands
     bool trapOnError_;
     wasm::TrapOffset trapOffset_;
 
@@ -7101,7 +7101,7 @@ class MDiv : public MBinaryArithInstruction
 
 class MMod : public MBinaryArithInstruction
 {
-    bool unsigned_;
+    bool unsigned_;             // If false, signedness will be derived from operands
     bool canBeNegativeDividend_;
     bool canBePowerOfTwoDivisor_;
     bool canBeDivideByZero_;
