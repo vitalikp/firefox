f3cad7b8a02add38287bca52ad5b952f455bc72d
Lars T Hansen <lhansen@mozilla.com>
2017-03-20 12:36:47 +0100

    Bug 1343007 - Do not insert redundant truncation nodes in constant folding. r=nbp

--- js/src/jit/MIR.cpp
+++ js/src/jit/MIR.cpp
@@ -3172,7 +3172,8 @@ MBinaryArithInstruction::foldsTo(TempAllocator& alloc)
         if (isTruncated()) {
             if (!folded->block())
                 block()->insertBefore(this, folded);
-            return MTruncateToInt32::New(alloc, folded);
+            if (folded->type() != MIRType::Int32)
+                return MTruncateToInt32::New(alloc, folded);
         }
         return folded;
     }
