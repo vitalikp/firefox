1ce55e3dd3533bbc9a7fbae1a3015c352b2209b2
Nathan Froyd <froydnj@mozilla.com>
2017-03-21 10:20:36 -0500

    Bug 1312087 - part 1 - move mozilla::{Mutex,CondVar} to use mozglue locking primitives; r=erahm
    This change moves us away from NSPR primitives for our primary
    synchronization primitives.  We're still using PRMonitor for
    ReentrantMonitor, however.
    
    The benefits of this change:
    
    * Slightly faster, as we don't have to deal with some of NSPR's overhead;
    
    * Smaller datatypes.  On POSIX platforms in particular, PRLock is
      enormous. PRCondVar also has some unnecessary overhead.
    
    * Less dynamic memory allocation.  Out of necessity, Mutex and CondVar
      allocated the NSPR data structures they needed, which lead to
      unnecessary checks for failure.
    
      While sizeof(Mutex) and sizeof(CondVar) may get bigger, since they're
      embedding structures now, the total memory usage should be less.
    
    * Less NSPR usage.  This shouldn't need any explanation.

--- xpcom/threads/MozPromise.h
+++ xpcom/threads/MozPromise.h
@@ -189,7 +189,7 @@ protected:
     , mHaveRequest(false)
     , mIsCompletionPromise(aIsCompletionPromise)
 #ifdef PROMISE_DEBUG
-    , mMagic4(mMutex.mLock)
+    , mMagic4(&mMutex)
 #endif
   {
     PROMISE_LOG("%s creating MozPromise (%p)", mCreationSite, this);
@@ -698,7 +698,7 @@ public:
   void ThenInternal(AbstractThread* aResponseThread, ThenValueBase* aThenValue,
                     const char* aCallSite)
   {
-    PROMISE_ASSERT(mMagic1 == sMagic && mMagic2 == sMagic && mMagic3 == sMagic && mMagic4 == mMutex.mLock);
+    PROMISE_ASSERT(mMagic1 == sMagic && mMagic2 == sMagic && mMagic3 == sMagic && mMagic4 == &mMutex);
     MOZ_ASSERT(aResponseThread);
     MutexAutoLock lock(mMutex);
     MOZ_DIAGNOSTIC_ASSERT(!IsExclusive || !mHaveRequest);
@@ -855,7 +855,7 @@ public:
   // AssertIsDead() only.
   void AssertIsDead()
   {
-    PROMISE_ASSERT(mMagic1 == sMagic && mMagic2 == sMagic && mMagic3 == sMagic && mMagic4 == mMutex.mLock);
+    PROMISE_ASSERT(mMagic1 == sMagic && mMagic2 == sMagic && mMagic3 == sMagic && mMagic4 == &mMutex);
     MutexAutoLock lock(mMutex);
     for (auto&& then : mThenValues) {
       then->AssertIsDead();
@@ -950,7 +950,7 @@ public:
   template<typename ResolveValueT_>
   void Resolve(ResolveValueT_&& aResolveValue, const char* aResolveSite)
   {
-    PROMISE_ASSERT(mMagic1 == sMagic && mMagic2 == sMagic && mMagic3 == sMagic && mMagic4 == mMutex.mLock);
+    PROMISE_ASSERT(mMagic1 == sMagic && mMagic2 == sMagic && mMagic3 == sMagic && mMagic4 == &mMutex);
     MutexAutoLock lock(mMutex);
     MOZ_ASSERT(IsPending());
     PROMISE_LOG("%s resolving MozPromise (%p created at %s)", aResolveSite, this, mCreationSite);
@@ -961,7 +961,7 @@ public:
   template<typename RejectValueT_>
   void Reject(RejectValueT_&& aRejectValue, const char* aRejectSite)
   {
-    PROMISE_ASSERT(mMagic1 == sMagic && mMagic2 == sMagic && mMagic3 == sMagic && mMagic4 == mMutex.mLock);
+    PROMISE_ASSERT(mMagic1 == sMagic && mMagic2 == sMagic && mMagic3 == sMagic && mMagic4 == &mMutex);
     MutexAutoLock lock(mMutex);
     MOZ_ASSERT(IsPending());
     PROMISE_LOG("%s rejecting MozPromise (%p created at %s)", aRejectSite, this, mCreationSite);
@@ -972,7 +972,7 @@ public:
   template<typename ResolveOrRejectValue_>
   void ResolveOrReject(ResolveOrRejectValue_&& aValue, const char* aSite)
   {
-    PROMISE_ASSERT(mMagic1 == sMagic && mMagic2 == sMagic && mMagic3 == sMagic && mMagic4 == mMutex.mLock);
+    PROMISE_ASSERT(mMagic1 == sMagic && mMagic2 == sMagic && mMagic3 == sMagic && mMagic4 == &mMutex);
     MutexAutoLock lock(mMutex);
     MOZ_ASSERT(IsPending());
     PROMISE_LOG("%s resolveOrRejecting MozPromise (%p created at %s)", aSite, this, mCreationSite);
