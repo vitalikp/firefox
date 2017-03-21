8bb48997b566dd8cac8e31ab1d1a93445351001f
Jan de Mooij <jdemooij@mozilla.com>
2017-03-21 09:11:36 +0100

    Bug 1348905 part 2 - Clean up and fix HasTypePropertyId. r=h4writer

--- js/src/vm/TypeInference.cpp
+++ js/src/vm/TypeInference.cpp
@@ -1342,7 +1342,7 @@ js::EnsureTrackPropertyTypes(JSContext* cx, JSObject* obj, jsid id)
         }
     }
 
-    MOZ_ASSERT(obj->group()->unknownProperties() || TrackPropertyTypes(cx, obj, id));
+    MOZ_ASSERT(obj->group()->unknownProperties() || TrackPropertyTypes(obj, id));
 }
 
 bool
