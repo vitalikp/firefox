0759e0cee610d1e1a2718c053d40d69e1f216d93
Jan de Mooij <jdemooij@mozilla.com>
2017-03-21 09:12:18 +0100

    Bug 1348905 part 3 - Make SetNativeDataProperty skip the type barrier if the IC knows we don't need one. r=h4writer

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -2190,7 +2190,8 @@ SetPropIRGenerator::tryAttachNativeSetSlot(HandleObject obj, ObjOperandId objId,
     }
 
     if (mode_ == ICState::Mode::Megamorphic && cacheKind_ == CacheKind::SetProp) {
-        writer.megamorphicStoreSlot(objId, JSID_TO_ATOM(id)->asPropertyName(), rhsId);
+        writer.megamorphicStoreSlot(objId, JSID_TO_ATOM(id)->asPropertyName(), rhsId,
+                                    typeCheckInfo_.needsTypeBarrier());
         writer.returnFromIC();
         trackAttached("MegamorphicNativeSlot");
         return true;
