d655f91e41cb0eef1a15bb281d6f632173fba86f
Andrea Marchesini <amarchesini@mozilla.com>
2017-03-21 07:47:25 +0100

    Bug 1347557 - Move 2 static methods about the IPC serialization of streams into an helper class, r=qdot

--- netwerk/base/nsBufferedStreams.cpp
+++ netwerk/base/nsBufferedStreams.cpp
@@ -498,7 +498,8 @@ nsBufferedInputStream::Serialize(InputStreamParams& aParams,
         MOZ_ASSERT(stream);
 
         InputStreamParams wrappedParams;
-        SerializeInputStream(stream, wrappedParams, aFileDescriptors);
+        InputStreamHelper::SerializeInputStream(stream, wrappedParams,
+                                                aFileDescriptors);
 
         params.optionalStream() = wrappedParams;
     }
@@ -526,8 +527,9 @@ nsBufferedInputStream::Deserialize(const InputStreamParams& aParams,
 
     nsCOMPtr<nsIInputStream> stream;
     if (wrappedParams.type() == OptionalInputStreamParams::TInputStreamParams) {
-        stream = DeserializeInputStream(wrappedParams.get_InputStreamParams(),
-                                        aFileDescriptors);
+        stream =
+          InputStreamHelper::DeserializeInputStream(wrappedParams.get_InputStreamParams(),
+                                                    aFileDescriptors);
         if (!stream) {
             NS_WARNING("Failed to deserialize wrapped stream!");
             return false;
