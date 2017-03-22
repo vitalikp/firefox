ff1063526c8e211723504f6c9be26a940905781a
Luke Wagner <luke@mozilla.com>
2017-03-22 17:11:48 -0500

    Bug 1334504 - Baldr: always enable profiling prologue (r=bbouvier)
    MozReview-Commit-ID: 6bPt7dtuwPU

--- js/src/jit/arm/MacroAssembler-arm.cpp
+++ js/src/jit/arm/MacroAssembler-arm.cpp
@@ -5133,7 +5133,7 @@ MacroAssembler::nopPatchableToCall(const wasm::CallSiteDesc& desc)
 {
     CodeOffset offset(currentOffset());
     ma_nop();
-    append(desc, CodeOffset(currentOffset()), framePushed());
+    append(desc, CodeOffset(currentOffset()));
     return offset;
 }
 
