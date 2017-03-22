b6497bebb7070b1d80eb5bf525cb08b5ceed9485
Andrea Marchesini <amarchesini@mozilla.com>
2017-03-22 11:38:17 +0100

    Bug 1343933 - Renaming Principal classes - part 1 - ExpandedPrincipal, r=qdot

--- js/xpconnect/src/XPCJSContext.cpp
+++ js/xpconnect/src/XPCJSContext.cpp
@@ -348,7 +348,7 @@ PrincipalImmuneToScriptPolicy(nsIPrincipal* aPrincipal)
     if (nsXPConnect::SecurityManager()->IsSystemPrincipal(aPrincipal))
         return true;
 
-    // nsExpandedPrincipal gets a free pass.
+    // ExpandedPrincipal gets a free pass.
     nsCOMPtr<nsIExpandedPrincipal> ep = do_QueryInterface(aPrincipal);
     if (ep)
         return true;
