01a0c0b3a436a3df87ddd70d9e4d026d0add3743
Nicholas Hurley <hurley@mozilla.com>
2017-03-23 14:01:21 -0700

    Bug 1348461 - Use a process-unique uint64 instead of a uuid for channel ids r=mcmanus

--- netwerk/protocol/http/HttpBaseChannel.h
+++ netwerk/protocol/http/HttpBaseChannel.h
@@ -104,7 +104,7 @@ public:
                                      nsProxyInfo *aProxyInfo,
                                      uint32_t aProxyResolveFlags,
                                      nsIURI *aProxyURI,
-                                     const nsID& aChannelId);
+                                     uint64_t aChannelId);
 
   // nsIRequest
   NS_IMETHOD GetName(nsACString& aName) override;
@@ -191,8 +191,8 @@ public:
   NS_IMETHOD GetIsMainDocumentChannel(bool* aValue) override;
   NS_IMETHOD SetIsMainDocumentChannel(bool aValue) override;
   NS_IMETHOD GetProtocolVersion(nsACString & aProtocolVersion) override;
-  NS_IMETHOD GetChannelId(nsACString& aChannelId) override;
-  NS_IMETHOD SetChannelId(const nsACString& aChannelId) override;
+  NS_IMETHOD GetChannelId(uint64_t *aChannelId) override;
+  NS_IMETHOD SetChannelId(uint64_t aChannelId) override;
   NS_IMETHOD GetTopLevelContentWindowId(uint64_t *aContentWindowId) override;
   NS_IMETHOD SetTopLevelContentWindowId(uint64_t aContentWindowId) override;
   NS_IMETHOD GetTopLevelOuterContentWindowId(uint64_t *aWindowId) override;
@@ -590,7 +590,7 @@ protected:
 
   bool mForceMainDocumentChannel;
 
-  nsID mChannelId;
+  uint64_t mChannelId;
 
   nsString mIntegrityMetadata;
 };
--- netwerk/protocol/http/nsHttpChannel.h
+++ netwerk/protocol/http/nsHttpChannel.h
@@ -133,7 +133,7 @@ public:
                                        nsProxyInfo *aProxyInfo,
                                        uint32_t aProxyResolveFlags,
                                        nsIURI *aProxyURI,
-                                       const nsID& aChannelId) override;
+                                       uint64_t aChannelId) override;
 
     MOZ_MUST_USE nsresult OnPush(const nsACString &uri,
                                  Http2PushedStream *pushedStream);
