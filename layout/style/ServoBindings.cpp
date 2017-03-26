b426f2f6d257aa16bccb09317cf3f17d9598811f
Emilio Cobos Álvarez <emilio@crisal.io>
2017-03-26 19:06:12 +0200

    Bug 1350140: Avoid dumb copy-constructor. r=heycam
    MozReview-Commit-ID: CoCbXXYTfH4

--- layout/style/ServoBindings.cpp
+++ layout/style/ServoBindings.cpp
@@ -991,8 +991,7 @@ void
 Gecko_InitializeImageCropRect(nsStyleImage* aImage)
 {
   MOZ_ASSERT(aImage);
-  nsStyleSides cropRect;
-  aImage->SetCropRect(MakeUnique<nsStyleSides>(cropRect));
+  aImage->SetCropRect(MakeUnique<nsStyleSides>());
 }
 
 void
