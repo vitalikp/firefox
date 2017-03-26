4aa098fa7a428d633d78953642349b161e784053
Manish Goregaokar <manishearth@gmail.com>
2017-03-26 13:53:34 -0700

    Bug 1341648 - stylo: Include content rules from HTMLTableCellElement::WalkContentStyleRules ; r=bz
    This also removes the TABLE_ATTRS_DIRTY optimization. Constructing nsMappedAttributes isn't really expensive and we do it all the time anyway.
    
    MozReview-Commit-ID: 2krt1nFUzgl

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -167,6 +167,8 @@ RawServoDeclarationBlockStrongBorrowedOrNull
 Gecko_GetStyleAttrDeclarationBlock(RawGeckoElementBorrowed element);
 RawServoDeclarationBlockStrongBorrowedOrNull
 Gecko_GetHTMLPresentationAttrDeclarationBlock(RawGeckoElementBorrowed element);
+RawServoDeclarationBlockStrongBorrowedOrNull
+Gecko_GetExtraContentStyleDeclarations(RawGeckoElementBorrowed element);
 
 // Animations
 bool
