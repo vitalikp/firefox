ef76e551807a86b04be23979134d8f34484a1ecd
Tooru Fujisawa <arai_a@mac.com>
2017-03-27 23:20:18 +0900

    Bug 1331092 - Part 6: Support JSOP_TOASYNCGEN in JIT. r=jandem

--- js/src/jit/MOpcodes.h
+++ js/src/jit/MOpcodes.h
@@ -80,6 +80,7 @@ namespace jit {
     _(BitNot)                                                               \
     _(TypeOf)                                                               \
     _(ToAsync)                                                              \
+    _(ToAsyncGen)                                                           \
     _(ToId)                                                                 \
     _(BitAnd)                                                               \
     _(BitOr)                                                                \
