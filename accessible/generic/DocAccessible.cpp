a8d9dad483923e6805b2d9b3ea9a2baf655d6ccb
David Parks <dparks@mozilla.com>
2017-03-27 10:03:43 -0700

    Bug 1332690 - Assign Managers to DocAccessibleChilds. r=tbsaunde
    The Manager is set by IPDL for remotely constructed objects but our DocAccessibleChilds are created on the child process side, so we need to assign a manager in the constructor so that we can find it when needed.

--- accessible/generic/DocAccessible.cpp
+++ accessible/generic/DocAccessible.cpp
@@ -1484,7 +1484,7 @@ DocAccessible::DoInitialUpdate()
     if (IPCAccessibilityActive()) {
       nsIDocShell* docShell = mDocumentNode->GetDocShell();
       if (RefPtr<dom::TabChild> tabChild = dom::TabChild::GetFrom(docShell)) {
-        DocAccessibleChild* ipcDoc = new DocAccessibleChild(this);
+        DocAccessibleChild* ipcDoc = new DocAccessibleChild(this, tabChild);
         SetIPCDoc(ipcDoc);
 
 #if defined(XP_WIN)
