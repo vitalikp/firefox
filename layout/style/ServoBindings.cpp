8e1907a5540ab80cf77c5e1a3e398e614da34f25
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2017-03-27 19:42:53 +0900

    Bug 1350754 - Assert in Gecko_ElementHasCSSAnimations if pseudo element is neither ::before nor ::after. r=heycam
    Gecko_ElementHasCSSAnimations is not called against pseudo elements
    other than ::before or ::after.
    
    MozReview-Commit-ID: EtHoLhX5P9h

--- layout/style/ServoBindings.cpp
+++ layout/style/ServoBindings.cpp
@@ -423,11 +423,9 @@ bool
 Gecko_ElementHasCSSAnimations(RawGeckoElementBorrowed aElement,
                               nsIAtom* aPseudoTagOrNull)
 {
-  if (aPseudoTagOrNull &&
-      aPseudoTagOrNull != nsCSSPseudoElements::before &&
-      aPseudoTagOrNull != nsCSSPseudoElements::after) {
-    return false;
-  }
+  MOZ_ASSERT(!aPseudoTagOrNull ||
+             aPseudoTagOrNull == nsCSSPseudoElements::before ||
+             aPseudoTagOrNull == nsCSSPseudoElements::after);
 
   CSSPseudoElementType pseudoType =
     nsCSSPseudoElements::GetPseudoType(aPseudoTagOrNull,
