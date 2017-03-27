5baf8e266da01260f1275e0eaa63b0a651de626f
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2017-03-27 19:43:09 +0900

    Bug 1350754 - Use ServoComputedValuesWithParent for nsAnimationManager::UpdateAnimations(). r=birtles
    MozReview-Commit-ID: GkLoACgqVON

--- layout/style/ServoBindings.cpp
+++ layout/style/ServoBindings.cpp
@@ -414,9 +414,12 @@ Gecko_UpdateAnimations(RawGeckoElementBorrowed aElement,
   }
 
   if (presContext->IsDynamic() && aElement->IsInComposedDoc()) {
+    const ServoComputedValuesWithParent servoValues =
+      { aComputedValues, aParentComputedValues };
+
     presContext->AnimationManager()->
       UpdateAnimations(const_cast<dom::Element*>(aElement), aPseudoTagOrNull,
-                       aComputedValues, aParentComputedValues);
+                       servoValues);
   }
 }
 
