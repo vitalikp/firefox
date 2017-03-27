67bb9bc597d63333565870338ddd1121290d5a7a
Jon Coppeard <jcoppeard@mozilla.com>
2017-03-27 10:38:29 +0100

    Bug 1345177 - Make RegExpShared a GC thing r=sfink

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -6112,7 +6112,7 @@ JS_GetRegExpFlags(JSContext* cx, HandleObject obj)
     RegExpGuard shared(cx);
     if (!RegExpToShared(cx, obj, &shared))
         return false;
-    return shared.re()->getFlags();
+    return shared->getFlags();
 }
 
 JS_PUBLIC_API(JSString*)
@@ -6124,7 +6124,7 @@ JS_GetRegExpSource(JSContext* cx, HandleObject obj)
     RegExpGuard shared(cx);
     if (!RegExpToShared(cx, obj, &shared))
         return nullptr;
-    return shared.re()->getSource();
+    return shared->getSource();
 }
 
 /************************************************************************/
