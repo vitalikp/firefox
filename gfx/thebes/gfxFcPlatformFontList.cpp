c2e11a31618a7dd5d873242aa3ead6a4aae438ab
Masayuki Nakano <masayuki@d-toybox.com>
2017-03-29 23:21:47 +0900

    Bug 1344990 part.1 gfx and layout should refer "font.name-list.*" when "font.name.*" is empty r=jfkthame
    When "font.name.*" is empty, it means "default", i.e., use the first font of "font.name-list.*".
    
    In most cases, we don't need to change existing "font.name.*" handlers which refer "font.name-list.*".  However, handlers which refer only "font.name.*", we need to add the code to refer "font.name-list.*" too.
    
    MozReview-Commit-ID: B37y1Ld9Azg

--- gfx/thebes/gfxFcPlatformFontList.cpp
+++ gfx/thebes/gfxFcPlatformFontList.cpp
@@ -1647,6 +1647,7 @@ gfxFcPlatformFontList::GetStandardFamilyName(const nsAString& aFontName,
 }
 
 static const char kFontNamePrefix[] = "font.name.";
+static const char kFontNameListPrefix[] = "font.name-list.";
 
 void
 gfxFcPlatformFontList::AddGenericFonts(mozilla::FontFamilyType aGenericType,
@@ -1682,6 +1683,17 @@ gfxFcPlatformFontList::AddGenericFonts(mozilla::FontFamilyType aGenericType,
         prefFontName.Append('.');
         prefFontName.Append(langGroupStr);
         nsAdoptingString fontlistValue = Preferences::GetString(prefFontName.get());
+        if (fontlistValue.IsEmpty()) {
+            nsAutoCString prefFontNameListName(kFontNameListPrefix);
+            prefFontNameListName.Append(generic);
+            prefFontNameListName.Append('.');
+            prefFontNameListName.Append(langGroupStr);
+            // The font name list may have two or more family names as comma
+            // separated list.  In such case, not matching with generic font
+            // name is fine because if the list prefers specific font, we
+            // should try to use the pref with complicated path.
+            fontlistValue = Preferences::GetString(prefFontNameListName.get());
+        }
         if (fontlistValue) {
             if (!fontlistValue.EqualsLiteral("serif") &&
                 !fontlistValue.EqualsLiteral("sans-serif") &&
@@ -1860,6 +1872,11 @@ gfxFcPlatformFontList::PrefFontListsUseOnlyGenerics()
             //   Ex: font.name.serif.ar ==> "serif" (ok)
             //   Ex: font.name.serif.ar ==> "monospace" (return false)
             //   Ex: font.name.serif.ar ==> "DejaVu Serif" (return false)
+            //   Ex: font.name.serif.ar ==> "" and
+            //       font.name-list.serif.ar ==> "serif" (ok)
+            //   Ex: font.name.serif.ar ==> "" and
+            //       font.name-list.serif.ar ==> "Something, serif"
+            //                                           (return false)
 
             nsDependentCString prefName(names[i] +
                                         ArrayLength(kFontNamePrefix) - 1);
@@ -1867,6 +1884,17 @@ gfxFcPlatformFontList::PrefFontListsUseOnlyGenerics()
             const nsDependentCSubstring& generic = tokenizer.nextToken();
             const nsDependentCSubstring& langGroup = tokenizer.nextToken();
             nsAdoptingCString fontPrefValue = Preferences::GetCString(names[i]);
+            if (fontPrefValue.IsEmpty()) {
+                nsAutoCString nameListPrefName(kFontNameListPrefix);
+                nameListPrefName.Append(generic);
+                nameListPrefName.Append('.');
+                nameListPrefName.Append(langGroup);
+                // The font name list may have two or more family names as comma
+                // separated list.  In such case, not matching with generic font
+                // name is fine because if the list prefers specific font, this
+                // should return false.
+                fontPrefValue = Preferences::GetCString(nameListPrefName.get());
+            }
 
             if (!langGroup.EqualsLiteral("x-math") &&
                 !generic.Equals(fontPrefValue)) {
--- layout/base/StaticPresData.cpp
+++ layout/base/StaticPresData.cpp
@@ -160,6 +160,7 @@ LangGroupFontPrefs::Initialize(nsIAtom* aLangGroupAtom)
     // set the default variable font (the other fonts are seen as 'generic' fonts
     // in GFX and will be queried there when hunting for alternative fonts)
     if (eType == eDefaultFont_Variable) {
+      // XXX "font.name.variable."?  There is no such pref...
       MAKE_FONT_PREF_KEY(pref, "font.name.variable.", langGroup);
 
       nsAdoptingString value = Preferences::GetString(pref.get());
