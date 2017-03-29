4566c9056bcdd720cc74f67424498053692c2dc5
Olli Pettay <Olli.Pettay@helsinki.fi>
2017-03-29 14:37:38 -0400

    Bug 1350964 - Add more assertions to TabGroup and DocGroup to ensure non-main-threads aren't accessing main thread only member variables, r=mystor

--- dom/base/DocGroup.cpp
+++ dom/base/DocGroup.cpp
@@ -47,6 +47,7 @@ DocGroup::GetKey(nsIPrincipal* aPrincipal, nsACString& aKey)
 void
 DocGroup::RemoveDocument(nsIDocument* aDocument)
 {
+  MOZ_ASSERT(NS_IsMainThread());
   MOZ_ASSERT(mDocuments.Contains(aDocument));
   mDocuments.RemoveElement(aDocument);
 }
