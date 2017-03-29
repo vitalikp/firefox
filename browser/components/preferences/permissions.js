ab6b0937c17c9a0d79f7aa05174172593114bf80
Andrea Marchesini <amarchesini@mozilla.com>
2017-03-29 15:28:46 +0200

    Bug 1347817 - Principal must always have a valid origin - part 6 - fixing tests, r=ehsan

--- browser/components/preferences/permissions.js
+++ browser/components/preferences/permissions.js
@@ -89,12 +89,13 @@ var gPermissionManager = {
       // permissions from being entered by the user.
       let uri;
       try {
-        uri = Services.io.newURI(input_url, null, null);
+        uri = Services.io.newURI(input_url);
         principal = Services.scriptSecurityManager.createCodebasePrincipal(uri, {});
-        // If we have ended up with an unknown scheme, the following will throw.
-        principal.origin;
+        if (principal.origin.startsWith("moz-nullprincipal:")) {
+          throw "Null principal";
+        }
       } catch (ex) {
-        uri = Services.io.newURI("http://" + input_url, null, null);
+        uri = Services.io.newURI("http://" + input_url);
         principal = Services.scriptSecurityManager.createCodebasePrincipal(uri, {});
         // If we have ended up with an unknown scheme, the following will throw.
         principal.origin;
