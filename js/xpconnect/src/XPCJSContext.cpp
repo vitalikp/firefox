bd3d0376eb0dbadc56eaf6d85199c286182091c3
Christoph Diehl <cdiehl@mozilla.com>
2017-03-29 08:20:00 -0400

    Bug 1351708 - Fix Preferences::UnregisterCallback typo for fuzzing.enabled. r=froydnj

--- js/xpconnect/src/XPCJSContext.cpp
+++ js/xpconnect/src/XPCJSContext.cpp
@@ -1636,7 +1636,7 @@ XPCJSContext::~XPCJSContext()
                                           JS_OPTIONS_DOT_STR, this);
 
 #ifdef FUZZING
-    Preferences::UnRegisterCallback(ReloadPrefsCallback, "fuzzing.enabled", this);
+    Preferences::UnregisterCallback(ReloadPrefsCallback, "fuzzing.enabled", this);
 #endif
 }
 
