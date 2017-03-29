d400752060a812a7c8ceab2e80ed672088b2fd83
Andrea Marchesini <amarchesini@mozilla.com>
2017-03-29 08:47:49 +0200

    Bug 1351250 - StructuredCloneAlgorithm must cleanup mWasmModule correctly, r=qdot

--- dom/base/StructuredCloneHolder.cpp
+++ dom/base/StructuredCloneHolder.cpp
@@ -307,6 +307,7 @@ StructuredCloneHolder::Read(nsISupports* aParent,
   // If we are tranferring something, we cannot call 'Read()' more than once.
   if (mSupportsTransferring) {
     mBlobImplArray.Clear();
+    mWasmModuleArray.Clear();
     mClonedSurfaces.Clear();
     Clear();
   }
