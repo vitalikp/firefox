ec7f8ab1497c6634bd0c1a1c84b8056be2f6bdfc
Shih-Chiang Chien <schien@mozilla.com>
2017-03-29 16:34:15 +0800

    Bug 1351558 - double check mIPCOpen flag before we send IPC message. r=dragana.
    MozReview-Commit-ID: 3CqA3e4frd8

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -3039,6 +3039,11 @@ void
 HttpChannelChild::TrySendDeletingChannel()
 {
   if (NS_IsMainThread()) {
+    if (NS_WARN_IF(!mIPCOpen)) {
+      // IPC actor is detroyed already, do not send more messages.
+      return;
+    }
+
     Unused << PHttpChannelChild::SendDeletingChannel();
     return;
   }
