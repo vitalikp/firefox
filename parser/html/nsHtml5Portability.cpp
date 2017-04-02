f2857c15847bad971555bb19ab6956618cb8b873
Olli Pettay <Olli.Pettay@helsinki.fi>
2017-04-02 22:40:06 +0300

    Bug 1352734, use memcmp for nsIAtom Equals to improve performance, r=hsivonen

--- parser/html/nsHtml5Portability.cpp
+++ parser/html/nsHtml5Portability.cpp
@@ -81,7 +81,7 @@ nsHtml5Portability::newLocalFromLocal(nsIAtom* local, nsHtml5AtomTable* interner
 bool
 nsHtml5Portability::localEqualsBuffer(nsIAtom* local, char16_t* buf, int32_t offset, int32_t length)
 {
-  return local->Equals(nsDependentSubstring(buf + offset, buf + offset + length));
+  return local->Equals(buf + offset, length);
 }
 
 bool
