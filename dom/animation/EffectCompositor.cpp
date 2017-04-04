201acdae81e8ad6752c1d45fea4c6b50dd37ba9b
Cameron McCormack <cam@mcc.id.au>
2017-04-04 19:12:54 +0800

    Bug 1351535 - Part 5: Add EffectCompositor method to pre-traverse within a specific subtree. r=hiro
    It's not great that we traverse up the tree looking for the subtree root for
    each animating element that is in mElementsToRestyle.  An alternative could
    be to traverse the entire subtree and build a hash set of the elements
    within it, so that each test in the mElementsToRestyle loop can be done
    quickly.  I suspect that most of the time looking up the tree for
    each animation will be quicker, but it does have worse worst case behavior.
    
    MozReview-Commit-ID: FWoQ7fD9YZC

--- dom/animation/EffectCompositor.cpp
+++ dom/animation/EffectCompositor.cpp
@@ -951,6 +951,26 @@ EffectCompositor::SetPerformanceWarning(
 bool
 EffectCompositor::PreTraverse()
 {
+  return PreTraverseInSubtree(nullptr);
+}
+
+static bool
+IsFlattenedTreeDescendantOf(nsINode* aPossibleDescendant,
+                            nsINode* aPossibleAncestor)
+{
+  do {
+    if (aPossibleDescendant == aPossibleAncestor) {
+      return true;
+    }
+    aPossibleDescendant = aPossibleDescendant->GetFlattenedTreeParentNode();
+  } while (aPossibleDescendant);
+
+  return false;
+}
+
+bool
+EffectCompositor::PreTraverseInSubtree(Element* aRoot)
+{
   MOZ_ASSERT(NS_IsMainThread());
   MOZ_ASSERT(mPresContext->RestyleManager()->IsServo());
 
@@ -964,6 +984,12 @@ EffectCompositor::PreTraverse()
       }
 
       NonOwningAnimationTarget target = iter.Key();
+
+      // Ignore restyles that aren't in the flattened tree subtree rooted at
+      // aRoot.
+      if (aRoot && !IsFlattenedTreeDescendantOf(target.mElement, aRoot)) {
+        continue;
+      }
 
       // We need to post restyle hints even if the target is not in EffectSet to
       // ensure the final restyling for removed animations.
