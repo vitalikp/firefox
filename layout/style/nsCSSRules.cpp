faaf145275f742397a36af6998d10e85c6474f0b
Xidorn Quan <me@upsuper.org>
2017-04-04 16:09:20 +1000

    Bug 1343964 part 7 - Use URLExtraData for declaration modification directly for Servo backend. r=heycam
    MozReview-Commit-ID: 78JRYKk9hgV

--- layout/style/nsCSSRules.cpp
+++ layout/style/nsCSSRules.cpp
@@ -1595,6 +1595,13 @@ nsCSSKeyframeStyleDeclaration::GetCSSParsingEnvironment(CSSParsingEnvironment& a
   GetCSSParsingEnvironmentForRule(mRule, aCSSParseEnv);
 }
 
+URLExtraData*
+nsCSSKeyframeStyleDeclaration::GetURLData() const
+{
+  MOZ_ASSERT_UNREACHABLE("GetURLData shouldn't be calling on a Gecko rule");
+  return GetURLDataForRule(mRule);
+}
+
 NS_IMETHODIMP
 nsCSSKeyframeStyleDeclaration::GetParentRule(nsIDOMCSSRule **aParent)
 {
@@ -2103,6 +2110,13 @@ void
 nsCSSPageStyleDeclaration::GetCSSParsingEnvironment(CSSParsingEnvironment& aCSSParseEnv)
 {
   GetCSSParsingEnvironmentForRule(mRule, aCSSParseEnv);
+}
+
+URLExtraData*
+nsCSSPageStyleDeclaration::GetURLData() const
+{
+  MOZ_ASSERT_UNREACHABLE("GetURLData shouldn't be calling on a Gecko rule");
+  return GetURLDataForRule(mRule);
 }
 
 NS_IMETHODIMP
--- layout/style/nsDOMCSSDeclaration.cpp
+++ layout/style/nsDOMCSSDeclaration.cpp
@@ -117,9 +117,17 @@ nsDOMCSSDeclaration::SetCssText(const nsAString& aCssText)
   }
 
   CSSParsingEnvironment env;
-  GetCSSParsingEnvironment(env);
-  if (!env.mPrincipal) {
-    return NS_ERROR_NOT_AVAILABLE;
+  URLExtraData* urlData = nullptr;
+  if (olddecl->IsGecko()) {
+    GetCSSParsingEnvironment(env);
+    if (!env.mPrincipal) {
+      return NS_ERROR_NOT_AVAILABLE;
+    }
+  } else {
+    urlData = GetURLData();
+    if (!urlData) {
+      return NS_ERROR_NOT_AVAILABLE;
+    }
   }
 
   // For nsDOMCSSAttributeDeclaration, SetCSSDeclaration will lead to
@@ -131,9 +139,7 @@ nsDOMCSSDeclaration::SetCssText(const nsAString& aCssText)
 
   RefPtr<DeclarationBlock> newdecl;
   if (olddecl->IsServo()) {
-    RefPtr<URLExtraData> data =
-      new URLExtraData(env.mBaseURI, env.mSheetURI, env.mPrincipal);
-    newdecl = ServoDeclarationBlock::FromCssText(aCssText, data);
+    newdecl = ServoDeclarationBlock::FromCssText(aCssText, urlData);
   } else {
     RefPtr<css::Declaration> decl(new css::Declaration());
     decl->InitializeEmpty();
@@ -266,6 +272,15 @@ nsDOMCSSDeclaration::GetCSSParsingEnvironmentForRule(css::Rule* aRule,
   aCSSParseEnv.mCSSLoader = document ? document->CSSLoader() : nullptr;
 }
 
+/* static */ URLExtraData*
+nsDOMCSSDeclaration::GetURLDataForRule(const css::Rule* aRule)
+{
+  if (StyleSheet* sheet = aRule ? aRule->GetStyleSheet() : nullptr) {
+    return sheet->AsServo()->URLData();
+  }
+  return nullptr;
+}
+
 template<typename GeckoFunc, typename ServoFunc>
 nsresult
 nsDOMCSSDeclaration::ModifyDeclaration(GeckoFunc aGeckoFunc,
@@ -277,9 +292,17 @@ nsDOMCSSDeclaration::ModifyDeclaration(GeckoFunc aGeckoFunc,
   }
 
   CSSParsingEnvironment env;
-  GetCSSParsingEnvironment(env);
-  if (!env.mPrincipal) {
-    return NS_ERROR_NOT_AVAILABLE;
+  URLExtraData* urlData = nullptr;
+  if (olddecl->IsGecko()) {
+    GetCSSParsingEnvironment(env);
+    if (!env.mPrincipal) {
+      return NS_ERROR_NOT_AVAILABLE;
+    }
+  } else {
+    urlData = GetURLData();
+    if (!urlData) {
+      return NS_ERROR_NOT_AVAILABLE;
+    }
   }
 
   // For nsDOMCSSAttributeDeclaration, SetCSSDeclaration will lead to
