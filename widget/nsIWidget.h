a8500cd74b28c912f1e74cd306ed0e478dac7749
Brendan Dahl <bdahl@mozilla.com>
2017-04-04 10:22:00 -0400

    Bug 1338004 - Add headless browser mode. r=jrmuizel, r=ted
    Supports creating a windowless browser on Linux without an X server. Most of the
    changes are just adding branches to avoid calls in to GTK which calls
    into X. Some of the bigger additions were adding a separate headless widget
    which implements just enough to render a page. A headless look and
    feel were also added since there are many calls into GTK in the platform
    specific one.

--- widget/nsIWidget.h
+++ widget/nsIWidget.h
@@ -1871,6 +1871,9 @@ public:
     static already_AddRefed<nsIWidget>
     CreatePuppetWidget(TabChild* aTabChild);
 
+    static already_AddRefed<nsIWidget>
+    CreateHeadlessWidget();
+
     /**
      * Allocate and return a "plugin proxy widget", a subclass of PuppetWidget
      * used in wrapping a PPluginWidget connection for remote widgets. Note
--- widget/nsXPLookAndFeel.cpp
+++ widget/nsXPLookAndFeel.cpp
@@ -9,6 +9,7 @@
 
 #include "nsXPLookAndFeel.h"
 #include "nsLookAndFeel.h"
+#include "HeadlessLookAndFeel.h"
 #include "nsCRT.h"
 #include "nsFont.h"
 #include "mozilla/dom/ContentChild.h"
@@ -249,11 +250,11 @@ bool nsXPLookAndFeel::sUseNativeColors = true;
 bool nsXPLookAndFeel::sUseStandinsForNativeColors = false;
 bool nsXPLookAndFeel::sFindbarModalHighlight = false;
 
-nsLookAndFeel* nsXPLookAndFeel::sInstance = nullptr;
+nsXPLookAndFeel* nsXPLookAndFeel::sInstance = nullptr;
 bool nsXPLookAndFeel::sShutdown = false;
 
 // static
-nsLookAndFeel*
+nsXPLookAndFeel*
 nsXPLookAndFeel::GetInstance()
 {
   if (sInstance) {
@@ -262,7 +263,11 @@ nsXPLookAndFeel::GetInstance()
 
   NS_ENSURE_TRUE(!sShutdown, nullptr);
 
-  sInstance = new nsLookAndFeel();
+  if (gfxPlatform::IsHeadless()) {
+    sInstance = new widget::HeadlessLookAndFeel();
+  } else {
+    sInstance = new nsLookAndFeel();
+  }
   return sInstance;
 }
 
