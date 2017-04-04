e1b8ff620ef3d0ce3b49b75f117440f1e3676942
Jan de Mooij <jdemooij@mozilla.com>
2017-04-04 15:59:41 +0200

    Bug 1346217 part 6 - Inline NativeObject::getChildPropertyOnDictionary into its only caller. r=till

--- js/src/vm/NativeObject.h
+++ js/src/vm/NativeObject.h
@@ -797,8 +797,6 @@ class NativeObject : public ShapedObject
     void freeSlot(JSContext* cx, uint32_t slot);
 
   private:
-    static Shape* getChildPropertyOnDictionary(JSContext* cx, HandleNativeObject obj,
-                                               HandleShape parent, MutableHandle<StackShape> child);
     static Shape* getChildProperty(JSContext* cx, HandleNativeObject obj,
                                    HandleShape parent, MutableHandle<StackShape> child);
 
