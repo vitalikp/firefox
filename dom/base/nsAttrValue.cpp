637884a695222fa8c798e6c634fb498051982526
Xidorn Quan <me@upsuper.org>
2017-04-04 10:04:13 +1000

    Bug 1343964 part 1 - Move URLExtraData into its own header. r=heycam
    This patch does the following in addition to a simple move:
    * change the type of the pointers from RefPtr to nsCOMPtr
    * move it from mozilla::css namespace to mozilla
    
    MozReview-Commit-ID: 72MYq6kWm4s

--- dom/base/nsAttrValue.cpp
+++ dom/base/nsAttrValue.cpp
@@ -1716,8 +1716,8 @@ nsAttrValue::ParseStyleAttribute(const nsAString& aString,
 
   RefPtr<DeclarationBlock> decl;
   if (ownerDoc->GetStyleBackendType() == StyleBackendType::Servo) {
-    RefPtr<css::URLExtraData> data =
-      new css::URLExtraData(baseURI, docURI, aElement->NodePrincipal());
+    RefPtr<URLExtraData> data = new URLExtraData(baseURI, docURI,
+                                                 aElement->NodePrincipal());
     decl = ServoDeclarationBlock::FromCssText(aString, data);
   } else {
     css::Loader* cssLoader = ownerDoc->CSSLoader();
