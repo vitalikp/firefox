55361489f7724b4e7a0b9df9eac6cf39221cc940
Kyle Machulis <kyle@nonpolynomial.com>
2017-04-06 12:39:32 -0700

    Bug 1351490 - Only load either flash and/or test plugins. Period. r=bsmedberg
    Before this patch, mochitests could still load ALL plugins, meaning
    MIME type checking would fail when another plugin snuck in. We should
    only load exactly the plugins we expect to be there now.
    
    MozReview-Commit-ID: KowCihZuceH

--- dom/plugins/base/nsPluginHost.h
+++ dom/plugins/base/nsPluginHost.h
@@ -369,10 +369,6 @@ private:
 
   void UpdateInMemoryPluginInfo(nsPluginTag* aPluginTag);
 
-  // On certain platforms, we only want to load certain plugins. This function
-  // centralizes loading rules.
-  bool ShouldAddPlugin(nsPluginTag* aPluginTag);
-
   RefPtr<nsPluginTag> mPlugins;
   RefPtr<nsPluginTag> mCachedPlugins;
   RefPtr<nsInvalidPluginTag> mInvalidPlugins;
