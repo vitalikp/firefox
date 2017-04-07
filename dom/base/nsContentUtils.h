9c96b810239c4209b3b4c01e837d4d259035e166
Ehsan Akhgari <ehsan@mozilla.com>
2017-04-07 15:48:24 -0400

    Bug 1354643 - Part 3: Stop calling Preferences::GetBool() from nsINode::HasBoxQuadsSupport(); r=mystor

--- dom/base/nsContentUtils.h
+++ dom/base/nsContentUtils.h
@@ -2160,6 +2160,14 @@ public:
   }
 
   /**
+   * Returns true if the getBoxQuads API should be enabled.
+   */
+  static bool GetBoxQuadsEnabled()
+  {
+    return sGetBoxQuadsEnabled;
+  }
+
+  /**
    * Return true if this doc is controlled by a ServiceWorker.
    */
   static bool IsControlledByServiceWorker(nsIDocument* aDocument);
@@ -2908,6 +2916,7 @@ private:
   static bool sUseActivityCursor;
   static bool sAnimationsAPICoreEnabled;
   static bool sAnimationsAPIElementAnimateEnabled;
+  static bool sGetBoxQuadsEnabled;
   static uint32_t sCookiesLifetimePolicy;
   static uint32_t sCookiesBehavior;
 
