faa2c76d6ceb84bae4c4de57bc164a96786600d3
Boris Chiou <boris.chiou@gmail.com>
2017-04-12 16:26:59 +0800

    Bug 1343753 - Part 4: Introduce AnimationValue::IsInterpolableWith. r=birtles,manishearth
    We have different interpolation implementations on Gecko and Servo, so
    wrap the "Can be Interpolated" in AnimationValue. This patch also introduces
    the FFI, Servo_Animationvalues_IsInterpolable.
    
    MozReview-Commit-ID: 92Yf1u84A3c

--- layout/style/ServoBindingList.h
+++ layout/style/ServoBindingList.h
@@ -144,6 +144,9 @@ SERVO_BINDING_FUNC(Servo_AnimationValues_Interpolate,
                    RawServoAnimationValueBorrowed from,
                    RawServoAnimationValueBorrowed to,
                    double progress)
+SERVO_BINDING_FUNC(Servo_AnimationValues_IsInterpolable, bool,
+                   RawServoAnimationValueBorrowed from,
+                   RawServoAnimationValueBorrowed to)
 SERVO_BINDING_FUNC(Servo_AnimationValue_Serialize, void,
                    RawServoAnimationValueBorrowed value,
                    nsCSSPropertyID property,
