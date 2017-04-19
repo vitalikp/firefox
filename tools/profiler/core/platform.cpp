cc7b107e4279bef6c8b602f6c01d501f3486e8bd
Nicholas Nethercote <nnethercote@mozilla.com>
2017-04-20 09:24:17 +1000

    Bug 1357936 - Don't leak ProfilerIOInterposeObserver. r=mstange.
    The patch adds a missing |delete|. (This leak only occurred when the
    "mainthreadio" feature was enabled, which is not the default.)
    
    The patch also makes the unregistering of the interpose observer conditional on
    there being one in the first place, avoiding a harmless but useless
    unregistering of |nullptr|.

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -147,6 +147,7 @@ public:
     , mFeatureJS(false)
     , mFeatureLayersDump(false)
     , mFeatureLeaf(false)
+    , mFeatureMainThreadIO(false)
     , mFeatureMemory(false)
     , mFeaturePrivacy(false)
     , mFeatureRestyle(false)
@@ -187,6 +188,7 @@ public:
   GET_AND_SET(bool, FeatureJS)
   GET_AND_SET(bool, FeatureLayersDump)
   GET_AND_SET(bool, FeatureLeaf)
+  GET_AND_SET(bool, FeatureMainThreadIO)
   GET_AND_SET(bool, FeatureMemory)
   GET_AND_SET(bool, FeaturePrivacy)
   GET_AND_SET(bool, FeatureRestyle)
@@ -257,6 +259,7 @@ private:
   bool mFeatureJS;
   bool mFeatureLayersDump;
   bool mFeatureLeaf;
+  bool mFeatureMainThreadIO;
   bool mFeatureMemory;
   bool mFeaturePrivacy;
   bool mFeatureRestyle;
@@ -2326,6 +2329,7 @@ locked_profiler_start(PS::LockRef aLock, int aEntries, double aInterval,
   gPS->SetFeatureLayersDump(aLock, HAS_FEATURE("layersdump"));
   gPS->SetFeatureLeaf(aLock, HAS_FEATURE("leaf"));
   bool featureMainThreadIO = HAS_FEATURE("mainthreadio");
+  gPS->SetFeatureMainThreadIO(aLock, featureMainThreadIO);
   gPS->SetFeatureMemory(aLock, HAS_FEATURE("memory"));
   gPS->SetFeaturePrivacy(aLock, HAS_FEATURE("privacy"));
   gPS->SetFeatureRestyle(aLock, HAS_FEATURE("restyle"));
@@ -2454,9 +2458,12 @@ locked_profiler_stop(PS::LockRef aLock)
   // We clear things in roughly reverse order to their setting in
   // locked_profiler_start().
 
-  mozilla::IOInterposer::Unregister(mozilla::IOInterposeObserver::OpAll,
-                                    gPS->InterposeObserver(aLock));
-  gPS->SetInterposeObserver(aLock, nullptr);
+  if (gPS->FeatureMainThreadIO(aLock)) {
+    mozilla::IOInterposer::Unregister(mozilla::IOInterposeObserver::OpAll,
+                                      gPS->InterposeObserver(aLock));
+    delete gPS->InterposeObserver(aLock);
+    gPS->SetInterposeObserver(aLock, nullptr);
+  }
 
   // The Stop() call doesn't actually stop Run(); that happens in this
   // function's caller when the sampler thread is destroyed. Stop() just gives
