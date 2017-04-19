bef5151986129dd97271337375fc9423de1dabd4
Kan-Ru Chen <kanru@kanru.info>
2017-04-19 13:24:09 +0800

    Bug 1313200 - Init AbstractThread properly and early. r=froydnj
    Separate AbstractThread::InitTLS and
    AbstractThread::InitMainThread. Init AbstractThread main thread when
    init nsThreadManager. Init AbstractThread TLS for all content process
    types because for plugin and gmp processes we are doing IPC even
    without init XPCOM and for content process init XPCOM requires IPC.
    
    MozReview-Commit-ID: DhLub23oZz8

--- toolkit/xre/nsEmbedFunctions.cpp
+++ toolkit/xre/nsEmbedFunctions.cpp
@@ -50,7 +50,9 @@
 #include "base/process_util.h"
 #include "chrome/common/child_process.h"
 
+#include "mozilla/AbstractThread.h"
 #include "mozilla/FilePreferences.h"
+
 #include "mozilla/ipc/BrowserProcessSubThread.h"
 #include "mozilla/ipc/GeckoChildProcessHost.h"
 #include "mozilla/ipc/IOThreadChild.h"
@@ -339,6 +341,10 @@ XRE_InitChildProcess(int aArgc,
 
   PROFILER_LABEL("Startup", "XRE_InitChildProcess",
     js::ProfileEntry::Category::OTHER);
+
+  // Ensure AbstractThread is minimally setup, so async IPC messages
+  // work properly.
+  AbstractThread::InitTLS();
 
   // Complete 'task_t' exchange for Mac OS X. This structure has the same size
   // regardless of architecture so we don't have any cross-arch issues here.
