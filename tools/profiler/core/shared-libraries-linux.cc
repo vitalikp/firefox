1df2bb32e8e4576e5ae43cf637cecbc475c7f891
Markus Stange <mstange@themasta.com>
2017-04-21 14:30:18 -0400

    Bug 1358360 - Correctly fill in SharedLibrary fields for the main executable. r=jseward
    MozReview-Commit-ID: 3ZJ8FB2Zjs8

--- tools/profiler/core/shared-libraries-linux.cc
+++ tools/profiler/core/shared-libraries-linux.cc
@@ -68,6 +68,24 @@ int dl_iterate_phdr(
 # error "Unexpected configuration"
 #endif
 
+static SharedLibrary
+SharedLibraryAtPath(const char* path, unsigned long libStart,
+                    unsigned long libEnd, unsigned long offset = 0)
+{
+  nsAutoString pathStr;
+  mozilla::Unused <<
+    NS_WARN_IF(NS_FAILED(NS_CopyNativeToUnicode(nsDependentCString(path),
+                                                pathStr)));
+
+  nsAutoString nameStr = pathStr;
+  int32_t pos = nameStr.RFindChar('/');
+  if (pos != kNotFound) {
+    nameStr.Cut(0, pos + 1);
+  }
+
+  return SharedLibrary(libStart, libEnd, offset, nameStr, pathStr, nameStr,
+                       pathStr, "", "");
+}
 
 // Config cases (1) and (2) use dl_iterate_phdr.
 #if defined(CONFIG_CASE_1) || defined(CONFIG_CASE_2)
@@ -93,22 +111,9 @@ dl_iterate_callback(struct dl_phdr_info *dl_info, size_t size, void *data)
     if (end > libEnd)
       libEnd = end;
   }
-  const char *path = dl_info->dlpi_name;
 
-  nsAutoString pathStr;
-  mozilla::Unused <<
-    NS_WARN_IF(NS_FAILED(NS_CopyNativeToUnicode(nsDependentCString(path),
-                                                pathStr)));
-
-  nsAutoString nameStr = pathStr;
-  int32_t pos = nameStr.RFindChar('/');
-  if (pos != kNotFound) {
-    nameStr.Cut(0, pos + 1);
-  }
-
-  SharedLibrary shlib(libStart, libEnd, 0, nameStr, pathStr, nameStr,
-                      pathStr, "", "");
-  info.AddSharedLibrary(shlib);
+  info.AddSharedLibrary(
+    SharedLibraryAtPath(dl_info->dlpi_name, libStart, libEnd));
 
   return 0;
 }
@@ -206,20 +211,7 @@ SharedLibraryInfo SharedLibraryInfo::GetInfoForSelf()
 #if !defined(CONFIG_CASE_1)
     // This section has to be conditionalised so as to avoid compiler warnings
     // about dead code in case (1).
-    nsAutoString pathStr;
-    mozilla::Unused <<
-      NS_WARN_IF(NS_FAILED(NS_CopyNativeToUnicode(
-                             nsDependentCString(modulePath), pathStr)));
-
-    nsAutoString nameStr = pathStr;
-    int32_t pos = nameStr.RFindChar('/');
-    if (pos != kNotFound) {
-      nameStr.Cut(0, pos + 1);
-    }
-
-    SharedLibrary shlib(start, end, offset, nameStr, pathStr, nameStr,
-                        pathStr, "", "");
-    info.AddSharedLibrary(shlib);
+    info.AddSharedLibrary(SharedLibraryAtPath(modulePath, start, end, offset));
     if (info.GetSize() > 10000) {
       LOG("SharedLibraryInfo::GetInfoForSelf(): "
           "implausibly large number of mappings acquired");
@@ -242,11 +234,9 @@ SharedLibraryInfo SharedLibraryInfo::GetInfoForSelf()
   for (size_t i = 0; i < info.GetSize(); i++) {
     SharedLibrary& lib = info.GetMutableEntry(i);
     if (lib.GetStart() == exeExeAddr && lib.GetNativeDebugPath() == "") {
-      nsAutoString exeNameStr;
-      mozilla::Unused <<
-        NS_WARN_IF(NS_FAILED(NS_CopyNativeToUnicode(
-                               nsDependentCString(exeName), exeNameStr)));
-      lib.SetNativeDebugPath(exeNameStr);
+      lib = SharedLibraryAtPath(exeName, lib.GetStart(), lib.GetEnd(),
+                                lib.GetOffset());
+
       // We only expect to see one such entry.
       break;
     }
--- tools/profiler/public/shared-libraries.h
+++ tools/profiler/public/shared-libraries.h
@@ -102,10 +102,6 @@ public:
   const std::string &GetVersion() const { return mVersion; }
   const std::string &GetArch() const { return mArch; }
 
-  void SetNativeDebugPath(nsString path) {
-    mDebugPath = path;
-  }
-
 private:
   SharedLibrary() {}
 
