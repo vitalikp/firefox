672790c3bc48bb9d1a94573a1b3765486e6369a2
Bill McCloskey <billm@mozilla.com>
2017-04-24 13:54:27 -0700

    Bug 1359245 - Initial support for cooperative contexts (r=mccr8)
    This patch adds initial support for cooperatively scheduled
    CycleCollectedJSContexts.
    
    MozReview-Commit-ID: 5pfPubHUanL

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -401,7 +401,8 @@ private:
 class XPCJSContext final : public mozilla::CycleCollectedJSContext
 {
 public:
-    static XPCJSContext* newXPCJSContext();
+    static void InitTLS();
+    static XPCJSContext* NewXPCJSContext(XPCJSContext* aPrimaryContext);
     static XPCJSContext* Get();
     static XPCJSContext* GetOnly() { return XPCJSContext::Get(); }
 
@@ -489,7 +490,7 @@ private:
     XPCJSContext();
 
     MOZ_IS_CLASS_INIT
-    nsresult Initialize();
+    nsresult Initialize(XPCJSContext* aPrimaryContext);
 
     XPCCallContext*          mCallContext;
     AutoMarkingPtr*          mAutoRoots;
