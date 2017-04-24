04268fe3997923f84efc03c3d9cdc45fd8ea2738
Cameron McCormack <cam@mcc.id.au>
2017-04-24 17:50:57 +0800

    Bug 1358968 - Part 4: Give nsComboboxDisplayFrame a name in frame tree dumps. r=bz
    MozReview-Commit-ID: 67FhXW1C8lG

--- layout/forms/nsComboboxControlFrame.cpp
+++ layout/forms/nsComboboxControlFrame.cpp
@@ -1291,6 +1291,13 @@ public:
   // depends on the available inline size.
   virtual nsIAtom* GetType() const override;
 
+#ifdef DEBUG_FRAME_DUMP
+  nsresult GetFrameName(nsAString& aResult) const
+  {
+    return MakeFrameName(NS_LITERAL_STRING("ComboboxDisplay"), aResult);
+  }
+#endif
+
   virtual bool IsFrameOfType(uint32_t aFlags) const override
   {
     return nsBlockFrame::IsFrameOfType(aFlags &
