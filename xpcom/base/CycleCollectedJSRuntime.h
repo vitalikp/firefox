6d5528aee8053a8482da0531a87d820f9c9cc9b0
Steve Fink <sfink@mozilla.com>
2017-04-25 13:24:34 -0700

    Bug 1322560 - Record minor GC timings in profiles, r=jonco, mccr8, mstange

--- xpcom/base/CycleCollectedJSRuntime.h
+++ xpcom/base/CycleCollectedJSRuntime.h
@@ -297,6 +297,10 @@ private:
   JS::GCSliceCallback mPrevGCSliceCallback;
   JS::GCNurseryCollectionCallback mPrevGCNurseryCollectionCallback;
 
+#ifdef MOZ_GECKO_PROFILER
+  mozilla::TimeStamp mLatestNurseryCollectionStart;
+#endif
+
   nsDataHashtable<nsPtrHashKey<void>, nsScriptObjectTracer*> mJSHolders;
 
   typedef nsDataHashtable<nsFuncPtrHashKey<DeferredFinalizeFunction>, void*>
