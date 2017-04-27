2b520b0f12c8387949a75aa6a582b058266e5ede
Michael Layzell <michael@thelayzells.com>
2017-04-27 12:44:53 -0400

    Bug 1339537 - Part 4: Pass FilterPrimitiveDescription by const reference in AddLightingAttributes, r=mstange
    MozReview-Commit-ID: J3m2uzv7jW0

--- dom/svg/nsSVGFilters.h
+++ dom/svg/nsSVGFilters.h
@@ -204,7 +204,7 @@ protected:
   AttributeMap ComputeLightAttributes(nsSVGFilterInstance* aInstance);
 
   FilterPrimitiveDescription
-    AddLightingAttributes(FilterPrimitiveDescription aDescription,
+    AddLightingAttributes(const FilterPrimitiveDescription& aDescription,
                           nsSVGFilterInstance* aInstance);
 
   enum { SURFACE_SCALE, DIFFUSE_CONSTANT, SPECULAR_CONSTANT, SPECULAR_EXPONENT };
