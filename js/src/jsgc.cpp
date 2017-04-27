faaaf92a58d66f581e3293528dc46ff27198dc0e
Bill McCloskey <billm@mozilla.com>
2017-04-27 15:53:19 -0700

    Bug 1359245 - Fix compiler error on CLOSED TREE
    MozReview-Commit-ID: 57cVUcTV1Rv

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -7317,7 +7317,6 @@ AutoAssertNoNurseryAlloc::~AutoAssertNoNurseryAlloc()
 }
 
 JS::AutoEnterCycleCollection::AutoEnterCycleCollection(JSRuntime* rt)
-  : runtime(rt)
 {
     MOZ_ASSERT(!JS::CurrentThreadIsHeapBusy());
     TlsContext.get()->heapState = HeapState::CycleCollecting;
