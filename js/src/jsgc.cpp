f1354850a95f79cf342d641a487c4ea7c6612aaf
Bill McCloskey <billm@mozilla.com>
2017-04-27 14:10:15 -0700

    Bug 1359245 - Remove references to context from the cycle collector (r=mccr8)
    MozReview-Commit-ID: 1QoNEiZMvBf

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -7316,8 +7316,8 @@ AutoAssertNoNurseryAlloc::~AutoAssertNoNurseryAlloc()
     TlsContext.get()->allowNurseryAlloc();
 }
 
-JS::AutoEnterCycleCollection::AutoEnterCycleCollection(JSContext* cx)
-  : runtime(cx->runtime())
+JS::AutoEnterCycleCollection::AutoEnterCycleCollection(JSRuntime* rt)
+  : runtime(rt)
 {
     MOZ_ASSERT(!JS::CurrentThreadIsHeapBusy());
     TlsContext.get()->heapState = HeapState::CycleCollecting;
--- js/src/jspubtd.h
+++ js/src/jspubtd.h
@@ -203,11 +203,11 @@ class MOZ_STACK_CLASS JS_PUBLIC_API(AutoEnterCycleCollection)
     JSRuntime* runtime;
 
   public:
-    explicit AutoEnterCycleCollection(JSContext* cx);
+    explicit AutoEnterCycleCollection(JSRuntime* rt);
     ~AutoEnterCycleCollection();
 #else
   public:
-    explicit AutoEnterCycleCollection(JSContext* cx) {}
+    explicit AutoEnterCycleCollection(JSRuntime* rt) {}
     ~AutoEnterCycleCollection() {}
 #endif
 };
