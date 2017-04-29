cc0003cfb00cc3927aad0e497fc3912db35c14e2
Cameron McCormack <cam@mcc.id.au>
2017-04-29 11:08:19 +0800

    Bug 1358968 - Static analysis fixup. r=me (CLOSED TREE)

--- layout/forms/nsComboboxControlFrame.cpp
+++ layout/forms/nsComboboxControlFrame.cpp
@@ -1292,7 +1292,7 @@ public:
   virtual nsIAtom* GetType() const override;
 
 #ifdef DEBUG_FRAME_DUMP
-  nsresult GetFrameName(nsAString& aResult) const
+  nsresult GetFrameName(nsAString& aResult) const override
   {
     return MakeFrameName(NS_LITERAL_STRING("ComboboxDisplay"), aResult);
   }
