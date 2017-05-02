f92926211262227b7d2ae1cd23086b87094d7ddc
Mats Palmgren <mats@mozilla.com>
2017-05-02 03:50:16 +0200

    Bug 1360867 - Make a few classes final to possibly devirtualize some calls. r=dholbert
    MozReview-Commit-ID: EyBsvXbfJvK

--- layout/xul/tree/nsTreeColFrame.cpp
+++ layout/xul/tree/nsTreeColFrame.cpp
@@ -51,7 +51,8 @@ nsTreeColFrame::DestroyFrom(nsIFrame* aDestructRoot)
   nsBoxFrame::DestroyFrom(aDestructRoot);
 }
 
-class nsDisplayXULTreeColSplitterTarget : public nsDisplayItem {
+class nsDisplayXULTreeColSplitterTarget final : public nsDisplayItem
+{
 public:
   nsDisplayXULTreeColSplitterTarget(nsDisplayListBuilder* aBuilder,
                                     nsIFrame* aFrame) :
