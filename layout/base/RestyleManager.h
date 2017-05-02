2e9c317b324bf1a1e2ad2b29fdeea1302ef42e88
Boris Chiou <boris.chiou@gmail.com>
2017-05-02 14:03:16 +0800

    Bug 1334036 - Part 8: Add AddLayerChangesForAnimation in ServoRestyleManager. r=birtles
    Some changes to animations don't affect the computed style and yet still
    require the layer to be updated. Therefore, we also need to call
    AddLayerChangesForAnimation in ServoRestyleManager. In this patch, we
    factor out this function from GeckoRestyleManager, so we can reuse it.
    
    MozReview-Commit-ID: LL7D1oGS65l

--- layout/base/RestyleManager.h
+++ layout/base/RestyleManager.h
@@ -199,6 +199,11 @@ public:
   // such as changes made through the Web Animations API.
   void IncrementAnimationGeneration();
 
+  static void AddLayerChangesForAnimation(nsIFrame* aFrame,
+                                          nsIContent* aContent,
+                                          nsStyleChangeList&
+                                            aChangeListToProcess);
+
 protected:
   RestyleManager(StyleBackendType aType, nsPresContext* aPresContext);
 
