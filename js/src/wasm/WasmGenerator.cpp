f1579265ee9b3c7c1c60bb5365b37a4265083e87
Lars T Hansen <lhansen@mozilla.com>
2017-05-02 18:05:14 +0200

    Bug 1338217 - Only preserve bytecode when it's needed, rs=luke

--- js/src/wasm/WasmGenerator.cpp
+++ js/src/wasm/WasmGenerator.cpp
@@ -1193,7 +1193,11 @@ ModuleGenerator::finish(const ShareableBytes& bytecode)
             return nullptr;
     }
 
-    SharedCode code = js_new<Code>(Move(codeSegment), *metadata_, &bytecode);
+    const ShareableBytes* maybeBytecode = nullptr;
+    if (metadata_->debugEnabled || !metadata_->funcNames.empty())
+        maybeBytecode = &bytecode;
+
+    SharedCode code = js_new<Code>(Move(codeSegment), *metadata_, maybeBytecode);
     if (!code)
         return nullptr;
 
