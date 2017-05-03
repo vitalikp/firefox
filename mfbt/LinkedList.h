4d5d67186f5bbe5c4c043f7837556286d569c3d5
Kris Maglione <maglione.k@gmail.com>
2017-05-02 17:54:18 -0700

    Bug 1359653: Part 3 - Add a clear() method and move asssignment operator to AutoCleanLinkedList. r=waldo
    MozReview-Commit-ID: 2bUTMPviJzg

--- mfbt/LinkedList.h
+++ mfbt/LinkedList.h
@@ -646,6 +646,17 @@ class AutoCleanLinkedList : public LinkedList<T>
 public:
   ~AutoCleanLinkedList()
   {
+    clear();
+  }
+
+  AutoCleanLinkedList& operator=(AutoCleanLinkedList&& aOther)
+  {
+    LinkedList<T>::operator=(Forward<LinkedList<T>>(aOther));
+    return *this;
+  }
+
+  void clear()
+  {
     while (T* element = this->popFirst()) {
       delete element;
     }
