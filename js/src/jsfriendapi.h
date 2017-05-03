7597f8c9c3f6a0dd1ef57dd853ba0f8a9317b58a
Ting-Yu Chou <janus926@gmail.com>
2017-05-03 13:56:54 +0800

    Bug 816784 part 2 - Optimize NukeCrossCompartmentWrappers() to iterate only the targetting wrappers. r=jonco
    Changed the type of argument |targetFilter| of NukeCrossCompartmentWrappers()
    from CompartmentFilter to JSCompartment* because it is always a single target
    compartment, and we can optimize the iteration not to iterate the outer map.
    
    MozReview-Commit-ID: 7cDCgJI0H9z

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -1207,7 +1207,7 @@ struct CompartmentsWithPrincipals : public CompartmentFilter {
 extern JS_FRIEND_API(bool)
 NukeCrossCompartmentWrappers(JSContext* cx,
                              const CompartmentFilter& sourceFilter,
-                             const CompartmentFilter& targetFilter,
+                             JSCompartment* target,
                              NukeReferencesToWindow nukeReferencesToWindow,
                              NukeReferencesFromTarget nukeReferencesFromTarget);
 
