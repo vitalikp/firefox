183f98300a7beeb208a26985d4141e4110d4ec1a
Dragana Damjanovic <dd.mozilla@gmail.com>
2017-05-04 12:15:33 +0200

    Bug 1360515 - During TCP Fast Open let necko works as it is used to, but only limit amount of data we write to lower layers, i.e. nss, tcpFastOpenLayer, tcp socket. r=mcmanus

--- netwerk/protocol/http/nsHttpConnectionMgr.cpp
+++ netwerk/protocol/http/nsHttpConnectionMgr.cpp
@@ -3463,7 +3463,7 @@ nsHalfOpenSocket::OnOutputStreamReady(nsIAsyncOutputStream *out)
         }
     }
 
-    return SetupConn(out, nullptr);
+    return SetupConn(out, false);
 }
 
 bool
@@ -3512,9 +3512,8 @@ nsHalfOpenSocket::FastOpenEnabled()
 
 nsresult
 nsHttpConnectionMgr::
-nsHalfOpenSocket::StartFastOpen(PRFileDesc *fd)
+nsHalfOpenSocket::StartFastOpen()
 {
-    MOZ_ASSERT(fd);
     MOZ_ASSERT(mStreamOut);
     MOZ_ASSERT(mEnt && !mBackupTransport);
     mUsingFastOpen = true;
@@ -3526,7 +3525,7 @@ nsHalfOpenSocket::StartFastOpen(PRFileDesc *fd)
     mStreamOut->AsyncWait(nullptr, 0, 0, nullptr);
     mSocketTransport->SetEventSink(nullptr, nullptr);
     gHttpHandler->ConnMgr()->RecvdConnect();
-    nsresult rv = SetupConn(mStreamOut, fd);
+    nsresult rv = SetupConn(mStreamOut, true);
     if (NS_FAILED(rv)) {
         mStreamOut = nullptr;
         mStreamIn = nullptr;
@@ -3592,6 +3591,7 @@ nsHalfOpenSocket::SetFastOpenConnected(nsresult aError, bool aWillRetry)
         mStreamIn = nullptr;
     }
 
+    mConnectionNegotiatingFastOpen->SetFastOpen(false);
     mConnectionNegotiatingFastOpen = nullptr;
 }
 
@@ -3614,7 +3614,7 @@ nsHalfOpenSocket::FastOpenNotSupported()
 nsresult
 nsHttpConnectionMgr::
 nsHalfOpenSocket::SetupConn(nsIAsyncOutputStream *out,
-                            PRFileDesc *aFastOpen)
+                            bool aFastOpen)
 {
     MOZ_ASSERT(!aFastOpen || (out == mStreamOut));
     // assign the new socket to the http connection
@@ -3772,8 +3772,6 @@ nsHalfOpenSocket::SetupConn(nsIAsyncOutputStream *out,
         // If it is fast open create a new tranaction for backup stream.
         mTransaction = new NullHttpTransaction(mEnt->mConnInfo,
                                                callbacks, mCaps);
-
-        conn->SetFastOpen(nullptr);
         mConnectionNegotiatingFastOpen = conn;
     }
 
