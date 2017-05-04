e7670ad4ae44f26f3c1f286c9e001a94bc707850
Dragana Damjanovic <dd.mozilla@gmail.com>
2017-05-04 12:14:04 +0200

    Bug 1188435 - Implement fast open necko part. r=mcmanus

--- netwerk/base/nsSyncStreamListener.cpp
+++ netwerk/base/nsSyncStreamListener.cpp
@@ -13,7 +13,7 @@ nsSyncStreamListener::Init()
 {
     return NS_NewPipe(getter_AddRefs(mPipeIn),
                       getter_AddRefs(mPipeOut),
-                      nsIOService::gDefaultSegmentSize,
+                      mozilla::net::nsIOService::gDefaultSegmentSize,
                       UINT32_MAX, // no size limit
                       false,
                       false);
