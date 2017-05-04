04485a0bbcba4c06a422d13f133f09eca2c500e7
Jessica Jong <jjong@mozilla.com>
2017-05-04 11:54:29 +0800

    Bug 1345767 - Part 2: Factor IsTooLong/Short() out of HTMLInputElement. r=smaug
    MozReview-Commit-ID: 5svYqBEFgzk

--- dom/html/HTMLInputElement.h
+++ dom/html/HTMLInputElement.h
@@ -1130,11 +1130,6 @@ protected:
    */
   bool DoesAutocompleteApply() const;
 
-  /**
-   * Returns if the minlength or maxlength attributes apply for the current type.
-   */
-  bool MinOrMaxLengthApplies() const { return IsSingleLineTextControl(false, mType); }
-
   void FreeData();
   nsTextEditorState *GetEditorState() const;
 
