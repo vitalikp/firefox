62f583bb6c503d3165dc2361c857763daec5b43c
btian <btian@mozilla.com>
2017-05-05 11:30:19 +0800

    Bug 1338059 - Part 2: Implement PromiseRejectionEvent, r=bz
    MozReview-Commit-ID: 6BZBL9BjO03

--- dom/base/nsContentUtils.h
+++ dom/base/nsContentUtils.h
@@ -2714,7 +2714,7 @@ public:
                                                 nsIHttpChannel* aChannel,
                                                 mozilla::net::ReferrerPolicy aReferrerPolicy);
 
-    /*
+  /*
    * Parse a referrer policy from a Referrer-Policy header
    * https://www.w3.org/TR/referrer-policy/#parse-referrer-policy-from-header
    *
@@ -2722,6 +2722,8 @@ public:
    * @return referrer policy from the response header.
    */
   static mozilla::net::ReferrerPolicy GetReferrerPolicyFromHeader(const nsAString& aHeader);
+
+  static bool PromiseRejectionEventsEnabled(JSContext* aCx, JSObject* aObj);
 
   static bool PushEnabled(JSContext* aCx, JSObject* aObj);
 
