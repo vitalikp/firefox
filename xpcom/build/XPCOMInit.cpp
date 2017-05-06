a311f482ca4c6e31bb735e4624ce3568c9540502
Kris Maglione <maglione.k@gmail.com>
2017-05-06 12:24:22 -0700

    Bug 1359653: Part 5 - Pre-load scripts needed during startup in a background thread. r=shu,erahm
    One of the things that I've noticed in profiling startup overhead is that,
    even with the startup cache, we spend about 130ms just loading and decoding
    scripts from the startup cache on my machine.
    
    I think we should be able to do better than that by doing some of that work in
    the background for scripts that we know we'll need during startup. With this
    change, we seem to consistently save about 3-5% on non-e10s startup overhead
    on talos. But there's a lot of room for tuning, and I think we get some
    considerable improvement with a few ongoing tweeks.
    
    Some notes about the approach:
    
    - Setting up the off-thread compile is fairly expensive, since we need to
    create a global object, and a lot of its built-in prototype objects for each
    compile. So in order for there to be a performance improvement for OMT
    compiles, the script has to be pretty large. Right now, the tipping point
    seems to be about 20K.
    
      There's currently no easy way to improve the per-compile setup overhead, but
    we should be able to combine the off-thread compiles for multiple smaller
    scripts into a single operation without any additional per-script overhead.
    
    - The time we spend setting up scripts for OMT compile is almost entirely
    CPU-bound. That means that we have a chunk of about 20-50ms where we can
    safely schedule thread-safe IO work during early startup, so if we schedule
    some of our current synchronous IO operations on background threads during the
    script cache setup, we basically get them for free, and can probably increase
    the number of scripts we compile in the background.
    
    - I went with an uncompressed mmap of the raw XDR data for a storage format.
    That currently occupies about 5MB of disk space. Gzipped, it's ~1.2MB, so
    compressing it might save some startup disk IO, but keeping it uncompressed
    simplifies a lot of the OMT and even main thread decoding process, but, more
    importantly:
    
    - We currently don't use the startup cache in content processes, for a variety
    of reasons. However, with this approach, I think we can safely store the
    cached script data from a content process before we load any untrusted code
    into it, and then share mmapped startup cache data between all content
    processes. That should speed up content process startup *a lot*, and very
    likely save memory, too. And:
    
    - If we're especially concerned about saving per-process memory, and we keep
    the cache data mapped for the lifetime of the JS runtime, I think that with
    some effort we can probably share the static string data from scripts between
    content processes, without any copying. Right now, it looks like for the main
    process, there's about 1.5MB of string-ish data in the XDR dumps. It's
    probably less for content processes, but if we could save .5MB per process
    this way, it might make it easier to increase the number of content processes
    we allow.
    
    MozReview-Commit-ID: CVJahyNktKB

--- xpcom/build/XPCOMInit.cpp
+++ xpcom/build/XPCOMInit.cpp
@@ -113,6 +113,7 @@ extern nsresult nsStringInputStreamConstructor(nsISupports*, REFNSIID, void**);
 #include "mozilla/Services.h"
 #include "mozilla/Omnijar.h"
 #include "mozilla/HangMonitor.h"
+#include "mozilla/ScriptPreloader.h"
 #include "mozilla/SystemGroup.h"
 #include "mozilla/Telemetry.h"
 #include "mozilla/BackgroundHangMonitor.h"
@@ -654,6 +655,7 @@ NS_InitXPCOM2(nsIServiceManager** aResult,
   nsCOMPtr<nsISupports> componentLoader =
     do_GetService("@mozilla.org/moz/jsloader;1");
 
+  Unused << mozilla::ScriptPreloader::GetSingleton().InitCache();
   mozilla::scache::StartupCache::GetSingleton();
   mozilla::AvailableMemoryTracker::Activate();
 
