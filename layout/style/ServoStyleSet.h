39ceba76bab9f59b7f15177ee984d233e72f6644
Boris Zbarsky <bzbarsky@mit.edu>
2017-05-10 13:13:50 -0400

    Bug 1361843 part 4. Remove the now-unused mBatching member of ServoStyleSet. r=emilio
    MozReview-Commit-ID: DvyZkQh6q8A

--- layout/style/ServoStyleSet.h
+++ layout/style/ServoStyleSet.h
@@ -467,7 +467,6 @@ private:
   UniquePtr<RawServoStyleSet> mRawSet;
   EnumeratedArray<SheetType, SheetType::Count,
                   nsTArray<Entry>> mEntries;
-  int32_t mBatching;
   uint32_t mUniqueIDCounter;
   bool mAllowResolveStaleStyles;
   bool mAuthorStyleDisabled;
