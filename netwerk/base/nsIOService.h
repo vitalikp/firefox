c2f60d6e7557c1665f1e0006d702d348baeca74c
Valentin Gosu <valentin.gosu@gmail.com>
2017-05-10 19:23:54 +0200

    Bug 1363700 - Add RCWN stats to about:networking r=bz,michal
    MozReview-Commit-ID: GVt1omCfL6t

--- netwerk/base/nsIOService.h
+++ netwerk/base/nsIOService.h
@@ -97,6 +97,15 @@ public:
 
     static bool IsInheritSecurityContextForDataURIEnabled();
 
+    // Used to count the total number of HTTP requests made
+    void IncrementRequestNumber() { mTotalRequests++; }
+    uint32_t GetTotalRequestNumber() { return mTotalRequests; }
+    // Used to keep "race cache with network" stats
+    void IncrementCacheWonRequestNumber() { mCacheWon++; }
+    uint32_t GetCacheWonRequestNumber() { return mCacheWon; }
+    void IncrementNetWonRequestNumber() { mNetWon++; }
+    uint32_t GetNetWonRequestNumber() { return mNetWon; }
+
     // Used to trigger a recheck of the captive portal status
     nsresult RecheckCaptivePortal();
 private:
@@ -179,6 +188,10 @@ private:
     static bool                          sTelemetryEnabled;
 
     static bool                          sDataURIInheritSecurityContext;
+
+    uint32_t mTotalRequests;
+    uint32_t mCacheWon;
+    uint32_t mNetWon;
 
     // These timestamps are needed for collecting telemetry on PR_Connect,
     // PR_ConnectContinue and PR_Close blocking time.  If we spend very long
