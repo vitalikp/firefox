cf23a0d200d2d4a6798237d4278b3b9f8fb26c5f
Xidorn Quan <me@upsuper.org>
2017-05-11 14:47:24 +1000

    Bug 1328319 part 7 - Enable querying counter-style rule on Servo backend. r=heycam
    MozReview-Commit-ID: Aj3AcW1KmRe

--- layout/style/ServoStyleSet.cpp
+++ layout/style/ServoStyleSet.cpp
@@ -1089,6 +1089,12 @@ ServoStyleSet::AppendFontFaceRules(nsTArray<nsFontFaceRuleContainer>& aArray)
   return true;
 }
 
+nsCSSCounterStyleRule*
+ServoStyleSet::CounterStyleRuleForName(nsIAtom* aName)
+{
+  return Servo_StyleSet_GetCounterStyleRule(mRawSet.get(), aName);
+}
+
 already_AddRefed<ServoComputedValues>
 ServoStyleSet::ResolveForDeclarations(
   ServoComputedValuesBorrowedOrNull aParentOrNull,
