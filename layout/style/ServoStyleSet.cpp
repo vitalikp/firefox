bd748358ea1d6a1d18d9b06f60917e31274aac61
Emilio Cobos Álvarez <emilio@crisal.io>
2017-05-13 19:29:14 +0200

    Bug 1364412: Don't ignore the pseudo-element argument in ResolvePseudoElementStyle. r=heycam
    MozReview-Commit-ID: 4KA4bj2Ww5W
    Signed-off-by: Emilio Cobos Álvarez <emilio@crisal.io>

--- layout/style/ServoStyleSet.cpp
+++ layout/style/ServoStyleSet.cpp
@@ -481,10 +481,6 @@ ServoStyleSet::ResolvePseudoElementStyle(Element* aOriginatingElement,
                                          nsStyleContext* aParentContext,
                                          Element* aPseudoElement)
 {
-  if (aPseudoElement) {
-    NS_WARNING("stylo: We don't support CSS_PSEUDO_ELEMENT_SUPPORTS_USER_ACTION_STATE yet");
-  }
-
   MaybeRebuildStylist();
 
   // NB: We ignore aParentContext, on the assumption that pseudo element styles
@@ -493,9 +489,19 @@ ServoStyleSet::ResolvePseudoElementStyle(Element* aOriginatingElement,
   MOZ_ASSERT(aType < CSSPseudoElementType::Count);
   nsIAtom* pseudoTag = nsCSSPseudoElements::GetPseudoAtom(aType);
 
-  RefPtr<ServoComputedValues> computedValues =
-    Servo_ResolvePseudoStyle(aOriginatingElement, pseudoTag,
-                             /* is_probe = */ false, mRawSet.get()).Consume();
+  RefPtr<ServoComputedValues> computedValues;
+  if (aPseudoElement) {
+    MOZ_ASSERT(aType == aPseudoElement->GetPseudoElementType());
+    computedValues = Servo_ResolveStyle(aPseudoElement, mRawSet.get(),
+                                        mAllowResolveStaleStyles).Consume();
+  } else {
+    computedValues =
+      Servo_ResolvePseudoStyle(aOriginatingElement,
+                               pseudoTag,
+                               /* is_probe = */ false,
+                               mRawSet.get()).Consume();
+  }
+
   MOZ_ASSERT(computedValues);
 
   bool isBeforeOrAfter = aType == CSSPseudoElementType::before ||
