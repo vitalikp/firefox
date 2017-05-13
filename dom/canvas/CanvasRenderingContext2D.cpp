9aed626d0f5d61fac08a7b76ad914f29f30621a0
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2017-05-13 19:47:38 +0900

    Bug 1357295 - Rename LengthParsingMode to ParsingMode and LengthParsingMode::SVG to PasingMode::AllowUnitlessLength. r=emilio
    We need another flag that represents allow-negative-number for SMIL, so
    this enum will also comprise the another parsing mode that allows negative number.
    
    MozReview-Commit-ID: IDFNppjGeB

--- dom/canvas/CanvasRenderingContext2D.cpp
+++ dom/canvas/CanvasRenderingContext2D.cpp
@@ -2794,7 +2794,7 @@ CreateDeclarationForServo(nsCSSPropertyID aProperty,
     Servo_ParseProperty(aProperty,
                         &value,
                         data,
-                        LengthParsingMode::Default).Consume();
+                        ParsingMode::Default).Consume();
 
   if (!servoDeclarations) {
     // We got a syntax error.  The spec says this value must be ignored.
@@ -2810,7 +2810,7 @@ CreateDeclarationForServo(nsCSSPropertyID aProperty,
                                            &normalString,
                                            false,
                                            data,
-                                           LengthParsingMode::Default);
+                                           ParsingMode::Default);
   }
 
   return servoDeclarations.forget();
