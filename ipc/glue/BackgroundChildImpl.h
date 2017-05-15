c785067fb6fc1d41c0531f68e0ff5aebcade5e62
Shih-Chiang Chien <schien@mozilla.com>
2017-05-15 16:01:10 +0800

    Bug 1015466 - Part 1, skeleton of PHttpBackgroundChannel. r=mayhemer
    PHttpBackgroundChannel is introduced to deliver OnStopRequest/OnTransportAndData/OnProgress/OnStatus on background thread.
    FlushedForDiversion/DivertMessages is moved to PHttpBackgroundChannel to ensure no more channel event are delivered after these two messages on the background channel.
    OnStartRequestSent is introduced to synchronize the execution order between OnStartRequest (main thread IPC) and all other messages on PHttpBackgroundChannel.
    
    MozReview-Commit-ID: HfqvPML6EKw

--- ipc/glue/BackgroundChildImpl.h
+++ ipc/glue/BackgroundChildImpl.h
@@ -189,6 +189,12 @@ protected:
 
   virtual bool
   DeallocPWebAuthnTransactionChild(PWebAuthnTransactionChild* aActor) override;
+
+  virtual PHttpBackgroundChannelChild*
+  AllocPHttpBackgroundChannelChild(const uint64_t& aChannelId) override;
+
+  virtual bool
+  DeallocPHttpBackgroundChannelChild(PHttpBackgroundChannelChild* aActor) override;
 };
 
 class BackgroundChildImpl::ThreadLocal final
