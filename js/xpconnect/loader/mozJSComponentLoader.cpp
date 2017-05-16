3574d733984a393a84abe54ec588e67c05cddca4
Andrew McCreight <continuation@gmail.com>
2017-05-16 15:51:14 -0700

    Bug 1365760 - Fix two tiny issues in mozJSComponentLoader::ImportInto(). r=kmag
    MozReview-Commit-ID: 84I37uRi11H

--- js/xpconnect/loader/mozJSComponentLoader.cpp
+++ js/xpconnect/loader/mozJSComponentLoader.cpp
@@ -954,7 +954,7 @@ mozJSComponentLoader::ImportInto(const nsACString& aLocation,
     vp.set(mod->obj);
 
     if (targetObj) {
-        // cxhelper must be created before jsapi, so that jsapi is detroyed and
+        // cxhelper must be created before jsapi, so that jsapi is destroyed and
         // pops any context it has pushed before we report to the caller context.
         JSCLContextHelper cxhelper(callercx);
 
@@ -1038,7 +1038,7 @@ mozJSComponentLoader::ImportInto(const nsACString& aLocation,
 
             JSAutoCompartment target_ac(cx, targetObj);
 
-	    JS_MarkCrossZoneId(cx, symbolId);
+            JS_MarkCrossZoneId(cx, symbolId);
 
             if (!JS_WrapValue(cx, &value) ||
                 !JS_SetPropertyById(cx, targetObj, symbolId, value)) {
