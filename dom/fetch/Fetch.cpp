58aec6b855cb6852338cfb3a19fecb460c3ef1f2
Bevis Tseng <btseng@mozilla.com>
2017-05-16 16:59:29 +0800

    Bug 1363318 - Part 1: Label FetchSignalProxy. r=baku
    Replace the use of NS_DispatchToMainThread, NS_ReleaseOnMainThread with WorkerPrivate->MainThreadEventTarget().

--- dom/fetch/Fetch.cpp
+++ dom/fetch/Fetch.cpp
@@ -58,6 +58,9 @@ class FetchSignalProxy final : public FetchSignal::Follower
   // This is created and released on the main-thread.
   RefPtr<FetchSignal> mSignalMainThread;
 
+  // The main-thread event target for runnable dispatching.
+  nsCOMPtr<nsIEventTarget> mMainThreadEventTarget;
+
   // This value is used only for the creation of FetchSignal on the
   // main-thread. They are not updated.
   const bool mAborted;
@@ -86,9 +89,11 @@ class FetchSignalProxy final : public FetchSignal::Follower
 public:
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(FetchSignalProxy)
 
-  explicit FetchSignalProxy(FetchSignal* aSignal)
-    : mAborted(aSignal->Aborted())
+  FetchSignalProxy(FetchSignal* aSignal, nsIEventTarget* aMainThreadEventTarget)
+    : mMainThreadEventTarget(aMainThreadEventTarget)
+    , mAborted(aSignal->Aborted())
   {
+    MOZ_ASSERT(mMainThreadEventTarget);
     Follow(aSignal);
   }
 
@@ -97,7 +102,7 @@ public:
   {
     RefPtr<FetchSignalProxyRunnable> runnable =
       new FetchSignalProxyRunnable(this);
-    NS_DispatchToMainThread(runnable);
+    mMainThreadEventTarget->Dispatch(runnable.forget(), NS_DISPATCH_NORMAL);
   }
 
   FetchSignal*
@@ -119,7 +124,7 @@ public:
 private:
   ~FetchSignalProxy()
   {
-    NS_ReleaseOnMainThread(mSignalMainThread.forget());
+    NS_ProxyRelease(mMainThreadEventTarget, mSignalMainThread.forget());
   }
 };
 
@@ -151,7 +156,8 @@ public:
 
     RefPtr<FetchSignalProxy> signalProxy;
     if (aSignal) {
-      signalProxy = new FetchSignalProxy(aSignal);
+      signalProxy =
+        new FetchSignalProxy(aSignal, aWorkerPrivate->MainThreadEventTarget());
     }
 
     RefPtr<WorkerFetchResolver> r =
