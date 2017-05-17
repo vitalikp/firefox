f0980c475e4bc31d26d8d9b77f0eb29824af0003
Jonathan Kew <jkew@mozilla.com>
2017-05-17 15:47:42 +0100

    Bug 1365356 - patch 4 - Split low-level nsIFrame methods taking an optional nsStyleDisplay pointer into two versions, one taking a (non-null) nsStyleDisplay parameter, so we can bypass StyleDisplayWithOptionalParam and its null-check + branch in hot calls. r=mats

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -1652,7 +1652,10 @@ public:
    * @param aStyleDisplay:  If the caller has this->StyleDisplay(), providing
    *   it here will improve performance.
    */
-  bool IsTransformed(const nsStyleDisplay* aStyleDisplay = nullptr) const;
+  bool IsTransformed(const nsStyleDisplay* aStyleDisplay) const;
+  bool IsTransformed() const {
+    return IsTransformed(StyleDisplay());
+  }
 
   /**
    * True if this frame has any animation of transform in effect.
@@ -1703,7 +1706,10 @@ public:
    * @param aStyleDisplay:  If the caller has this->StyleDisplay(), providing
    *   it here will improve performance.
    */
-  bool Extend3DContext(const nsStyleDisplay* aStyleDisplay = nullptr) const;
+  bool Extend3DContext(const nsStyleDisplay* aStyleDisplay) const;
+  bool Extend3DContext() const {
+    return Extend3DContext(StyleDisplay());
+  }
 
   /**
    * Returns whether this frame has a parent that Extend3DContext() and has
@@ -1713,8 +1719,10 @@ public:
    * @param aStyleDisplay:  If the caller has this->StyleDisplay(), providing
    *   it here will improve performance.
    */
-  bool Combines3DTransformWithAncestors(const nsStyleDisplay* aStyleDisplay
-                                          = nullptr) const;
+  bool Combines3DTransformWithAncestors(const nsStyleDisplay* aStyleDisplay) const;
+  bool Combines3DTransformWithAncestors() const {
+    return Combines3DTransformWithAncestors(StyleDisplay());
+  }
 
   /**
    * Returns whether this frame has a hidden backface and has a parent that
@@ -1723,15 +1731,26 @@ public:
    */
   bool In3DContextAndBackfaceIsHidden() const;
 
-  bool IsPreserve3DLeaf(const nsStyleDisplay* aStyleDisplay = nullptr) const {
-    const nsStyleDisplay* disp = StyleDisplayWithOptionalParam(aStyleDisplay);
-    return Combines3DTransformWithAncestors(disp) &&
-           !Extend3DContext(disp);
+  bool IsPreserve3DLeaf(const nsStyleDisplay* aStyleDisplay) const {
+    return Combines3DTransformWithAncestors(aStyleDisplay) &&
+           !Extend3DContext(aStyleDisplay);
+  }
+  bool IsPreserve3DLeaf() const {
+    return IsPreserve3DLeaf(StyleDisplay());
   }
 
-  bool HasPerspective(const nsStyleDisplay* aStyleDisplay = nullptr) const;
+  bool HasPerspective(const nsStyleDisplay* aStyleDisplay) const;
+  bool HasPerspective() const {
+    return HasPerspective(StyleDisplay());
+  }
 
-  bool ChildrenHavePerspective(const nsStyleDisplay* aStyleDisplay = nullptr) const;
+  bool ChildrenHavePerspective(const nsStyleDisplay* aStyleDisplay) const {
+    MOZ_ASSERT(aStyleDisplay == StyleDisplay());
+    return aStyleDisplay->HasPerspectiveStyle();
+  }
+  bool ChildrenHavePerspective() const {
+    return ChildrenHavePerspective(StyleDisplay());
+  }
 
   /**
    * Includes the overflow area of all descendants that participate in the current
@@ -2711,8 +2730,11 @@ public:
     // this and return the outer scroll frame.
     SKIP_SCROLLED_FRAME = 0x01
   };
-  nsIFrame* GetContainingBlock(uint32_t aFlags = 0,
-                               const nsStyleDisplay* aStyleDisplay = nullptr) const;
+  nsIFrame* GetContainingBlock(uint32_t aFlags,
+                               const nsStyleDisplay* aStyleDisplay) const;
+  nsIFrame* GetContainingBlock(uint32_t aFlags = 0) const {
+    return GetContainingBlock(aFlags, StyleDisplay());
+  }
 
   /**
    * Is this frame a containing block for floating elements?
@@ -3654,8 +3676,12 @@ public:
    * @param aStyleDisplay:  If the caller has this->StyleDisplay(), providing
    *   it here will improve performance.
    */
-  bool BackfaceIsHidden(const nsStyleDisplay* aStyleDisplay = nullptr) const {
-    return StyleDisplayWithOptionalParam(aStyleDisplay)->BackfaceIsHidden();
+  bool BackfaceIsHidden(const nsStyleDisplay* aStyleDisplay) const {
+    MOZ_ASSERT(aStyleDisplay == StyleDisplay());
+    return aStyleDisplay->BackfaceIsHidden();
+  }
+  bool BackfaceIsHidden() const {
+    return StyleDisplay()->BackfaceIsHidden();
   }
 
   /**
