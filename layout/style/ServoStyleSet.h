0d27864c4d60177cc7b77ec18aa43bb7a7648e32
Emilio Cobos Álvarez <emilio@crisal.io>
2017-05-17 18:33:32 +0200

    Bug 1365902: Make ServoStyleSet::RebuildAllStyleData async. r=heycam
    MozReview-Commit-ID: D9Ri4gO3F6G

--- layout/style/ServoStyleSet.h
+++ layout/style/ServoStyleSet.h
@@ -352,12 +352,6 @@ public:
 #endif
 
   /**
-   * Rebuild the style data. This will force a stylesheet flush, and also
-   * recompute the default computed styles.
-   */
-  void RebuildData();
-
-  /**
    * Clears the style data, both style sheet data and cached non-inheriting
    * style contexts, and marks the stylist as needing an unconditional full
    * rebuild, including a device reset.
@@ -488,6 +482,12 @@ private:
                                               nsIAtom* aPseudoTag,
                                               CSSPseudoElementType aPseudoType,
                                               LazyComputeBehavior aMayCompute);
+
+  /**
+   * Rebuild the style data. This will force a stylesheet flush, and also
+   * recompute the default computed styles.
+   */
+  void RebuildData();
 
   /**
    * Gets the pending snapshots to handle from the restyle manager.
