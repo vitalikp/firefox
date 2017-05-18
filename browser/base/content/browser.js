cc26266f68c188ab1ce48f40bc40671633cd25a1
Marco Bonardo <mbonardo@mozilla.com>
2017-05-18 16:48:23 +0200

    Bug 1337858 - Replace CopyFavicon in the Docshell with a proper favicons API. r=standard8
    MozReview-Commit-ID: 8wjSbj0FTwE

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -3826,7 +3826,6 @@ function FillHistoryMenu(aParent) {
       let item = existingIndex < children.length ?
                    children[existingIndex] : document.createElement("menuitem");
 
-      let entryURI = Services.io.newURI(entry.url, entry.charset);
       item.setAttribute("uri", uri);
       item.setAttribute("label", entry.title || uri);
       item.setAttribute("index", j);
@@ -3835,12 +3834,8 @@ function FillHistoryMenu(aParent) {
       item.setAttribute("historyindex", j - index);
 
       if (j != index) {
-        PlacesUtils.favicons.getFaviconURLForPage(entryURI, function(aURI) {
-          if (aURI) {
-            let iconURL = PlacesUtils.favicons.getFaviconLinkForIcon(aURI).spec;
-            item.style.listStyleImage = "url(" + iconURL + ")";
-          }
-        });
+        item.setAttribute("image",
+                          PlacesUtils.urlWithSizeRef(window, "page-icon:" + uri, 16));
       }
 
       if (j < index) {
