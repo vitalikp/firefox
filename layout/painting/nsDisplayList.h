4e7297805a6cbd39e84b01d90d1ddb7766367ea6
cku <cku@mozilla.com>
2017-05-18 22:03:45 +0200

    Bug 1351440 - Part 3. Pass imgDrawingParams to nsSVGMaskFrame::GetMaskForMaskedFrame. r=jwatt
    The reason of this change is the same as for Part 2, except this commit fixes
    nsSVGMaskFrame::GetMaskForMaskedFrame rather than PaintSVG.
    
    MozReview-Commit-ID: DS0eG6eKDgs

--- layout/painting/nsDisplayList.h
+++ layout/painting/nsDisplayList.h
@@ -4245,7 +4245,7 @@ public:
   /*
    * Paint mask onto aMaskContext in mFrame's coordinate space.
    */
-  bool PaintMask(nsDisplayListBuilder* aBuilder, gfxContext* aMaskContext);
+  void PaintMask(nsDisplayListBuilder* aBuilder, gfxContext* aMaskContext);
 
   const nsTArray<nsRect>& GetDestRects()
   {
