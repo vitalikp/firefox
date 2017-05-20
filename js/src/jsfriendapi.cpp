6fb0efaf12e456914df018a5fd5e78d83d54fe03
Kris Maglione <maglione.k@gmail.com>
2017-05-20 11:54:51 -0700

    Bug 1354733: Part 1 - Allow creating DeadObjectProxies directly. r=till
    MozReview-Commit-ID: HnFgiRcA7l5

--- js/src/jsfriendapi.cpp
+++ js/src/jsfriendapi.cpp
@@ -560,6 +560,12 @@ JS_IsDeadWrapper(JSObject* obj)
     return IsDeadProxyObject(obj);
 }
 
+JS_FRIEND_API(JSObject*)
+JS_NewDeadWrapper(JSContext* cx, JSObject* origObj)
+{
+    return NewDeadProxyObject(cx, origObj);
+}
+
 void
 js::TraceWeakMaps(WeakMapTracer* trc)
 {
