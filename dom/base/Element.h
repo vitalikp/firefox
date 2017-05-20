cb2e63f01040866fad4dc7ac9baa43385e973ef1
Emilio Cobos Álvarez <emilio@crisal.io>
2017-05-20 13:02:43 +0200

    Bug 1366474: While we're here, devirtualize GetSMILOverrideStyle too. r=birtles
    Also, update an outdated comment that was leftover.
    
    MozReview-Commit-ID: CC865rj3o3S

--- dom/base/Element.h
+++ dom/base/Element.h
@@ -321,7 +321,7 @@ public:
    * Note: This method is analogous to the 'GetStyle' method in
    * nsGenericHTMLElement and nsStyledElement.
    */
-  virtual nsICSSDeclaration* GetSMILOverrideStyle();
+  nsICSSDeclaration* GetSMILOverrideStyle();
 
   /**
    * Returns if the element is labelable as per HTML specification.
