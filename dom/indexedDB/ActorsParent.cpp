305eda80788c56f88c6f06844f06890db41cee64
Luke Wagner <luke@mozilla.com>
2017-05-24 10:23:02 -0500

    Bug 1351488 - Baldr: remove unintentional shadowing of 'rv' (r=janv)
    MozReview-Commit-ID: 3oggzFUoqYP

--- dom/indexedDB/ActorsParent.cpp
+++ dom/indexedDB/ActorsParent.cpp
@@ -19881,8 +19881,7 @@ DatabaseOperationBase::GetStructuredCloneReadInfoFromSource(
   } else {
     const uint8_t* blobData;
     uint32_t blobDataLength;
-    nsresult rv =
-      aSource->GetSharedBlob(aDataIndex, &blobDataLength, &blobData);
+    rv = aSource->GetSharedBlob(aDataIndex, &blobDataLength, &blobData);
     if (NS_WARN_IF(NS_FAILED(rv))) {
       return rv;
     }
