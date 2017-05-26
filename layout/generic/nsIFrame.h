c42921bb7abc3affe0c3b598550c130c6edfead8
Mats Palmgren <mats@mozilla.com>
2017-05-26 12:11:11 +0200

    Bug 1364805 part 3 - Implement Type() by indexing a static array using the mClass field. Remove the nsIFrame::mType field. r=jfkthame
    In the nsWindowMemoryReporter.cpp / nsArenaMemoryStats.h I'm only
    including the concrete frame classes now - we obviously never have
    instances of the other IDs so there's no need to collect stats for them.
    
    MozReview-Commit-ID: 48uFCZ3xKBC

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -615,7 +615,6 @@ public:
     , mNextSibling(nullptr)
     , mPrevSibling(nullptr)
     , mState(NS_FRAME_FIRST_REFLOW | NS_FRAME_IS_DIRTY)
-    , mType(aType)
     , mClass(aID)
     , mMayHaveRoundedCorners(false)
     , mHasImageRequest(false)
@@ -2656,12 +2655,15 @@ public:
    *
    * @see mozilla::LayoutFrameType
    */
-  mozilla::LayoutFrameType Type() const { return mType; }
+  mozilla::LayoutFrameType Type() const {
+    MOZ_ASSERT(uint8_t(mClass) < mozilla::ArrayLength(sLayoutFrameTypes));
+    return sLayoutFrameTypes[uint8_t(mClass)];
+  }
 
 #define FRAME_TYPE(name_)                                                      \
   bool Is##name_##Frame() const                                                \
   {                                                                            \
-    return mType == mozilla::LayoutFrameType::name_;                           \
+    return Type() == mozilla::LayoutFrameType::name_;                          \
   }
 #include "mozilla/FrameTypeList.h"
 #undef FRAME_TYPE
@@ -3865,9 +3867,6 @@ protected:
   /** @see GetWritingMode() */
   mozilla::WritingMode mWritingMode;
 
-  /** The type of the frame. */
-  mozilla::LayoutFrameType mType;
-
   /** The ClassID of the concrete class of this instance. */
   ClassID mClass; // 1 byte
 
@@ -4003,6 +4002,15 @@ private:
 
   bool HasOpacityInternal(float aThreshold,
                           mozilla::EffectSet* aEffectSet = nullptr) const;
+
+  // Maps mClass to LayoutFrameType.
+  static const mozilla::LayoutFrameType sLayoutFrameTypes[
+#define FRAME_ID(...) 1 +
+#define ABSTRACT_FRAME_ID(...)
+#include "nsFrameIdList.h"
+#undef FRAME_ID
+#undef ABSTRACT_FRAME_ID
+  0];
 
 #ifdef DEBUG_FRAME_DUMP
 public:
