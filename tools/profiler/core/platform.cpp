a0a8e9b9a6b5721e2ca1c04753bb771d67db6433
Nicholas Nethercote <nnethercote@mozilla.com>
2017-05-31 15:14:50 +1000

    Bug 1368915 (part 1) - Rename a parameter of locked_register_thread(). r=mstange.

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -1991,7 +1991,7 @@ FindLiveThreadInfo(PSLockRef aLock, int* aIndexOut = nullptr)
 }
 
 static void
-locked_register_thread(PSLockRef aLock, const char* aName, void* stackTop)
+locked_register_thread(PSLockRef aLock, const char* aName, void* aStackTop)
 {
   MOZ_RELEASE_ASSERT(CorePS::Exists());
 
@@ -2002,7 +2002,7 @@ locked_register_thread(PSLockRef aLock, const char* aName, void* stackTop)
   }
 
   ThreadInfo* info = new ThreadInfo(aName, Thread::GetCurrentId(),
-                                    NS_IsMainThread(), stackTop);
+                                    NS_IsMainThread(), aStackTop);
   TLSInfo::SetInfo(aLock, info);
 
   if (ActivePS::Exists(aLock) && ActivePS::ShouldProfileThread(aLock, info)) {
