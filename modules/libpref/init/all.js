f6b6375119347b5d5e6b08dd3a7d0dcbc2b035f2
Lars T Hansen <lhansen@mozilla.com>
2017-06-02 18:58:29 +0300

    Bug 1391196 - Let javascript.options.wasm_baselinejit default to true. r=luke

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1203,7 +1203,7 @@ pref("javascript.options.ion",              true);
 pref("javascript.options.asmjs",            true);
 pref("javascript.options.wasm",             true);
 pref("javascript.options.wasm_ionjit",      true);
-pref("javascript.options.wasm_baselinejit", false);
+pref("javascript.options.wasm_baselinejit", true);
 pref("javascript.options.native_regexp",    true);
 pref("javascript.options.parallel_parsing", true);
 #if !defined(RELEASE_OR_BETA) && !defined(ANDROID) && !defined(MOZ_B2G) && !defined(XP_IOS)
