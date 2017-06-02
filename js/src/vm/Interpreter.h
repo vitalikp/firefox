3d6da9b07d75b62c129bff50062680cc5ce8a64b
Ted Campbell <tcampbell@mozilla.com>
2017-06-02 16:12:48 -0400

    Bug 1169745 - Support JSOP_SUPERBASE in Baseline. r=jandem
    MozReview-Commit-ID: QA2j2Wsrs8

--- js/src/vm/Interpreter.h
+++ js/src/vm/Interpreter.h
@@ -591,6 +591,9 @@ FunWithProtoOperation(JSContext* cx, HandleFunction fun, HandleObject parent, Ha
 JSFunction*
 MakeDefaultConstructor(JSContext* cx, HandleScript script, jsbytecode* pc, HandleObject proto);
 
+JSObject*
+HomeObjectSuperBase(JSContext* cx, HandleObject homeObj);
+
 }  /* namespace js */
 
 #endif /* vm_Interpreter_h */
