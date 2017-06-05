8811365ecd232cf27f222c2529267564e35d0b4e
Olli Pettay <Olli.Pettay@helsinki.fi>
2017-06-05 16:49:54 +0300

    Bug 1368493, TimerThread::FindNextFireTimeForCurrentThread should look at timers in order, r=bkelly

--- xpcom/threads/TimerThread.h
+++ xpcom/threads/TimerThread.h
@@ -114,6 +114,11 @@ private:
       // the front of the heap.  We want that to be the earliest timer.
       return aRight->mTimeout < aLeft->mTimeout;
     }
+
+    TimeStamp Timeout() const
+    {
+      return mTimeout;
+    }
   };
 
   nsTArray<UniquePtr<Entry>> mTimers;
