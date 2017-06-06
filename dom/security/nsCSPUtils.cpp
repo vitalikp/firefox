89b8908bb42761a6d35707335675d8e14845f81a
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2017-06-06 09:12:13 +0200

    Bug 1367531: CSP should only check host (not including path) when performing frame ancestors checks. r=dveditz

--- dom/security/nsCSPUtils.cpp
+++ dom/security/nsCSPUtils.cpp
@@ -521,6 +521,7 @@ nsCSPSchemeSrc::toString(nsAString& outStr) const
 nsCSPHostSrc::nsCSPHostSrc(const nsAString& aHost)
   : mHost(aHost)
   , mGeneratedFromSelfKeyword(false)
+  , mWithinFrameAncstorsDir(false)
 {
   ToLowerCase(mHost);
 }
@@ -703,6 +704,11 @@ nsCSPHostSrc::permits(nsIURI* aUri, const nsAString& aNonce, bool aWasRedirected
     nsAutoCString uriPath;
     rv = url->GetFilePath(uriPath);
     NS_ENSURE_SUCCESS(rv, false);
+
+    if (mWithinFrameAncstorsDir) {
+      // no path matching for frame-ancestors to not leak any path information.
+      return true;
+    }
 
     nsString decodedUriPath;
     CSP_PercentDecodeStr(NS_ConvertUTF8toUTF16(uriPath), decodedUriPath);
