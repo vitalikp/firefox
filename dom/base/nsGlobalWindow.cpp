47aea10b8410d6903281b6bb15c5ad33250a0e5b
Boris Zbarsky <bzbarsky@mit.edu>
2017-06-06 21:21:44 -0400

    Bug 1364816 part 2. Add a way to ask the WebIDLGlobalNameHash for only the names that have not yet been resolved on the given window. r=qdot

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -4790,7 +4790,8 @@ nsGlobalWindow::GetOwnPropertyNames(JSContext* aCx, nsTArray<nsString>& aNames,
   if (nameSpaceManager) {
     JS::Rooted<JSObject*> wrapper(aCx, GetWrapper());
 
-    WebIDLGlobalNameHash::GetNames(aCx, wrapper, aNames);
+    WebIDLGlobalNameHash::GetNames(aCx, wrapper, WebIDLGlobalNameHash::AllNames,
+                                   aNames);
 
     for (auto i = nameSpaceManager->GlobalNameIter(); !i.Done(); i.Next()) {
       const GlobalNameMapEntry* entry = i.Get();
