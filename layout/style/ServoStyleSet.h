566aa9712f7f9fbd2f43f741acf91a71aeaec60d
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2017-06-09 06:19:37 +0900

    Bug 1358966 - Use Servo_GetAnimationValues. r=birtles
    MozReview-Commit-ID: 7ag2IFUViDX

--- layout/style/ServoStyleSet.h
+++ layout/style/ServoStyleSet.h
@@ -380,6 +380,12 @@ public:
                                dom::Element* aElement,
                                ServoComputedValuesBorrowed aComputedValues);
 
+  void
+  GetAnimationValues(RawServoDeclarationBlock* aDeclarations,
+                     dom::Element* aElement,
+                     ServoComputedValuesBorrowed aComputedValues,
+                     nsTArray<RefPtr<RawServoAnimationValue>>& aAnimationValues);
+
   bool AppendFontFaceRules(nsTArray<nsFontFaceRuleContainer>& aArray);
 
   nsCSSCounterStyleRule* CounterStyleRuleForName(nsIAtom* aName);
