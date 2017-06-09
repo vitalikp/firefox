8a8fbc14cca288a6ac6d4c9e5d82e0455844dc19
JW Wang <jwwang@mozilla.com>
2017-06-09 11:27:42 +0800

    Bug 1371514. P2 - remove MediaDecoder::mStateMachineIsShutdown. r=gerald
    MozReview-Commit-ID: Dv2pdHW2tgI

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -394,7 +394,6 @@ MediaDecoder::MediaDecoder(MediaDecoderInit& aInit)
   , mForcedHidden(false)
   , mHasSuspendTaint(aInit.mHasSuspendTaint)
   , mPlaybackRate(aInit.mPlaybackRate)
-  , INIT_MIRROR(mStateMachineIsShutdown, true)
   , INIT_MIRROR(mBuffered, TimeIntervals())
   , INIT_MIRROR(mNextFrameStatus, MediaDecoderOwner::NEXT_FRAME_UNAVAILABLE)
   , INIT_MIRROR(mCurrentPosition, TimeUnit::Zero())
@@ -1459,7 +1458,6 @@ MediaDecoder::ConnectMirrors(MediaDecoderStateMachine* aObject)
   MOZ_ASSERT(aObject);
   mStateMachineDuration.Connect(aObject->CanonicalDuration());
   mBuffered.Connect(aObject->CanonicalBuffered());
-  mStateMachineIsShutdown.Connect(aObject->CanonicalIsShutdown());
   mNextFrameStatus.Connect(aObject->CanonicalNextFrameStatus());
   mCurrentPosition.Connect(aObject->CanonicalCurrentPosition());
   mPlaybackPosition.Connect(aObject->CanonicalPlaybackOffset());
@@ -1472,7 +1470,6 @@ MediaDecoder::DisconnectMirrors()
   MOZ_ASSERT(NS_IsMainThread());
   mStateMachineDuration.DisconnectIfConnected();
   mBuffered.DisconnectIfConnected();
-  mStateMachineIsShutdown.DisconnectIfConnected();
   mNextFrameStatus.DisconnectIfConnected();
   mCurrentPosition.DisconnectIfConnected();
   mPlaybackPosition.DisconnectIfConnected();
--- dom/media/MediaDecoder.h
+++ dom/media/MediaDecoder.h
@@ -745,9 +745,6 @@ protected:
   // PlaybackRate and pitch preservation status we should start at.
   double mPlaybackRate;
 
-  // Whether the state machine is shut down.
-  Mirror<bool> mStateMachineIsShutdown;
-
   // Buffered range, mirrored from the reader.
   Mirror<media::TimeIntervals> mBuffered;
 
