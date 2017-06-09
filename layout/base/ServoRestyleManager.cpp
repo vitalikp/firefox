6d3b55f758001f8eed760dd1f0cf01cb0d798a07
Emilio Cobos Álvarez <emilio@crisal.io>
2017-06-09 17:16:29 +0200

    Bug 1368240: Record whether an snapshot is recording a class attribute change or id change. r=heycam
    I'll use this information in order to get fewer dependencies out of the
    dependency set.
    
    MozReview-Commit-ID: 5HlmKmSNO8p

--- layout/base/ServoRestyleManager.cpp
+++ layout/base/ServoRestyleManager.cpp
@@ -896,7 +896,7 @@ ServoRestyleManager::AttributeWillChange(Element* aElement,
   }
 
   ServoElementSnapshot& snapshot = SnapshotFor(aElement);
-  snapshot.AddAttrs(aElement);
+  snapshot.AddAttrs(aElement, aNameSpaceID, aAttribute);
 
   if (AttributeInfluencesOtherPseudoClassState(aElement, aAttribute)) {
     snapshot.AddOtherPseudoClassState(aElement);
