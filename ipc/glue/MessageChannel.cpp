3fdbfb055b8148ef4216c1bd4327fb24da990a0e
Bill McCloskey <billm@mozilla.com>
2017-06-09 14:34:11 -0700

    Bug 1366072 - Remove MessageLoopAbstractThreadWrapper (r=kanru)
    MozReview-Commit-ID: 8i9QTIRbP3j

--- ipc/glue/MessageChannel.cpp
+++ ipc/glue/MessageChannel.cpp
@@ -7,8 +7,6 @@
 
 #include "mozilla/ipc/MessageChannel.h"
 
-#include "MessageLoopAbstractThreadWrapper.h"
-#include "mozilla/AbstractThread.h"
 #include "mozilla/Assertions.h"
 #include "mozilla/DebugOnly.h"
 #include "mozilla/dom/ScriptSettings.h"
@@ -712,23 +710,6 @@ MessageChannel::Clear()
     }
 }
 
-class AbstractThreadWrapperCleanup : public MessageLoop::DestructionObserver
-{
-public:
-    explicit AbstractThreadWrapperCleanup(already_AddRefed<AbstractThread> aWrapper)
-        : mWrapper(aWrapper)
-    {}
-    virtual ~AbstractThreadWrapperCleanup() override {}
-    virtual void WillDestroyCurrentMessageLoop() override
-    {
-        mWrapper = nullptr;
-        MessageLoop::current()->RemoveDestructionObserver(this);
-        delete this;
-    }
-private:
-    RefPtr<AbstractThread> mWrapper;
-};
-
 bool
 MessageChannel::Open(Transport* aTransport, MessageLoop* aIOLoop, Side aSide)
 {
@@ -739,13 +720,6 @@ MessageChannel::Open(Transport* aTransport, MessageLoop* aIOLoop, Side aSide)
     mWorkerThread = GetCurrentVirtualThread();
     mWorkerLoop->AddDestructionObserver(this);
     mListener->SetIsMainThreadProtocol();
-
-    if (!AbstractThread::GetCurrent()) {
-        mWorkerLoop->AddDestructionObserver(
-            new AbstractThreadWrapperCleanup(
-                MessageLoopAbstractThreadWrapper::Create(mWorkerLoop)));
-    }
-
 
     ProcessLink *link = new ProcessLink(this);
     link->Open(aTransport, aIOLoop, aSide); // :TODO: n.b.: sets mChild
@@ -825,12 +799,6 @@ MessageChannel::CommonThreadOpenInit(MessageChannel *aTargetChan, Side aSide)
     mWorkerThread = GetCurrentVirtualThread();
     mWorkerLoop->AddDestructionObserver(this);
     mListener->SetIsMainThreadProtocol();
-
-    if (!AbstractThread::GetCurrent()) {
-        mWorkerLoop->AddDestructionObserver(
-            new AbstractThreadWrapperCleanup(
-                MessageLoopAbstractThreadWrapper::Create(mWorkerLoop)));
-    }
 
     mLink = new ThreadLink(this, aTargetChan);
     mSide = aSide;
