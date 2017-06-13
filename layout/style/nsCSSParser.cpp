0bbd47b48bbf662d4998227af3ed5ff949890666
Xidorn Quan <me@upsuper.org>
2017-06-13 16:00:37 +1000

    Bug 1372488 - Make counter style 'disc' not overridable. r=dbaron
    MozReview-Commit-ID: 11ZpZtRQSUF

--- layout/style/nsCSSParser.cpp
+++ layout/style/nsCSSParser.cpp
@@ -4808,6 +4808,7 @@ CSSParserImpl::ParseCounterStyleName(bool aForDefinition)
   static const nsCSSKeyword kReservedNames[] = {
     eCSSKeyword_none,
     eCSSKeyword_decimal,
+    eCSSKeyword_disc,
     eCSSKeyword_UNKNOWN
   };
 
