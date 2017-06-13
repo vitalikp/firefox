ee74cbaa75dbd88ebe7c4c70841b8a99cf6c5656
Bevis Tseng <btseng@mozilla.com>
2017-06-13 18:10:13 +0800

    Bug 1372453 - Part 1: Support to name the callers of ProxyReleaseEvent. r=froydnj
    MozReview-Commit-ID: 8rmwqzPrzM7

--- xpcom/threads/nsThreadUtils.h
+++ xpcom/threads/nsThreadUtils.h
@@ -338,14 +338,15 @@ public:
 
 protected:
   virtual ~Runnable() {}
-private:
-  Runnable(const Runnable&) = delete;
-  Runnable& operator=(const Runnable&) = delete;
-  Runnable& operator=(const Runnable&&) = delete;
 
 #ifndef RELEASE_OR_BETA
   const char* mName = nullptr;
 #endif
+
+private:
+  Runnable(const Runnable&) = delete;
+  Runnable& operator=(const Runnable&) = delete;
+  Runnable& operator=(const Runnable&&) = delete;
 };
 
 // This class is designed to be subclassed.
