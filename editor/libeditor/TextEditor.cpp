6c58cecf238dc44cb6baf5131bf27d5002eccf94
Henri Sivonen <hsivonen@hsivonen.fi>
2017-06-15 10:35:22 +0300

    Bug 1372994 - Clear up leftover ISO-8859-1 encoding names. r=emk
    MozReview-Commit-ID: IBuUPa9H9Q2

--- editor/libeditor/TextEditor.cpp
+++ editor/libeditor/TextEditor.cpp
@@ -1357,7 +1357,7 @@ TextEditor::OutputToString(const nsAString& aFormatType,
   nsAutoCString charsetStr;
   rv = GetDocumentCharacterSet(charsetStr);
   if (NS_FAILED(rv) || charsetStr.IsEmpty()) {
-    charsetStr.AssignLiteral("ISO-8859-1");
+    charsetStr.AssignLiteral("windows-1252");
   }
 
   nsCOMPtr<nsIDocumentEncoder> encoder =
