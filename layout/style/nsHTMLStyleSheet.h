d6998935a5160ceb0a16d3366302c116bcec82dd
Emilio Cobos Álvarez <emilio@crisal.io>
2017-06-16 23:59:08 +0200

    Bug 1370802: Make the lang rule operate on atoms, not strings. r=heycam
    MozReview-Commit-ID: KG9i4Mc3ovN

--- layout/style/nsHTMLStyleSheet.h
+++ layout/style/nsHTMLStyleSheet.h
@@ -14,6 +14,7 @@
 
 #include "nsColor.h"
 #include "nsCOMPtr.h"
+#include "nsIAtom.h"
 #include "nsIStyleRule.h"
 #include "nsIStyleRuleProcessor.h"
 #include "PLDHashTable.h"
@@ -67,9 +68,9 @@ public:
   // and converting the ruledata to Servo specified values.
   void CalculateMappedServoDeclarations(nsPresContext* aPresContext);
 
-  nsIStyleRule* LangRuleFor(const nsString& aLanguage);
+  nsIStyleRule* LangRuleFor(const nsIAtom* aLanguage);
 
-private: 
+private:
   nsHTMLStyleSheet(const nsHTMLStyleSheet& aCopy) = delete;
   nsHTMLStyleSheet& operator=(const nsHTMLStyleSheet& aCopy) = delete;
 
@@ -156,7 +157,7 @@ public: // for mLangRuleTable structures only
   private:
     ~LangRule() {}
   public:
-    explicit LangRule(const nsAString& aLang) : mLang(aLang) {}
+    explicit LangRule(nsIAtom* aLang) : mLang(aLang) {}
 
     NS_DECL_ISUPPORTS
 
@@ -169,7 +170,7 @@ public: // for mLangRuleTable structures only
     virtual void List(FILE* out = stdout, int32_t aIndent = 0) const override;
   #endif
 
-    nsString mLang;
+    nsCOMPtr<nsIAtom> mLang;
   };
 
 private:
