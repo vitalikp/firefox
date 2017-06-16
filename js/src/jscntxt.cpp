d4d2fb46e6be18e21f696bfc9b98e6264a6ca8fc
Jan de Mooij <jdemooij@mozilla.com>
2017-06-16 10:40:57 +0200

    Bug 1368325 - Remove JSContext::allowGCBarriers and related code as it's not used anywhere. r=billm

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -1156,7 +1156,6 @@ JSContext::JSContext(JSRuntime* runtime, const JS::ContextOptions& options)
     dtoaState(nullptr),
     heapState(JS::HeapState::Idle),
     suppressGC(0),
-    allowGCBarriers(true),
 #ifdef DEBUG
     ionCompiling(false),
     ionCompilingSafeForMinorGC(false),
