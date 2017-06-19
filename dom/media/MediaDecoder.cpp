5d06b4f6ef6cb7b245eaed50468df8845f7fb205
JW Wang <jwwang@mozilla.com>
2017-06-19 14:24:40 +0800

    Bug 1374184. P2 - merge NotifyDataArrived() and NotifyDownloadProgressed(). r=gerald
    We also move NotifyDownloadProgressed() to private
    since it won't be called outside MediaDecoder.
    
    MozReview-Commit-ID: GISbJEW7wwx

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -1558,6 +1558,7 @@ void
 MediaDecoder::NotifyDataArrived()
 {
   NotifyDataArrivedInternal();
+  NotifyDownloadProgressed();
 }
 
 // Provide access to the state machine object
--- dom/media/MediaDecoder.h
+++ dom/media/MediaDecoder.h
@@ -254,10 +254,6 @@ public:
   // Return true if the stream is infinite (see SetInfinite).
   bool IsInfinite() const;
 
-  // Called by MediaResource when some data has been received.
-  // Call on the main thread only.
-  void NotifyDownloadProgressed();
-
   // Called as data arrives on the stream and is read into the cache.  Called
   // on the main thread only.
   void NotifyDataArrived();
@@ -587,6 +583,10 @@ protected:
 
 private:
   void NotifyDataArrivedInternal();
+
+  // Called by MediaResource when some data has been received.
+  // Call on the main thread only.
+  void NotifyDownloadProgressed();
 
   nsCString GetDebugInfo();
 
