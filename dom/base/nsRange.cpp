acd3e39a4d0cce6b949fa36d158d361b58d70df1
Masayuki Nakano <masayuki@d-toybox.com>
2017-06-23 13:21:47 +0900

    Bug 1375502 - part1: ContentEventHandler shouldn't use nsRange for temporary use r=mats
    Allocating and initializing nsRange is too expensive especially for temporary
    use.  However, ContentEventHandler uses nsRange only for representing two DOM
    points.  So, it should use simpler helper class, RawRange, for reducing some
    unnecessary runtime cost.
    
    Note that this still uses nsRange for initializing nsIContentIterator.  This
    will be fixed by the following patch.
    
    MozReview-Commit-ID: 5TUy6yJf7HA

--- dom/base/nsRange.cpp
+++ dom/base/nsRange.cpp
@@ -1197,8 +1197,9 @@ nsRange::IsValidOffset(nsINode* aNode, uint32_t aOffset)
          static_cast<size_t>(aOffset) <= aNode->Length();
 }
 
+/* static */
 nsINode*
-nsRange::IsValidBoundary(nsINode* aNode)
+nsRange::ComputeRootNode(nsINode* aNode, bool aMaySpanAnonymousSubtrees)
 {
   if (!aNode) {
     return nullptr;
@@ -1211,7 +1212,7 @@ nsRange::IsValidBoundary(nsINode* aNode)
 
     nsIContent* content = static_cast<nsIContent*>(aNode);
 
-    if (!mMaySpanAnonymousSubtrees) {
+    if (!aMaySpanAnonymousSubtrees) {
       // If the node is in a shadow tree then the ShadowRoot is the root.
       ShadowRoot* containingShadow = content->GetContainingShadow();
       if (containingShadow) {
