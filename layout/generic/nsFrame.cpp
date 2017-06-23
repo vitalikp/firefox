347a83aebba465e66a65f3cc2333a55140224e36
Emilio Cobos Álvarez <emilio@crisal.io>
2017-06-23 19:30:16 +0200

    Bug 1375674: Don't use the table change hints for the table wrapper frame. r=heycam
    MozReview-Commit-ID: 7Qe07N6zvIt

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -10252,7 +10252,7 @@ nsIFrame::UpdateStyleOfOwnedChildFrame(nsIFrame* aChildFrame,
   //
   // 1) Even if all the child's changes are due to properties it inherits from
   //    us, it's possible that no one ever asked us for those style structs and
-  //    hence changes to them aren't reflected in aHintForThisFrame at all.
+  //    hence changes to them aren't reflected in the changes handled at all.
   //
   // 2) Content can change stylesheets that change the styles of pseudos, and
   //    extensions can add/remove stylesheets that change the styles of
