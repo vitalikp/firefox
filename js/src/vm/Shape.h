bfb49e45362896141bf68b5151c04418ad1f8955
Jon Coppeard <jcoppeard@mozilla.com>
2017-06-26 14:44:14 -0700

    Bug 1376101 - Remove some more unnecessary read barriers r=sfink

--- js/src/vm/Shape.h
+++ js/src/vm/Shape.h
@@ -1273,7 +1273,8 @@ struct InitialShapeEntry
         {}
 
         explicit Lookup(const InitialShapeEntry& entry)
-          : proto(entry.proto)
+          : proto(entry.proto.key(),
+                  entry.proto.proto().unbarrieredGet())
         {
             const Shape* shape = entry.shape.unbarrieredGet();
             clasp = shape->getObjectClass();
