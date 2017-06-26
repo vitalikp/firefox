f75190c4c8280242f2280688c4f2a5df78126421
Jon Coppeard <jcoppeard@mozilla.com>
2017-06-26 09:46:54 -0700

    Bug 1376101 - Remove some unnecessary read barriers r=sfink

--- js/src/gc/Heap.h
+++ js/src/gc/Heap.h
@@ -1300,6 +1300,7 @@ TenuredCell::readBarrier(TenuredCell* thing)
 {
     MOZ_ASSERT(!CurrentThreadIsIonCompiling());
     MOZ_ASSERT(thing);
+    MOZ_ASSERT(CurrentThreadCanAccessZone(thing->zoneFromAnyThread()));
 
     // It would be good if barriers were never triggered during collection, but
     // at the moment this can happen e.g. when rekeying tables containing
