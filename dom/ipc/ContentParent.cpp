cb6fb8e1a1ba707e6cbfd986289b71247608ac96
Mats Palmgren <mats@mozilla.com>
2017-06-28 01:03:14 +0200

    Bug 1376293 part 1 - Remove an unnecessary GetWeak call before Remove, and use Lookup instead of GetWeak+Remove, to avoid unnecessary hashtable lookups. r=froydnj
    MozReview-Commit-ID: HaVrGEDB41i

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -4793,11 +4793,7 @@ ContentParent::RecvGetFilesRequest(const nsID& aUUID,
 mozilla::ipc::IPCResult
 ContentParent::RecvDeleteGetFilesRequest(const nsID& aUUID)
 {
-  GetFilesHelper* helper = mGetFilesPendingRequests.GetWeak(aUUID);
-  if (helper) {
-    mGetFilesPendingRequests.Remove(aUUID);
-  }
-
+  mGetFilesPendingRequests.Remove(aUUID);
   return IPC_OK();
 }
 
@@ -4805,9 +4801,8 @@ void
 ContentParent::SendGetFilesResponseAndForget(const nsID& aUUID,
                                              const GetFilesResponseResult& aResult)
 {
-  GetFilesHelper* helper = mGetFilesPendingRequests.GetWeak(aUUID);
-  if (helper) {
-    mGetFilesPendingRequests.Remove(aUUID);
+  if (auto entry = mGetFilesPendingRequests.Lookup(aUUID)) {
+    entry.Remove();
     Unused << SendGetFilesResponse(aUUID, aResult);
   }
 }
