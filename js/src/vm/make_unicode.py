3b969f722f04f6a58314834256e2eb1162fccc9c
Jan de Mooij <jdemooij@mozilla.com>
2017-06-27 12:03:10 -0700

    Bug 1373195 - Speed up TextNode::MakeCaseIndependent for the unicode '.' character class ranges. r=arai

--- js/src/vm/make_unicode.py
+++ js/src/vm/make_unicode.py
@@ -1482,6 +1482,7 @@ def make_irregexp_tables(version,
         character_range('Surrogate', surrogate_chars)
 
         character_range('LineTerminator', line_terminator)
+        character_range('LineTerminatorAndSurrogate', line_terminator + surrogate_chars)
 
 def update_unicode(args):
     import urllib2
