a0147565cd490d9371bfdd93d19cdc61a12fe688
Ted Campbell <tcampbell@mozilla.com>
2017-06-28 14:59:36 -0400

    Bug 1376547 - JITs should throw if ClassConstructor used without new. r=jandem
    MozReview-Commit-ID: mqAG3F7Mbi

--- js/src/jit/CodeGenerator.cpp
+++ js/src/jit/CodeGenerator.cpp
@@ -4559,6 +4559,10 @@ CodeGenerator::emitApplyGeneric(T* apply)
     // Guard that calleereg is an interpreted function with a JSScript.
     masm.branchIfFunctionHasNoScript(calleereg, &invoke);
 
+    // Guard that calleereg is not a class constrcuctor
+    masm.branchFunctionKind(Assembler::Equal, JSFunction::ClassConstructor,
+                            calleereg, objreg, &invoke);
+
     // Knowing that calleereg is a non-native function, load the JSScript.
     masm.loadPtr(Address(calleereg, JSFunction::offsetOfNativeOrScript()), objreg);
 
