b8624e1198fc8691030fefaf0e82a5b942b877f7
Lars T Hansen <lhansen@mozilla.com>
2017-06-29 15:37:01 -0700

    Bug 1277562 - Part 16b: Implement javascript.options.wasm_ionjit. r=billm

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1235,6 +1235,7 @@ pref("javascript.options.baselinejit",      true);
 pref("javascript.options.ion",              true);
 pref("javascript.options.asmjs",            true);
 pref("javascript.options.wasm",             true);
+pref("javascript.options.wasm_ionjit",      true);
 pref("javascript.options.wasm_baselinejit", false);
 pref("javascript.options.native_regexp",    true);
 pref("javascript.options.parallel_parsing", true);
