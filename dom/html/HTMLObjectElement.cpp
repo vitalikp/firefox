3b01b00903b19f908b1150c50db0b818ef7f69c4
Kyle Machulis <kyle@nonpolynomial.com>
2017-07-10 16:00:30 -0700

    Bug 1279218 - Remove Applet tag; r=bz
    Removes applet tag interfaces, and changes HTML5 parser to output
    HTMLUnknownElement when tag is found. Removes tag process from various
    places in the browser.
    
    MozReview-Commit-ID: 2zHhK2U2esX

--- dom/html/HTMLObjectElement.cpp
+++ dom/html/HTMLObjectElement.cpp
@@ -556,7 +556,7 @@ HTMLObjectElement::IntrinsicState() const
 uint32_t
 HTMLObjectElement::GetCapabilities() const
 {
-  return nsObjectLoadingContent::GetCapabilities() | eSupportClassID;
+  return nsObjectLoadingContent::GetCapabilities();
 }
 
 void
