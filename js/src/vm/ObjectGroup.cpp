0299477a5176bd1ba4811f192f2a62a1df986eda
Jon Coppeard <jcoppeard@mozilla.com>
2017-07-10 18:25:40 +0100

    Bug 1367795 - Specialise JS::WeakCache for GCHashSet so we can add barriers r=sfink

--- js/src/vm/ObjectGroup.cpp
+++ js/src/vm/ObjectGroup.cpp
@@ -1621,7 +1621,7 @@ ObjectGroupCompartment::removeDefaultNewGroup(const Class* clasp, TaggedProto pr
     auto p = defaultNewTable->lookup(NewEntry::Lookup(clasp, proto, associated));
     MOZ_RELEASE_ASSERT(p);
 
-    defaultNewTable->get().remove(p);
+    defaultNewTable->remove(p);
     defaultNewGroupCache.purge();
 }
 
@@ -1633,7 +1633,7 @@ ObjectGroupCompartment::replaceDefaultNewGroup(const Class* clasp, TaggedProto p
 
     auto p = defaultNewTable->lookup(lookup);
     MOZ_RELEASE_ASSERT(p);
-    defaultNewTable->get().remove(p);
+    defaultNewTable->remove(p);
     defaultNewGroupCache.purge();
     {
         AutoEnterOOMUnsafeRegion oomUnsafe;
