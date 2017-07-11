f40371646d8e9d974419ba0d3aed1aa39ccb6749
Mike Hommey <mh+mozilla@glandium.org>
2017-07-11 14:24:35 +0900

    Bug 1379897 - Remove opt_abort. r=njn
    It causes abort() on errors from munmap/VirtualFree (which in practice
    don't happen except maybe in case of memory corruption), and was only
    enabled on debug builds.

--- memory/mozjemalloc/mozjemalloc.cpp
+++ memory/mozjemalloc/mozjemalloc.cpp
@@ -1004,11 +1004,9 @@ const uint8_t kAllocJunk = 0xe4;
 const uint8_t kAllocPoison = 0xe5;
 
 #ifdef MOZ_DEBUG
-static bool	opt_abort = true;
 static bool	opt_junk = true;
 static bool	opt_zero = false;
 #else
-static bool	opt_abort = false;
 static const bool	opt_junk = false;
 static const bool	opt_zero = false;
 #endif
@@ -1523,8 +1521,6 @@ pages_unmap(void *addr, size_t size)
 	if (VirtualFree(addr, 0, MEM_RELEASE) == 0) {
 		_malloc_message(_getprogname(),
 		    ": (malloc) Error in VirtualFree()\n");
-		if (opt_abort)
-			moz_abort();
 	}
 }
 #else
@@ -1589,8 +1585,6 @@ pages_map(void *addr, size_t size)
 				_malloc_message(_getprogname(),
 					": (malloc) Error in munmap(): ", buf, "\n");
 			}
-			if (opt_abort)
-				moz_abort();
 		}
 		ret = nullptr;
 	}
@@ -1619,8 +1613,6 @@ pages_unmap(void *addr, size_t size)
 			_malloc_message(_getprogname(),
 				": (malloc) Error in munmap(): ", buf, "\n");
 		}
-		if (opt_abort)
-			moz_abort();
 	}
 }
 #endif
@@ -3930,8 +3922,6 @@ arenas_fallback()
 	 */
 	_malloc_message(_getprogname(),
 	    ": (malloc) Error initializing arena\n");
-	if (opt_abort)
-		moz_abort();
 
 	return arenas[0];
 }
@@ -4301,12 +4291,6 @@ MALLOC_OUT:
 
 			for (j = 0; j < nreps; j++) {
 				switch (opts[i]) {
-				case 'a':
-					opt_abort = false;
-					break;
-				case 'A':
-					opt_abort = true;
-					break;
 				case 'f':
 					opt_dirty_max >>= 1;
 					break;
@@ -4762,7 +4746,6 @@ jemalloc_stats_impl(jemalloc_stats_t *stats)
 	/*
 	 * Gather runtime settings.
 	 */
-	stats->opt_abort = opt_abort;
 	stats->opt_junk = opt_junk;
 	stats->opt_zero = opt_zero;
 	stats->narenas = narenas;
