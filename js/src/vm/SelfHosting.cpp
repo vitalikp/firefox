2259fd8ef9c05a02a126929529d40cc9635d5fc6
André Bargull <andre.bargull@gmail.com>
2017-07-11 11:56:01 -0700

    Bug 1380087 - Add an intrinsic to test for Map and Set objects. r=till

--- js/src/vm/SelfHosting.cpp
+++ js/src/vm/SelfHosting.cpp
@@ -2370,11 +2370,14 @@ static const JSFunctionSpec intrinsic_functions[] = {
           CallNonGenericSelfhostedMethod<Is<StarGeneratorObject>>, 2, 0),
 
     JS_INLINABLE_FN("GuardToMapObject", intrinsic_GuardToBuiltin<MapObject>, 1, 0,
-          IntrinsicGuardToMapObject),
+                    IntrinsicGuardToMapObject),
+    JS_FN("CallMapMethodIfWrapped", CallNonGenericSelfhostedMethod<Is<MapObject>>, 2, 0),
     JS_FN("IsWeakSet", intrinsic_IsInstanceOfBuiltin<WeakSetObject>, 1,0),
 
     JS_INLINABLE_FN("GuardToSetObject", intrinsic_GuardToBuiltin<SetObject>, 1, 0,
-          IntrinsicGuardToSetObject),
+                    IntrinsicGuardToSetObject),
+    JS_FN("CallSetMethodIfWrapped", CallNonGenericSelfhostedMethod<Is<SetObject>>, 2, 0),
+
     JS_FN("CallWeakSetMethodIfWrapped",
           CallNonGenericSelfhostedMethod<Is<WeakSetObject>>, 2, 0),
 
