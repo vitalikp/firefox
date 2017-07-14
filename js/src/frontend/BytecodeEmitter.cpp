bdaf9af2c89de14343fe1a7c19be7174d7f914c6
Tom Tromey <tom@tromey.com>
2017-07-14 13:29:52 -0600

    Bug 1370648 - use final token as end location of statement list; r=jimb
    This changes the parser to use the final token of a statement list as
    it's end location.  This works around some confusing behavior, such as a
    breakpoint firing on the marked line:
    
        <script>
          if (1 !== 1) {
            console.log("dead code!?"); // set breakpoint here
          }
        </script>
    
    MozReview-Commit-ID: 3Sk1ERw5Q6z

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -4894,6 +4894,9 @@ BytecodeEmitter::emitScript(ParseNode* body)
             return false;
     }
 
+    if (!updateSourceCoordNotes(body->pn_pos.end))
+        return false;
+
     if (!emit1(JSOP_RETRVAL))
         return false;
 
