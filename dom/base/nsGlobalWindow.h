0346e4df336ef4dc98afd377f639b534b78586a9
Zibi Braniecki <zbraniecki@mozilla.com>
2017-07-15 02:47:23 +0300

    Bug 1379905 - Switch mozIntl, datetimebox and DateTimeFormat to use GetRegionalPrefsLocales. r=jfkthame,smaug
    MozReview-Commit-ID: IqGbT6ZUfuN

--- dom/base/nsGlobalWindow.h
+++ dom/base/nsGlobalWindow.h
@@ -892,7 +892,7 @@ public:
   GetPaintWorklet(mozilla::ErrorResult& aRv);
 
   void
-  GetAppLocalesAsBCP47(nsTArray<nsString>& aLocales);
+  GetRegionalPrefsLocales(nsTArray<nsString>& aLocales);
 
 #ifdef ENABLE_INTL_API
   mozilla::dom::IntlUtils*
