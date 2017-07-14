4b07c043b82baf1c843ca835d4475b0f34eac187
Boris Zbarsky <bzbarsky@mit.edu>
2017-07-13 23:45:49 -0400

    Bug 1380393 part 1. Remove some unused nsIDOMHTMLElement bits. r=mccr8

--- dom/html/nsGenericHTMLElement.cpp
+++ dom/html/nsGenericHTMLElement.cpp
@@ -215,13 +215,6 @@ nsGenericHTMLElement::CopyInnerTo(Element* aDst, bool aPreallocateChildren)
   return NS_OK;
 }
 
-NS_IMETHODIMP
-nsGenericHTMLElement::GetDataset(nsISupports** aDataset)
-{
-  *aDataset = Dataset().take();
-  return NS_OK;
-}
-
 static const nsAttrValue::EnumTable kDirTable[] = {
   { "ltr", eDir_LTR },
   { "rtl", eDir_RTL },
@@ -364,15 +357,6 @@ nsGenericHTMLElement::GetOffsetRect(CSSIntRect& aRect)
   aRect = CSSIntRect::FromAppUnitsRounded(rcFrame);
 
   return offsetParent ? offsetParent->AsElement() : nullptr;
-}
-
-NS_IMETHODIMP
-nsGenericHTMLElement::InsertAdjacentHTML(const nsAString& aPosition,
-                                         const nsAString& aText)
-{
-  ErrorResult rv;
-  Element::InsertAdjacentHTML(aPosition, aText, rv);
-  return rv.StealNSResult();
 }
 
 bool
@@ -1689,13 +1673,6 @@ nsGenericHTMLElement::GetContextMenu() const
     }
   }
   return nullptr;
-}
-
-NS_IMETHODIMP
-nsGenericHTMLElement::GetContextMenu(nsIDOMHTMLMenuElement** aContextMenu)
-{
-  NS_IF_ADDREF(*aContextMenu = GetContextMenu());
-  return NS_OK;
 }
 
 bool
