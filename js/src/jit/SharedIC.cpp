9c09fa4694de4cc8d4ac9867c6a2bfec44ff7cc3
Jan de Mooij <jdemooij@mozilla.com>
2017-07-17 16:29:52 +0200

    Bug 1381438 - Fix boxDouble to not clobber the source register on x86 CPUs without SSE 4.1. r=evilpie

--- js/src/jit/SharedIC.cpp
+++ js/src/jit/SharedIC.cpp
@@ -1073,7 +1073,7 @@ ICBinaryArith_Double::Compiler::generateStubCode(MacroAssembler& masm)
         MOZ_CRASH("Unexpected op");
     }
 
-    masm.boxDouble(FloatReg0, R0);
+    masm.boxDouble(FloatReg0, R0, FloatReg0);
     EmitReturnFromIC(masm);
 
     // Failure case - jump to next stub
@@ -1330,7 +1330,7 @@ ICUnaryArith_Double::Compiler::generateStubCode(MacroAssembler& masm)
 
     if (op == JSOP_NEG) {
         masm.negateDouble(FloatReg0);
-        masm.boxDouble(FloatReg0, R0);
+        masm.boxDouble(FloatReg0, R0, FloatReg0);
     } else {
         // Truncate the double to an int32.
         Register scratchReg = R1.scratchReg();
