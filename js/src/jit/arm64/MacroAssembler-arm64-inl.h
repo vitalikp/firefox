b26426df750e1bec6e8f4984a0c01cda7c66935c
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2017-07-18 12:08:22 +0000

    Bug 1364908 - Add MacroAssembler::branchToComputedAddress. r=jandem

--- js/src/jit/arm64/MacroAssembler-arm64-inl.h
+++ js/src/jit/arm64/MacroAssembler-arm64-inl.h
@@ -1645,6 +1645,12 @@ MacroAssembler::branchTestMagic(Condition cond, const Address& valaddr, JSWhyMag
     B(label, cond);
 }
 
+void
+MacroAssembler::branchToComputedAddress(const BaseIndex& addr)
+{
+    MOZ_CRASH("branchToComputedAddress");
+}
+
 // ========================================================================
 // Memory access primitives.
 void
