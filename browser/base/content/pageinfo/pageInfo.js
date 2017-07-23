6b88cf1b7988b88fb71e7be42b0d1289504206aa
Jorg K <jorgk@jorgk.com>
2017-07-22 23:18:00 -0400

    Bug 1383463 - Use mozIntl for cookies and page info. r=zibi

--- browser/base/content/pageinfo/pageInfo.js
+++ browser/base/content/pageinfo/pageInfo.js
@@ -1002,9 +1002,10 @@ function formatDate(datestr, unknown) {
   if (!date.valueOf())
     return unknown;
 
-  const dtOptions = { year: "numeric", month: "long", day: "numeric",
-                      hour: "numeric", minute: "numeric", second: "numeric" };
-  return date.toLocaleString(undefined, dtOptions);
+  const dateTimeFormatter = Services.intl.createDateTimeFormat(undefined, {
+    dateStyle: "long", timeStyle: "long"
+  });
+  return dateTimeFormatter.format(date);
 }
 
 function doCopy() {
