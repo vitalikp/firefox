d523411027ac8e37d4e737f61a6590f6a9e008f9
Kris Maglione <maglione.k@gmail.com>
2017-07-25 15:23:12 -0700

    Bug 1382329: Part 3 - Wait for pending parse tasks to finish before freeing scripts. r=erahm,nbp
    Off-thread parse tasks depend on the memory allocated by the main-thread
    script preloader, so that memory needs to be kept alive until they finish. In
    normal use cases, this is guaranteed, but when the browser shuts down very
    quickly (as sometimes happens in tests), or we invalidate the startup caches
    in the middle of the startup process, they can sometimes be freed too early.
    
    MozReview-Commit-ID: GQmkVbWgTH9

--- js/xpconnect/loader/ScriptPreloader.h
+++ js/xpconnect/loader/ScriptPreloader.h
@@ -360,6 +360,7 @@ private:
     void ForceWriteCacheFile();
     void Cleanup();
 
+    void FinishPendingParses(MonitorAutoLock& aMal);
     void InvalidateCache();
 
     // Opens the cache file for reading.
@@ -384,7 +385,7 @@ private:
     void DecodeNextBatch(size_t chunkSize);
 
     static void OffThreadDecodeCallback(void* token, void* context);
-    void FinishOffThreadDecode();
+    void MaybeFinishOffThreadDecode();
     void DoFinishOffThreadDecode();
 
     size_t ShallowHeapSizeOfIncludingThis(mozilla::MallocSizeOf mallocSizeOf)
