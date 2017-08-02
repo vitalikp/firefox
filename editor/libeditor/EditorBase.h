4e42cecd84ce6db721d9d631228ed98077c17eb9
Ehsan Akhgari <ehsan@mozilla.com>
2017-08-01 22:37:35 -0400

    Bug 1386484 - Remove the SetTextTransaction class and embed its functionality into EditorBase::SetTextImpl(); r=masayuki
    Besides some unnecessary copying and malloc overhead that this removes, it
    also removes a call to dom::Text::GetData() which we used to only make to
    get the old length of the text node before modifying it.  Turns out that
    this old length was already obtained once in SetTextImpl().

--- editor/libeditor/EditorBase.h
+++ editor/libeditor/EditorBase.h
@@ -121,7 +121,6 @@ class InsertTextTransaction;
 class JoinNodeTransaction;
 class PlaceholderTransaction;
 class RemoveStyleSheetTransaction;
-class SetTextTransaction;
 class SplitNodeTransaction;
 class TextComposition;
 class TextEditor;
