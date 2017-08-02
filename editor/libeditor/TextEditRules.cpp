882c6a5e10a1e44a7447c0b3523bbccb228c51f7
Ehsan Akhgari <ehsan@mozilla.com>
2017-08-01 23:26:31 -0400

    Bug 1386485 - Part 5: Avoid manipulating the refcount of all visited nodes in CreateBogusNodeIfNeeded(); r=masayuki

--- editor/libeditor/TextEditRules.cpp
+++ editor/libeditor/TextEditRules.cpp
@@ -1318,7 +1318,7 @@ TextEditRules::CreateBogusNodeIfNeeded(Selection* aSelection)
   // looking for editable content. If no editable content is found, insert the
   // bogus node.
   bool bodyEditable = mTextEditor->IsEditable(body);
-  for (nsCOMPtr<nsIContent> bodyChild = body->GetFirstChild();
+  for (nsIContent* bodyChild = body->GetFirstChild();
        bodyChild;
        bodyChild = bodyChild->GetNextSibling()) {
     if (mTextEditor->IsMozEditorBogusNode(bodyChild) ||
