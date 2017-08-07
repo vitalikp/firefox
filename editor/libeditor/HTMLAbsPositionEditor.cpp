83daeae2c8e6ebd598b0d64636103827f01a4180
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2017-08-07 14:09:51 +0900

    Bug 1387945 - Use nsIContent for CreateAnonymousElement. r=masayuki
    CreateAnonymousElement still uses nsIDOMNode, but we should use nsIContent for it.
    
    MozReview-Commit-ID: 2xgzlE6NVra

--- editor/libeditor/HTMLAbsPositionEditor.cpp
+++ editor/libeditor/HTMLAbsPositionEditor.cpp
@@ -256,11 +256,11 @@ HTMLEditor::GetElementZIndex(Element* aElement,
 }
 
 ManualNACPtr
-HTMLEditor::CreateGrabber(nsINode* aParentNode)
+HTMLEditor::CreateGrabber(nsIContent& aParentContent)
 {
   // let's create a grabber through the element factory
   ManualNACPtr ret =
-    CreateAnonymousElement(nsGkAtoms::span, GetAsDOMNode(aParentNode),
+    CreateAnonymousElement(nsGkAtoms::span, aParentContent,
                            NS_LITERAL_STRING("mozGrabber"), false);
   if (NS_WARN_IF(!ret)) {
     return nullptr;
@@ -281,7 +281,7 @@ HTMLEditor::RefreshGrabber()
 
   nsresult rv =
     GetPositionAndDimensions(
-      static_cast<nsIDOMElement*>(GetAsDOMNode(mAbsolutelyPositionedObject)),
+      *mAbsolutelyPositionedObject,
       mPositionedObjectX,
       mPositionedObjectY,
       mPositionedObjectWidth,
@@ -347,7 +347,12 @@ HTMLEditor::ShowGrabberOnElement(nsIDOMElement* aElement)
   // first, let's keep track of that element...
   mAbsolutelyPositionedObject = element;
 
-  mGrabber = CreateGrabber(element->GetParentNode());
+  nsIContent* parentContent = element->GetParent();
+  if (NS_WARN_IF(!parentContent)) {
+    return NS_ERROR_FAILURE;
+  }
+
+  mGrabber = CreateGrabber(*parentContent);
   NS_ENSURE_TRUE(mGrabber, NS_ERROR_FAILURE);
 
   // and set its position
@@ -357,11 +362,14 @@ HTMLEditor::ShowGrabberOnElement(nsIDOMElement* aElement)
 nsresult
 HTMLEditor::StartMoving(nsIDOMElement* aHandle)
 {
-  nsCOMPtr<nsINode> parentNode = mGrabber->GetParentNode();
+  nsCOMPtr<nsIContent> parentContent = mGrabber->GetParent();
+  if (NS_WARN_IF(!parentContent) || NS_WARN_IF(!mAbsolutelyPositionedObject)) {
+    return NS_ERROR_FAILURE;
+  }
 
   // now, let's create the resizing shadow
-  mPositioningShadow = CreateShadow(GetAsDOMNode(parentNode),
-      static_cast<nsIDOMElement*>(GetAsDOMNode(mAbsolutelyPositionedObject)));
+  mPositioningShadow =
+    CreateShadow(*parentContent, *mAbsolutelyPositionedObject);
   NS_ENSURE_TRUE(mPositioningShadow, NS_ERROR_FAILURE);
   nsresult rv = SetShadowPosition(mPositioningShadow,
                                   mAbsolutelyPositionedObject,
@@ -510,7 +518,7 @@ HTMLEditor::AbsolutelyPositionElement(nsIDOMElement* aElement,
 
   if (aEnabled) {
     int32_t x, y;
-    GetElementOrigin(aElement, x, y);
+    GetElementOrigin(*element, x, y);
 
     mCSSEditUtils->SetCSSProperty(*element, *nsGkAtoms::position,
                                   NS_LITERAL_STRING("absolute"));
