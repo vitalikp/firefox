ec71ed2c80c798da494579f4931569922cb9d1fc
Kyle Machulis <kyle@nonpolynomial.com>
2017-08-09 12:05:08 -0700

    Bug 1387184 - Remove nsIDOMHTMLEmbedElement; r=bz
    Removes nsIDOMHTMLEmbedElement and all references. HTML elements are
    now handled by WebIDL. With the deprecation of extensions, XPCOM
    interfaces to HTML elements are no longer needed.
    
    MozReview-Commit-ID: DI4XVvdgPDI

--- dom/webbrowserpersist/WebBrowserPersistLocalDocument.cpp
+++ dom/webbrowserpersist/WebBrowserPersistLocalDocument.cpp
@@ -24,7 +24,6 @@
 #include "nsIDOMHTMLBaseElement.h"
 #include "nsIDOMHTMLCollection.h"
 #include "nsIDOMHTMLDocument.h"
-#include "nsIDOMHTMLEmbedElement.h"
 #include "nsIDOMHTMLFrameElement.h"
 #include "nsIDOMHTMLIFrameElement.h"
 #include "nsIDOMHTMLImageElement.h"
@@ -531,8 +530,7 @@ ResourceReader::OnWalkDOMNode(nsIDOMNode* aNode)
         return OnWalkAttribute(aNode, "href", "http://www.w3.org/1999/xlink");
     }
 
-    nsCOMPtr<nsIDOMHTMLEmbedElement> nodeAsEmbed = do_QueryInterface(aNode);
-    if (nodeAsEmbed) {
+    if (content->IsHTMLElement(nsGkAtoms::embed)) {
         return OnWalkAttribute(aNode, "src");
     }
 
@@ -1062,8 +1060,7 @@ PersistNodeFixup::FixupNode(nsIDOMNode *aNodeIn,
         return rv;
     }
 
-        nsCOMPtr<nsIDOMHTMLEmbedElement> nodeAsEmbed = do_QueryInterface(aNodeIn);
-    if (nodeAsEmbed) {
+    if (content->IsHTMLElement(nsGkAtoms::embed)) {
         rv = GetNodeToFixup(aNodeIn, aNodeOut);
         if (NS_SUCCEEDED(rv) && *aNodeOut) {
             FixupAttribute(*aNodeOut, "src");
