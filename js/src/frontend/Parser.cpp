3b5774e7df5de2acbb2f412862a76e02d4a0e418
Tooru Fujisawa <arai_a@mac.com>
2017-08-15 20:18:00 +0900

    Bug 1352312 - Enable Async Iteration. r=till

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -3896,12 +3896,6 @@ Parser<ParseHandler, CharT>::functionStmt(uint32_t toStringStart, YieldHandling 
 
     GeneratorKind generatorKind = NotGenerator;
     if (tt == TOK_MUL) {
-        if (!asyncIterationSupported()) {
-            if (asyncKind != SyncFunction) {
-                error(JSMSG_ASYNC_GENERATOR);
-                return null();
-            }
-        }
         generatorKind = StarGenerator;
         if (!tokenStream.getToken(&tt))
             return null();
@@ -3972,12 +3966,6 @@ Parser<ParseHandler, CharT>::functionExpr(uint32_t toStringStart, InvokedPredict
         return null();
 
     if (tt == TOK_MUL) {
-        if (!asyncIterationSupported()) {
-            if (asyncKind != SyncFunction) {
-                error(JSMSG_ASYNC_GENERATOR);
-                return null();
-            }
-        }
         generatorKind = StarGenerator;
         if (!tokenStream.getToken(&tt))
             return null();
@@ -6298,16 +6286,14 @@ Parser<ParseHandler, CharT>::forStatement(YieldHandling yieldHandling)
         }
     }
 
-    if (asyncIterationSupported()) {
-        if (pc->isAsync()) {
-            bool matched;
-            if (!tokenStream.matchToken(&matched, TOK_AWAIT))
-                return null();
+    if (pc->isAsync()) {
+        bool matched;
+        if (!tokenStream.matchToken(&matched, TOK_AWAIT))
+            return null();
 
-            if (matched) {
-                iflags |= JSITER_FORAWAITOF;
-                iterKind = IteratorKind::Async;
-            }
+        if (matched) {
+            iflags |= JSITER_FORAWAITOF;
+            iterKind = IteratorKind::Async;
         }
     }
 
@@ -9695,12 +9681,6 @@ Parser<ParseHandler, CharT>::propertyName(YieldHandling yieldHandling,
     }
 
     if (ltok == TOK_MUL) {
-        if (!asyncIterationSupported()) {
-            if (isAsync) {
-                error(JSMSG_ASYNC_GENERATOR);
-                return null();
-            }
-        }
         isGenerator = true;
         if (!tokenStream.getToken(&ltok))
             return null();
