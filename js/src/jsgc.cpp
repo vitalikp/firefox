6d1f759a274ac3b2b7efc7e09ea5b8b459527f90
Paul Bone <pbone@mozilla.com>
2017-08-17 10:57:41 +1000

    Bug 1386660 - Part 2: Use float for zoneAllocThresholdFactor. r=sfink

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -2971,7 +2971,7 @@ GCRuntime::maybeAllocTriggerZoneGC(Zone* zone, const AutoLockGC& lock)
     } else {
         bool wouldInterruptCollection;
         size_t igcThresholdBytes;
-        double zoneAllocThresholdFactor;
+        float zoneAllocThresholdFactor;
 
         wouldInterruptCollection = isIncrementalGCInProgress() &&
             !zone->isCollecting();
