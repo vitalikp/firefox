3d44a503b0ed68e836420a611ac0fa68f7d7061e
Jan de Mooij <jdemooij@mozilla.com>
2017-08-23 07:50:25 +0200

    Bug 1392530 part 3 - Make MDefinition::valueHash() overloads more consistent. r=nbp

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -551,7 +551,9 @@ class MDefinition : public MNode
         setBlockAndKind(block, Kind::Definition);
     }
 
-    static HashNumber addU32ToHash(HashNumber hash, uint32_t data);
+    static HashNumber addU32ToHash(HashNumber hash, uint32_t data) {
+        return data + (hash << 6) + (hash << 16) - hash;
+    }
 
   public:
     explicit MDefinition(Opcode op)
@@ -1337,6 +1339,8 @@ class MNullaryInstruction
     explicit MNullaryInstruction(Opcode op)
       : MAryInstruction(op)
     { }
+
+    HashNumber valueHash() const override;
 };
 
 class MUnaryInstruction : public MAryInstruction<1>
@@ -1347,6 +1351,8 @@ class MUnaryInstruction : public MAryInstruction<1>
     {
         initOperand(0, ins);
     }
+
+    HashNumber valueHash() const override;
 
   public:
     NAMED_OPERANDS((0, input))
@@ -1371,13 +1377,8 @@ class MBinaryInstruction : public MAryInstruction<2>
     }
 
   protected:
-    HashNumber valueHash() const
-    {
-        MDefinition* lhs = getOperand(0);
-        MDefinition* rhs = getOperand(1);
+    HashNumber valueHash() const override;
 
-        return HashNumber(op()) + lhs->id() + rhs->id();
-    }
     bool binaryCongruentTo(const MDefinition* ins) const
     {
         if (op() != ins->op())
@@ -1433,15 +1434,7 @@ class MTernaryInstruction : public MAryInstruction<3>
         initOperand(2, third);
     }
 
-  protected:
-    HashNumber valueHash() const
-    {
-        MDefinition* first = getOperand(0);
-        MDefinition* second = getOperand(1);
-        MDefinition* third = getOperand(2);
-
-        return HashNumber(op()) + first->id() + second->id() + third->id();
-    }
+    HashNumber valueHash() const override;
 };
 
 class MQuaternaryInstruction : public MAryInstruction<4>
@@ -1458,17 +1451,7 @@ class MQuaternaryInstruction : public MAryInstruction<4>
         initOperand(3, fourth);
     }
 
-  protected:
-    HashNumber valueHash() const
-    {
-        MDefinition* first = getOperand(0);
-        MDefinition* second = getOperand(1);
-        MDefinition* third = getOperand(2);
-        MDefinition* fourth = getOperand(3);
-
-        return HashNumber(op()) + first->id() + second->id() +
-            third->id() + fourth->id();
-    }
+    HashNumber valueHash() const override;
 };
 
 template <class T>
@@ -14232,7 +14215,7 @@ class MWasmLoadTls
     }
 
     HashNumber valueHash() const override {
-        return HashNumber(op()) + offset();
+        return addU32ToHash(HashNumber(op()), offset());
     }
 
     AliasSet getAliasSet() const override {
