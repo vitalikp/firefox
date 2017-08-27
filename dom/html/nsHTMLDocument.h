895f7520451d235c56c2ddf756292ced0b49ec46
Ehsan Akhgari <ehsan@mozilla.com>
2017-08-26 22:44:34 -0400

    Bug 1392891 - Part 2: Add an API to return nsHTMLDocument::mForms/mFormControls without creating a new nsContentList; r=smaug

--- dom/html/nsHTMLDocument.h
+++ dom/html/nsHTMLDocument.h
@@ -78,6 +78,16 @@ public:
 
   virtual nsContentList* GetFormControls() override;
 
+  nsContentList* GetExistingForms() const
+  {
+    return mForms;
+  }
+
+  nsContentList* GetExistingFormControls() const
+  {
+    return mFormControls;
+  }
+
   // nsIDOMDocument interface
   using nsDocument::CreateElement;
   using nsDocument::CreateElementNS;
