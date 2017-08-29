f031cb8a614ee58efcdfbc9a44b3875e012f7547
Xidorn Quan <me@upsuper.org>
2017-08-29 14:44:14 +1000

    Bug 1393189 part 1 - Statically initialize builtin style table with their name atom included. r=dholbert
    Doing this at compile time would save a bit of our startup time, which
    I've promised to do since @counter-style was initially implemented, see
    bug 966166 comment 103 (the paragraph with "constexpr").
    
    Also, having this implemented here makes using atom instead of string
    on name of builtin counter styles easier, for later patches.
    
    MozReview-Commit-ID: C9HYcuShBQv

--- layout/build/nsLayoutStatics.cpp
+++ layout/build/nsLayoutStatics.cpp
@@ -67,7 +67,6 @@
 #include "CacheObserver.h"
 #include "DisplayItemClip.h"
 #include "ActiveLayerTracker.h"
-#include "CounterStyleManager.h"
 #include "FrameLayerBuilder.h"
 #include "AnimationCommon.h"
 #include "LayerAnimationInfo.h"
@@ -284,8 +283,6 @@ nsLayoutStatics::Initialize()
 #endif
 
   CacheObserver::Init();
-
-  CounterStyleManager::InitializeBuiltinCounterStyles();
 
   IMEStateManager::Init();
 
