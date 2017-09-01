0291107f7311e530e24121116a902c16503392e5
Tom Schuster <evilpies@gmail.com>
2017-09-01 12:32:05 +0200

    Bug 1395513 - Simply nop Object(object) calls. r=nbp

--- js/src/jit/InlinableNatives.h
+++ js/src/jit/InlinableNatives.h
@@ -92,6 +92,7 @@
     _(IntrinsicStringReplaceString) \
     _(IntrinsicStringSplitString)   \
                                     \
+    _(Object)                       \
     _(ObjectCreate)                 \
     _(ObjectToString)               \
                                     \
