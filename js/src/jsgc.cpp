c2f3d789e5f10c52b88708facf26554953f93dae
Jon Coppeard <jcoppeard@mozilla.com>
2017-09-01 10:13:51 +0100

    Bug 1384513 - Remove AutoAssertNoAlloc r=sfink

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -7269,6 +7269,8 @@ js::gc::AssertGCThingHasType(js::gc::Cell* cell, JS::TraceKind kind)
 }
 #endif
 
+#ifdef MOZ_DIAGNOSTIC_ASSERT_ENABLED
+
 JS::AutoAssertNoGC::AutoAssertNoGC(JSContext* maybecx)
   : cx_(maybecx ? maybecx : TlsContext.get())
 {
@@ -7284,25 +7286,9 @@ JS::AutoAssertNoGC::~AutoAssertNoGC()
     }
 }
 
+#endif // MOZ_DIAGNOSTIC_ASSERT_ENABLED
+
 #ifdef DEBUG
-JS::AutoAssertNoAlloc::AutoAssertNoAlloc(JSContext* cx)
-  : gc(nullptr)
-{
-    disallowAlloc(cx->runtime());
-}
-
-void JS::AutoAssertNoAlloc::disallowAlloc(JSRuntime* rt)
-{
-    MOZ_ASSERT(!gc);
-    gc = &rt->gc;
-    TlsContext.get()->disallowAlloc();
-}
-
-JS::AutoAssertNoAlloc::~AutoAssertNoAlloc()
-{
-    if (gc)
-        TlsContext.get()->allowAlloc();
-}
 
 AutoAssertNoNurseryAlloc::AutoAssertNoNurseryAlloc()
 {
@@ -7332,7 +7318,8 @@ JS::AutoAssertGCCallback::AutoAssertGCCallback()
 {
     MOZ_ASSERT(JS::CurrentThreadIsHeapCollecting());
 }
-#endif
+
+#endif // DEBUG
 
 JS_FRIEND_API(const char*)
 JS::GCTraceKindToAscii(JS::TraceKind kind)
