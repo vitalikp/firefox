c1cc4955b7989cdd5227ab8d81b835f8c358b21c
Michael Layzell <michael@thelayzells.com>
2017-09-06 11:23:01 -0400

    Bug 1199729 - Part 1: Add the Protected mode to DataTransfer, r=baku

--- dom/base/nsCopySupport.cpp
+++ dom/base/nsCopySupport.cpp
@@ -841,7 +841,7 @@ nsCopySupport::FireClipboardEvent(EventMessage aEventMessage,
     // from reading the clipboard contents after the paste event has fired.
     if (clipboardData) {
       clipboardData->ClearAll();
-      clipboardData->SetReadOnly();
+      clipboardData->SetMode(DataTransfer::Mode::Protected);
     }
 
     if (aActionTaken) {
