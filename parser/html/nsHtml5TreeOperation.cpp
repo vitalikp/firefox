885742c2659a223238acc25180bdf69a7d381c64
John Dai <jdai@mozilla.com>
2017-09-12 00:40:00 -0400

    Bug 1398401 - Add preference check for custom element setup. r=smaug

--- parser/html/nsHtml5TreeOperation.cpp
+++ parser/html/nsHtml5TreeOperation.cpp
@@ -446,7 +446,9 @@ nsHtml5TreeOperation::CreateHTMLElement(
                             false);
 
         // Custom element setup may be needed if there is an "is" attribute.
-        if (kNameSpaceID_None == nsuri && !prefix && nsGkAtoms::is == localName) {
+        if (nsContentUtils::IsWebComponentsEnabled() &&
+            kNameSpaceID_None == nsuri &&
+            !prefix && nsGkAtoms::is == localName) {
           nsContentUtils::SetupCustomElement(newContent, &value);
         }
       }
