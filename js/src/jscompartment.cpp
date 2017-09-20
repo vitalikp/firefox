f2168104471469572374d2d808991bd9a15d01fd
Tooru Fujisawa <arai_a@mac.com>
2017-09-20 18:43:01 +0900

    Bug 1394682 - Use template object for iterator result object. r=djvj

--- js/src/jscompartment.cpp
+++ js/src/jscompartment.cpp
@@ -93,6 +93,7 @@ JSCompartment::JSCompartment(Zone* zone, const JS::CompartmentOptions& options =
     jitCompartment_(nullptr),
     mappedArgumentsTemplate_(nullptr),
     unmappedArgumentsTemplate_(nullptr),
+    iterResultTemplate_(nullptr),
     lcovOutput()
 {
     PodArrayZero(sawDeprecatedLanguageExtension);
@@ -964,6 +965,9 @@ JSCompartment::sweepTemplateObjects()
 
     if (unmappedArgumentsTemplate_ && IsAboutToBeFinalized(&unmappedArgumentsTemplate_))
         unmappedArgumentsTemplate_.set(nullptr);
+
+    if (iterResultTemplate_ && IsAboutToBeFinalized(&iterResultTemplate_))
+        iterResultTemplate_.set(nullptr);
 }
 
 /* static */ void
