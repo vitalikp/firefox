966699040becf3f933499cf2a4261872f609c4b8
Ted Campbell <tcampbell@mozilla.com>
2017-09-20 07:50:20 +0300

    Bug 1401014 - Fix resume point in IonBuilder::setPropTryInlineAccess r=jandem
    MozReview-Commit-ID: L5VpnS41tiH

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -12066,9 +12066,11 @@ IonBuilder::setPropTryInlineAccess(bool* emitted, MDefinition* obj,
             current->add(MPostWriteBarrier::New(alloc(), obj, value));
 
         const UnboxedLayout::Property* property = group->unboxedLayout().lookup(name);
-        storeUnboxedProperty(obj, property->offset, property->type, value);
+        MInstruction* store = storeUnboxedProperty(obj, property->offset, property->type, value);
 
         current->push(value);
+
+        MOZ_TRY(resumeAfter(store));
 
         trackOptimizationOutcome(TrackedOutcome::Monomorphic);
         *emitted = true;
