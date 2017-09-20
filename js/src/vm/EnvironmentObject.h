29d6f596448eda715c646352b7be2faf560824db
Jon Coppeard <jcoppeard@mozilla.com>
2017-09-20 13:19:32 +0100

    Bug 1400278 - Replace use of RuntimeAllocPolicy with ZoneAllocPolicy r=sfink

--- js/src/vm/EnvironmentObject.h
+++ js/src/vm/EnvironmentObject.h
@@ -948,7 +948,7 @@ class DebugEnvironments
     typedef HashMap<MissingEnvironmentKey,
                     ReadBarrieredDebugEnvironmentProxy,
                     MissingEnvironmentKey,
-                    RuntimeAllocPolicy> MissingEnvironmentMap;
+                    ZoneAllocPolicy> MissingEnvironmentMap;
     MissingEnvironmentMap missingEnvs;
 
     /*
@@ -962,7 +962,7 @@ class DebugEnvironments
     typedef GCHashMap<ReadBarriered<JSObject*>,
                       LiveEnvironmentVal,
                       MovableCellHasher<ReadBarriered<JSObject*>>,
-                      RuntimeAllocPolicy> LiveEnvironmentMap;
+                      ZoneAllocPolicy> LiveEnvironmentMap;
     LiveEnvironmentMap liveEnvs;
 
   public:
