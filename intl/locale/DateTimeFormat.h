b7d8a372667dee49de0a406056025675624aa617
Zibi Braniecki <zbraniecki@mozilla.com>
2017-09-22 09:33:07 +0300

    Bug 1402234 - Remove the ENABLE_INTL_API=no code from intl/locale. r=jfkthame
    MozReview-Commit-ID: IDgkmSb0bmV

--- intl/locale/DateTimeFormat.h
+++ intl/locale/DateTimeFormat.h
@@ -12,9 +12,7 @@
 #include "nsIScriptableDateFormat.h"
 #include "nsString.h"
 #include "prtime.h"
-#ifdef ENABLE_INTL_API
 #include "unicode/udat.h"
-#endif
 
 namespace mozilla {
 
@@ -48,14 +46,12 @@ private:
   FRIEND_TEST(DateTimeFormat, FormatPRExplodedTime);
   FRIEND_TEST(DateTimeFormat, DateFormatSelectors);
 
-#ifdef ENABLE_INTL_API
   // performs a locale sensitive date formatting operation on the UDate parameter
   static nsresult FormatUDateTime(const nsDateFormatSelector aDateFormatSelector,
                                   const nsTimeFormatSelector aTimeFormatSelector,
                                   const UDate aUDateTime,
                                   const PRTimeParameters* aTimeParameters,
                                   nsAString& aStringOut);
-#endif
 
   static nsCString* mLocale;
 };
