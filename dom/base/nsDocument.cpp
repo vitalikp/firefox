14c8ae3324168fef175f2e1f62e7be0387540e02
John Dai <jdai@mozilla.com>
2017-09-25 01:33:00 -0400

    Bug 1301024 - Part 1: Set CreateElement/CreateElementNS is attribute. r=smaug

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -5786,6 +5786,10 @@ nsDocument::CreateElement(const nsAString& aTagName,
     elem->SetPseudoElementType(pseudoType);
   }
 
+  if (is) {
+    elem->SetAttr(kNameSpaceID_None, nsGkAtoms::is, *is, true);
+  }
+
   return elem.forget();
 }
 
@@ -5835,6 +5839,10 @@ nsDocument::CreateElementNS(const nsAString& aNamespaceURI,
                      NOT_FROM_PARSER, is);
   if (rv.Failed()) {
     return nullptr;
+  }
+
+  if (is) {
+    element->SetAttr(kNameSpaceID_None, nsGkAtoms::is, *is, true);
   }
 
   return element.forget();
