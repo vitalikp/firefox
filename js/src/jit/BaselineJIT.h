98a943c3d33a0df5f3e8dd6d0d6c483215dbd1e7
Jan de Mooij <jdemooij@mozilla.com>
2017-10-14 12:45:54 +0200

    Bug 1407607 - Use a single entry point for C++ -> JIT calls. r=nbp

--- js/src/jit/BaselineJIT.h
+++ js/src/jit/BaselineJIT.h
@@ -541,9 +541,6 @@ MethodStatus
 CanEnterBaselineAtBranch(JSContext* cx, InterpreterFrame* fp, bool newType);
 
 JitExecStatus
-EnterBaselineMethod(JSContext* cx, RunState& state);
-
-JitExecStatus
 EnterBaselineAtBranch(JSContext* cx, InterpreterFrame* fp, jsbytecode* pc);
 
 void
@@ -625,6 +622,8 @@ MarkActiveBaselineScripts(Zone* zone);
 
 MethodStatus
 BaselineCompile(JSContext* cx, JSScript* script, bool forceDebugInstrumentation = false);
+
+static const unsigned BASELINE_MAX_ARGS_LENGTH = 20000;
 
 } // namespace jit
 } // namespace js
