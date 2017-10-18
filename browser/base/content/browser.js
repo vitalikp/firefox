35929b1fb97c24f281c8846a1e5af3b162676713
Brian Grinstead <bgrinstead@mozilla.com>
2017-10-18 11:41:21 -0700

    Bug 1409784 - Remove mStringBundle from tabbrowser binding and expose gTabBrowserBundle instead;r=dao
    MozReview-Commit-ID: 2P0OIuQwQv1

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -80,6 +80,9 @@ XPCOMUtils.defineLazyGetter(this, "BrowserToolboxProcess", function() {
 XPCOMUtils.defineLazyGetter(this, "gBrowserBundle", function() {
   return Services.strings.createBundle("chrome://browser/locale/browser.properties");
 });
+XPCOMUtils.defineLazyGetter(this, "gTabBrowserBundle", function() {
+  return Services.strings.createBundle("chrome://browser/locale/tabbrowser.properties");
+});
 
 XPCOMUtils.defineLazyGetter(this, "gCustomizeMode", function() {
   let scope = {};
@@ -8025,8 +8028,8 @@ TabModalPromptBox.prototype = {
       allowFocusCheckbox = document.createElementNS(XUL_NS, "checkbox");
       let spacer = document.createElementNS(XUL_NS, "spacer");
       allowFocusRow.appendChild(spacer);
-      let label = gBrowser.mStringBundle.getFormattedString("tabs.allowTabFocusByPromptForSite",
-                                                            [hostForAllowFocusCheckbox]);
+      let label = gTabBrowserBundle.formatStringFromName("tabs.allowTabFocusByPromptForSite",
+                                                      [hostForAllowFocusCheckbox], 1);
       allowFocusCheckbox.setAttribute("label", label);
       allowFocusRow.appendChild(allowFocusCheckbox);
       newPrompt.appendChild(allowFocusRow);
