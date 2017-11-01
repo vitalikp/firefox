9c60062787b0d3202abbf07b8e299a2bb34598fe
Jon Coppeard <jcoppeard@mozilla.com>
2017-11-01 14:07:13 +0300

    Bug 1413218 - Make the malloc threashold grow a little slower r=sfink

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -291,6 +291,15 @@ namespace TuningDefaults {
     static const float AllocThresholdFactorAvoidInterrupt = 0.9f;
 
     /* no parameter */
+    static const float MallocThresholdGrowFactor = 1.5f;
+
+    /* no parameter */
+    static const float MallocThresholdShrinkFactor = 0.9f;
+
+    /* no parameter */
+    static const size_t MallocThresholdLimit = 1024 * 1024 * 1024;
+
+    /* no parameter */
     static const size_t ZoneAllocDelayBytes = 1024 * 1024;
 
     /* JSGC_DYNAMIC_HEAP_GROWTH */
@@ -1383,7 +1392,7 @@ GCSchedulingTunables::setParameter(JSGCParamKey key, uint32_t value, const AutoL
 void
 GCSchedulingTunables::setMaxMallocBytes(size_t value)
 {
-    maxMallocBytes_ = value;
+    maxMallocBytes_ = std::min(value, TuningDefaults::MallocThresholdLimit);
 }
 
 void
@@ -1920,10 +1929,13 @@ MemoryCounter::updateOnGCEnd(const GCSchedulingTunables& tunables, const AutoLoc
     // Update the trigger threshold at the end of GC and adjust the current
     // byte count to reflect bytes allocated since the start of GC.
     MOZ_ASSERT(bytes_ >= bytesAtStartOfGC_);
-    if (shouldTriggerGC(tunables))
-        maxBytes_ *= 2;
-    else
-        maxBytes_ = std::max(tunables.maxMallocBytes(), size_t(maxBytes_ * 0.9));
+    if (shouldTriggerGC(tunables)) {
+        maxBytes_ = std::min(TuningDefaults::MallocThresholdLimit,
+                             size_t(maxBytes_ * TuningDefaults::MallocThresholdGrowFactor));
+    } else {
+        maxBytes_ = std::max(tunables.maxMallocBytes(),
+                             size_t(maxBytes_ * TuningDefaults::MallocThresholdShrinkFactor));
+    }
     bytes_ -= bytesAtStartOfGC_;
     triggered_ = NoTrigger;
 }
