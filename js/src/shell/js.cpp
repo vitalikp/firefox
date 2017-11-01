2c1c88ef7fc9b2ff7742fe1b52d2f9c3fb9116a7
Brian Hackett <bhackett1024@gmail.com>
2017-11-01 09:53:51 -0600

    Bug 1412298 - Wait for cooperative threads before worker threads when shutting down the JS shell, r=jandem.

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -3849,6 +3849,13 @@ KillWorkerThreads(JSContext* cx)
 {
     MOZ_ASSERT_IF(!CanUseExtraThreads(), workerThreads.empty());
 
+    // Yield until all other cooperative threads in the main runtime finish.
+    while (cooperationState->numThreads) {
+        CooperativeBeginWait(cx);
+        CooperativeYield();
+        CooperativeEndWait(cx);
+    }
+
     if (!workerThreadsLock) {
         MOZ_ASSERT(workerThreads.empty());
         return;
@@ -3872,13 +3879,6 @@ KillWorkerThreads(JSContext* cx)
 
     js_delete(workerThreadsLock);
     workerThreadsLock = nullptr;
-
-    // Yield until all other cooperative threads in the main runtime finish.
-    while (cooperationState->numThreads) {
-        CooperativeBeginWait(cx);
-        CooperativeYield();
-        CooperativeEndWait(cx);
-    }
 
     js_delete(cooperationState);
     cooperationState = nullptr;
