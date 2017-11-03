ddb3131176bbfe16286cbfa88a7c35ab83c877cb
Jon Coppeard <jcoppeard@mozilla.com>
2017-11-03 10:25:25 +0000

    Bug 1413997 - Improve readability of js::DumpObject() output r=jandem

--- js/src/vm/String.cpp
+++ js/src/vm/String.cpp
@@ -156,6 +156,13 @@ JSString::dump()
 void
 JSString::dump(js::GenericPrinter& out)
 {
+    dumpNoNewline(out);
+    out.putChar('\n');
+}
+
+void
+JSString::dumpNoNewline(js::GenericPrinter& out)
+{
     if (JSLinearString* linear = ensureLinear(nullptr)) {
         AutoCheckCannotGC nogc;
         if (hasLatin1Chars()) {
@@ -172,7 +179,6 @@ JSString::dump(js::GenericPrinter& out)
     } else {
         out.put("(oom in JSString::dump)");
     }
-    out.putChar('\n');
 }
 
 
