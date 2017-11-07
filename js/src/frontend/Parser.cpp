ae9be76149a9029eaed06b42acdeef464d6b8cf0
André Bargull <andre.bargull@gmail.com>
2017-11-07 06:10:30 -0800

    Bug 1414546 - Don't treat "yield" as a keyword in JS1.7. r=jandem

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -8766,7 +8766,7 @@ GeneralParser<ParseHandler, CharT>::checkLabelOrIdentifierReference(PropertyName
         return true;
     if (TokenKindIsContextualKeyword(tt)) {
         if (tt == TOK_YIELD) {
-            if (yieldHandling == YieldIsKeyword || versionNumber() >= JSVERSION_1_7) {
+            if (yieldHandling == YieldIsKeyword) {
                 errorAt(offset, JSMSG_RESERVED_ID, "yield");
                 return false;
             }
