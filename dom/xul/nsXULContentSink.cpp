b4ebbc7138b507896969c74cf9ee61e2c077e39b
Jan de Mooij <jdemooij@mozilla.com>
2017-11-18 23:16:14 +0100

    Bug 1418294 - Remove JSVersion related code from dom/xul. r=bz

--- dom/xul/nsXULContentSink.cpp
+++ dom/xul/nsXULContentSink.cpp
@@ -837,7 +837,6 @@ XULContentSinkImpl::OpenScript(const char16_t** aAttributes,
                                const uint32_t aLineNumber)
 {
   bool isJavaScript = true;
-  uint32_t version = JSVERSION_DEFAULT;
   nsresult rv;
 
   // Look for SRC attribute and look for a LANGUAGE attribute
@@ -863,7 +862,6 @@ XULContentSinkImpl::OpenScript(const char16_t** aAttributes,
 
           if (nsContentUtils::IsJavascriptMIMEType(mimeType)) {
               isJavaScript = true;
-              version = JSVERSION_DEFAULT;
 
               // Get the version string, and ensure that JavaScript supports it.
               nsAutoString versionName;
@@ -890,7 +888,6 @@ XULContentSinkImpl::OpenScript(const char16_t** aAttributes,
           nsAutoString lang(aAttributes[1]);
           if (nsContentUtils::IsJavaScriptLanguage(lang)) {
               isJavaScript = true;
-              version = JSVERSION_DEFAULT;
           }
       }
       aAttributes += 2;
@@ -906,7 +903,7 @@ XULContentSinkImpl::OpenScript(const char16_t** aAttributes,
   if (doc)
       globalObject = do_QueryInterface(doc->GetWindow());
   RefPtr<nsXULPrototypeScript> script =
-      new nsXULPrototypeScript(aLineNumber, version);
+      new nsXULPrototypeScript(aLineNumber);
 
   // If there is a SRC attribute...
   if (! src.IsEmpty()) {
