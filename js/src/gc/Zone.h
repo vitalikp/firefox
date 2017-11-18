e5a9731ecbdca5158ab8b0d4861431796ee5d25e
Ted Campbell <tcampbell@mozilla.com>
2017-11-18 17:27:11 -0500

    Bug 1418841 - Fix DeletePolicy of WasmInstanceScope::Data. r=jonco
    MozReview-Commit-ID: EgLb7uTE52G

--- js/src/gc/Zone.h
+++ js/src/gc/Zone.h
@@ -1064,6 +1064,11 @@ struct DeletePolicy<js::ModuleScope::Data>
   : public js::GCManagedDeletePolicy<js::ModuleScope::Data>
 { };
 
+template <>
+struct DeletePolicy<js::WasmInstanceScope::Data>
+  : public js::GCManagedDeletePolicy<js::WasmInstanceScope::Data>
+{ };
+
 } // namespace JS
 
 #endif // gc_Zone_h
