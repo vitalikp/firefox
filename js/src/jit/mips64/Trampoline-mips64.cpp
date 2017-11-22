2ea09d0682a6e16a68a26f52f5d003052b6b7284
Jan de Mooij <jdemooij@mozilla.com>
2017-11-22 09:40:00 +0100

    Bug 1419497 part 2 - Optimize pre-barriers in jit code by handling more cases without calling into C++. r=jonco

--- js/src/jit/mips64/Trampoline-mips64.cpp
+++ js/src/jit/mips64/Trampoline-mips64.cpp
@@ -814,6 +814,22 @@ JitRuntime::generatePreBarrier(JSContext* cx, MacroAssembler& masm, MIRType type
 {
     uint32_t offset = startTrampolineCode(masm);
 
+    MOZ_ASSERT(PreBarrierReg == a1);
+    Register temp1 = a0;
+    Register temp2 = a2;
+    Register temp3 = a3;
+    masm.push(temp1);
+    masm.push(temp2);
+    masm.push(temp3);
+
+    Label noBarrier;
+    masm.emitPreBarrierFastPath(cx->runtime(), type, temp1, temp2, temp3, &noBarrier);
+
+    // Call into C++ to mark this GC thing.
+    masm.pop(temp3);
+    masm.pop(temp2);
+    masm.pop(temp1);
+
     LiveRegisterSet save;
     if (cx->runtime()->jitSupportsFloatingPoint) {
         save.set() = RegisterSet(GeneralRegisterSet(Registers::VolatileMask),
@@ -825,7 +841,6 @@ JitRuntime::generatePreBarrier(JSContext* cx, MacroAssembler& masm, MIRType type
     save.add(ra);
     masm.PushRegsInMask(save);
 
-    MOZ_ASSERT(PreBarrierReg == a1);
     masm.movePtr(ImmPtr(cx->runtime()), a0);
 
     masm.setupUnalignedABICall(a2);
@@ -836,6 +851,12 @@ JitRuntime::generatePreBarrier(JSContext* cx, MacroAssembler& masm, MIRType type
     save.take(AnyRegister(ra));
     masm.PopRegsInMask(save);
     masm.ret();
+
+    masm.bind(&noBarrier);
+    masm.pop(temp3);
+    masm.pop(temp2);
+    masm.pop(temp1);
+    masm.abiret();
 
     return offset;
 }
