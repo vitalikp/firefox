ba37561df38a7f33c710f1cf30d9fcbdd8e064f6
Paul Bone <pbone@mozilla.com>
2017-12-01 04:06:57 +0300

    Bug 1420537 - Bailout from triggerZoneGC when the heap is busy r=jonco reland DONTBUILD
    CLOSED TREE

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -3251,7 +3251,7 @@ GCRuntime::triggerZoneGC(Zone* zone, JS::gcreason::Reason reason, size_t used, s
     MOZ_ASSERT(CurrentThreadCanAccessRuntime(rt));
 
     /* GC is already running. */
-    if (JS::CurrentThreadIsHeapCollecting())
+    if (JS::CurrentThreadIsHeapBusy())
         return false;
 
 #ifdef JS_GC_ZEAL
