3538d88ebfc2db5b0953129933e9fbf554e390d6
Jeff Walden <jwalden@mit.edu>
2017-12-12 15:20:59 -0600

    Bug 1424946 - Move GeneralParser::declareFunctionArgumentsObject into a baser class. r=arai

--- js/src/frontend/Parser.h
+++ js/src/frontend/Parser.h
@@ -338,6 +338,7 @@ class PerHandlerParser
     bool finishFunction(bool isStandaloneFunction = false);
 
     bool declareFunctionThis();
+    bool declareFunctionArgumentsObject();
 
     inline Node newName(PropertyName* name);
     inline Node newName(PropertyName* name, TokenPos pos);
@@ -527,6 +528,7 @@ class GeneralParser
 
   private:
     using Base::declareDotGeneratorName;
+    using Base::declareFunctionArgumentsObject;
     using Base::declareFunctionThis;
     using Base::finishFunction;
     using Base::hasUsedName;
@@ -1008,8 +1010,6 @@ class GeneralParser
     // Indicate if the next token (tokenized as Operand) is |in| or |of|.  If
     // so, consume it.
     bool matchInOrOf(bool* isForInp, bool* isForOfp);
-
-    bool declareFunctionArgumentsObject();
 
     bool leaveInnerFunction(ParseContext* outerpc);
 
