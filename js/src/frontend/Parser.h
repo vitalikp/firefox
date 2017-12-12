1e435d08906d4c0d16d38418eee335bf482235e1
Jeff Walden <jwalden@mit.edu>
2017-12-12 15:20:57 -0600

    Bug 1424946 - Move GeneralParser::noteDestructuredPositionalFormalParameter into a baser class. r=arai

--- js/src/frontend/Parser.h
+++ js/src/frontend/Parser.h
@@ -290,6 +290,8 @@ class PerHandlerParser
 
     const char* nameIsArgumentsOrEval(Node node);
 
+    bool noteDestructuredPositionalFormalParameter(Node fn, Node destruct);
+
   public:
     bool isValidSimpleAssignmentTarget(Node node,
                                        FunctionCallBehavior behavior = ForbidAssignmentToFunctionCalls);
@@ -366,6 +368,9 @@ class GeneralParser
     using Base::isValidSimpleAssignmentTarget;
     using Base::pc;
     using Base::usedNames;
+
+  private:
+    using Base::noteDestructuredPositionalFormalParameter;
 
   private:
     inline FinalParser* asFinalParser();
@@ -865,7 +870,6 @@ class GeneralParser
                              uint32_t prevPos);
     bool notePositionalFormalParameter(Node fn, HandlePropertyName name, uint32_t beginPos,
                                        bool disallowDuplicateParams, bool* duplicatedParam);
-    bool noteDestructuredPositionalFormalParameter(Node fn, Node destruct);
 
     bool checkLexicalDeclarationDirectlyWithinBlock(ParseContext::Statement& stmt,
                                                     DeclarationKind kind, TokenPos pos);
