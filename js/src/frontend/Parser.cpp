0784a3f7186e111abbf67fdf78479847823dfd48
Jeff Walden <jwalden@mit.edu>
2017-12-12 15:20:57 -0600

    Bug 1424946 - Move GeneralParser::newFunctionBox into a baser class. r=arai

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -918,13 +918,12 @@ ParserBase::newObjectBox(JSObject* obj)
     return objbox;
 }
 
-template <class ParseHandler, typename CharT>
+template <class ParseHandler>
 FunctionBox*
-GeneralParser<ParseHandler, CharT>::newFunctionBox(Node fn, JSFunction* fun,
-                                                   uint32_t toStringStart,
-                                                   Directives inheritedDirectives,
-                                                   GeneratorKind generatorKind,
-                                                   FunctionAsyncKind asyncKind)
+PerHandlerParser<ParseHandler>::newFunctionBox(Node fn, JSFunction* fun, uint32_t toStringStart,
+                                               Directives inheritedDirectives,
+                                               GeneratorKind generatorKind,
+                                               FunctionAsyncKind asyncKind)
 {
     MOZ_ASSERT(fun);
 
@@ -936,9 +935,8 @@ GeneralParser<ParseHandler, CharT>::newFunctionBox(Node fn, JSFunction* fun,
      * function.
      */
     FunctionBox* funbox =
-        alloc.template new_<FunctionBox>(context, traceListHead, fun, toStringStart,
-                                         inheritedDirectives, options().extraWarningsOption,
-                                         generatorKind, asyncKind);
+        alloc.new_<FunctionBox>(context, traceListHead, fun, toStringStart, inheritedDirectives,
+                                options().extraWarningsOption, generatorKind, asyncKind);
     if (!funbox) {
         ReportOutOfMemory(context);
         return nullptr;
--- js/src/frontend/Parser.h
+++ js/src/frontend/Parser.h
@@ -287,7 +287,9 @@ class PerHandlerParser
     bool isValidSimpleAssignmentTarget(Node node,
                                        FunctionCallBehavior behavior = ForbidAssignmentToFunctionCalls);
 
-
+    FunctionBox* newFunctionBox(Node fn, JSFunction* fun, uint32_t toStringStart,
+                                Directives directives, GeneratorKind generatorKind,
+                                FunctionAsyncKind asyncKind);
 };
 
 enum class ExpressionClosure { Allowed, Forbidden };
@@ -344,6 +346,7 @@ class GeneralParser
     using Base::keepAtoms;
     using Base::nameIsArgumentsOrEval;
     using Base::newFunction;
+    using Base::newFunctionBox;
     using Base::options;
     using Base::pos;
     using Base::setLocalStrictMode;
@@ -527,10 +530,6 @@ class GeneralParser
      * Parse a top-level JS script.
      */
     Node parse();
-
-    FunctionBox* newFunctionBox(Node fn, JSFunction* fun, uint32_t toStringStart,
-                                Directives directives,
-                                GeneratorKind generatorKind, FunctionAsyncKind asyncKind);
 
     void trace(JSTracer* trc);
 
