f38261f97a7e1afdd4628d4767d54b0b885cd965
Jan de Mooij <jdemooij@mozilla.com>
2017-12-17 05:43:52 +0300

    Bug 1420462 - Add an EnumerableOwnProperties fast path for unboxed objects. r=anba

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -5441,6 +5441,28 @@ IsLatin1(JSContext* cx, unsigned argc, Value* vp)
     return true;
 }
 
+static bool
+UnboxedObjectsEnabled(JSContext* cx, unsigned argc, Value* vp)
+{
+    // Note: this also returns |false| if we're using --ion-eager or if the
+    // JITs are disabled, since that affects how unboxed objects are used.
+
+    CallArgs args = CallArgsFromVp(argc, vp);
+    args.rval().setBoolean(!jit::JitOptions.disableUnboxedObjects &&
+                           !jit::JitOptions.eagerCompilation &&
+                           jit::IsIonEnabled(cx));
+    return true;
+}
+
+static bool
+IsUnboxedObject(JSContext* cx, unsigned argc, Value* vp)
+{
+    CallArgs args = CallArgsFromVp(argc, vp);
+    args.rval().setBoolean(args.get(0).isObject() &&
+                           args[0].toObject().is<UnboxedPlainObject>());
+    return true;
+}
+
 // Set the profiling stack for each cooperating context in a runtime.
 static bool
 EnsureAllContextProfilingStacks(JSContext* cx)
@@ -6950,6 +6972,14 @@ static const JSFunctionSpecWithHelp shell_functions[] = {
     JS_FN_HELP("isLatin1", IsLatin1, 1, 0,
 "isLatin1(s)",
 "  Return true iff the string's characters are stored as Latin1."),
+
+    JS_FN_HELP("unboxedObjectsEnabled", UnboxedObjectsEnabled, 0, 0,
+"unboxedObjectsEnabled()",
+"  Return true if unboxed objects are enabled."),
+
+    JS_FN_HELP("isUnboxedObject", IsUnboxedObject, 1, 0,
+"isUnboxedObject(o)",
+"  Return true iff the object is an unboxed object."),
 
     JS_FN_HELP("stackPointerInfo", StackPointerInfo, 0, 0,
 "stackPointerInfo()",
