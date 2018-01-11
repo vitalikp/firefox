b88d29b17193fa816c7da47c1a4c6de96757d78c
Jan de Mooij <jdemooij@mozilla.com>
2018-01-11 14:22:22 +0300

    Bug 1425687 - Don't use copy-on-write elements for small arrays. r=evilpie

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -5464,6 +5464,16 @@ IsUnboxedObject(JSContext* cx, unsigned argc, Value* vp)
     return true;
 }
 
+static bool
+HasCopyOnWriteElements(JSContext* cx, unsigned argc, Value* vp)
+{
+    CallArgs args = CallArgsFromVp(argc, vp);
+    args.rval().setBoolean(args.get(0).isObject() &&
+                           args[0].toObject().isNative() &&
+                           args[0].toObject().as<NativeObject>().denseElementsAreCopyOnWrite());
+    return true;
+}
+
 // Set the profiling stack for each cooperating context in a runtime.
 static bool
 EnsureAllContextProfilingStacks(JSContext* cx)
@@ -6981,6 +6991,10 @@ static const JSFunctionSpecWithHelp shell_functions[] = {
     JS_FN_HELP("isUnboxedObject", IsUnboxedObject, 1, 0,
 "isUnboxedObject(o)",
 "  Return true iff the object is an unboxed object."),
+
+    JS_FN_HELP("hasCopyOnWriteElements", HasCopyOnWriteElements, 1, 0,
+"hasCopyOnWriteElements(o)",
+"  Return true iff the object has copy-on-write dense elements."),
 
     JS_FN_HELP("stackPointerInfo", StackPointerInfo, 0, 0,
 "stackPointerInfo()",
