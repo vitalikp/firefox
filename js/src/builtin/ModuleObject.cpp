250b641b5a809852f8e6ac286848536a20a88514
Jon Coppeard <jcoppeard@mozilla.com>
2018-01-30 17:57:40 +0000

    Bug 1432794 - Skip prototype and constructor intialization for off-thread parsing r=jandem

--- js/src/builtin/ModuleObject.cpp
+++ js/src/builtin/ModuleObject.cpp
@@ -144,7 +144,10 @@ ImportEntryObject::create(JSContext* cx,
 {
     MOZ_ASSERT(lineNumber > 0);
 
-    RootedObject proto(cx, cx->global()->getImportEntryPrototype());
+    RootedObject proto(cx, GlobalObject::getOrCreateImportEntryPrototype(cx, cx->global()));
+    if (!proto)
+        return nullptr;
+
     RootedObject obj(cx, NewObjectWithGivenProto(cx, &class_, proto));
     if (!obj)
         return nullptr;
@@ -230,7 +233,10 @@ ExportEntryObject::create(JSContext* cx,
     // Line and column numbers are optional for export entries since direct
     // entries are checked at parse time.
 
-    RootedObject proto(cx, cx->global()->getExportEntryPrototype());
+    RootedObject proto(cx, GlobalObject::getOrCreateExportEntryPrototype(cx, cx->global()));
+    if (!proto)
+        return nullptr;
+
     RootedObject obj(cx, NewObjectWithGivenProto(cx, &class_, proto));
     if (!obj)
         return nullptr;
@@ -298,7 +304,10 @@ RequestedModuleObject::create(JSContext* cx,
 {
     MOZ_ASSERT(lineNumber > 0);
 
-    RootedObject proto(cx, cx->global()->getRequestedModulePrototype());
+    RootedObject proto(cx, GlobalObject::getOrCreateRequestedModulePrototype(cx, cx->global()));
+    if (!proto)
+        return nullptr;
+
     RootedObject obj(cx, NewObjectWithGivenProto(cx, &class_, proto));
     if (!obj)
         return nullptr;
@@ -738,7 +747,10 @@ ModuleObject::isInstance(HandleValue value)
 /* static */ ModuleObject*
 ModuleObject::create(JSContext* cx)
 {
-    RootedObject proto(cx, cx->global()->getModulePrototype());
+    RootedObject proto(cx, GlobalObject::getOrCreateModulePrototype(cx, cx->global()));
+    if (!proto)
+        return nullptr;
+
     RootedObject obj(cx, NewObjectWithGivenProto(cx, &class_, proto));
     if (!obj)
         return nullptr;
