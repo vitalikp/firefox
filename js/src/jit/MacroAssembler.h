a2095b9e8a965325aa25fe7199b9000a1a73c532
Aanchal <aanchal138@gmail.com>
2018-01-31 07:51:20 +0300

    Bug 1431698 - Change the IntConversion enumerations to enum classes. r=bbouvier

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -2375,7 +2375,7 @@ class MacroAssembler : public MacroAssemblerSpecific
                            Label* truncateDoubleSlow,
                            Register stringReg, FloatRegister temp, Register output,
                            Label* fail, IntConversionBehavior behavior,
-                           IntConversionInputKind conversion = IntConversion_Any);
+                           IntConversionInputKind conversion = IntConversionInputKind::Any);
     void convertValueToInt(ValueOperand value, FloatRegister temp, Register output, Label* fail,
                            IntConversionBehavior behavior)
     {
@@ -2396,12 +2396,12 @@ class MacroAssembler : public MacroAssemblerSpecific
     void convertValueToInt32(ValueOperand value, MDefinition* input,
                              FloatRegister temp, Register output, Label* fail,
                              bool negativeZeroCheck,
-                             IntConversionInputKind conversion = IntConversion_Any)
+                             IntConversionInputKind conversion = IntConversionInputKind::Any)
     {
         convertValueToInt(value, input, nullptr, nullptr, nullptr, InvalidReg, temp, output, fail,
                           negativeZeroCheck
-                          ? IntConversion_NegativeZeroCheck
-                          : IntConversion_Normal,
+                          ? IntConversionBehavior::NegativeZeroCheck
+                          : IntConversionBehavior::Normal,
                           conversion);
     }
 
@@ -2413,7 +2413,7 @@ class MacroAssembler : public MacroAssemblerSpecific
                               Register stringReg, FloatRegister temp, Register output, Label* fail)
     {
         convertValueToInt(value, input, handleStringEntry, handleStringRejoin, truncateDoubleSlow,
-                          stringReg, temp, output, fail, IntConversion_Truncate);
+                          stringReg, temp, output, fail, IntConversionBehavior::Truncate);
     }
 
     void truncateValueToInt32(ValueOperand value, FloatRegister temp, Register output, Label* fail)
@@ -2427,7 +2427,7 @@ class MacroAssembler : public MacroAssemblerSpecific
                                                         FloatRegister temp, Register output,
                                                         Label* fail)
     {
-        return convertConstantOrRegisterToInt(cx, src, temp, output, fail, IntConversion_Truncate);
+        return convertConstantOrRegisterToInt(cx, src, temp, output, fail, IntConversionBehavior::Truncate);
     }
 
     // Convenience functions for clamping values to uint8.
@@ -2436,7 +2436,7 @@ class MacroAssembler : public MacroAssemblerSpecific
                            Register stringReg, FloatRegister temp, Register output, Label* fail)
     {
         convertValueToInt(value, input, handleStringEntry, handleStringRejoin, nullptr,
-                          stringReg, temp, output, fail, IntConversion_ClampToUint8);
+                          stringReg, temp, output, fail, IntConversionBehavior::ClampToUint8);
     }
 
     MOZ_MUST_USE bool clampConstantOrRegisterToUint8(JSContext* cx,
@@ -2445,7 +2445,7 @@ class MacroAssembler : public MacroAssemblerSpecific
                                                      Label* fail)
     {
         return convertConstantOrRegisterToInt(cx, src, temp, output, fail,
-                                              IntConversion_ClampToUint8);
+                                              IntConversionBehavior::ClampToUint8);
     }
 
   public:
