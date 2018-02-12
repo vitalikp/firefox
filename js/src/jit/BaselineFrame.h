a85898dc9efe24beac095aaeea6bc2a301acbaf9
Jason Orendorff <jorendorff@mozilla.com>
2018-02-12 13:38:44 -0600

    Bug 1429206 - Part 6: Rename jscompartment* -> vm/JSCompartment*. r=jandem.

--- js/src/jit/BaselineFrame.h
+++ js/src/jit/BaselineFrame.h
@@ -44,7 +44,7 @@ class BaselineFrame
 
         // Frame has execution observed by a Debugger.
         //
-        // See comment above 'isDebuggee' in jscompartment.h for explanation of
+        // See comment above 'isDebuggee' in vm/JSCompartment.h for explanation of
         // invariants of debuggee compartments, scripts, and frames.
         DEBUGGEE         = 1 << 6,
 
