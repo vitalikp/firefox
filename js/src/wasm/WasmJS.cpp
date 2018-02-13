05948d69848a009e8fd2cf94a7454b8499db769b
Benjamin Bouvier <benj@benj.me>
2018-02-13 14:46:28 +0300

    Bug 1422043: Use pointer arithmetic to determine a wasm function index; r=luke
    MozReview-Commit-ID: 8Dn0zqqmIdD

--- js/src/wasm/WasmJS.cpp
+++ js/src/wasm/WasmJS.cpp
@@ -1279,7 +1279,7 @@ wasm::ExportedFunctionToFuncIndex(JSFunction* fun)
 {
     MOZ_ASSERT(IsExportedFunction(fun));
     Instance& instance = ExportedFunctionToInstanceObject(fun)->instance();
-    return instance.code().lookupFuncIndex(fun);
+    return instance.code().getFuncIndex(fun);
 }
 
 // ============================================================================
