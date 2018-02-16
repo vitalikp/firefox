15ea1cb039a6430f5f99c63a9c15315208723ca6
Luke Wagner <luke@mozilla.com>
2018-02-15 20:42:43 -0600

    Bug 1428453 - Baldr: use new traps for indirect call to null trap (r=bbouvier)

--- js/src/wasm/WasmStubs.cpp
+++ js/src/wasm/WasmStubs.cpp
@@ -1397,6 +1397,7 @@ wasm::GenerateStubs(const ModuleEnvironment& env, const FuncImportVector& import
           case Trap::IntegerOverflow:
           case Trap::InvalidConversionToInteger:
           case Trap::IntegerDivideByZero:
+          case Trap::IndirectCallToNull:
           case Trap::ImpreciseSimdConversion:
           case Trap::StackOverflow:
           case Trap::ThrowReported:
@@ -1404,7 +1405,6 @@ wasm::GenerateStubs(const ModuleEnvironment& env, const FuncImportVector& import
           // The TODO list of "old" traps to convert to new traps:
           case Trap::OutOfBounds:
           case Trap::UnalignedAccess:
-          case Trap::IndirectCallToNull:
           case Trap::IndirectCallBadSig: {
             CallableOffsets offsets;
             if (!GenerateOldTrapExit(masm, trap, &throwLabel, &offsets))
