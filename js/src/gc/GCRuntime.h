9943d29196e284081857e1641691de2b77150a8d
Jon Coppeard <jcoppeard@mozilla.com>
2018-02-27 13:19:47 +0300

    Bug 1439284 - Preserve scheduled zones over call to GC end callback r=sfink

--- js/src/gc/GCRuntime.h
+++ js/src/gc/GCRuntime.h
@@ -552,8 +552,7 @@ class GCRuntime
                                  AutoTraceSession& session);
 
     friend class AutoCallGCCallbacks;
-    void maybeCallBeginCallback();
-    void maybeCallEndCallback();
+    void maybeCallGCCallback(JSGCStatus status);
 
     void pushZealSelectedObjects();
     void purgeRuntime();
@@ -919,7 +918,7 @@ class GCRuntime
 
     ActiveThreadData<bool> fullCompartmentChecks;
 
-    ActiveThreadData<uint32_t> gcBeginCallbackDepth;
+    ActiveThreadData<uint32_t> gcCallbackDepth;
 
     Callback<JSGCCallback> gcCallback;
     Callback<JS::DoCycleCollectionCallback> gcDoCycleCollectionCallback;
