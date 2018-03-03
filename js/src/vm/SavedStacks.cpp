ccc48b0945aee14726484bf67fe23e07fbdcc3af
Jim Blandy <jimb@mozilla.com>
2018-03-03 10:56:26 +0300

    Bug 1438121: Part 13: Make LiveSavedFrameCache::Entry's members const. r=fitzgen
    MozReview-Commit-ID: GbJhE9nylaa

--- js/src/vm/SavedStacks.cpp
+++ js/src/vm/SavedStacks.cpp
@@ -81,7 +81,7 @@ LiveSavedFrameCache::trace(JSTracer* trc)
 }
 
 bool
-LiveSavedFrameCache::insert(JSContext* cx, FramePtr& framePtr, jsbytecode* pc,
+LiveSavedFrameCache::insert(JSContext* cx, FramePtr& framePtr, const jsbytecode* pc,
                             HandleSavedFrame savedFrame)
 {
     MOZ_ASSERT(initialized());
@@ -112,7 +112,7 @@ LiveSavedFrameCache::find(JSContext* cx, FrameIter& frameIter, MutableHandleSave
     MOZ_ASSERT(maybeFramePtr.isSome());
 
     FramePtr framePtr(*maybeFramePtr);
-    jsbytecode* pc = frameIter.pc();
+    const jsbytecode* pc = frameIter.pc();
     size_t numberStillValid = 0;
 
     frame.set(nullptr);
