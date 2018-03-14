4931b38c58b7aafda98a849bd2e35237e5330b6a
Luke Wagner <luke@mozilla.com>
2018-03-14 22:33:20 +0300

    Bug 1435360 - Baldr: do baseline stack-overflow check after initializing DebugFrame (again) (r=lth)

--- js/src/wasm/WasmBaselineCompile.cpp
+++ js/src/wasm/WasmBaselineCompile.cpp
@@ -1451,7 +1451,7 @@ class BaseStackFrame final : public BaseStackFrameAllocator {
     // Note the platform scratch register may be used by branchPtr(), so
     // generally tmp must be something else.
 
-    void allocStack(Register tmp, BytecodeOffset trapOffset) {
+    void checkStack(Register tmp, BytecodeOffset trapOffset) {
         stackAddOffset_ = masm.sub32FromStackPtrWithPatch(tmp);
         Label ok;
         masm.branchPtr(Assembler::Below,
@@ -1461,9 +1461,8 @@ class BaseStackFrame final : public BaseStackFrameAllocator {
         masm.bind(&ok);
     }
 
-    void patchAllocStack() {
-        masm.patchSub32FromStackPtr(stackAddOffset_,
-                                    Imm32(int32_t(maxFramePushed_)));
+    void patchCheckStack() {
+        masm.patchSub32FromStackPtr(stackAddOffset_, Imm32(int32_t(maxFramePushed_)));
     }
 
     // Very large frames are implausible, probably an attack.
@@ -1499,11 +1498,6 @@ class BaseStackFrame final : public BaseStackFrameAllocator {
     };
 
     typedef Vector<Local, 8, SystemAllocPolicy> LocalVector;
-
-    void endFunctionPrologue() {
-        MOZ_ASSERT(masm.framePushed() == fixedAllocSize());
-        MOZ_ASSERT(fixedAllocSize() % WasmStackAlignment == 0);
-    }
 
     // Initialize `localInfo` based on the types of `locals` and `args`.
     bool setupLocals(const ValTypeVector& locals, const ValTypeVector& args, bool debugEnabled,
@@ -3296,26 +3290,27 @@ class BaseCompiler final : public BaseCompilerInterface
     void beginFunction() {
         JitSpew(JitSpew_Codegen, "# Emitting wasm baseline code");
 
-        // We are unconditionally checking for overflow in fr.allocStack(), so
-        // pass IsLeaf = true to avoid a second check in the prologue.
-        IsLeaf isLeaf = true;
-        SigIdDesc sigId = env_.funcSigs[func_.index]->id;
-        BytecodeOffset trapOffset(func_.lineOrBytecode);
-        GenerateFunctionPrologue(masm, fr.fixedAllocSize(), isLeaf, sigId, trapOffset, &offsets_,
-                                 mode_ == CompileMode::Tier1 ? Some(func_.index) : Nothing());
+        GenerateFunctionPrologue(masm,
+                                 env_.funcSigs[func_.index]->id,
+                                 mode_ == CompileMode::Tier1 ? Some(func_.index) : Nothing(),
+                                 &offsets_);
 
-        fr.endFunctionPrologue();
-
+        // Initialize DebugFrame fields before the stack overflow trap so that
+        // we have the invariant that all observable Frames in a debugEnabled
+        // Module have valid DebugFrames.
         if (debugEnabled_) {
-            // Initialize funcIndex and flag fields of DebugFrame.
-            size_t debugFrame = masm.framePushed() - DebugFrame::offsetOfFrame();
+#ifdef JS_CODEGEN_ARM64
+            static_assert(DebugFrame::offsetOfFrame() % WasmStackAlignment == 0, "aligned");
+#endif
+            masm.reserveStack(DebugFrame::offsetOfFrame());
             masm.store32(Imm32(func_.index),
-                         Address(masm.getStackPointer(), debugFrame + DebugFrame::offsetOfFuncIndex()));
+                         Address(masm.getStackPointer(), DebugFrame::offsetOfFuncIndex()));
             masm.storePtr(ImmWord(0),
-                          Address(masm.getStackPointer(), debugFrame + DebugFrame::offsetOfFlagsWord()));
+                          Address(masm.getStackPointer(), DebugFrame::offsetOfFlagsWord()));
         }
 
-        fr.allocStack(ABINonArgReg0, trapOffset);
+        fr.checkStack(ABINonArgReg0, BytecodeOffset(func_.lineOrBytecode));
+        masm.reserveStack(fr.fixedAllocSize() - masm.framePushed());
         fr.onFixedStackAllocated();
 
         // Copy arguments from registers to stack.
@@ -3412,7 +3407,7 @@ class BaseCompiler final : public BaseCompilerInterface
         if (masm.oom())
             return false;
 
-        fr.patchAllocStack();
+        fr.patchCheckStack();
 
         masm.bind(&returnLabel_);
 
