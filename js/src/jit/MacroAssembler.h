66e7023d5f220fce3a3cf0ab415cbf79dac84301
Jan de Mooij <jdemooij@mozilla.com>
2018-03-21 18:59:07 +0300

    Bug 1447578 part 5 - Assert WasmMacroAssembler does not have GC relocation data. r=jonco

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -2574,6 +2574,9 @@ class MOZ_RAII WasmMacroAssembler : public MacroAssembler
     explicit WasmMacroAssembler(TempAllocator& alloc)
       : MacroAssembler(WasmToken(), alloc)
     {}
+    ~WasmMacroAssembler() {
+        assertNoGCThings();
+    }
 };
 
 // Heap-allocated MacroAssembler used for Ion off-thread code generation.
