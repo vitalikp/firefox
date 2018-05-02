4f9300b923edf9711c7c6ca317e94a65e6fd44ca
Jeff Walden <jwalden@mit.edu>
2018-05-02 05:06:27 +0300

    Bug 1459384 - Move decimal number tokenizing into a separate function and eliminate the final two labels from TokenStreamSpecific::getTokenInternal. r=arai

--- js/src/frontend/TokenStream.cpp
+++ js/src/frontend/TokenStream.cpp
@@ -1530,6 +1530,102 @@ GeneralTokenStreamChars<CharT, AnyCharsAccess>::consumeRestOfSingleLineComment()
 
 template<typename CharT, class AnyCharsAccess>
 MOZ_MUST_USE bool
+TokenStreamSpecific<CharT, AnyCharsAccess>::decimalNumber(int c, Token* tp,
+                                                          const CharT* numStart)
+{
+    // Consume integral component digits.
+    while (JS7_ISDEC(c))
+        c = getCharIgnoreEOL();
+
+    // Numbers contain no escapes, so we can read directly from |sourceUnits|.
+    double dval;
+    DecimalPoint decimalPoint = NoDecimal;
+    if (c != '.' && c != 'e' && c != 'E') {
+        ungetCharIgnoreEOL(c);
+
+        // Most numbers are pure decimal integers without fractional component
+        // or exponential notation.  Handle that with optimized code.
+        if (!GetDecimalInteger(anyCharsAccess().cx, numStart, sourceUnits.addressOfNextCodeUnit(),
+                               &dval))
+        {
+            return false;
+        }
+    } else {
+        // Consume any decimal dot and fractional component.
+        if (c == '.') {
+            decimalPoint = HasDecimal;
+            do {
+                c = getCharIgnoreEOL();
+            } while (JS7_ISDEC(c));
+        }
+
+        // Consume any exponential notation.
+        if (c == 'e' || c == 'E') {
+            c = getCharIgnoreEOL();
+            if (c == '+' || c == '-')
+                c = getCharIgnoreEOL();
+
+            // Exponential notation must contain at least one digit.
+            if (!JS7_ISDEC(c)) {
+                ungetCharIgnoreEOL(c);
+                error(JSMSG_MISSING_EXPONENT);
+                return false;
+            }
+
+            // Consume exponential digits.
+            do {
+                c = getCharIgnoreEOL();
+            } while (JS7_ISDEC(c));
+        }
+
+        ungetCharIgnoreEOL(c);
+
+        const CharT* dummy;
+        if (!js_strtod(anyCharsAccess().cx, numStart, sourceUnits.addressOfNextCodeUnit(), &dummy,
+                       &dval))
+        {
+           return false;
+        }
+    }
+
+    // Number followed by IdentifierStart is an error.  (This is the only place
+    // in ECMAScript where token boundary is inadequate to properly separate
+    // two tokens, necessitating this unaesthetic lookahead.)
+    if (c != EOF) {
+        if (unicode::IsIdentifierStart(char16_t(c))) {
+            error(JSMSG_IDSTART_AFTER_NUMBER);
+            return false;
+        }
+
+        consumeKnownCharIgnoreEOL(c);
+
+        uint32_t codePoint;
+        if (!matchMultiUnitCodePoint(c, &codePoint))
+            return false;
+
+        if (codePoint) {
+            // In all cases revert the get of the overall code point.
+            ungetCodePointIgnoreEOL(codePoint);
+
+            if (unicode::IsIdentifierStart(codePoint)) {
+                // This will properly point at the start of the code point.
+                error(JSMSG_IDSTART_AFTER_NUMBER);
+                return false;
+            }
+        } else {
+            // If not a multi-unit code point, we only need to unget the single
+            // code unit consumed.
+            ungetCharIgnoreEOL(c);
+        }
+    }
+
+    tp->type = TokenKind::Number;
+    tp->setNumber(dval, decimalPoint);
+    return true;
+}
+
+template<typename CharT, class AnyCharsAccess>
+MOZ_MUST_USE bool
 TokenStreamSpecific<CharT, AnyCharsAccess>::getTokenInternal(TokenKind* const ttp,
                                                              const Modifier modifier)
 {
@@ -1586,9 +1682,6 @@ TokenStreamSpecific<CharT, AnyCharsAccess>::getTokenInternal(TokenKind* const tt
     do {
         int c;
         FirstCharKind c1kind;
-        bool hasExp;
-        const CharT* numStart;
-        DecimalPoint decimalPoint;
 
         if (MOZ_UNLIKELY(!sourceUnits.hasRawChars())) {
             tp = newToken(0);
@@ -1720,82 +1813,13 @@ TokenStreamSpecific<CharT, AnyCharsAccess>::getTokenInternal(TokenKind* const tt
         //
         if (c1kind == Dec) {
             tp = newToken(-1);
-            numStart = sourceUnits.addressOfNextCodeUnit() - 1;
 
-          decimal:
-            decimalPoint = NoDecimal;
-            hasExp = false;
-            while (JS7_ISDEC(c))
-                c = getCharIgnoreEOL();
-
-            if (c == '.') {
-                decimalPoint = HasDecimal;
-              decimal_dot:
-                do {
-                    c = getCharIgnoreEOL();
-                } while (JS7_ISDEC(c));
-            }
-            if (c == 'e' || c == 'E') {
-                hasExp = true;
-                c = getCharIgnoreEOL();
-                if (c == '+' || c == '-')
-                    c = getCharIgnoreEOL();
-
-                if (!JS7_ISDEC(c)) {
-                    ungetCharIgnoreEOL(c);
-                    reportError(JSMSG_MISSING_EXPONENT);
-                    BadToken();
-                    return false;
-                }
-
-                do {
-                    c = getCharIgnoreEOL();
-                } while (JS7_ISDEC(c));
-            }
-            ungetCharIgnoreEOL(c);
-
-            if (c != EOF) {
-                if (unicode::IsIdentifierStart(char16_t(c))) {
-                    reportError(JSMSG_IDSTART_AFTER_NUMBER);
-                    BadToken();
-                    return false;
-                }
-
-                uint32_t codePoint;
-                if (!matchMultiUnitCodePoint(c, &codePoint)) {
-                    BadToken();
-                    return false;
-                }
-                if (codePoint && unicode::IsIdentifierStart(codePoint)) {
-                    reportError(JSMSG_IDSTART_AFTER_NUMBER);
-                    BadToken();
-                    return false;
-                }
+            const CharT* numStart = sourceUnits.addressOfNextCodeUnit() - 1;
+            if (!decimalNumber(c, tp, numStart)) {
+                BadToken();
+                return false;
             }
 
-            // Unlike identifiers and strings, numbers cannot contain escaped
-            // chars, so we don't need to use tokenbuf.  Instead we can just
-            // convert the char16_t characters in sourceUnits to the numeric
-            // value.
-            double dval;
-            if (!((decimalPoint == HasDecimal) || hasExp)) {
-                if (!GetDecimalInteger(anyCharsAccess().cx, numStart,
-                                       sourceUnits.addressOfNextCodeUnit(), &dval))
-                {
-                    BadToken();
-                    return false;
-                }
-            } else {
-                const CharT* dummy;
-                if (!js_strtod(anyCharsAccess().cx, numStart, sourceUnits.addressOfNextCodeUnit(),
-                               &dummy, &dval))
-                {
-                    BadToken();
-                    return false;
-                }
-            }
-            tp->type = TokenKind::Number;
-            tp->setNumber(dval, decimalPoint);
             FinishToken();
             return true;
         }
@@ -1836,6 +1860,7 @@ TokenStreamSpecific<CharT, AnyCharsAccess>::getTokenInternal(TokenKind* const tt
             tp = newToken(-1);
 
             int radix;
+            const CharT* numStart;
             c = getCharIgnoreEOL();
             if (c == 'x' || c == 'X') {
                 radix = 16;
@@ -1887,7 +1912,7 @@ TokenStreamSpecific<CharT, AnyCharsAccess>::getTokenInternal(TokenKind* const tt
                 // one past the '0'
                 numStart = sourceUnits.addressOfNextCodeUnit() - 1;
 
-                while (JS7_ISDEC(c)) {
+                do {
                     // Octal integer literals are not permitted in strict mode
                     // code.
                     if (!reportStrictModeError(JSMSG_DEPRECATED_OCTAL)) {
@@ -1906,33 +1931,61 @@ TokenStreamSpecific<CharT, AnyCharsAccess>::getTokenInternal(TokenKind* const tt
                         }
 
                         // Use the decimal scanner for the rest of the number.
-                        goto decimal;
+                        if (!decimalNumber(c, tp, numStart)) {
+                            BadToken();
+                            return false;
+                        }
+
+                        FinishToken();
+                        return true;
                     }
+
                     c = getCharIgnoreEOL();
-                }
+                } while (JS7_ISDEC(c));
             } else {
-                // '0' not followed by [xX0-9];  scan as a decimal number.
+                // '0' not followed by [XxBbOo0-9];  scan as a decimal number.
                 numStart = sourceUnits.addressOfNextCodeUnit() - 1;
-                goto decimal;
+
+                if (!decimalNumber(c, tp, numStart)) {
+                    BadToken();
+                    return false;
+                }
+
+                FinishToken();
+                return true;
             }
             ungetCharIgnoreEOL(c);
 
             if (c != EOF) {
                 if (unicode::IsIdentifierStart(char16_t(c))) {
-                    reportError(JSMSG_IDSTART_AFTER_NUMBER);
+                    error(JSMSG_IDSTART_AFTER_NUMBER);
                     BadToken();
                     return false;
                 }
+
+                consumeKnownCharIgnoreEOL(c);
 
                 uint32_t codePoint;
                 if (!matchMultiUnitCodePoint(c, &codePoint)) {
                     BadToken();
                     return false;
                 }
-                if (codePoint && unicode::IsIdentifierStart(codePoint)) {
-                    reportError(JSMSG_IDSTART_AFTER_NUMBER);
-                    BadToken();
-                    return false;
+
+                if (codePoint) {
+                    // In all cases revert the get of the overall code point.
+                    ungetCodePointIgnoreEOL(codePoint);
+
+                    if (unicode::IsIdentifierStart(codePoint)) {
+                        // This will properly point at the start of the code
+                        // point.
+                        error(JSMSG_IDSTART_AFTER_NUMBER);
+                        BadToken();
+                        return false;
+                    }
+                } else {
+                    // If not a multi-unit code point, we only need to unget
+                    // the single code unit consumed.
+                    ungetCharIgnoreEOL(c);
                 }
             }
 
@@ -1959,11 +2012,17 @@ TokenStreamSpecific<CharT, AnyCharsAccess>::getTokenInternal(TokenKind* const tt
           case '.':
             c = getCharIgnoreEOL();
             if (JS7_ISDEC(c)) {
-                numStart = sourceUnits.addressOfNextCodeUnit() - 2;
-                decimalPoint = HasDecimal;
-                hasExp = false;
-                goto decimal_dot;
+                const CharT* numStart = sourceUnits.addressOfNextCodeUnit() - 2;
+
+                if (!decimalNumber('.', tp, numStart)) {
+                    BadToken();
+                    return false;
+                }
+
+                FinishToken();
+                return true;
             }
+
             if (c == '.') {
                 if (matchChar('.')) {
                     tp->type = TokenKind::TripleDot;
