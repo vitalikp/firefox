746ff440ed13615ba8c7ad32628a0aac90782a81
Jeff Walden <jwalden@mit.edu>
2018-05-04 08:02:32 +0300

    Bug 1459384 - Make getDirectives handle calling badToken() itself if needed. r=arai

--- js/src/frontend/TokenStream.cpp
+++ js/src/frontend/TokenStream.cpp
@@ -1151,8 +1151,12 @@ TokenStreamSpecific<CharT, AnyCharsAccess>::getDirectives(bool isMultiline,
     // comment. To avoid potentially expensive lookahead and backtracking, we
     // only check for this case if we encounter a '#' character.
 
-    return getDisplayURL(isMultiline, shouldWarnDeprecated) &&
-           getSourceMappingURL(isMultiline, shouldWarnDeprecated);
+    bool res = getDisplayURL(isMultiline, shouldWarnDeprecated) &&
+               getSourceMappingURL(isMultiline, shouldWarnDeprecated);
+    if (!res)
+        badToken();
+
+    return res;
 }
 
 template<>
@@ -2145,7 +2149,7 @@ TokenStreamSpecific<CharT, AnyCharsAccess>::getTokenInternal(TokenKind* const tt
                 if (c == '@' || c == '#') {
                     bool shouldWarn = c == '@';
                     if (!getDirectives(false, shouldWarn))
-                        return badToken();
+                        return false;
                 } else {
                     ungetCharIgnoreEOL(c);
                 }
@@ -2174,7 +2178,7 @@ TokenStreamSpecific<CharT, AnyCharsAccess>::getTokenInternal(TokenKind* const tt
                     if (c == '@' || c == '#') {
                         bool shouldWarn = c == '@';
                         if (!getDirectives(true, shouldWarn))
-                            return badToken();
+                            return false;
                     }
                 } while (true);
 
