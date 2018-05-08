b409329f32cf294cde8c54536699547181ba1f2b
Luke Wagner <luke@mozilla.com>
2018-05-08 21:25:00 +0300

    Bug 1458029 - Baldr: tweak function display name interface (r=bbouvier)

--- js/src/vm/SavedStacks.cpp
+++ js/src/vm/SavedStacks.cpp
@@ -1390,7 +1390,9 @@ SavedStacks::insertFrames(JSContext* cx, MutableHandleSavedFrame frame,
             if (!cx->compartment()->savedStacks().getLocation(cx, iter, &location))
                 return false;
         }
-        auto displayAtom = (iter.isWasm() || iter.isFunctionFrame()) ? iter.functionDisplayAtom() : nullptr;
+
+        RootedAtom displayAtom(cx, iter.maybeFunctionDisplayAtom());
+
         auto principals = iter.compartment()->principals();
         MOZ_ASSERT_IF(framePtr && !iter.isWasm(), iter.pc());
 
