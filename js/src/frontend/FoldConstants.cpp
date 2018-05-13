f9ab9187463ba85ac833e7c9207ef65936cb62d3
Jan de Mooij <jdemooij@mozilla.com>
2018-05-13 13:24:23 +0300

    Bug 1461034 - Don't clear an OOM exception in FoldElement. r=jonco

--- js/src/frontend/FoldConstants.cpp
+++ js/src/frontend/FoldConstants.cpp
@@ -1239,10 +1239,8 @@ FoldElement(JSContext* cx, ParseNode** nodePtr, PerHandlerParser<FullParseHandle
             // isn't an array index, so it converts to a string ("3.14"),
             // enabling optimization 3 below.
             JSAtom* atom = NumberToAtom(cx, number);
-            if (!atom) {
-                cx->recoverFromOutOfMemory();
+            if (!atom)
                 return false;
-            }
             name = atom->asPropertyName();
         }
     }
