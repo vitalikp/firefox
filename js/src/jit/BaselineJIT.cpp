37f5d619e9fcf920f7e98406d715cca69e1d8122
Jason Orendorff <jorendorff@mozilla.com>
2018-06-27 20:00:55 +0300

    Bug 1440481 - Part 1: Make Debugger stepping work the same in baseline as in the interpreter. r=nbp

--- js/src/jit/BaselineJIT.cpp
+++ js/src/jit/BaselineJIT.cpp
@@ -912,8 +912,6 @@ BaselineScript::toggleDebugTraps(JSScript* script, jsbytecode* pc)
     if (!hasDebugInstrumentation())
         return;
 
-    SrcNoteLineScanner scanner(script->notes(), script->lineno());
-
     AutoWritableJitCode awjc(method());
 
     for (uint32_t i = 0; i < numPCMappingIndexEntries(); i++) {
@@ -930,11 +928,8 @@ BaselineScript::toggleDebugTraps(JSScript* script, jsbytecode* pc)
             if (b & 0x80)
                 nativeOffset += reader.readUnsigned();
 
-            scanner.advanceTo(script->pcToOffset(curPC));
-
             if (!pc || pc == curPC) {
-                bool enabled = (script->stepModeEnabled() && scanner.isLineHeader()) ||
-                    script->hasBreakpointsAt(curPC);
+                bool enabled = script->stepModeEnabled() || script->hasBreakpointsAt(curPC);
 
                 // Patch the trap.
                 CodeLocationLabel label(method(), CodeOffset(nativeOffset));
