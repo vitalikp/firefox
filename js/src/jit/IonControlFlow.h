4c7605639dcecda4000e2aa2dd2557cc1a8e87df
Tooru Fujisawa <arai_a@mac.com>
2018-08-10 01:49:18 +0300

    Bug 1456404 - Part 7: Remove unused code/comment for SRC_FOR. r=jwalden

--- js/src/jit/IonControlFlow.h
+++ js/src/jit/IonControlFlow.h
@@ -886,7 +886,6 @@ class ControlFlowGenerator
     ControlStatus processContinue(JSOp op);
     ControlStatus processBreak(JSOp op, jssrcnote* sn);
     ControlStatus processReturn(JSOp op);
-    ControlStatus maybeLoop(JSOp op, jssrcnote* sn);
     ControlStatus snoopControlFlow(JSOp op);
     ControlStatus processBrokenLoop(CFGState& state);
     ControlStatus finishLoop(CFGState& state, CFGBlock* successor);
