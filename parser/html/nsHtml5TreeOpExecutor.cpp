3313b9ca61a9f1624aa8d824511dbcad9fecb478
Henri Sivonen <hsivonen@hsivonen.fi>
2018-12-12 14:24:31 +0000

    Bug 1510114. r=bzbarsky
    Differential Revision: https://phabricator.services.mozilla.com/D13679

--- parser/html/nsHtml5TreeOpExecutor.cpp
+++ parser/html/nsHtml5TreeOpExecutor.cpp
@@ -355,13 +355,18 @@ nsHtml5TreeOpExecutor::RunFlushLoop()
     // There's already a RunFlushLoop() on the call stack.
     return;
   }
-  
+
   nsHtml5FlushLoopGuard guard(this); // this is also the self-kungfu!
-  
+
   RefPtr<nsParserBase> parserKungFuDeathGrip(mParser);
+  RefPtr<nsHtml5StreamParser> streamParserGrip;
+  if (mParser) {
+    streamParserGrip = GetParser()->GetStreamParser();
+  }
+  mozilla::Unused << streamParserGrip; // Intentionally not used within function
 
   // Remember the entry time
-  (void) nsContentSink::WillParseImpl();
+  (void)nsContentSink::WillParseImpl();
 
   for (;;) {
     if (!mParser) {
@@ -417,11 +422,6 @@ nsHtml5TreeOpExecutor::RunFlushLoop()
         mOpQueue.Clear(); // clear in order to be able to assert in destructor
         return;
       }
-      // Not sure if this grip is still needed, but previously, the code
-      // gripped before calling ParseUntilBlocked();
-      RefPtr<nsHtml5StreamParser> streamKungFuDeathGrip = 
-        GetParser()->GetStreamParser();
-      mozilla::Unused << streamKungFuDeathGrip; // Not used within function
       // Now parse content left in the document.write() buffer queue if any.
       // This may generate tree ops on its own or dequeue a speculation.
       nsresult rv = GetParser()->ParseUntilBlocked();
@@ -539,6 +539,11 @@ nsHtml5TreeOpExecutor::FlushDocumentWrite()
   RefPtr<nsHtml5TreeOpExecutor> kungFuDeathGrip(this);
   RefPtr<nsParserBase> parserKungFuDeathGrip(mParser);
   mozilla::Unused << parserKungFuDeathGrip; // Intentionally not used within function
+  RefPtr<nsHtml5StreamParser> streamParserGrip;
+  if (mParser) {
+    streamParserGrip = GetParser()->GetStreamParser();
+  }
+  mozilla::Unused << streamParserGrip; // Intentionally not used within function
 
   NS_ASSERTION(!mReadingFromStage,
     "Got doc write flush when reading from stage");
