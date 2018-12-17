debc31439c71feb923a3c5e6f8b151cac13d09e6
Luke Wagner <luke@mozilla.com>
2018-12-17 10:12:01 -0600

    Bug 1514143 - Only use trap state for unwinding innermost wasm frame (r=lth)

--- js/src/wasm/WasmFrameIter.cpp
+++ js/src/wasm/WasmFrameIter.cpp
@@ -47,9 +47,11 @@ WasmFrameIter::WasmFrameIter(JitActivation* activation, wasm::Frame* fp)
 
     // When the stack is captured during a trap (viz., to create the .stack
     // for an Error object), use the pc/bytecode information captured by the
-    // signal handler in the runtime.
+    // signal handler in the runtime. Take care not to use this trap unwind
+    // state for wasm frames in the middle of a JitActivation, i.e., wasm frames
+    // that called into JIT frames before the trap.
 
-    if (activation->isWasmTrapping()) {
+    if (activation->isWasmTrapping() && fp_ == activation->wasmExitFP()) {
         code_ = &fp_->tls->instance->code();
         MOZ_ASSERT(code_ == LookupCode(activation->wasmTrapPC()));
 
