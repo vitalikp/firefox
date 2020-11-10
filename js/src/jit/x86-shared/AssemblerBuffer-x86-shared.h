Floorp-Projects/Floorp

Bug 1322445 - Extend PageProtectingVector to protect unused pages as well. r=jandem
--HG--
extra : rebase_source : 0db66dfc1ffab86dbdac0640843131161207b151

js/src/jit/x86-shared/AssemblerBuffer-x86-shared.h
@@ -84,12 +84,7 @@ namespace jit {
         }
 
     public:
-        AssemblerBuffer()
-            : m_oom(false)
-        {
-            // Provide memory protection once the buffer starts to get big.
-            m_buffer.setLowerBoundForProtection(32 * 1024);
-        }
+        AssemblerBuffer() : m_oom(false) {}
 
         void ensureSpace(size_t space)
         {
@@ -141,13 +136,6 @@ namespace jit {
             return m_buffer.begin();
         }
 
-        void unprotectDataRegion(size_t firstByteOffset, size_t lastByteOffset) {
-            m_buffer.unprotectRegion(firstByteOffset, lastByteOffset);
-        }
-        void reprotectDataRegion(size_t firstByteOffset, size_t lastByteOffset) {
-            m_buffer.reprotectRegion(firstByteOffset, lastByteOffset);
-        }
-
     protected:
         /*
          * OOM handling: This class can OOM in the ensureSpace() method trying
@@ -168,7 +156,13 @@ namespace jit {
             m_buffer.clear();
         }
 
-        PageProtectingVector<unsigned char, 256, SystemAllocPolicy> m_buffer;
+#ifndef RELEASE_OR_BETA
+        PageProtectingVector<unsigned char, 256, SystemAllocPolicy,
+                             /* ProtectUsed = */ false, /* ProtectUnused = */ true,
+                             /* InitialLowerBound = */ 32 * 1024> m_buffer;
+#else
+        mozilla::Vector<unsigned char, 256, SystemAllocPolicy> m_buffer;
+#endif
         bool m_oom;
     };
 

vitalikp/firefox

js/src/jit/x86-shared/AssemblerBuffer-x86-shared.h
@@ -111,12 +111,7 @@ namespace jit {
         }
 
     public:
-        AssemblerBuffer()
-            : m_oom(false)
-        {
-            // Provide memory protection once the buffer starts to get big.
-            m_buffer.setLowerBoundForProtection(32 * 1024);
-        }
+        AssemblerBuffer() : m_oom(false) {}
 
         void ensureSpace(size_t space)
         {
@@ -171,13 +166,6 @@ namespace jit {
             return m_buffer.begin();
         }
 
-        void unprotectDataRegion(size_t firstByteOffset, size_t lastByteOffset) {
-            m_buffer.unprotectRegion(firstByteOffset, lastByteOffset);
-        }
-        void reprotectDataRegion(size_t firstByteOffset, size_t lastByteOffset) {
-            m_buffer.reprotectRegion(firstByteOffset, lastByteOffset);
-        }
-
     protected:
         /*
          * OOM handling: This class can OOM in the ensureSpace() method trying
@@ -198,7 +186,13 @@ namespace jit {
             m_buffer.clear();
         }
 
-        PageProtectingVector<unsigned char, 256, AssemblerBufferAllocPolicy> m_buffer;
+#ifndef RELEASE_OR_BETA
+        PageProtectingVector<unsigned char, 256, AssemblerBufferAllocPolicy,
+                             /* ProtectUsed = */ false, /* ProtectUnused = */ true,
+                             /* InitialLowerBound = */ 32 * 1024> m_buffer;
+#else
+        mozilla::Vector<unsigned char, 256, AssemblerBufferAllocPolicy> m_buffer;
+#endif
         bool m_oom;
     };
 