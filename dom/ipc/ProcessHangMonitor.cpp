653797c8c7d7b63794de904738fbfda880181950
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2020-12-20 10:23:37 +0200

    Revert "Bug 1328423 - Remove force paint assertion (r=dvander, a=jcristau)"
    This reverts commit a891cb2fa60257c3a8cb84bb0356fba508e42f3e.

--- dom/ipc/ProcessHangMonitor.cpp
+++ dom/ipc/ProcessHangMonitor.cpp
@@ -313,6 +313,7 @@ HangMonitorChild::InterruptCallback()
   if (forcePaint) {
     RefPtr<TabChild> tabChild = TabChild::FindTabChild(forcePaintTab);
     if (tabChild) {
+      JS::AutoAssertNoGC nogc(mContext);
       tabChild->ForcePaint(forcePaintEpoch);
     }
   }
