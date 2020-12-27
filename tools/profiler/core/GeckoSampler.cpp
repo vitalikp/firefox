f6c63214e497c3cc37ceb22a1bb645c68457a258
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2020-12-27 18:52:48 +0200

    Revert "Bug 1322559 - Grab the registers when using LUL. r=jseward, a=jcristau"
    This reverts commit d13a909fc8a10c8a691f4fc386b290a995d18f32.

--- tools/profiler/core/GeckoSampler.cpp
+++ tools/profiler/core/GeckoSampler.cpp
@@ -1279,7 +1279,7 @@ SyncProfile* GeckoSampler::GetBacktrace()
   TickSample sample;
   sample.threadProfile = profile;
 
-#if defined(HAVE_NATIVE_UNWIND) || defined(USE_LUL_STACKWALK)
+#if defined(HAVE_NATIVE_UNWIND)
 #if defined(XP_WIN) || defined(LINUX)
   tickcontext_t context;
   sample.PopulateContext(&context);
