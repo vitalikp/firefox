451aa41f57888b0c08690909713cce2cfe0ba9d3
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2021-01-12 22:27:00 +0200

    Revert "Bug 1319951 follow-up: Fix build bustage a=shutthehookup"
    This reverts commit b0440bb8c935c3ad0f471293bb5a4417e85f0a50.

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -3043,11 +3043,11 @@ nsDocShell::GetSessionStorageForPrincipal(nsIPrincipal* aPrincipal,
   AssertOriginAttributesMatchPrivateBrowsing();
   if (aCreate) {
     return manager->CreateStorage(domWin->GetCurrentInnerWindow(), aPrincipal,
-                                  aDocumentURI, UsePrivateBrowsing(), aStorage);
+                                  aDocumentURI, aStorage);
   }
 
   return manager->GetStorage(domWin->GetCurrentInnerWindow(), aPrincipal,
-                             UsePrivateBrowsing(), aStorage);
+                             aStorage);
 }
 
 nsresult
