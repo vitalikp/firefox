Floorp-Projects/Floorp

Bug 1323129 part 1: Remove support for installing multiple xpis in InstallTrigger r=rhelmer
MozReview-Commit-ID: 567cHYjbjO4

--HG--
extra : rebase_source : c386aa73b5b3e895b7bab5415b1490a210d3f263
extra : intermediate-source : 99ab629f9cadbd2404d1c62a484b558475005959
extra : source : 070c629adccf921965486524a3c65ce63b5323c1

toolkit/mozapps/extensions/addonManager.js
@@ -21,7 +21,7 @@ const UNSUPPORTED_TYPE  = -244;
 const SUCCESS           = 0;
 
 const MSG_INSTALL_ENABLED  = "WebInstallerIsInstallEnabled";
-const MSG_INSTALL_ADDONS   = "WebInstallerInstallAddonsFromWebpage";
+const MSG_INSTALL_ADDON    = "WebInstallerInstallAddonFromWebpage";
 const MSG_INSTALL_CALLBACK = "WebInstallerInstallCallback";
 
 const MSG_PROMISE_REQUEST  = "WebAPIPromiseRequest";
@@ -44,7 +44,7 @@ function amManager() {
 
   Services.mm.loadFrameScript(CHILD_SCRIPT, true);
   Services.mm.addMessageListener(MSG_INSTALL_ENABLED, this);
-  Services.mm.addMessageListener(MSG_INSTALL_ADDONS, this);
+  Services.mm.addMessageListener(MSG_INSTALL_ADDON, this);
   Services.mm.addMessageListener(MSG_PROMISE_REQUEST, this);
   Services.mm.addMessageListener(MSG_INSTALL_CLEANUP, this);
   Services.mm.addMessageListener(MSG_ADDON_EVENT_REQ, this);
@@ -80,74 +80,53 @@ amManager.prototype = {
     return !!id.value;
   },
 
-  /**
-   * @see amIWebInstaller.idl
-   */
-  isInstallEnabled(aMimetype, aReferer) {
-    return AddonManager.isInstallEnabled(aMimetype);
-  },
-
-  /**
-   * @see amIWebInstaller.idl
-   */
-  installAddonsFromWebpage(aMimetype, aBrowser, aInstallingPrincipal,
-                                     aUris, aHashes, aNames, aIcons, aCallback) {
-    if (aUris.length == 0)
-      return false;
-
+  installAddonFromWebpage(aMimetype, aBrowser, aInstallingPrincipal,
+                                    aUri, aHash, aName, aIcon, aCallback) {
     let retval = true;
     if (!AddonManager.isInstallAllowed(aMimetype, aInstallingPrincipal)) {
       aCallback = null;
       retval = false;
     }
 
-    let installs = [];
-    function buildNextInstall() {
-      if (aUris.length == 0) {
-        AddonManager.installAddonsFromWebpage(aMimetype, aBrowser, aInstallingPrincipal, installs);
+    AddonManager.getInstallForURL(aUri, function(aInstall) {
+      function callCallback(uri, status) {
+        try {
+          aCallback.onInstallEnded(uri, status);
+        } catch (e) {
+          Components.utils.reportError(e);
+        }
+      }
+
+      if (!aInstall) {
+        aCallback.onInstallEnded(aUri, UNSUPPORTED_TYPE);
         return;
       }
-      let uri = aUris.shift();
-      AddonManager.getInstallForURL(uri, function(aInstall) {
-        function callCallback(aUri, aStatus) {
-          try {
-            aCallback.onInstallEnded(aUri, aStatus);
-          } catch (e) {
-            Components.utils.reportError(e);
+
+      if (aCallback) {
+        aInstall.addListener({
+          onDownloadCancelled(aInstall) {
+            callCallback(aUri, USER_CANCELLED);
+          },
+
+          onDownloadFailed(aInstall) {
+            if (aInstall.error == AddonManager.ERROR_CORRUPT_FILE)
+              callCallback(aUri, CANT_READ_ARCHIVE);
+            else
+              callCallback(aUri, DOWNLOAD_ERROR);
+          },
+
+          onInstallFailed(aInstall) {
+            callCallback(aUri, EXECUTION_ERROR);
+          },
+
+          onInstallEnded(aInstall, aStatus) {
+            callCallback(aUri, SUCCESS);
           }
-        }
+        });
+      }
 
-        if (aInstall) {
-          installs.push(aInstall);
-          if (aCallback) {
-            aInstall.addListener({
-              onDownloadCancelled(aInstall) {
-                callCallback(uri, USER_CANCELLED);
-              },
-
-              onDownloadFailed(aInstall) {
-                if (aInstall.error == AddonManager.ERROR_CORRUPT_FILE)
-                  callCallback(uri, CANT_READ_ARCHIVE);
-                else
-                  callCallback(uri, DOWNLOAD_ERROR);
-              },
-
-              onInstallFailed(aInstall) {
-                callCallback(uri, EXECUTION_ERROR);
-              },
-
-              onInstallEnded(aInstall, aStatus) {
-                callCallback(uri, SUCCESS);
-              }
-            });
-          }
-        } else if (aCallback) {
-          aCallback.onInstallEnded(uri, UNSUPPORTED_TYPE);
-        }
-        buildNextInstall();
-      }, aMimetype, aHashes.shift(), aNames.shift(), aIcons.shift(), null, aBrowser);
-    }
-    buildNextInstall();
+      AddonManager.installAddonFromWebpage(aMimetype, aBrowser, aInstallingPrincipal, aInstall);
+    }, aMimetype, aHash, aName, aIcon, null, aBrowser);
 
     return retval;
   },
@@ -201,7 +180,7 @@ amManager.prototype = {
       case MSG_INSTALL_ENABLED:
         return AddonManager.isInstallEnabled(payload.mimetype);
 
-      case MSG_INSTALL_ADDONS: {
+      case MSG_INSTALL_ADDON: {
         let callback = null;
         if (payload.callbackID != -1) {
           let mm = aMessage.target.messageManager;
@@ -216,9 +195,9 @@ amManager.prototype = {
           };
         }
 
-        return this.installAddonsFromWebpage(payload.mimetype,
-          aMessage.target, payload.principalToInherit, payload.uris,
-          payload.hashes, payload.names, payload.icons, callback);
+        return this.installAddonFromWebpage(payload.mimetype,
+          aMessage.target, payload.principalToInherit, payload.uri,
+          payload.hash, payload.name, payload.icon, callback);
       }
 
       case MSG_PROMISE_REQUEST: {
@@ -284,7 +263,6 @@ amManager.prototype = {
     }
   },
   QueryInterface: XPCOMUtils.generateQI([Ci.amIAddonManager,
-                                         Ci.amIWebInstaller,
                                          Ci.nsITimerCallback,
                                          Ci.nsIObserver,
                                          Ci.nsIMessageListener])

toolkit/mozapps/extensions/amInstallTrigger.js
@@ -14,17 +14,16 @@ Cu.import("resource://gre/modules/Log.jsm");
 const XPINSTALL_MIMETYPE   = "application/x-xpinstall";
 
 const MSG_INSTALL_ENABLED  = "WebInstallerIsInstallEnabled";
-const MSG_INSTALL_ADDONS   = "WebInstallerInstallAddonsFromWebpage";
+const MSG_INSTALL_ADDON    = "WebInstallerInstallAddonFromWebpage";
 const MSG_INSTALL_CALLBACK = "WebInstallerInstallCallback";
 
 
 var log = Log.repository.getLogger("AddonManager.InstallTrigger");
 log.level = Log.Level[Preferences.get("extensions.logging.enabled", false) ? "Warn" : "Trace"];
 
-function CallbackObject(id, callback, urls, mediator) {
+function CallbackObject(id, callback, mediator) {
   this.id = id;
   this.callback = callback;
-  this.urls = new Set(urls);
   this.callCallback = function(url, status) {
     try {
       this.callback(url, status);
@@ -32,9 +31,7 @@ function CallbackObject(id, callback, urls, mediator) {
       log.warn("InstallTrigger callback threw an exception: " + e);
     }
 
-    this.urls.delete(url);
-    if (this.urls.size == 0)
-      mediator._callbacks.delete(id);
+    mediator._callbacks.delete(id);
   };
 }
 
@@ -71,12 +68,12 @@ RemoteMediator.prototype = {
     return this.mm.sendSyncMessage(MSG_INSTALL_ENABLED, params)[0];
   },
 
-  install(installs, principal, callback, window) {
-    let callbackID = this._addCallback(callback, installs.uris);
+  install(install, principal, callback, window) {
+    let callbackID = this._addCallback(callback);
 
-    installs.mimetype = XPINSTALL_MIMETYPE;
-    installs.principalToInherit = principal;
-    installs.callbackID = callbackID;
+    install.mimetype = XPINSTALL_MIMETYPE;
+    install.principalToInherit = principal;
+    install.callbackID = callbackID;
 
     if (Services.appinfo.processType == Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT) {
       // When running in the main process this might be a frame inside an
@@ -91,9 +88,9 @@ RemoteMediator.prototype = {
         let listener = Cc["@mozilla.org/addons/integration;1"].
                        getService(Ci.nsIMessageListener);
         return listener.wrappedJSObject.receiveMessage({
-          name: MSG_INSTALL_ADDONS,
+          name: MSG_INSTALL_ADDON,
           target: element,
-          data: installs,
+          data: install,
         });
       }
     }
@@ -105,15 +102,15 @@ RemoteMediator.prototype = {
                                .QueryInterface(Ci.nsIInterfaceRequestor)
                                .getInterface(Ci.nsIContentFrameMessageManager);
 
-    return messageManager.sendSyncMessage(MSG_INSTALL_ADDONS, installs)[0];
+    return messageManager.sendSyncMessage(MSG_INSTALL_ADDON, install)[0];
   },
 
-  _addCallback(callback, urls) {
+  _addCallback(callback) {
     if (!callback || typeof callback != "function")
       return -1;
 
     let callbackID = this._windowID + "-" + ++this._lastCallbackID;
-    let callbackObject = new CallbackObject(callbackID, callback, urls, this);
+    let callbackObject = new CallbackObject(callbackID, callback, this);
     this._callbacks.set(callbackID, callbackObject);
     return callbackID;
   },
@@ -161,40 +158,39 @@ InstallTrigger.prototype = {
   },
 
   install(installs, callback) {
-    let installData = {
-      uris: [],
-      hashes: [],
-      names: [],
-      icons: [],
-    };
-
-    for (let name of Object.keys(installs)) {
-      let item = installs[name];
-      if (typeof item === "string") {
-        item = { URL: item };
-      }
-      if (!item.URL) {
-        throw new this._window.Error("Missing URL property for '" + name + "'");
-      }
-
-      let url = this._resolveURL(item.URL);
-      if (!this._checkLoadURIFromScript(url)) {
-        throw new this._window.Error("Insufficient permissions to install: " + url.spec);
-      }
-
-      let iconUrl = null;
-      if (item.IconURL) {
-        iconUrl = this._resolveURL(item.IconURL);
-        if (!this._checkLoadURIFromScript(iconUrl)) {
-          iconUrl = null; // If page can't load the icon, just ignore it
-        }
-      }
-
-      installData.uris.push(url.spec);
-      installData.hashes.push(item.Hash || null);
-      installData.names.push(name);
-      installData.icons.push(iconUrl ? iconUrl.spec : null);
+    let keys = Object.keys(installs);
+    if (keys.length > 1) {
+      throw new this._window.Error("Only one XPI may be installed at a time");
     }
+
+    let item = installs[keys[0]];
+
+    if (typeof item === "string") {
+      item = { URL: item };
+    }
+    if (!item.URL) {
+      throw new this._window.Error("Missing URL property for '" + name + "'");
+    }
+
+    let url = this._resolveURL(item.URL);
+    if (!this._checkLoadURIFromScript(url)) {
+      throw new this._window.Error("Insufficient permissions to install: " + url.spec);
+    }
+
+    let iconUrl = null;
+    if (item.IconURL) {
+      iconUrl = this._resolveURL(item.IconURL);
+      if (!this._checkLoadURIFromScript(iconUrl)) {
+        iconUrl = null; // If page can't load the icon, just ignore it
+      }
+    }
+
+    let installData = {
+      uri: url.spec,
+      hash: item.Hash || null,
+      name: item.name,
+      icon: iconUrl ? iconUrl.spec : null,
+    };
 
     return this._mediator.install(installData, this._principal, callback, this._window);
   },

vitalikp/firefox

toolkit/mozapps/extensions/addonManager.js
@@ -21,7 +21,7 @@ const UNSUPPORTED_TYPE  = -244;
 const SUCCESS           = 0;
 
 const MSG_INSTALL_ENABLED  = "WebInstallerIsInstallEnabled";
-const MSG_INSTALL_ADDONS   = "WebInstallerInstallAddonsFromWebpage";
+const MSG_INSTALL_ADDON    = "WebInstallerInstallAddonFromWebpage";
 const MSG_INSTALL_CALLBACK = "WebInstallerInstallCallback";
 
 const MSG_PROMISE_REQUEST  = "WebAPIPromiseRequest";
@@ -44,7 +44,7 @@ function amManager() {
 
   Services.mm.loadFrameScript(CHILD_SCRIPT, true);
   Services.mm.addMessageListener(MSG_INSTALL_ENABLED, this);
-  Services.mm.addMessageListener(MSG_INSTALL_ADDONS, this);
+  Services.mm.addMessageListener(MSG_INSTALL_ADDON, this);
   Services.mm.addMessageListener(MSG_PROMISE_REQUEST, this);
   Services.mm.addMessageListener(MSG_INSTALL_CLEANUP, this);
   Services.mm.addMessageListener(MSG_ADDON_EVENT_REQ, this);
@@ -80,74 +80,53 @@ amManager.prototype = {
     return !!id.value;
   },
 
-  /**
-   * @see amIWebInstaller.idl
-   */
-  isInstallEnabled(aMimetype, aReferer) {
-    return AddonManager.isInstallEnabled(aMimetype);
-  },
-
-  /**
-   * @see amIWebInstaller.idl
-   */
-  installAddonsFromWebpage(aMimetype, aBrowser, aInstallingPrincipal,
-                                     aUris, aHashes, aNames, aIcons, aCallback) {
-    if (aUris.length == 0)
-      return false;
-
+  installAddonFromWebpage(aMimetype, aBrowser, aInstallingPrincipal,
+                                    aUri, aHash, aName, aIcon, aCallback) {
     let retval = true;
     if (!AddonManager.isInstallAllowed(aMimetype, aInstallingPrincipal)) {
       aCallback = null;
       retval = false;
     }
 
-    let installs = [];
-    function buildNextInstall() {
-      if (aUris.length == 0) {
-        AddonManager.installAddonsFromWebpage(aMimetype, aBrowser, aInstallingPrincipal, installs);
+    AddonManager.getInstallForURL(aUri, function(aInstall) {
+      function callCallback(uri, status) {
+        try {
+          aCallback.onInstallEnded(uri, status);
+        } catch (e) {
+          Components.utils.reportError(e);
+        }
+      }
+
+      if (!aInstall) {
+        aCallback.onInstallEnded(aUri, UNSUPPORTED_TYPE);
         return;
       }
-      let uri = aUris.shift();
-      AddonManager.getInstallForURL(uri, function(aInstall) {
-        function callCallback(aUri, aStatus) {
-          try {
-            aCallback.onInstallEnded(aUri, aStatus);
-          } catch (e) {
-            Components.utils.reportError(e);
+
+      if (aCallback) {
+        aInstall.addListener({
+          onDownloadCancelled(aInstall) {
+            callCallback(aUri, USER_CANCELLED);
+          },
+
+          onDownloadFailed(aInstall) {
+            if (aInstall.error == AddonManager.ERROR_CORRUPT_FILE)
+              callCallback(aUri, CANT_READ_ARCHIVE);
+            else
+              callCallback(aUri, DOWNLOAD_ERROR);
+          },
+
+          onInstallFailed(aInstall) {
+            callCallback(aUri, EXECUTION_ERROR);
+          },
+
+          onInstallEnded(aInstall, aStatus) {
+            callCallback(aUri, SUCCESS);
           }
-        }
+        });
+      }
 
-        if (aInstall) {
-          installs.push(aInstall);
-          if (aCallback) {
-            aInstall.addListener({
-              onDownloadCancelled(aInstall) {
-                callCallback(uri, USER_CANCELLED);
-              },
-
-              onDownloadFailed(aInstall) {
-                if (aInstall.error == AddonManager.ERROR_CORRUPT_FILE)
-                  callCallback(uri, CANT_READ_ARCHIVE);
-                else
-                  callCallback(uri, DOWNLOAD_ERROR);
-              },
-
-              onInstallFailed(aInstall) {
-                callCallback(uri, EXECUTION_ERROR);
-              },
-
-              onInstallEnded(aInstall, aStatus) {
-                callCallback(uri, SUCCESS);
-              }
-            });
-          }
-        } else if (aCallback) {
-          aCallback.onInstallEnded(uri, UNSUPPORTED_TYPE);
-        }
-        buildNextInstall();
-      }, aMimetype, aHashes.shift(), aNames.shift(), aIcons.shift(), null, aBrowser);
-    }
-    buildNextInstall();
+      AddonManager.installAddonFromWebpage(aMimetype, aBrowser, aInstallingPrincipal, aInstall);
+    }, aMimetype, aHash, aName, aIcon, null, aBrowser);
 
     return retval;
   },
@@ -201,7 +180,7 @@ amManager.prototype = {
       case MSG_INSTALL_ENABLED:
         return AddonManager.isInstallEnabled(payload.mimetype);
 
-      case MSG_INSTALL_ADDONS: {
+      case MSG_INSTALL_ADDON: {
         let callback = null;
         if (payload.callbackID != -1) {
           let mm = aMessage.target.messageManager;
@@ -216,9 +195,9 @@ amManager.prototype = {
           };
         }
 
-        return this.installAddonsFromWebpage(payload.mimetype,
-          aMessage.target, payload.triggeringPrincipal, payload.uris,
-          payload.hashes, payload.names, payload.icons, callback);
+        return this.installAddonFromWebpage(payload.mimetype,
+          aMessage.target, payload.triggeringPrincipal, payload.uri,
+          payload.hash, payload.name, payload.icon, callback);
       }
 
       case MSG_PROMISE_REQUEST: {
@@ -284,7 +263,6 @@ amManager.prototype = {
     }
   },
   QueryInterface: XPCOMUtils.generateQI([Ci.amIAddonManager,
-                                         Ci.amIWebInstaller,
                                          Ci.nsITimerCallback,
                                          Ci.nsIObserver,
                                          Ci.nsIMessageListener])

toolkit/mozapps/extensions/amInstallTrigger.js
@@ -14,17 +14,16 @@ Cu.import("resource://gre/modules/Log.jsm");
 const XPINSTALL_MIMETYPE   = "application/x-xpinstall";
 
 const MSG_INSTALL_ENABLED  = "WebInstallerIsInstallEnabled";
-const MSG_INSTALL_ADDONS   = "WebInstallerInstallAddonsFromWebpage";
+const MSG_INSTALL_ADDON    = "WebInstallerInstallAddonFromWebpage";
 const MSG_INSTALL_CALLBACK = "WebInstallerInstallCallback";
 
 
 var log = Log.repository.getLogger("AddonManager.InstallTrigger");
 log.level = Log.Level[Preferences.get("extensions.logging.enabled", false) ? "Warn" : "Trace"];
 
-function CallbackObject(id, callback, urls, mediator) {
+function CallbackObject(id, callback, mediator) {
   this.id = id;
   this.callback = callback;
-  this.urls = new Set(urls);
   this.callCallback = function(url, status) {
     try {
       this.callback(url, status);
@@ -32,9 +31,7 @@ function CallbackObject(id, callback, urls, mediator) {
       log.warn("InstallTrigger callback threw an exception: " + e);
     }
 
-    this.urls.delete(url);
-    if (this.urls.size == 0)
-      mediator._callbacks.delete(id);
+    mediator._callbacks.delete(id);
   };
 }
 
@@ -71,12 +68,12 @@ RemoteMediator.prototype = {
     return this.mm.sendSyncMessage(MSG_INSTALL_ENABLED, params)[0];
   },
 
-  install(installs, principal, callback, window) {
-    let callbackID = this._addCallback(callback, installs.uris);
+  install(install, principal, callback, window) {
+    let callbackID = this._addCallback(callback);
 
-    installs.mimetype = XPINSTALL_MIMETYPE;
-    installs.triggeringPrincipal = principal;
-    installs.callbackID = callbackID;
+    install.mimetype = XPINSTALL_MIMETYPE;
+    install.triggeringPrincipal = principal;
+    install.callbackID = callbackID;
 
     if (Services.appinfo.processType == Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT) {
       // When running in the main process this might be a frame inside an
@@ -91,9 +88,9 @@ RemoteMediator.prototype = {
         let listener = Cc["@mozilla.org/addons/integration;1"].
                        getService(Ci.nsIMessageListener);
         return listener.wrappedJSObject.receiveMessage({
-          name: MSG_INSTALL_ADDONS,
+          name: MSG_INSTALL_ADDON,
           target: element,
-          data: installs,
+          data: install,
         });
       }
     }
@@ -105,15 +102,15 @@ RemoteMediator.prototype = {
                                .QueryInterface(Ci.nsIInterfaceRequestor)
                                .getInterface(Ci.nsIContentFrameMessageManager);
 
-    return messageManager.sendSyncMessage(MSG_INSTALL_ADDONS, installs)[0];
+    return messageManager.sendSyncMessage(MSG_INSTALL_ADDON, install)[0];
   },
 
-  _addCallback(callback, urls) {
+  _addCallback(callback) {
     if (!callback || typeof callback != "function")
       return -1;
 
     let callbackID = this._windowID + "-" + ++this._lastCallbackID;
-    let callbackObject = new CallbackObject(callbackID, callback, urls, this);
+    let callbackObject = new CallbackObject(callbackID, callback, this);
     this._callbacks.set(callbackID, callbackObject);
     return callbackID;
   },
@@ -161,40 +158,39 @@ InstallTrigger.prototype = {
   },
 
   install(installs, callback) {
-    let installData = {
-      uris: [],
-      hashes: [],
-      names: [],
-      icons: [],
-    };
-
-    for (let name of Object.keys(installs)) {
-      let item = installs[name];
-      if (typeof item === "string") {
-        item = { URL: item };
-      }
-      if (!item.URL) {
-        throw new this._window.Error("Missing URL property for '" + name + "'");
-      }
-
-      let url = this._resolveURL(item.URL);
-      if (!this._checkLoadURIFromScript(url)) {
-        throw new this._window.Error("Insufficient permissions to install: " + url.spec);
-      }
-
-      let iconUrl = null;
-      if (item.IconURL) {
-        iconUrl = this._resolveURL(item.IconURL);
-        if (!this._checkLoadURIFromScript(iconUrl)) {
-          iconUrl = null; // If page can't load the icon, just ignore it
-        }
-      }
-
-      installData.uris.push(url.spec);
-      installData.hashes.push(item.Hash || null);
-      installData.names.push(name);
-      installData.icons.push(iconUrl ? iconUrl.spec : null);
+    let keys = Object.keys(installs);
+    if (keys.length > 1) {
+      throw new this._window.Error("Only one XPI may be installed at a time");
     }
+
+    let item = installs[keys[0]];
+
+    if (typeof item === "string") {
+      item = { URL: item };
+    }
+    if (!item.URL) {
+      throw new this._window.Error("Missing URL property for '" + name + "'");
+    }
+
+    let url = this._resolveURL(item.URL);
+    if (!this._checkLoadURIFromScript(url)) {
+      throw new this._window.Error("Insufficient permissions to install: " + url.spec);
+    }
+
+    let iconUrl = null;
+    if (item.IconURL) {
+      iconUrl = this._resolveURL(item.IconURL);
+      if (!this._checkLoadURIFromScript(iconUrl)) {
+        iconUrl = null; // If page can't load the icon, just ignore it
+      }
+    }
+
+    let installData = {
+      uri: url.spec,
+      hash: item.Hash || null,
+      name: item.name,
+      icon: iconUrl ? iconUrl.spec : null,
+    };
 
     return this._mediator.install(installData, this._principal, callback, this._window);
   },