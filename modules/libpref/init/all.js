6b2236c18e5d08313aa7d0b32bf4067b8d97258c
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2022-01-19 14:08:22 +0200

    * remove “services.settings.server” pref;
    - used by removed “blocklist-updater.js” and “blocklist-clients.js”;

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2154,9 +2154,6 @@ pref("security.cert_pinning.process_headers_from_non_builtin_roots", false);
 // their protocol with the inner URI of the view-source URI
 pref("security.view-source.reachable-from-inner-protocol", false);
 
-// Services security settings
-pref("services.settings.server", "https://firefox.settings.services.mozilla.com/v1");
-
 // Blocklist preferences
 pref("extensions.blocklist.enabled", true);
 pref("extensions.blocklist.detailsURL", "https://www.mozilla.com/%LOCALE%/blocklist/");
