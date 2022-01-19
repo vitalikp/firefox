6f724a4843e37efa7ec7590ca3b78828db63ce7b
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2022-01-19 14:10:48 +0200

    * mach: remove “webidl-parser-test” command;

--- dom/bindings/mach_commands.py
+++ dom/bindings/mach_commands.py
@@ -15,10 +15,6 @@ from mach.decorators import (
 
 from mozbuild.base import MachCommandBase
 
-def get_test_parser():
-    import runtests
-    return runtests.get_parser
-
 @CommandProvider
 class WebIDLProvider(MachCommandBase):
     @Command('webidl-example', category='misc',
@@ -31,25 +27,3 @@ class WebIDLProvider(MachCommandBase):
         manager = self._spawn(BuildSystemWebIDL).manager
         for i in interface:
             manager.generate_example_files(i)
-
-    @Command('webidl-parser-test', category='testing', parser=get_test_parser,
-             description='Run WebIDL tests (Interface Browser parser).')
-    def webidl_test(self, **kwargs):
-        sys.path.insert(0, os.path.join(self.topsrcdir, 'other-licenses',
-                        'ply'))
-
-        # Make sure we drop our cached grammar bits in the objdir, not
-        # wherever we happen to be running from.
-        os.chdir(self.topobjdir)
-
-        if kwargs["verbose"] is None:
-            kwargs["verbose"] = False
-
-        # Now we're going to create the cached grammar file in the
-        # objdir.  But we're going to try loading it as a python
-        # module, so we need to make sure the objdir is in our search
-        # path.
-        sys.path.insert(0, self.topobjdir)
-
-        import runtests
-        return runtests.run_tests(kwargs["tests"], verbose=kwargs["verbose"])
