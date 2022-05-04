BrowserWorks/Waterfox-Classic

Bug 1386646 - Remove most GetJitContext calls. r=luke

js/src/jit/MacroAssembler.cpp
@@ -1180,7 +1180,8 @@ MacroAssembler::typeOfObject(Register obj, Register scratch, Label* slow,
 void
 MacroAssembler::loadJSContext(Register dest)
 {
-    movePtr(ImmPtr(GetJitContext()->runtime->mainContextPtr()), dest);
+    JitContext* jcx = GetJitContext();
+    movePtr(ImmPtr(jcx->runtime->mainContextPtr()), dest);
 }
 
 void

vitalikp/firefox

js/src/jit/MacroAssembler.cpp
@@ -1475,18 +1475,19 @@ MacroAssembler::typeOfObject(Register obj, Register scratch, Label* slow,
 void
 MacroAssembler::loadJSContext(Register dest)
 {
-    CompileCompartment* compartment = GetJitContext()->compartment;
+    JitContext* jcx = GetJitContext();
+    CompileCompartment* compartment = jcx->compartment;
     if (compartment->zone()->isAtomsZone()) {
         // If we are in the atoms zone then we are generating a runtime wide
         // trampoline which can run in any zone. Load the context which is
         // currently running using cooperative scheduling in the runtime.
         // (This will need to be fixed when we have preemptive scheduling,
         // bug 1323066).
-        loadPtr(AbsoluteAddress(GetJitContext()->runtime->addressOfActiveJSContext()), dest);
+        loadPtr(AbsoluteAddress(jcx->runtime->addressOfActiveJSContext()), dest);
     } else {
         // If we are in a specific zone then the current context will be stored
         // in the containing zone group.
-        loadPtr(AbsoluteAddress(GetJitContext()->compartment->zone()->addressOfJSContext()), dest);
+        loadPtr(AbsoluteAddress(compartment->zone()->addressOfJSContext()), dest);
     }
 }
 