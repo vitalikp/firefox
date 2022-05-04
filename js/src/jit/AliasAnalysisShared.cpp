BrowserWorks/Waterfox-Classic

Bug 1392530 part 1 - Make MDefinition::Opcode an enum class. r=nbp

js/src/jit/AliasAnalysisShared.cpp
@@ -74,78 +74,78 @@ GetObject(const MDefinition* ins)
     // I.e. the poperty isn't on the prototype chain.
     const MDefinition* object = nullptr;
     switch (ins->op()) {
-      case MDefinition::Op_InitializedLength:
-      case MDefinition::Op_LoadElement:
-      case MDefinition::Op_LoadUnboxedScalar:
-      case MDefinition::Op_LoadUnboxedObjectOrNull:
-      case MDefinition::Op_LoadUnboxedString:
-      case MDefinition::Op_StoreElement:
-      case MDefinition::Op_StoreUnboxedObjectOrNull:
-      case MDefinition::Op_StoreUnboxedString:
-      case MDefinition::Op_StoreUnboxedScalar:
-      case MDefinition::Op_SetInitializedLength:
-      case MDefinition::Op_ArrayLength:
-      case MDefinition::Op_SetArrayLength:
-      case MDefinition::Op_TypedObjectDescr:
-      case MDefinition::Op_Slots:
-      case MDefinition::Op_Elements:
-      case MDefinition::Op_MaybeCopyElementsForWrite:
-      case MDefinition::Op_MaybeToDoubleElement:
-      case MDefinition::Op_TypedArrayLength:
-      case MDefinition::Op_SetTypedObjectOffset:
-      case MDefinition::Op_SetDisjointTypedElements:
-      case MDefinition::Op_ArrayPopShift:
-      case MDefinition::Op_ArrayPush:
-      case MDefinition::Op_LoadTypedArrayElementHole:
-      case MDefinition::Op_StoreTypedArrayElementHole:
-      case MDefinition::Op_LoadFixedSlot:
-      case MDefinition::Op_LoadFixedSlotAndUnbox:
-      case MDefinition::Op_StoreFixedSlot:
-      case MDefinition::Op_GetPropertyPolymorphic:
-      case MDefinition::Op_SetPropertyPolymorphic:
-      case MDefinition::Op_GuardShape:
-      case MDefinition::Op_GuardReceiverPolymorphic:
-      case MDefinition::Op_GuardObjectGroup:
-      case MDefinition::Op_GuardObjectIdentity:
-      case MDefinition::Op_GuardClass:
-      case MDefinition::Op_LoadSlot:
-      case MDefinition::Op_StoreSlot:
-      case MDefinition::Op_InArray:
-      case MDefinition::Op_LoadElementHole:
-      case MDefinition::Op_TypedArrayElements:
-      case MDefinition::Op_TypedObjectElements:
-      case MDefinition::Op_CopyLexicalEnvironmentObject:
-      case MDefinition::Op_IsPackedArray:
+      case MDefinition::Opcode::InitializedLength:
+      case MDefinition::Opcode::LoadElement:
+      case MDefinition::Opcode::LoadUnboxedScalar:
+      case MDefinition::Opcode::LoadUnboxedObjectOrNull:
+      case MDefinition::Opcode::LoadUnboxedString:
+      case MDefinition::Opcode::StoreElement:
+      case MDefinition::Opcode::StoreUnboxedObjectOrNull:
+      case MDefinition::Opcode::StoreUnboxedString:
+      case MDefinition::Opcode::StoreUnboxedScalar:
+      case MDefinition::Opcode::SetInitializedLength:
+      case MDefinition::Opcode::ArrayLength:
+      case MDefinition::Opcode::SetArrayLength:
+      case MDefinition::Opcode::TypedObjectDescr:
+      case MDefinition::Opcode::Slots:
+      case MDefinition::Opcode::Elements:
+      case MDefinition::Opcode::MaybeCopyElementsForWrite:
+      case MDefinition::Opcode::MaybeToDoubleElement:
+      case MDefinition::Opcode::TypedArrayLength:
+      case MDefinition::Opcode::SetTypedObjectOffset:
+      case MDefinition::Opcode::SetDisjointTypedElements:
+      case MDefinition::Opcode::ArrayPopShift:
+      case MDefinition::Opcode::ArrayPush:
+      case MDefinition::Opcode::LoadTypedArrayElementHole:
+      case MDefinition::Opcode::StoreTypedArrayElementHole:
+      case MDefinition::Opcode::LoadFixedSlot:
+      case MDefinition::Opcode::LoadFixedSlotAndUnbox:
+      case MDefinition::Opcode::StoreFixedSlot:
+      case MDefinition::Opcode::GetPropertyPolymorphic:
+      case MDefinition::Opcode::SetPropertyPolymorphic:
+      case MDefinition::Opcode::GuardShape:
+      case MDefinition::Opcode::GuardReceiverPolymorphic:
+      case MDefinition::Opcode::GuardObjectGroup:
+      case MDefinition::Opcode::GuardObjectIdentity:
+      case MDefinition::Opcode::GuardClass:
+      case MDefinition::Opcode::LoadSlot:
+      case MDefinition::Opcode::StoreSlot:
+      case MDefinition::Opcode::InArray:
+      case MDefinition::Opcode::LoadElementHole:
+      case MDefinition::Opcode::TypedArrayElements:
+      case MDefinition::Opcode::TypedObjectElements:
+      case MDefinition::Opcode::CopyLexicalEnvironmentObject:
+      case MDefinition::Opcode::IsPackedArray:
         object = ins->getOperand(0);
         break;
-      case MDefinition::Op_GetPropertyCache:
-      case MDefinition::Op_CallGetProperty:
-      case MDefinition::Op_LoadTypedArrayElementStatic:
-      case MDefinition::Op_StoreTypedArrayElementStatic:
-      case MDefinition::Op_GetDOMProperty:
-      case MDefinition::Op_GetDOMMember:
-      case MDefinition::Op_Call:
-      case MDefinition::Op_Compare:
-      case MDefinition::Op_GetArgumentsObjectArg:
-      case MDefinition::Op_SetArgumentsObjectArg:
-      case MDefinition::Op_GetFrameArgument:
-      case MDefinition::Op_SetFrameArgument:
-      case MDefinition::Op_CompareExchangeTypedArrayElement:
-      case MDefinition::Op_AtomicExchangeTypedArrayElement:
-      case MDefinition::Op_AtomicTypedArrayElementBinop:
-      case MDefinition::Op_AsmJSLoadHeap:
-      case MDefinition::Op_AsmJSStoreHeap:
-      case MDefinition::Op_WasmLoadTls:
-      case MDefinition::Op_WasmLoad:
-      case MDefinition::Op_WasmStore:
-      case MDefinition::Op_AsmJSCompareExchangeHeap:
-      case MDefinition::Op_AsmJSAtomicBinopHeap:
-      case MDefinition::Op_WasmLoadGlobalVar:
-      case MDefinition::Op_WasmStoreGlobalVar:
-      case MDefinition::Op_ArrayJoin:
-      case MDefinition::Op_ArraySlice:
-      case MDefinition::Op_StoreElementHole:
-      case MDefinition::Op_FallibleStoreElement:
+      case MDefinition::Opcode::GetPropertyCache:
+      case MDefinition::Opcode::CallGetProperty:
+      case MDefinition::Opcode::LoadTypedArrayElementStatic:
+      case MDefinition::Opcode::StoreTypedArrayElementStatic:
+      case MDefinition::Opcode::GetDOMProperty:
+      case MDefinition::Opcode::GetDOMMember:
+      case MDefinition::Opcode::Call:
+      case MDefinition::Opcode::Compare:
+      case MDefinition::Opcode::GetArgumentsObjectArg:
+      case MDefinition::Opcode::SetArgumentsObjectArg:
+      case MDefinition::Opcode::GetFrameArgument:
+      case MDefinition::Opcode::SetFrameArgument:
+      case MDefinition::Opcode::CompareExchangeTypedArrayElement:
+      case MDefinition::Opcode::AtomicExchangeTypedArrayElement:
+      case MDefinition::Opcode::AtomicTypedArrayElementBinop:
+      case MDefinition::Opcode::AsmJSLoadHeap:
+      case MDefinition::Opcode::AsmJSStoreHeap:
+      case MDefinition::Opcode::WasmLoadTls:
+      case MDefinition::Opcode::WasmLoad:
+      case MDefinition::Opcode::WasmStore:
+      case MDefinition::Opcode::AsmJSCompareExchangeHeap:
+      case MDefinition::Opcode::AsmJSAtomicBinopHeap:
+      case MDefinition::Opcode::WasmLoadGlobalVar:
+      case MDefinition::Opcode::WasmStoreGlobalVar:
+      case MDefinition::Opcode::ArrayJoin:
+      case MDefinition::Opcode::ArraySlice:
+      case MDefinition::Opcode::StoreElementHole:
+      case MDefinition::Opcode::FallibleStoreElement:
         return nullptr;
       default:
 #ifdef DEBUG

vitalikp/firefox

js/src/jit/AliasAnalysisShared.cpp
@@ -74,83 +74,83 @@ GetObject(const MDefinition* ins)
     // I.e. the poperty isn't on the prototype chain.
     const MDefinition* object = nullptr;
     switch (ins->op()) {
-      case MDefinition::Op_InitializedLength:
-      case MDefinition::Op_LoadElement:
-      case MDefinition::Op_LoadUnboxedScalar:
-      case MDefinition::Op_LoadUnboxedObjectOrNull:
-      case MDefinition::Op_LoadUnboxedString:
-      case MDefinition::Op_StoreElement:
-      case MDefinition::Op_StoreUnboxedObjectOrNull:
-      case MDefinition::Op_StoreUnboxedString:
-      case MDefinition::Op_StoreUnboxedScalar:
-      case MDefinition::Op_SetInitializedLength:
-      case MDefinition::Op_ArrayLength:
-      case MDefinition::Op_SetArrayLength:
-      case MDefinition::Op_StoreElementHole:
-      case MDefinition::Op_FallibleStoreElement:
-      case MDefinition::Op_TypedObjectDescr:
-      case MDefinition::Op_Slots:
-      case MDefinition::Op_Elements:
-      case MDefinition::Op_MaybeCopyElementsForWrite:
-      case MDefinition::Op_MaybeToDoubleElement:
-      case MDefinition::Op_UnboxedArrayLength:
-      case MDefinition::Op_UnboxedArrayInitializedLength:
-      case MDefinition::Op_IncrementUnboxedArrayInitializedLength:
-      case MDefinition::Op_SetUnboxedArrayInitializedLength:
-      case MDefinition::Op_TypedArrayLength:
-      case MDefinition::Op_SetTypedObjectOffset:
-      case MDefinition::Op_SetDisjointTypedElements:
-      case MDefinition::Op_ArrayPopShift:
-      case MDefinition::Op_ArrayPush:
-      case MDefinition::Op_ArraySlice:
-      case MDefinition::Op_LoadTypedArrayElementHole:
-      case MDefinition::Op_StoreTypedArrayElementHole:
-      case MDefinition::Op_LoadFixedSlot:
-      case MDefinition::Op_LoadFixedSlotAndUnbox:
-      case MDefinition::Op_StoreFixedSlot:
-      case MDefinition::Op_GetPropertyPolymorphic:
-      case MDefinition::Op_SetPropertyPolymorphic:
-      case MDefinition::Op_GuardShape:
-      case MDefinition::Op_GuardReceiverPolymorphic:
-      case MDefinition::Op_GuardObjectGroup:
-      case MDefinition::Op_GuardObjectIdentity:
-      case MDefinition::Op_GuardClass:
-      case MDefinition::Op_GuardUnboxedExpando:
-      case MDefinition::Op_LoadUnboxedExpando:
-      case MDefinition::Op_LoadSlot:
-      case MDefinition::Op_StoreSlot:
-      case MDefinition::Op_InArray:
-      case MDefinition::Op_LoadElementHole:
-      case MDefinition::Op_TypedArrayElements:
-      case MDefinition::Op_TypedObjectElements:
-      case MDefinition::Op_CopyLexicalEnvironmentObject:
-      case MDefinition::Op_IsPackedArray:
+      case MDefinition::Opcode::InitializedLength:
+      case MDefinition::Opcode::LoadElement:
+      case MDefinition::Opcode::LoadUnboxedScalar:
+      case MDefinition::Opcode::LoadUnboxedObjectOrNull:
+      case MDefinition::Opcode::LoadUnboxedString:
+      case MDefinition::Opcode::StoreElement:
+      case MDefinition::Opcode::StoreUnboxedObjectOrNull:
+      case MDefinition::Opcode::StoreUnboxedString:
+      case MDefinition::Opcode::StoreUnboxedScalar:
+      case MDefinition::Opcode::SetInitializedLength:
+      case MDefinition::Opcode::ArrayLength:
+      case MDefinition::Opcode::SetArrayLength:
+      case MDefinition::Opcode::StoreElementHole:
+      case MDefinition::Opcode::FallibleStoreElement:
+      case MDefinition::Opcode::TypedObjectDescr:
+      case MDefinition::Opcode::Slots:
+      case MDefinition::Opcode::Elements:
+      case MDefinition::Opcode::MaybeCopyElementsForWrite:
+      case MDefinition::Opcode::MaybeToDoubleElement:
+      case MDefinition::Opcode::UnboxedArrayLength:
+      case MDefinition::Opcode::UnboxedArrayInitializedLength:
+      case MDefinition::Opcode::IncrementUnboxedArrayInitializedLength:
+      case MDefinition::Opcode::SetUnboxedArrayInitializedLength:
+      case MDefinition::Opcode::TypedArrayLength:
+      case MDefinition::Opcode::SetTypedObjectOffset:
+      case MDefinition::Opcode::SetDisjointTypedElements:
+      case MDefinition::Opcode::ArrayPopShift:
+      case MDefinition::Opcode::ArrayPush:
+      case MDefinition::Opcode::ArraySlice:
+      case MDefinition::Opcode::LoadTypedArrayElementHole:
+      case MDefinition::Opcode::StoreTypedArrayElementHole:
+      case MDefinition::Opcode::LoadFixedSlot:
+      case MDefinition::Opcode::LoadFixedSlotAndUnbox:
+      case MDefinition::Opcode::StoreFixedSlot:
+      case MDefinition::Opcode::GetPropertyPolymorphic:
+      case MDefinition::Opcode::SetPropertyPolymorphic:
+      case MDefinition::Opcode::GuardShape:
+      case MDefinition::Opcode::GuardReceiverPolymorphic:
+      case MDefinition::Opcode::GuardObjectGroup:
+      case MDefinition::Opcode::GuardObjectIdentity:
+      case MDefinition::Opcode::GuardClass:
+      case MDefinition::Opcode::GuardUnboxedExpando:
+      case MDefinition::Opcode::LoadUnboxedExpando:
+      case MDefinition::Opcode::LoadSlot:
+      case MDefinition::Opcode::StoreSlot:
+      case MDefinition::Opcode::InArray:
+      case MDefinition::Opcode::LoadElementHole:
+      case MDefinition::Opcode::TypedArrayElements:
+      case MDefinition::Opcode::TypedObjectElements:
+      case MDefinition::Opcode::CopyLexicalEnvironmentObject:
+      case MDefinition::Opcode::IsPackedArray:
         object = ins->getOperand(0);
         break;
-      case MDefinition::Op_GetPropertyCache:
-      case MDefinition::Op_LoadTypedArrayElementStatic:
-      case MDefinition::Op_StoreTypedArrayElementStatic:
-      case MDefinition::Op_GetDOMProperty:
-      case MDefinition::Op_GetDOMMember:
-      case MDefinition::Op_Call:
-      case MDefinition::Op_Compare:
-      case MDefinition::Op_GetArgumentsObjectArg:
-      case MDefinition::Op_SetArgumentsObjectArg:
-      case MDefinition::Op_GetFrameArgument:
-      case MDefinition::Op_SetFrameArgument:
-      case MDefinition::Op_CompareExchangeTypedArrayElement:
-      case MDefinition::Op_AtomicExchangeTypedArrayElement:
-      case MDefinition::Op_AtomicTypedArrayElementBinop:
-      case MDefinition::Op_AsmJSLoadHeap:
-      case MDefinition::Op_AsmJSStoreHeap:
-      case MDefinition::Op_WasmLoadTls:
-      case MDefinition::Op_WasmLoad:
-      case MDefinition::Op_WasmStore:
-      case MDefinition::Op_AsmJSCompareExchangeHeap:
-      case MDefinition::Op_AsmJSAtomicBinopHeap:
-      case MDefinition::Op_WasmLoadGlobalVar:
-      case MDefinition::Op_WasmStoreGlobalVar:
-      case MDefinition::Op_ArrayJoin:
+      case MDefinition::Opcode::GetPropertyCache:
+      case MDefinition::Opcode::LoadTypedArrayElementStatic:
+      case MDefinition::Opcode::StoreTypedArrayElementStatic:
+      case MDefinition::Opcode::GetDOMProperty:
+      case MDefinition::Opcode::GetDOMMember:
+      case MDefinition::Opcode::Call:
+      case MDefinition::Opcode::Compare:
+      case MDefinition::Opcode::GetArgumentsObjectArg:
+      case MDefinition::Opcode::SetArgumentsObjectArg:
+      case MDefinition::Opcode::GetFrameArgument:
+      case MDefinition::Opcode::SetFrameArgument:
+      case MDefinition::Opcode::CompareExchangeTypedArrayElement:
+      case MDefinition::Opcode::AtomicExchangeTypedArrayElement:
+      case MDefinition::Opcode::AtomicTypedArrayElementBinop:
+      case MDefinition::Opcode::AsmJSLoadHeap:
+      case MDefinition::Opcode::AsmJSStoreHeap:
+      case MDefinition::Opcode::WasmLoadTls:
+      case MDefinition::Opcode::WasmLoad:
+      case MDefinition::Opcode::WasmStore:
+      case MDefinition::Opcode::AsmJSCompareExchangeHeap:
+      case MDefinition::Opcode::AsmJSAtomicBinopHeap:
+      case MDefinition::Opcode::WasmLoadGlobalVar:
+      case MDefinition::Opcode::WasmStoreGlobalVar:
+      case MDefinition::Opcode::ArrayJoin:
         return nullptr;
       default:
 #ifdef DEBUG