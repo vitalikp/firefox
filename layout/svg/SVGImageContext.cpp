Floorp-Projects/Floorp

Bug 1359486 - Fix SVGImageContext::MaybeStoreContextPaint to work with DevTools chrome. r=dholbert
MozReview-Commit-ID: 7TL9cfWsiED

layout/svg/SVGImageContext.cpp
@@ -10,6 +10,7 @@
 // Keep others in (case-insensitive) order:
 #include "gfxUtils.h"
 #include "mozilla/Preferences.h"
+#include "nsContentUtils.h"
 #include "nsIFrame.h"
 #include "nsPresContext.h"
 
@@ -30,7 +31,7 @@ SVGImageContext::MaybeStoreContextPaint(Maybe<SVGImageContext>& aContext,
   }
 
   if (!sEnabledForContent &&
-      !aFromFrame->PresContext()->IsChrome()) {
+      !nsContentUtils::IsChromeDoc(aFromFrame->GetContent()->OwnerDoc())) {
     // Context paint is pref'ed off for content and this is a content doc.
     return;
   }

vitalikp/firefox

layout/svg/SVGImageContext.cpp
@@ -10,6 +10,7 @@
 // Keep others in (case-insensitive) order:
 #include "gfxUtils.h"
 #include "mozilla/Preferences.h"
+#include "nsContentUtils.h"
 #include "nsIFrame.h"
 #include "nsPresContext.h"
 
@@ -32,7 +33,7 @@ SVGImageContext::MaybeInitAndStoreContextPaint(Maybe<SVGImageContext>& aContext,
   }
 
   if (!sEnabledForContent &&
-      !aFromFrame->PresContext()->IsChrome()) {
+      !nsContentUtils::IsChromeDoc(aFromFrame->PresContext()->Document())) {
     // Context paint is pref'ed off for content and this is a content doc.
     return;
   }