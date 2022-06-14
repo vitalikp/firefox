BrowserWorks/Waterfox-Classic

Bug 1363200 - JSAPI for realms: JS_SetRealmNameCallback. r=sfink

js/src/vm/Runtime.h
@@ -382,6 +382,9 @@ struct JSRuntime : public js::MallocProvider<JSRuntime>
     /* Call this to get the name of a compartment. */
     js::MainThreadData<JSCompartmentNameCallback> compartmentNameCallback;
 
+    /* Call this to get the name of a realm. */
+    js::MainThreadData<JSRealmNameCallback> realmNameCallback;
+
     /* Callback for doing memory reporting on external strings. */
     js::MainThreadData<JSExternalStringSizeofCallback> externalStringSizeofCallback;
 

vitalikp/firefox

js/src/vm/Runtime.h
@@ -501,6 +501,9 @@ struct JSRuntime : public js::MallocProvider<JSRuntime>
     /* Call this to get the name of a compartment. */
     js::ActiveThreadData<JSCompartmentNameCallback> compartmentNameCallback;
 
+    /* Call this to get the name of a realm. */
+    js::ActiveThreadData<JSRealmNameCallback> realmNameCallback;
+
     /* Callback for doing memory reporting on external strings. */
     js::ActiveThreadData<JSExternalStringSizeofCallback> externalStringSizeofCallback;
 