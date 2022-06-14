BrowserWorks/Waterfox-Classic

Bug 1363200 - JSAPI for realms: JS::SetDestroyRealmCallback. r=sfink

js/src/vm/Runtime.h
@@ -382,8 +382,11 @@ struct JSRuntime : public js::MallocProvider<JSRuntime>
     /* Call this to get the name of a compartment. */
     js::MainThreadData<JSCompartmentNameCallback> compartmentNameCallback;
 
+    /* Realm destroy callback. */
+    js::MainThreadData<JS::DestroyRealmCallback> destroyRealmCallback;
+
     /* Call this to get the name of a realm. */
-    js::MainThreadData<JSRealmNameCallback> realmNameCallback;
+    js::MainThreadData<JS::RealmNameCallback> realmNameCallback;
 
     /* Callback for doing memory reporting on external strings. */
     js::MainThreadData<JSExternalStringSizeofCallback> externalStringSizeofCallback;

vitalikp/firefox

js/src/vm/Runtime.h
@@ -501,8 +501,11 @@ struct JSRuntime : public js::MallocProvider<JSRuntime>
     /* Call this to get the name of a compartment. */
     js::ActiveThreadData<JSCompartmentNameCallback> compartmentNameCallback;
 
+    /* Realm destroy callback. */
+    js::ActiveThreadData<JS::DestroyRealmCallback> destroyRealmCallback;
+
     /* Call this to get the name of a realm. */
-    js::ActiveThreadData<JSRealmNameCallback> realmNameCallback;
+    js::ActiveThreadData<JS::RealmNameCallback> realmNameCallback;
 
     /* Callback for doing memory reporting on external strings. */
     js::ActiveThreadData<JSExternalStringSizeofCallback> externalStringSizeofCallback;