998573f616d9e0addaa69e46b36ebc6c16c6ad4d
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2022-06-27 19:27:47 +0300

    * disable unified telemetry;

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -923,7 +923,7 @@ pref("toolkit.telemetry.infoURL", "https://www.mozilla.org/legal/privacy/firefox
 // i.e. dynamically constructed SQL strings or SQL executed by addons against addon DBs
 pref("toolkit.telemetry.debugSlowSql", false);
 // Whether to use the unified telemetry behavior, requires a restart.
-pref("toolkit.telemetry.unified", true);
+pref("toolkit.telemetry.unified", false);
 // AsyncShutdown delay before crashing in case of shutdown freeze
 pref("toolkit.asyncshutdown.crash_timeout", 60000);
 // Extra logging for AsyncShutdown barriers and phases
