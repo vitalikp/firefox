6c55b0c900470127440edc6b836b618836406fa7
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2023-07-02 18:57:01 +0300

    * accessible: remove “ipc/win” dir;

--- accessible/ipc/IPCTypes.h
+++ accessible/ipc/IPCTypes.h
@@ -12,29 +12,6 @@
  * define types used by PDocAccessible differently depending on platform.
  */
 
-#if defined(XP_WIN) && defined(ACCESSIBILITY)
-
-// So that we don't include a bunch of other Windows junk.
-#if !defined(COM_NO_WINDOWS_H)
-#define COM_NO_WINDOWS_H
-#endif // !defined(COM_NO_WINDOWS_H)
-
-// COM headers pull in MSXML which conflicts with our own XMLDocument class.
-// This define excludes those conflicting definitions.
-#if !defined(__XMLDocument_FWD_DEFINED__)
-#define __XMLDocument_FWD_DEFINED__
-#endif // !defined(__XMLDocument_FWD_DEFINED__)
-
-#include "mozilla/a11y/COMPtrTypes.h"
-
-// This define in rpcndr.h messes up our code, so we must undefine it after
-// COMPtrTypes.h has been included.
-#if defined(small)
-#undef small
-#endif // defined(small)
-
-#else
-
 namespace mozilla {
 namespace a11y {
 
@@ -42,8 +19,6 @@ typedef uint32_t IAccessibleHolder;
 
 } // namespace a11y
 } // namespace mozilla
-
-#endif // defined(XP_WIN) && defined(ACCESSIBILITY)
 
 #endif // mozilla_a11y_IPCTypes_h
 
