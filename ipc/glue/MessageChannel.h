Floorp-Projects/Floorp

Bug 1358697 - Don't capture the Promise raw pointer and make static analysis happy. r=billm
MozReview-Commit-ID: 7lMVMcNBDHJ

--HG--
extra : rebase_source : 4ea2e25c3a7e1f49a79b9b994024c00494f78047

ipc/glue/MessageChannel.h
@@ -98,7 +98,7 @@ class MessageChannel : HasResultCodes, MessageLoop::DestructionObserver
     struct PromiseHolder
     {
         RefPtr<MozPromiseRefcountable> mPromise;
-        std::function<void(const char*)> mRejectFunction;
+        std::function<void(MozPromiseRefcountable*, const char*)> mRejectFunction;
     };
     static Atomic<size_t> gUnresolvedPromises;
     friend class PromiseReporter;
@@ -186,8 +186,10 @@ class MessageChannel : HasResultCodes, MessageLoop::DestructionObserver
         }
         PromiseHolder holder;
         holder.mPromise = aPromise;
-        holder.mRejectFunction = [aPromise](const char* aRejectSite) {
-            aPromise->Reject(PromiseRejectReason::ChannelClosed, aRejectSite);
+        holder.mRejectFunction = [](MozPromiseRefcountable* aPromise,
+                                    const char* aRejectSite) {
+            static_cast<Promise*>(aPromise)->Reject(
+                PromiseRejectReason::ChannelClosed, aRejectSite);
         };
         mPendingPromises.insert(std::make_pair(seqno, Move(holder)));
         gUnresolvedPromises++;

vitalikp/firefox

ipc/glue/MessageChannel.h
@@ -93,7 +93,7 @@ class MessageChannel : HasResultCodes, MessageLoop::DestructionObserver
     struct PromiseHolder
     {
         RefPtr<MozPromiseRefcountable> mPromise;
-        std::function<void(const char*)> mRejectFunction;
+        std::function<void(MozPromiseRefcountable*, const char*)> mRejectFunction;
     };
     static Atomic<size_t> gUnresolvedPromises;
     friend class PromiseReporter;
@@ -181,8 +181,10 @@ class MessageChannel : HasResultCodes, MessageLoop::DestructionObserver
         }
         PromiseHolder holder;
         holder.mPromise = aPromise;
-        holder.mRejectFunction = [aPromise](const char* aRejectSite) {
-            aPromise->Reject(PromiseRejectReason::ChannelClosed, aRejectSite);
+        holder.mRejectFunction = [](MozPromiseRefcountable* aRejectPromise,
+                                    const char* aRejectSite) {
+            static_cast<Promise*>(aRejectPromise)->Reject(
+                PromiseRejectReason::ChannelClosed, aRejectSite);
         };
         mPendingPromises.insert(std::make_pair(seqno, Move(holder)));
         gUnresolvedPromises++;