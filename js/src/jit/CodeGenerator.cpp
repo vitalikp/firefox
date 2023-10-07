Floorp-Projects/Floorp

Bug 1400278 - Replace runtime allocation with system or zone allocation where possible r=jandem

js/src/jit/CodeGenerator.cpp
@@ -7912,7 +7912,7 @@ JitCompartment::generateStringConcatStub(JSContext* cx)
 }
 
 JitCode*
-JitRuntime::generateMallocStub(JSContext* cx)
+JitZone::generateMallocStub(JSContext* cx)
 {
     const Register regReturn = CallTempReg0;
     const Register regNBytes = CallTempReg0;
@@ -7928,12 +7928,12 @@ JitRuntime::generateMallocStub(JSContext* cx)
     masm.PushRegsInMask(save);
 
     const Register regTemp = regs.takeAnyGeneral();
-    const Register regRuntime = regTemp;
+    const Register regZone = regTemp;
     MOZ_ASSERT(regTemp != regNBytes);
 
     masm.setupUnalignedABICall(regTemp);
-    masm.movePtr(ImmPtr(cx->runtime()), regRuntime);
-    masm.passABIArg(regRuntime);
+    masm.movePtr(ImmPtr(cx->zone()), regZone);
+    masm.passABIArg(regZone);
     masm.passABIArg(regNBytes);
     masm.callWithABI(JS_FUNC_TO_DATA_PTR(void*, MallocWrapper));
     masm.storeCallWordResult(regReturn);

vitalikp/firefox

js/src/jit/CodeGenerator.cpp
@@ -7880,7 +7880,8 @@ JitCode*
 JitRuntime::generateMallocStub(JSContext* cx)
 {
     const Register regReturn = CallTempReg0;
-    const Register regNBytes = CallTempReg0;
+    const Register regZone = CallTempReg0;
+    const Register regNBytes = CallTempReg1;
 
     MacroAssembler masm(cx);
 
@@ -7888,17 +7889,16 @@ JitRuntime::generateMallocStub(JSContext* cx)
 #ifdef JS_USE_LINK_REGISTER
     masm.pushReturnAddress();
 #endif
+    regs.takeUnchecked(regZone);
     regs.takeUnchecked(regNBytes);
     LiveRegisterSet save(regs.asLiveSet());
     masm.PushRegsInMask(save);
 
     const Register regTemp = regs.takeAnyGeneral();
-    const Register regRuntime = regTemp;
-    MOZ_ASSERT(regTemp != regNBytes);
+    MOZ_ASSERT(regTemp != regNBytes && regTemp != regZone);
 
     masm.setupUnalignedABICall(regTemp);
-    masm.movePtr(ImmPtr(cx->runtime()), regRuntime);
-    masm.passABIArg(regRuntime);
+    masm.passABIArg(regZone);
     masm.passABIArg(regNBytes);
     masm.callWithABI(JS_FUNC_TO_DATA_PTR(void*, MallocWrapper));
     masm.storeCallPointerResult(regReturn);