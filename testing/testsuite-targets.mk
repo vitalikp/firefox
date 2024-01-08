78dd262a92580692fc60bdb2b0d1a93d8ec2e0c4
Vitaliy Perevertun <vitalik.perevertun@gmail.com>
2024-01-08 03:18:47 +0200

    * build: remove “automation-build.mk” file;

--- testing/testsuite-targets.mk
+++ testing/testsuite-targets.mk
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# include automation-build.mk to get the path to the binary
 TARGET_DEPTH = $(DEPTH)
 include $(topsrcdir)/build/binary-location.mk
 
