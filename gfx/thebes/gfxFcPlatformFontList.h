Floorp-Projects/Floorp

Bug 1463884 - patch 2 - Remove always-true argument aCheckEnvironment from gfxFcPlatformFontList::GetSampleLangForGroup. r=emilio

gfx/thebes/gfxFcPlatformFontList.h
@@ -328,8 +328,7 @@ public:
     }
 
     // map lang group ==> lang string
-    void GetSampleLangForGroup(nsAtom* aLanguage, nsACString& aLangStr,
-                               bool aCheckEnvironment = true);
+    void GetSampleLangForGroup(nsAtom* aLanguage, nsACString& aLangStr);
 
     static FT_Library GetFTLibrary();
 

vitalikp/firefox

gfx/thebes/gfxFcPlatformFontList.h
@@ -299,8 +299,7 @@ public:
     }
 
     // map lang group ==> lang string
-    void GetSampleLangForGroup(nsIAtom* aLanguage, nsACString& aLangStr,
-                               bool aCheckEnvironment = true);
+    void GetSampleLangForGroup(nsIAtom* aLanguage, nsACString& aLangStr);
 
     static FT_Library GetFTLibrary();
 