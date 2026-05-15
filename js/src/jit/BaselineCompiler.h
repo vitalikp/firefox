BrowserWorks/Waterfox-Classic

Bug 1427610 - Support import.meta in the JITs r=jandem

js/src/jit/BaselineCompiler.h
@@ -258,6 +258,7 @@ namespace jit {
     _(JSOP_FUNWITHPROTO)       \
     _(JSOP_CLASSCONSTRUCTOR)   \
     _(JSOP_DERIVEDCONSTRUCTOR) \
+    _(JSOP_IMPORTMETA)         \
     _(JSOP_COALESCE)
 
 class BaselineCompiler : public BaselineCompilerSpecific

vitalikp/firefox

js/src/jit/BaselineCompiler.h
@@ -256,7 +256,8 @@ namespace jit {
     _(JSOP_OBJWITHPROTO)       \
     _(JSOP_FUNWITHPROTO)       \
     _(JSOP_CLASSCONSTRUCTOR)   \
-    _(JSOP_DERIVEDCONSTRUCTOR)
+    _(JSOP_DERIVEDCONSTRUCTOR) \
+    _(JSOP_IMPORTMETA)
 
 class BaselineCompiler : public BaselineCompilerSpecific
 {